2084646ba7e6c011dc9c99cae1733e9e1e7e180e
Nguyen Thai <tknguyen@linagora.com>
2023-03-23 16:15:51 +0700

    Added initial files for Github Actions
    Fixed `flutter_webrtc` version
    
    Don't fail all other matrix jobs
    
    Added workflow for building APK
    
    Disabled integration test because it's too buggy

--- scripts/integration-start-avd.sh
+++ scripts/integration-start-avd.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-chmod 777 -R /dev/kvm
-adb start-server
-emulator -avd test -wipe-data  -no-audio -no-boot-anim -no-window -accel on -gpu swiftshader_indirect
+echo "y" | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install "system-images;android-31;default;x86_64"
+echo "no" | $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd -n test -k "system-images;android-31;default;x86_64"
+"$ANDROID_HOME"/platform-tools/adb start-server
+"$ANDROID_HOME"/emulator/emulator -avd test -wipe-data  -no-audio -no-boot-anim -no-window -accel auto -gpu swiftshader_indirect
