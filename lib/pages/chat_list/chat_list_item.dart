abdfbc6ca60369e7ea5ef8e07fbcb9a240ac1a06
Julian KOUNE <jkoune@linagora.com>
2023-03-28 03:47:09 +0200

    refactor: improve list tile

--- lib/pages/chat_list/chat_list_item.dart
+++ lib/pages/chat_list/chat_list_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:future_loading_dialog/future_loading_dialog.dart';
 import 'package:matrix/matrix.dart';
 import 'package:vrouter/vrouter.dart';
@@ -9,8 +10,10 @@ import 'package:vrouter/vrouter.dart';
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/utils/room_status_extension.dart';
+import 'package:fluffychat/utils/date_time_extension.dart';
+import 'package:fluffychat/utils/custom_svg_icons.dart';
+
 import '../../config/themes.dart';
-import '../../utils/date_time_extension.dart';
 import '../../widgets/avatar.dart';
 import '../../widgets/matrix.dart';
 import '../chat/send_file_dialog.dart';
@@ -69,8 +72,7 @@ class ChatListItem extends StatelessWidget {
       final shareContent = Matrix.of(context).shareContent;
       if (shareContent != null) {
         final shareFile = shareContent.tryGet<MatrixFile>('file');
-        if (shareContent.tryGet<String>('msgtype') ==
-                'chat.fluffy.shared_file' &&
+        if (shareContent.tryGet<String>('msgtype') == 'chat.fluffy.shared_file' &&
             shareFile != null) {
           await showDialog(
             context: context,
@@ -119,8 +121,7 @@ class ChatListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final isMuted = room.pushRuleState != PushRuleState.notify;
     final typingText = room.getLocalizedTypingText(context);
-    final ownMessage =
-        room.lastEvent?.senderId == Matrix.of(context).client.userID;
+    final ownMessage = room.lastEvent?.senderId == Matrix.of(context).client.userID;
     final unread = room.isUnread || room.membership == Membership.invite;
     final unreadBubbleSize = unread || room.hasNewMessages
         ? room.notificationCount > 0
@@ -143,195 +144,195 @@ class ChatListItem extends StatelessWidget {
             : activeChat
                 ? Theme.of(context).colorScheme.secondaryContainer
                 : Colors.transparent,
-        child: ListTile(
-          visualDensity: const VisualDensity(vertical: -0.5),
-          contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-          onLongPress: onLongPress,
-          leading: selected
-              ? SizedBox(
-                  width: Avatar.defaultSize,
-                  height: Avatar.defaultSize,
-                  child: Material(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(Avatar.defaultSize),
-                    child: const Icon(Icons.check, color: Colors.white),
-                  ),
-                )
-              : Avatar(
-                  mxContent: room.avatar,
-                  name: displayname,
-                  onTap: onLongPress,
-                ),
-          title: Row(
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  displayname,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  softWrap: false,
-                  style: TextStyle(
-                    fontWeight: unread ? FontWeight.bold : null,
-                  ),
-                ),
-              ),
-              if (isMuted)
-                const Padding(
-                  padding: EdgeInsets.only(left: 4.0),
-                  child: Icon(
-                    Icons.notifications_off_outlined,
-                    size: 16,
-                  ),
-                ),
-              if (room.isFavourite)
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: room.notificationCount > 0 ? 4.0 : 0.0,
-                  ),
-                  child: Icon(
-                    Icons.push_pin,
-                    size: 16,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              Padding(
-                padding: const EdgeInsets.only(left: 4.0),
-                child: Text(
-                  room.timeCreated.localizedTimeShort(context),
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: unread
-                        ? Theme.of(context).colorScheme.secondary
-                        : Theme.of(context).textTheme.bodyMedium!.color,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          subtitle: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (typingText.isEmpty &&
-                  ownMessage &&
-                  room.lastEvent!.status.isSending) ...[
-                const SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator.adaptive(strokeWidth: 2),
-                ),
-                const SizedBox(width: 4),
-              ],
-              AnimatedContainer(
-                width: typingText.isEmpty ? 0 : 18,
-                clipBehavior: Clip.hardEdge,
-                decoration: const BoxDecoration(),
-                duration: FluffyThemes.animationDuration,
-                curve: FluffyThemes.animationCurve,
-                padding: const EdgeInsets.only(right: 4),
-                child: Icon(
-                  Icons.edit_outlined,
-                  color: Theme.of(context).colorScheme.secondary,
-                  size: 14,
-                ),
-              ),
-              Expanded(
-                child: typingText.isNotEmpty
-                    ? Text(
-                        typingText,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                        maxLines: 1,
-                        softWrap: false,
-                      )
-                    : FutureBuilder<String>(
-                        future: room.lastEvent?.calcLocalizedBody(
-                              MatrixLocals(L10n.of(context)!),
-                              hideReply: true,
-                              hideEdit: true,
-                              plaintextBody: true,
-                              removeMarkdown: true,
-                              withSenderNamePrefix: !room.isDirectChat ||
-                                  room.directChatMatrixID !=
-                                      room.lastEvent?.senderId,
-                            ) ??
-                            Future.value(L10n.of(context)!.emptyChat),
-                        builder: (context, snapshot) {
-                          return Text(
-                            room.membership == Membership.invite
-                                ? L10n.of(context)!.youAreInvitedToThisChat
-                                : snapshot.data ??
-                                    room.lastEvent?.calcLocalizedBodyFallback(
-                                      MatrixLocals(L10n.of(context)!),
-                                      hideReply: true,
-                                      hideEdit: true,
-                                      plaintextBody: true,
-                                      removeMarkdown: true,
-                                      withSenderNamePrefix:
-                                          !room.isDirectChat ||
-                                              room.directChatMatrixID !=
-                                                  room.lastEvent?.senderId,
-                                    ) ??
-                                    L10n.of(context)!.emptyChat,
-                            softWrap: false,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontWeight: unread ? FontWeight.w600 : null,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onSurfaceVariant,
-                              decoration: room.lastEvent?.redacted == true
-                                  ? TextDecoration.lineThrough
-                                  : null,
-                            ),
-                          );
-                        },
+        child: Column(
+          children: [
+            ListTile(
+              visualDensity: const VisualDensity(vertical: -0.5),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+              onLongPress: onLongPress,
+              leading: selected
+                  ? SizedBox(
+                      width: Avatar.defaultSize,
+                      height: Avatar.defaultSize,
+                      child: Material(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(Avatar.defaultSize),
+                        child: const Icon(Icons.check, color: Colors.white),
                       ),
-              ),
-              const SizedBox(width: 8),
-              AnimatedContainer(
-                duration: FluffyThemes.animationDuration,
-                curve: FluffyThemes.animationCurve,
-                padding: const EdgeInsets.symmetric(horizontal: 7),
-                height: unreadBubbleSize,
-                width: room.notificationCount == 0 &&
-                        !unread &&
-                        !room.hasNewMessages
-                    ? 0
-                    : (unreadBubbleSize - 9) *
-                            room.notificationCount.toString().length +
-                        9,
-                decoration: BoxDecoration(
-                  color: room.highlightCount > 0 ||
-                          room.membership == Membership.invite
-                      ? Colors.red
-                      : room.notificationCount > 0 || room.markedUnread
-                          ? Theme.of(context).colorScheme.primary
-                          : Theme.of(context).colorScheme.primaryContainer,
-                  borderRadius: BorderRadius.circular(AppConfig.borderRadius),
-                ),
-                child: Center(
-                  child: room.notificationCount > 0
-                      ? Text(
-                          room.notificationCount.toString(),
-                          style: TextStyle(
-                            color: room.highlightCount > 0
-                                ? Colors.white
-                                : room.notificationCount > 0
-                                    ? Theme.of(context).colorScheme.onPrimary
-                                    : Theme.of(context)
-                                        .colorScheme
-                                        .onPrimaryContainer,
-                            fontSize: 13,
+                    )
+                  : Avatar(
+                      mxContent: room.avatar,
+                      name: displayname,
+                      onTap: onLongPress,
+                    ),
+              title: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Text(
+                          displayname,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: false,
+                          style: const TextStyle(
+                            fontFamily: 'SFProDisplayHeavy',
                           ),
-                        )
-                      : Container(),
-                ),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Wrap(
+                              spacing: 6,
+                              children: [
+                                if (room.isFavourite)
+                                  SvgPicture.asset(
+                                    CustomSVGIcons.pinIcon,
+                                    width: 16,
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                                if (isMuted)
+                                  SvgPicture.asset(
+                                    CustomSVGIcons.muteIcon,
+                                    width: 16,
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      room.timeCreated.localizedTimeShort(context),
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontFamily: 'SFProText',
+                        color: unread
+                            ? Theme.of(context).colorScheme.secondary
+                            : Theme.of(context).textTheme.bodyMedium!.color,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          onTap: () => clickAction(context),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (typingText.isEmpty && ownMessage && room.lastEvent!.status.isSending) ...[
+                    const SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: CircularProgressIndicator.adaptive(strokeWidth: 2),
+                    ),
+                    const SizedBox(width: 4),
+                  ],
+                  AnimatedContainer(
+                    width: typingText.isEmpty ? 0 : 18,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: const BoxDecoration(),
+                    duration: FluffyThemes.animationDuration,
+                    curve: FluffyThemes.animationCurve,
+                    padding: const EdgeInsets.only(right: 4),
+                    child: Icon(
+                      Icons.edit_outlined,
+                      color: Theme.of(context).colorScheme.secondary,
+                      size: 14,
+                    ),
+                  ),
+                  Expanded(
+                    child: typingText.isNotEmpty
+                        ? Text(
+                            typingText,
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                            maxLines: 1,
+                            softWrap: false,
+                          )
+                        : FutureBuilder<String>(
+                            future: room.lastEvent?.calcLocalizedBody(
+                                  MatrixLocals(L10n.of(context)!),
+                                  hideReply: true,
+                                  hideEdit: true,
+                                  plaintextBody: true,
+                                  removeMarkdown: true,
+                                  withSenderNamePrefix: !room.isDirectChat ||
+                                      room.directChatMatrixID != room.lastEvent?.senderId,
+                                ) ??
+                                Future.value(L10n.of(context)!.emptyChat),
+                            builder: (context, snapshot) {
+                              return Text(
+                                room.membership == Membership.invite
+                                    ? L10n.of(context)!.youAreInvitedToThisChat
+                                    : snapshot.data ??
+                                        room.lastEvent?.calcLocalizedBodyFallback(
+                                          MatrixLocals(L10n.of(context)!),
+                                          hideReply: true,
+                                          hideEdit: true,
+                                          plaintextBody: true,
+                                          removeMarkdown: true,
+                                          withSenderNamePrefix: !room.isDirectChat ||
+                                              room.directChatMatrixID != room.lastEvent?.senderId,
+                                        ) ??
+                                        L10n.of(context)!.emptyChat,
+                                softWrap: false,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                                  decoration: room.lastEvent?.redacted == true
+                                      ? TextDecoration.lineThrough
+                                      : null,
+                                ),
+                              );
+                            },
+                          ),
+                  ),
+                  const SizedBox(width: 8),
+                  AnimatedContainer(
+                    duration: FluffyThemes.animationDuration,
+                    curve: FluffyThemes.animationCurve,
+                    padding: const EdgeInsets.symmetric(horizontal: 7),
+                    height: unreadBubbleSize,
+                    width: room.notificationCount == 0 && !unread && !room.hasNewMessages
+                        ? 0
+                        : (unreadBubbleSize - 9) * room.notificationCount.toString().length + 9,
+                    decoration: BoxDecoration(
+                      color: room.highlightCount > 0 || room.membership == Membership.invite
+                          ? Colors.red
+                          : room.notificationCount > 0 || room.markedUnread
+                              ? Theme.of(context).colorScheme.primary
+                              : Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(AppConfig.borderRadius),
+                    ),
+                    child: Center(
+                      child: room.notificationCount > 0
+                          ? Text(
+                              room.notificationCount.toString(),
+                              style: TextStyle(
+                                color: room.highlightCount > 0
+                                    ? Colors.white
+                                    : room.notificationCount > 0
+                                        ? Theme.of(context).colorScheme.onPrimary
+                                        : Theme.of(context).colorScheme.onPrimaryContainer,
+                                fontSize: 13,
+                              ),
+                            )
+                          : Container(),
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () => clickAction(context),
+            ),
+            const Divider(
+              height: 0,
+              indent: 82,
+              thickness: 0.6,
+            )
+          ],
         ),
       ),
     );
