25746ed47b0100a4fc5fccfb7281fff2e18454c6
ManhNTX <tmnguyen@linagora.com>
2023-03-30 11:20:05 +0700

    create empty chat

---
+++ lib/pages/chat/chat_empty_view.dart
@@ -0,0 +1,149 @@
+import 'package:fluffychat/utils/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+class ChatEmptyView extends StatelessWidget {
+  const ChatEmptyView({
+    Key? key,
+    required this.isDirectChat,
+  }) : super(key: key);
+
+  final bool isDirectChat;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isDirectChat) {
+      return Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints(
+          maxWidth: 236,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              L10n.of(context)!.noMessageHereYet,
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 17,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              L10n.of(context)!.sendMessageGuide,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: Color(0xFF818C99),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Image.asset(
+              'assets/banner_empty_chat.png',
+              width: 128,
+              height: 128,
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints(
+          maxWidth: 256,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              L10n.of(context)!.youCreatedAChanel,
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 17,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              L10n.of(context)!.chanelCanHave,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: Color(0xFF818C99),
+              ),
+            ),
+            const SizedBox(height: 8),
+            _ruleChanel(L10n.of(context)!.upTo100000Members),
+            const SizedBox(height: 8),
+            _ruleChanel(L10n.of(context)!.persistentChatHistory),
+            const SizedBox(height: 8),
+            _ruleChanel(L10n.of(context)!.multiInteractionThreadedDiscussion),
+            const SizedBox(height: 24),
+            _buttonAddMember(context),
+          ],
+        ),
+      );
+    }
+  }
+
+  Widget _ruleChanel(String rule) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          'assets/ic_done.png',
+          width: 20,
+          height: 20,
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Text(
+            rule,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 15,
+              color: Color(0xFF818C99),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buttonAddMember(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/ic_add_member.png',
+          width: 20,
+          height: 20,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          L10n.of(context)!.addMember,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 15,
+            color: AppColor.primary,
+          ),
+        ),
+      ],
+    );
+  }
+
+}
