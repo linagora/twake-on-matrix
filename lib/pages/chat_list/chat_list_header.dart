e5ccdb349b29cb12e7aad4119b2c9ca95f792df0
Julian KOUNE <jkoune@linagora.com>
2023-03-30 03:51:12 +0200

    feat: improve search bar and add twake app bar

--- lib/pages/chat_list/chat_list_header.dart
+++ lib/pages/chat_list/chat_list_header.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/widgets/matrix.dart';
+import 'package:fluffychat/widgets/twake_components/twake_header.dart';
 import 'package:fluffychat/pages/chat_list/chat_list.dart';
 import 'package:fluffychat/pages/chat_list/client_chooser_button.dart';
-import '../../widgets/matrix.dart';
 
 class ChatListHeader extends StatelessWidget implements PreferredSizeWidget {
   final ChatListController controller;
@@ -16,145 +17,145 @@ class ChatListHeader extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     final selectMode = controller.selectMode;
 
-    return AppBar(
-      automaticallyImplyLeading: false,
-      leading: selectMode == SelectMode.normal
-          ? null
-          : IconButton(
-              tooltip: L10n.of(context)!.cancel,
-              icon: const Icon(Icons.close_outlined),
-              onPressed: controller.cancelAction,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-      title: selectMode == SelectMode.share
-          ? Text(
-              L10n.of(context)!.share,
-              key: const ValueKey(SelectMode.share),
-            )
-          : selectMode == SelectMode.select
-              ? Text(
-                  controller.selectedRoomIds.length.toString(),
-                  key: const ValueKey(SelectMode.select),
-                )
-              : SizedBox(
-                  height: 44,
-                  child: TextField(
-                    controller: controller.searchController,
-                    textInputAction: TextInputAction.search,
-                    onChanged: controller.onSearchEnter,
-                    decoration: InputDecoration(
-                      fillColor: Theme.of(context)
-                          .colorScheme
-                          .secondaryContainer
-                          .withAlpha(128),
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius:
-                            BorderRadius.circular(AppConfig.borderRadius),
-                      ),
-                      hintText: L10n.of(context)!.search,
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      prefixIcon: controller.isSearchMode
-                          ? IconButton(
-                              tooltip: L10n.of(context)!.cancel,
-                              icon: const Icon(Icons.close_outlined),
-                              onPressed: controller.cancelSearch,
-                              color: Theme.of(context).colorScheme.onBackground,
-                            )
-                          : Icon(
-                              Icons.search_outlined,
-                              color: Theme.of(context).colorScheme.onBackground,
-                            ),
-                      suffixIcon: controller.isSearchMode
-                          ? controller.isSearching
-                              ? const Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    vertical: 10.0,
-                                    horizontal: 12,
-                                  ),
-                                  child: SizedBox.square(
-                                    dimension: 24,
-                                    child: CircularProgressIndicator.adaptive(
-                                      strokeWidth: 2,
+    return Column(
+      children: [
+        TwakeHeader(controller: controller),
+        const SizedBox(height: 8),
+        Container(
+          height: 36,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              if (selectMode != SelectMode.normal)
+                IconButton(
+                  tooltip: L10n.of(context)!.cancel,
+                  icon: const Icon(Icons.close_outlined),
+                  onPressed: controller.cancelAction,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              Expanded(
+                child: selectMode == SelectMode.share
+                    ? Text(
+                        L10n.of(context)!.share,
+                        key: const ValueKey(SelectMode.share),
+                      )
+                    : selectMode == SelectMode.select
+                        ? Text(
+                            controller.selectedRoomIds.length.toString(),
+                            key: const ValueKey(SelectMode.select),
+                          )
+                        : TextField(
+                            controller: controller.searchController,
+                            textInputAction: TextInputAction.search,
+                            onChanged: controller.onSearchEnter,
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(0),
+                              fillColor: Theme.of(context).brightness == Brightness.light
+                                  ? const Color(0xfff2f3f5)
+                                  : const Color(0x222c2d2f),
+                              border: UnderlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(AppConfig.borderRadius),
+                              ),
+                              hintText: L10n.of(context)!.search,
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              prefixIcon: controller.isSearchMode
+                                  ? IconButton(
+                                      tooltip: L10n.of(context)!.cancel,
+                                      icon: const Icon(Icons.close_outlined),
+                                      onPressed: controller.cancelSearch,
+                                      color: Theme.of(context).colorScheme.onBackground,
+                                    )
+                                  : Icon(
+                                      Icons.search_outlined,
+                                      color: Theme.of(context).colorScheme.onBackground,
                                     ),
-                                  ),
-                                )
-                              : TextButton(
-                                  onPressed: controller.setServer,
-                                  style: TextButton.styleFrom(
-                                    textStyle: const TextStyle(fontSize: 12),
-                                  ),
-                                  child: Text(
-                                    controller.searchServer ??
-                                        Matrix.of(context)
-                                            .client
-                                            .homeserver!
-                                            .host,
-                                    maxLines: 2,
-                                  ),
-                                )
-                          : SizedBox(
-                              width: 0,
-                              child: ClientChooserButton(controller),
+                              suffixIcon: controller.isSearchMode
+                                  ? controller.isSearching
+                                      ? const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            vertical: 9.0,
+                                            horizontal: 14.0,
+                                          ),
+                                          child: SizedBox.square(
+                                            dimension: 20,
+                                            child: CircularProgressIndicator.adaptive(
+                                              strokeWidth: 2,
+                                            ),
+                                          ),
+                                        )
+                                      : TextButton(
+                                          onPressed: controller.setServer,
+                                          style: TextButton.styleFrom(
+                                            textStyle: const TextStyle(fontSize: 12),
+                                          ),
+                                          child: Text(
+                                            controller.searchServer ??
+                                                Matrix.of(context).client.homeserver!.host,
+                                            maxLines: 2,
+                                          ),
+                                        )
+                                  : const SizedBox.shrink(),
                             ),
-                    ),
-                  ),
-                ),
-      actions: selectMode == SelectMode.share
-          ? [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                  vertical: 8.0,
-                ),
-                child: ClientChooserButton(controller),
+                          ),
               ),
-            ]
-          : selectMode == SelectMode.select
-              ? [
-                  if (controller.spaces.isNotEmpty)
-                    IconButton(
-                      tooltip: L10n.of(context)!.addToSpace,
-                      icon: const Icon(Icons.workspaces_outlined),
-                      onPressed: controller.addToSpace,
-                    ),
-                  IconButton(
-                    tooltip: L10n.of(context)!.toggleUnread,
-                    icon: Icon(
-                      controller.anySelectedRoomNotMarkedUnread
-                          ? Icons.mark_chat_read_outlined
-                          : Icons.mark_chat_unread_outlined,
-                    ),
-                    onPressed: controller.toggleUnread,
+              if (selectMode == SelectMode.share)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
                   ),
-                  IconButton(
-                    tooltip: L10n.of(context)!.toggleFavorite,
-                    icon: Icon(
-                      controller.anySelectedRoomNotFavorite
-                          ? Icons.push_pin_outlined
-                          : Icons.push_pin,
-                    ),
-                    onPressed: controller.toggleFavouriteRoom,
+                  child: ClientChooserButton(controller),
+                ),
+              if (selectMode == SelectMode.select && controller.spaces.isNotEmpty)
+                IconButton(
+                  tooltip: L10n.of(context)!.addToSpace,
+                  icon: const Icon(Icons.workspaces_outlined),
+                  onPressed: controller.addToSpace,
+                ),
+              if (selectMode == SelectMode.select)
+                IconButton(
+                  tooltip: L10n.of(context)!.toggleUnread,
+                  icon: Icon(
+                    controller.anySelectedRoomNotMarkedUnread
+                        ? Icons.mark_chat_read_outlined
+                        : Icons.mark_chat_unread_outlined,
                   ),
-                  IconButton(
-                    icon: Icon(
-                      controller.anySelectedRoomNotMuted
-                          ? Icons.notifications_off_outlined
-                          : Icons.notifications_outlined,
-                    ),
-                    tooltip: L10n.of(context)!.toggleMuted,
-                    onPressed: controller.toggleMuted,
+                  onPressed: controller.toggleUnread,
+                ),
+              if (selectMode == SelectMode.select)
+                IconButton(
+                  tooltip: L10n.of(context)!.toggleFavorite,
+                  icon: Icon(
+                    controller.anySelectedRoomNotFavorite
+                        ? Icons.push_pin_outlined
+                        : Icons.push_pin,
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.delete_outlined),
-                    tooltip: L10n.of(context)!.archive,
-                    onPressed: controller.archiveAction,
+                  onPressed: controller.toggleFavouriteRoom,
+                ),
+              if (selectMode == SelectMode.select)
+                IconButton(
+                  icon: Icon(
+                    controller.anySelectedRoomNotMuted
+                        ? Icons.notifications_off_outlined
+                        : Icons.notifications_outlined,
                   ),
-                ]
-              : null,
+                  tooltip: L10n.of(context)!.toggleMuted,
+                  onPressed: controller.toggleMuted,
+                ),
+              if (selectMode == SelectMode.select)
+                IconButton(
+                  icon: const Icon(Icons.delete_outlined),
+                  tooltip: L10n.of(context)!.archive,
+                  onPressed: controller.archiveAction,
+                ),
+            ],
+          ),
+        )
+      ],
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(56);
+  Size get preferredSize => const Size.fromHeight(108);
 }
