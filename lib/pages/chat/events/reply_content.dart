d6eed65806e00d31bc320bb35327410e1ed0d340
ManhNTX <tmnguyen@linagora.com>
2023-04-04 13:40:02 +0700

    fix quote is not visible in dark mode

--- lib/pages/chat/events/reply_content.dart
+++ lib/pages/chat/events/reply_content.dart
@@ -76,7 +76,7 @@ class ReplyContent extends StatelessWidget {
           width: 3,
           height: fontSizeDisplayContent * 2 + 6,
           color: ownMessage
-              ? Theme.of(context).colorScheme.onPrimary
+              ? Theme.of(context).colorScheme.onSecondary
               : Theme.of(context).colorScheme.onPrimary,
         ),
         const SizedBox(width: 6),
@@ -95,7 +95,7 @@ class ReplyContent extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: ownMessage
-                          ? Theme.of(context).colorScheme.onPrimary
+                          ? Theme.of(context).colorScheme.onSecondary
                           : Theme.of(context).colorScheme.onPrimary,
                       fontSize: fontSizeDisplayName,
                     ),
