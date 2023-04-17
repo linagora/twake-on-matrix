51da9b5d7277d47154f455e9ae2bd356902290da
Nguyen Thai <tknguyen@linagora.com>
2023-04-17 16:51:04 +0700

    Added release workflow for Android

--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-flutter build appbundle --target-platform android-arm,android-arm64,android-x64
-mkdir -p build/android
-cp build/app/outputs/bundle/release/app-release.aab build/android/
-cd android
-bundle install
-bundle update fastlane
+flutter build appbundle --release
 bundle exec fastlane deploy_internal_test
-cd ..
