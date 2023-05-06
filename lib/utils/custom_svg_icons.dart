8e25f274c0c12862bc8b989b3c448d5dba188765
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-06 21:16:53 +0700

    Delete unused svg to use IconData instead

--- lib/utils/custom_svg_icons.dart
+++ lib/utils/custom_svg_icons.dart
@@ -3,10 +3,5 @@ class CustomSVGIcons {
 
   static String muteIcon = '${baseAssetUrl}mute.svg';
   static String pinIcon = '${baseAssetUrl}pin.svg';
-  static String addIcon = '${baseAssetUrl}add.svg';
-  static String editIcon = '${baseAssetUrl}edit.svg';
-  static String groupsIcon = '${baseAssetUrl}groups.svg';
-  static String messagesIcon = '${baseAssetUrl}messages.svg';
-  static String rectangleInfoIcon = '${baseAssetUrl}rectangle_info.svg';
-  static String channelsIcon = '${baseAssetUrl}channels.svg';
+  static String titleChatList = '${baseAssetUrl}logo_twake_title.svg';
 }
