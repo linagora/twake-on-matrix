dab948da4673f095afc41593398ba1a0b0d9e545
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-06 22:29:06 +0700

    Apply M3 styles for AppBar in Chat List

---
+++ lib/pages/chat_list/client_chooser_button_style.dart
@@ -0,0 +1,8 @@
+
+
+class ClientChooserButtonStyle {
+  static double get avatarSizeInAppBar => 40.0;
+  static double get avatarFontSizeInAppBar => 17.0;
+
+  static double get dropDownIconSize => 20.0;
+}
