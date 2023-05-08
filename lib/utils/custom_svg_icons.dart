b6011c6158e6bded4fea2699ca0764b04bd639ea
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-09 06:39:23 +0700

    [Temp] Apply M3 to chat list item

--- lib/utils/custom_svg_icons.dart
+++ lib/utils/custom_svg_icons.dart
@@ -1,7 +1,5 @@
 class CustomSVGIcons {
   static String baseAssetUrl = 'assets/';
 
-  static String muteIcon = '${baseAssetUrl}mute.svg';
-  static String pinIcon = '${baseAssetUrl}pin.svg';
   static String titleChatList = '${baseAssetUrl}logo_twake_title.svg';
 }
