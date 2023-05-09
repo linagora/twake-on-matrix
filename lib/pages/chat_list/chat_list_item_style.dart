bc68bf679c593fabd32564b843888958a2081ec0
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-09 14:08:45 +0700

    Apply M3 for title of Chat list items

---
+++ lib/pages/chat_list/chat_list_item_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
+
+class ChatListItemStyle {
+  static Color? get readIconColor => LinagoraRefColors.material().tertiary[20];
+  static double get readIconSize => 16;
+  static double get editIconSize => 14;
+}
