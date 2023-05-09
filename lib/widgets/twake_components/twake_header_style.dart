459bfc50d288262ee03890869049c3b8c63add3d
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-09 14:04:21 +0700

    Update new logo

--- lib/widgets/twake_components/twake_header_style.dart
+++ lib/widgets/twake_components/twake_header_style.dart
@@ -1,6 +1,6 @@
 class TwakeHeaderStyle {
   static get toolbarHeight => 56.0;
   static get leadingWidth => 76.0;
-  static get titleHeight => 32.0;
+  static get titleHeight => 36.0;
   static const moreIconSize = 24.0;
 }
