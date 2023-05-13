c11e5c75fb10d4507d9a00d5ee4aff95a613b10e
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 06:42:09 +0700

    Add homeserver summary extension with ToM information

---
+++ test/domain/model/discovery_information_test.dart
@@ -0,0 +1,93 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:matrix/matrix.dart';
+
+void main() {
+  group('Customization field test cases', () {
+    test('multiple valid customization fields', () {
+      final multipleCustomizationFields = {
+        "m.homeserver": {
+          "base_url": "matrix.tom-dev.xyz"
+        },
+        "m.identity_server": {
+          "base_url": "https://tom.tom-dev.xyz/"
+        },
+        "t.server": {
+          "base_url": "https://tom.tom-dev.xyz/"
+        },
+        "m.integrations": {
+          "jitsi": {
+            "preferredDomain": "jitsi.linagora.com",
+            "baseUrl": "https://jitsi.linagora.com",
+            "useJwt": false
+          }
+        },
+        "t.domain": {
+          "url": "tom-dev.xyz"
+        }
+      };
+
+      final actual = DiscoveryInformation.fromJson(multipleCustomizationFields);
+      expect(actual, isNotNull);
+      expect(actual.additionalProperties['m.integrations'], isNotNull);
+      expect(actual.additionalProperties['t.server'], isNotNull);
+      expect(actual.additionalProperties['t.domain'], isNotNull);
+    });
+
+    test('multiple valid customization from ToM', () {
+      final multipleCustomizationFields = {
+        "m.homeserver": {
+          "base_url": "matrix.tom-dev.xyz"
+        },
+        "m.identity_server": {
+          "base_url": "https://tom.tom-dev.xyz/"
+        },
+        "t.server": {
+          "base_url": "https://tom.tom-dev.xyz/",
+          "server_name": "tom-dev.xyz"
+        },
+        "m.integrations": {
+          "jitsi": {
+            "preferredDomain": "jitsi.linagora.com",
+            "baseUrl": "https://jitsi.linagora.com",
+            "useJwt": false
+          }
+        }
+      };
+
+      final actual = DiscoveryInformation.fromJson(multipleCustomizationFields);
+
+      final requestUri = Uri(path: '_matrix/client/versions');
+      final server = Uri.parse('https://${actual.mHomeserver.baseUrl.toString()}');
+      print(server.toString());
+      final request = server.resolveUri(requestUri);
+      print(request.toString());
+
+      expect(actual, isNotNull);
+      expect(actual.additionalProperties['m.integrations'], isNotNull);
+      expect(actual.additionalProperties['t.server'], isNotNull);
+    });
+
+    test('one invalid customization field', () {
+      final multipleCustomizationFields = {
+        "m.homeserver": {
+          "base_url": "matrix.tom-dev.xyz"
+        },
+        "m.identity_server": {
+          "base_url": "https://tom.tom-dev.xyz/"
+        },
+        "t.server": {
+          "base_url": "https://tom.tom-dev.xyz/"
+        },
+        "m.integrations": {
+          "jitsi": {
+            "preferredDomain": "jitsi.linagora.com",
+            "baseUrl": "https://jitsi.linagora.com",
+            "useJwt": false
+          }
+        },
+        "t.domain": "tom-dev.xyz"
+      };
+      expect(() => DiscoveryInformation.fromJson(multipleCustomizationFields), throwsA(isA<TypeError>()));
+    });
+  });
+}
