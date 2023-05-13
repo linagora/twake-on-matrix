797821f595f9c6c81b2bf84ecf30b9c49c79c94f
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 06:42:09 +0700

    Add homeserver summary extension with ToM information

---
+++ lib/domain/model/extensions/homeserver_summary_extensions.dart
@@ -0,0 +1,20 @@
+import 'package:fluffychat/domain/model/tom_server_information.dart';
+import 'package:matrix/matrix.dart';
+
+extension HomeserverSummaryExtensions on HomeserverSummary {
+  ToMServerInformation? get tomServer {
+    if (discoveryInformation?.additionalProperties == null) {
+      return null;
+    }
+    final tomServerJson = discoveryInformation?.additionalProperties['t.server'];
+    if (tomServerJson == null) {
+      return null;
+    }
+    try {
+      return ToMServerInformation.fromJson(tomServerJson);
+    } catch (e) {
+      Logs().e('Failed to parse t.server from homeserver summary', e);
+      return null;
+    }
+  }
+}
