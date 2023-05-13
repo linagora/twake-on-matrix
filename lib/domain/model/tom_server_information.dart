e9a6f64c4ac3c4ecf71d6878a536858b788ee92f
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 06:42:09 +0700

    Add homeserver summary extension with ToM information

---
+++ lib/domain/model/tom_server_information.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'tom_server_information.g.dart';
+
+@JsonSerializable()
+class ToMServerInformation with EquatableMixin {
+  @JsonKey(name: 'base_url')
+  final Uri? baseUrl;
+
+  @JsonKey(name: 'server_name')
+  final String? serverName;
+
+  ToMServerInformation({
+    this.baseUrl,
+    this.serverName,
+  });
+
+  factory ToMServerInformation.fromJson(Map<String, dynamic> json) => _$ToMServerInformationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ToMServerInformationToJson(this);
+
+  @override
+  List<Object?> get props => [baseUrl, serverName];
+}
