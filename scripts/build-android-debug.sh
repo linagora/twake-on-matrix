338560465dc1f079d44a8fe33843b47752316d45
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 07:00:29 +0700

    Add build_runner for build scripts

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-flutter pub get
+flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter build apk --debug
--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ve
 flutter config --enable-linux-desktop
 flutter clean
-flutter pub get
+flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter build linux --release -v
