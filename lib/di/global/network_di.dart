c3bdab1a42d4309bd873eecb73adf1a4becac8bc
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 10:42:11 +0700

    Setup ToM URL for DioClient

--- lib/di/global/network_di.dart
+++ lib/di/global/network_di.dart
@@ -2,19 +2,22 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:fluffychat/data/network/dio_client.dart';
-import 'package:fluffychat/data/network/interceptor/tom_authorization_interceptor.dart';
+import 'package:fluffychat/data/network/interceptor/dynamic_url_interceptor.dart';
 import 'package:fluffychat/di/base_di.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 
 class NetworkDI extends BaseDI {
 
+  static const tomServerUrlInterceptorName = 'tomServerDynamicUrlInterceptor';
+  static const identityUrlInterceptorName = 'identityDynamicUrlInterceptor';
   static const acceptHeaderDefault = 'application/json';
   static const contentTypeHeaderDefault = 'application/json';
 
   @override
   void setUp(GetIt get) {
     _bindBaseOption(get);
+    _bindInterceptor(get);
     _bindDio(get);
     _bindDioClient(get);
   }
@@ -28,10 +31,21 @@ class NetworkDI extends BaseDI {
     get.registerLazySingleton<BaseOptions>(() => BaseOptions(headers: headers));
   }
 
+  void _bindInterceptor(GetIt get) {
+    get.registerLazySingleton(
+      () => DynamicUrlInterceptors(),
+      instanceName: tomServerUrlInterceptorName,
+    );
+    get.registerLazySingleton(
+      () => DynamicUrlInterceptors(),
+      instanceName: identityUrlInterceptorName,
+    );
+  }
+
   void _bindDio(GetIt get) {
-    get.registerSingleton<ToMAuthorizationInterceptor>(ToMAuthorizationInterceptor());
     final dio = Dio(get.get<BaseOptions>());
-    dio.interceptors.add(get.get<ToMAuthorizationInterceptor>());
+    dio.interceptors.add(get.get<DynamicUrlInterceptors>(instanceName: tomServerUrlInterceptorName));
+    dio.interceptors.add(get.get<DynamicUrlInterceptors>(instanceName: identityUrlInterceptorName));
     if (kDebugMode) {
       dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
     }
