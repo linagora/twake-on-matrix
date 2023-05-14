e51f1e659fdc1ca90c1024c8274a6643ab6c530e
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-14 07:00:29 +0700

    Add build_runner for build scripts

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ve
 flutter config --enable-web
 flutter clean
-flutter pub get
+flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter build web --release --verbose --source-maps
 # bug of the Flutter engine
 chmod +r -R build/web
