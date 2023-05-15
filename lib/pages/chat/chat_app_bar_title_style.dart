6efb583329bd08a31836cf8f96db2e9bfaec683a
sherlock <quangkhai28999@gmail.com>
2023-05-15 11:47:54 +0700

    TF-81: apply M3 design to chat appbar screen

---
+++ lib/pages/chat/chat_app_bar_title_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatAppBarTitleStyle {
+  static Color get currentlyActiveColor => const Color(0xFF5AD439);
+  static Color get currentlyInactiveColor => const Color(0xFF818C99);
+  static double get avatarFontSize => 15.0;
+  static double get avatarSize => 40.0;
+
+  static double get statusSize => 15;
+  static Color get statusBorderColor => Colors.white;
+  static double get statusBorderSize => 2;
+
+  static double get letterSpacingRoomName => 0.15;
+  static double get letterSpacingStatusContent => 0.5;
+}
