a3baad181d052a598e5b6d5080c852faa01f0498
sherlock <quangkhai28999@gmail.com>
2023-05-15 11:47:12 +0700

    TF-81: add new widget, icon, package for chat screen

---
+++ lib/widgets/twake_components/twake_icon_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TwakeIconButton extends StatelessWidget {
+
+  final IconData? icon;
+
+  final String? imagePath;
+
+  final String tooltip;
+
+  final EdgeInsets margin;
+
+  final double? paddingAll;
+
+  final double? size;
+
+  final double? fill;
+
+  final double? weight;
+
+  final VoidCallback onPressed;
+
+  const TwakeIconButton({
+    Key? key, 
+    required this.onPressed,
+    required this.tooltip,
+    this.icon, 
+    this.imagePath,
+    this.paddingAll,
+    this.size, 
+    this.fill, 
+    this.weight, 
+    this.margin = const EdgeInsets.all(0),
+  }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: InkWell(
+        onTap: onPressed,
+        customBorder: const CircleBorder(),
+        radius: paddingAll,
+        child: Tooltip(
+          message: tooltip,
+          child: Padding(
+            padding: EdgeInsets.all(paddingAll ?? 8.0),
+            child: icon != null 
+              ? Icon(
+                icon,
+                size: size,
+                fill: fill,
+                weight: weight)
+              : imagePath != null ? SvgPicture.asset(imagePath!) : null,
+          ),
+        ),
+      ),
+    );
+  }
+
+}
