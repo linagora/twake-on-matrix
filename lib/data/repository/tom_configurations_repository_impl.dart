153f95c1b9c504c9c1a8e911398eb1dd8478fc82
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-16 11:07:34 +0700

    Implement for ToMRepository

---
+++ lib/data/repository/tom_configurations_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:fluffychat/data/datasource/tom_configurations_datasource.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/model/tom_configurations.dart';
+import 'package:fluffychat/domain/repository/tom_configurations_repository.dart';
+
+class ToMConfigurationsRepositoryImpl implements ToMConfigurationsRepository {
+  final ToMConfigurationsDatasource tomConfigurationsDatasource = getIt.get<ToMConfigurationsDatasource>();
+
+  @override
+  Future<ToMConfigurations> getTomConfigurations(String clientName) {
+    return tomConfigurationsDatasource.getTomConfigurations(clientName);
+  }
+
+  @override
+  Future<void> saveTomConfigurations(String clientName, ToMConfigurations toMConfigurations) {
+    return tomConfigurationsDatasource.saveTomConfigurations(clientName, toMConfigurations);
+  }
+
+
+}
