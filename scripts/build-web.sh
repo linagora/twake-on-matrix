715910c1f41df063455344c24ee98341dea365d5
Nguyen Thai <tknguyen@linagora.com>
2023-05-18 09:34:06 +0700

    Added iOS builds and releases

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ve
 flutter config --enable-web
 flutter clean
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-flutter build web --release --verbose --source-maps
-# bug of the Flutter engine
-chmod +r -R build/web
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter build web --release --verbose --source-maps --base-href="/web/"
+cp config.sample.json ./build/web/config.json
