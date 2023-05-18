0f4f537f1014a521369580d84946a15441e5439a
Nguyen Thai <tknguyen@linagora.com>
2023-05-18 09:34:06 +0700

    Added iOS builds and releases

--- scripts/release-ios-testflight.sh
+++ scripts/release-ios-testflight.sh
@@ -1,15 +1,2 @@
 #!/bin/sh -ve
-git apply ./scripts/enable-android-google-services.patch
-flutter clean
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-cd ios
-rm -rf Pods
-rm -f Podfile.lock
-arch -x86_64 pod install
-arch -x86_64 pod update
-cd ..
-flutter build ios --release
-cd ios
-bundle update fastlane
 bundle exec fastlane beta
-cd ..
