fed7a838969e8daf72a8cc896b29a60849b8b330
Nguyen Thai <tknguyen@linagora.com>
2023-05-18 09:34:06 +0700

    Added iOS builds and releases

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -7,3 +7,4 @@ echo "keyPassword=${ANDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=${ANDROID_KEY_ALIAS}" >> key.properties
 echo "storeFile=../android.jks" >> key.properties
 echo "$PLAYSTORE_DEPLOY_KEY" >> keys.json
+bundle exec fastlane set_build_code_internal
