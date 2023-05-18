2cf3f5f12a5af0b1ff795a8876722780e8946557
sherlock <quangkhai28999@gmail.com>
2023-05-18 12:25:19 +0700

    TF-81 add message time inside message bubble

---
+++ lib/pages/chat/events/message_content_style.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class MessageContentStyle {
+  static int get maxLengthTextInline => 180;
+  static double get appBarFontSize => 16.0;
+  static double get imageBubbleWidth => 256;
+  static double get imageBubbleHeight => 320;
+  static Color get backgroundColorButton => Colors.white.withAlpha(64);
+
+  static double get letterSpacingMessageContent => -0.15;
+}
---
+++ lib/pages/chat/events/message_reactions_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageReactionsStyle {
+  static double get moreReactionContainer => 28.0;
+  static double get moreReactionIconSize => 20.0;
+  static double get loadingReactionSize => 28.0;
+  static double get reactionContainerWidth => 50;
+
+  static Color get reactionBorderColor => Colors.white;
+  static double get reactionBorderRadius => 16.0;
+
+  static double get renderKeyFontSize => 15;
+  static Color get borderColor => Colors.white;
+  
+}
