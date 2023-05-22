e0fcdca1ae5123d58181391f23c79cddb49d1aeb
sherlock <quangkhai28999@gmail.com>
2023-05-22 10:23:53 +0700

    TF-69: add presentation model and its extension

---
+++ lib/domain/model/extensions/contact/presentation_contact_extension.dart
@@ -0,0 +1,25 @@
+import 'package:fluffychat/domain/model/contact/contact.dart';
+import 'package:fluffychat/pages/contacts/presentation/model/presentation_contact.dart';
+
+extension PresentaionContactExtension on PresentationContact {
+
+  bool matched({required bool Function(String) condition}) {
+    if (displayName != null && condition(displayName!)) {
+      return true;
+    }
+
+    if (matrixId != null && condition(matrixId!)) {
+      return true;
+    }
+
+    if (email != null && condition(email!)) {
+      return true;
+    } 
+    
+    if (status != null && condition(status.toString())) {
+      return true;
+    }
+
+    return false;
+  }
+}
