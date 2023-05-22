2aa0603b02a15419e0eba9cc4412faa2ca4861d3
sherlock <quangkhai28999@gmail.com>
2023-05-22 10:21:54 +0700

    TF-69: add dependency injection and route

---
+++ lib/pages/contacts/di/contact_di.dart
@@ -0,0 +1,44 @@
+import 'package:fluffychat/data/datasource/tom_contacts_datasource.dart';
+import 'package:fluffychat/data/datasource_impl/contact/tom_contacts_datasource_impl.dart';
+import 'package:fluffychat/data/network/contact/tom_contact_api.dart';
+import 'package:fluffychat/data/repository/contact/tom_contact_repository_impl.dart';
+import 'package:fluffychat/di/base_di.dart';
+import 'package:fluffychat/di/global/network_di.dart';
+import 'package:fluffychat/domain/repository/contact_repository.dart';
+import 'package:fluffychat/domain/usecase/fetch_contacts_interactor.dart';
+import 'package:fluffychat/domain/usecase/lookup_contacts_interactor.dart';
+import 'package:get_it/get_it.dart';
+import 'package:matrix/matrix.dart';
+
+class ContactDI extends BaseDI {
+
+  @override
+  String get scopeName => 'Contacts';
+
+  @override
+  void setUp(GetIt get) {
+    Logs().d('ContactDI::setUp()');
+
+    get.registerFactory<TomContactAPI>(
+      () => TomContactAPI(),
+    );
+
+    get.registerFactory<TomContactsDatasource>(
+      () => TomContactsDatasourceImpl(),
+    );
+
+    get.registerFactory<ContactRepository>(
+      () => TomContactRepositoryImpl(),
+    );
+    
+    get.registerFactory<LookupContactsInteractor>(
+      () => LookupContactsInteractor(),
+    );
+
+    get.registerFactory<FetchContactsInteractor>(
+      () => FetchContactsInteractor(),
+    );
+    
+    Logs().d('ContactDI::setUp() - done');
+  }
+}
