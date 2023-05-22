8dbba120c58defa28b4825cac4670e978ed64a67
sherlock <quangkhai28999@gmail.com>
2023-05-22 10:23:53 +0700

    TF-69: add presentation model and its extension

---
+++ lib/pages/contacts/presentation/model/presentation_contact.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:fluffychat/domain/model/contact/contact_status.dart';
+
+class PresentationContact extends Equatable {
+
+  final String? email;
+
+  final String? displayName;
+
+  final String? matrixId;
+
+  final ContactStatus? status;
+
+  const PresentationContact({
+    this.email,
+    this.displayName,
+    this.matrixId,
+    this.status
+  });
+  
+  @override
+  List<Object?> get props => [email, displayName, matrixId, status];
+}
