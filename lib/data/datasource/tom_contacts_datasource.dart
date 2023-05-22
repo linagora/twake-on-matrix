10e8e586d6623d335aff842265c9a2efb0166f13
sherlock <quangkhai28999@gmail.com>
2023-05-22 10:15:55 +0700

    TF-69: add api and datasource layer

---
+++ lib/data/datasource/tom_contacts_datasource.dart
@@ -0,0 +1,6 @@
+import 'package:fluffychat/domain/model/contact/contact.dart';
+import 'package:fluffychat/domain/model/contact/contact_query.dart';
+
+abstract class TomContactsDatasource {
+  Future<List<Contact>> searchContacts({required ContactQuery query});
+}
