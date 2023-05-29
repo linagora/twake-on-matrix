e0492f4c894461aa87ec4d8d1d9432fc45709792
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 14:38:58 +0700

    TW-79 Create RecoveryWords repository

---
+++ lib/data/datasource/recovery_words_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:fluffychat/domain/model/recovery_words/recovery_words.dart';
+
+abstract class RecoveryWordsDataSource {
+  Future<RecoveryWords> getRecoveryWords();
+
+  Future<bool> saveRecoveryWords(String recoveryWords);
+}
