645484c43fbd9ac14906ad50ffb061c5916f3b8d
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 15:42:25 +0700

    TW-79 Add SaveRecoveryWord interactor

---
+++ lib/domain/app_state/recovery_words/save_recovery_words_success.dart
@@ -0,0 +1,9 @@
+import 'package:fluffychat/app_state/success.dart';
+
+class SaveRecoveryWordsSuccess extends Success {
+
+  const SaveRecoveryWordsSuccess();
+
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/domain/usecases/save_recovery_words_interactor.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/app_state/recovery_words/save_recovery_words_failed.dart';
+import 'package:fluffychat/domain/app_state/recovery_words/save_recovery_words_success.dart';
+import 'package:fluffychat/domain/repository/recovery_words_repository.dart';
+
+class SaveRecoveryWordsInteractor {
+  final RecoveryWordsRepository recoveryWordsRepository = getIt.get<RecoveryWordsRepository>();
+
+  Future<Either<SaveRecoveryWordsFailed, SaveRecoveryWordsSuccess>> execute(String words) async {
+    try {
+      final bool response = await recoveryWordsRepository.saveRecoveryWords(words);
+      return response
+        ? const Right(SaveRecoveryWordsSuccess())
+        : const Left(SaveRecoveryWordsFailed());
+    } catch (e) {
+      return Left(SaveRecoveryWordsFailed(exception: e));
+    }
+  }
+}
