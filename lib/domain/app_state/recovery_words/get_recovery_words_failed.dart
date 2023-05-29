1e98fdea989332940d645347763afb7752e720b9
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 15:42:04 +0700

    TW-79 Add GetRecoveryInteractor

---
+++ lib/domain/app_state/recovery_words/get_recovery_words_failed.dart
@@ -0,0 +1,11 @@
+import 'package:fluffychat/app_state/failure.dart';
+
+class GetRecoveryWordsFailed extends Failure {
+
+  final dynamic exception;
+
+  const GetRecoveryWordsFailed({required this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
---
+++ lib/domain/usecases/get_recovery_words_interactor.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/app_state/recovery_words/get_recovery_words_failed.dart';
+import 'package:fluffychat/domain/app_state/recovery_words/get_recovery_words_success.dart';
+import 'package:fluffychat/domain/model/recovery_words/recovery_words.dart';
+import 'package:fluffychat/domain/repository/recovery_words_repository.dart';
+import 'package:matrix/matrix.dart';
+
+class GetRecoveryWordsInteractor {
+  final RecoveryWordsRepository _recoveryWordsRepository = getIt.get<RecoveryWordsRepository>();
+
+  Future<Either<GetRecoveryWordsFailed, GetRecoveryWordsSuccess>> execute() async {
+    try {
+      final RecoveryWords response = await _recoveryWordsRepository.getRecoveryWords();
+      return Right(GetRecoveryWordsSuccess(words: response));
+    } catch (e) {
+      Logs().e('GetRecoveryWordsInteractor::execute(): $e');
+      return Left(GetRecoveryWordsFailed(exception: e));
+    }
+  }
+}
