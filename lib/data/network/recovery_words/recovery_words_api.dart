73e2d60097d971db7495493dbb4549d2d3e64a0f
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 15:40:11 +0700

    TW-79 Add RecoveryAPI

---
+++ lib/data/network/recovery_words/recovery_words_api.dart
@@ -0,0 +1,27 @@
+import 'package:fluffychat/data/model/recovery_words_json.dart';
+import 'package:fluffychat/data/network/dio_client.dart';
+import 'package:fluffychat/data/network/tom_endpoint.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/di/global/network_di.dart';
+
+class RecoveryWordsAPI {
+  final DioClient _client = getIt.get<DioClient>(instanceName: NetworkDI.tomDioClientName);
+
+  RecoveryWordsAPI();
+
+  Future<RecoveryWordsResponse> getRecoveryWords() async {
+    final response = await _client
+      .get(TomEndpoint.recoveryWordsServicePath.path)
+      .onError((error, stackTrace) => throw Exception(error));
+    return RecoveryWordsResponse.fromJson(response);
+  }
+
+  Future<bool> saveRecoveryWords(String recoveryWords) async {
+    final response = await _client
+      .post(
+        TomEndpoint.recoveryWordsServicePath.path,
+        data: RecoveryWordsResponse(words: recoveryWords),)
+      .onError((error, stackTrace) => throw Exception(error));
+    return response.statusCode == 200;
+  }
+}
