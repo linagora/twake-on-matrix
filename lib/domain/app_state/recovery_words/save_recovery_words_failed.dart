20f18ab1eea28780e1b93b03d3167e3eab4141a3
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 15:42:25 +0700

    TW-79 Add SaveRecoveryWord interactor

---
+++ lib/domain/app_state/recovery_words/save_recovery_words_failed.dart
@@ -0,0 +1,12 @@
+import 'package:fluffychat/app_state/failure.dart';
+
+class SaveRecoveryWordsFailed extends Failure {
+
+  final dynamic exception;
+
+  const SaveRecoveryWordsFailed({this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+
+}
