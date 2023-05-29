3017958a4e4d2e02b4268e63c7da34803f5b149a
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 15:40:11 +0700

    TW-79 Add RecoveryAPI

---
+++ lib/data/network/tom_endpoint.dart
@@ -0,0 +1,7 @@
+import 'package:fluffychat/data/network/service_path.dart';
+
+class TomEndpoint {
+  static final ServicePath recoveryWordsServicePath = ServicePath(
+      '/_twake/recoveryWords',
+  );
+}
