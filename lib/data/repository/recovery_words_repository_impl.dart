625f0d7c0ba1dbb832c9c8296e24420dbf3c4613
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-05-29 14:41:45 +0700

    TW-79 Implement RecoveryWordsRepository

---
+++ lib/data/repository/recovery_words_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:fluffychat/data/datasource/recovery_words_data_source.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/model/recovery_words/recovery_words.dart';
+import 'package:fluffychat/domain/repository/recovery_words_repository.dart';
+
+class RecoveryWordsRepositoryImpl implements RecoveryWordsRepository {
+  final RecoveryWordsDataSource recoveryWordsDataSource = getIt.get<RecoveryWordsDataSource>();
+
+  @override
+  Future<RecoveryWords> getRecoveryWords() {
+    return recoveryWordsDataSource.getRecoveryWords();
+  }
+
+  @override
+  Future<bool> saveRecoveryWords(String recoveryWords) async {
+    return await recoveryWordsDataSource.saveRecoveryWords(recoveryWords);
+  }
+}
