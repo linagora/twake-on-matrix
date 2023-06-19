df37e3b8a4cfab378e29ba51e35e4ea03d3fd4d3
sherlock <quangkhai28999@gmail.com>
2023-06-19 17:11:21 +0700

    TW-132: sending multiples image in composer

--- lib/di/send_image/send_image_di.dart
+++ lib/di/send_image/send_image_di.dart
@@ -1,5 +1,8 @@
+import 'dart:collection';
+
 import 'package:fluffychat/di/base_di.dart';
 import 'package:fluffychat/domain/usecase/send_image_interactor.dart';
+import 'package:fluffychat/domain/usecase/send_images_interactor.dart';
 import 'package:get_it/get_it.dart';
 
 class SendImageDi extends BaseDI {
@@ -8,6 +11,9 @@ class SendImageDi extends BaseDI {
 
   @override
   void setUp(GetIt get) {
+    get.registerSingleton<Queue>(Queue());
+
     get.registerSingleton<SendImageInteractor>(SendImageInteractor());
+    get.registerSingleton<SendImagesInteractor>(SendImagesInteractor());
   }
 }
