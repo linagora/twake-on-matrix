2065f465971f69e7186978ad6036908909734c7e
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-06-20 06:54:19 +0200

    Support full matrixId for contact

--- lib/domain/model/extensions/contact/tom_contact_extension.dart
+++ lib/domain/model/extensions/contact/tom_contact_extension.dart
@@ -1,15 +1,14 @@
 import 'package:fluffychat/domain/model/contact/contact.dart';
 import 'package:fluffychat/domain/model/contact/contact_status.dart';
 import 'package:fluffychat/domain/model/contact/tom_contact.dart';
-import 'package:fluffychat/utils/string_extension.dart';
 
 extension TomContactExtension on TomContact {
   Contact toContact(ContactStatus status) {
     return Contact(
       emails: mail != null ? {mail!} : {},
-      displayName: mxid,
+      displayName: uid,
       phoneNumber: phoneNumber,
-      matrixId: mxid.toTomMatrixId(),
+      matrixId: address,
       status: status,
     );
   }
