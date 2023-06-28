a475563264fd90933385092420ecb51e8c6265f0
HuyNguyen <quanghnguyen@linagora.com>
2023-06-29 00:03:35 +0700

    TW-235: Handle send file in presentation layer

---
+++ lib/pages/chat/chat_actions_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatActionsStyle {
+  static const Color colorBackgroundGalleryBottom = Color(0xFFFFAB00);
+  static const Color colorBackgroundDocumentBottom = Color(0xFFEDE7F6);
+  static const Color colorBackgroundLocationBottom = Color(0xFFE0F2F1);
+  static const Color colorBackgroundContactBottom = Color(0xFFFCE4EC);
+
+  static const Color colorGalleryIcon = Color(0xFFFFFFFF);
+  static const Color colorDocumentIcon = Color(0xFF7C4DFF);
+  static const Color colorLocationIcon = Color(0xFF00BFA5);
+  static const Color colorContactIcon = Color(0xFF880E4F);
+}
