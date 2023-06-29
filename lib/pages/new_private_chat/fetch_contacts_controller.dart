a462c628f98c6a3f74e2c99db64cd0b9f3d1121b
sherlock <quangkhai28999@gmail.com>
2023-06-29 20:10:58 +0700

    TW-192: update controller components and mixin

--- lib/pages/new_private_chat/fetch_contacts_controller.dart
+++ lib/pages/new_private_chat/fetch_contacts_controller.dart
@@ -5,11 +5,18 @@ import 'package:dartz/dartz.dart';
 import 'package:fluffychat/app_state/failure.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/app_state/contact/get_contacts_success.dart';
+import 'package:fluffychat/domain/app_state/contact/get_network_contact_success.dart';
+import 'package:fluffychat/domain/model/extensions/contact/contact_extension.dart';
 import 'package:fluffychat/domain/usecase/fetch_contacts_interactor.dart';
+import 'package:fluffychat/domain/usecase/load_more_internal_contacts.dart';
+import 'package:fluffychat/presentation/mixin/load_more_contacts_mixin.dart';
+import 'package:fluffychat/presentation/model/presentation_contact.dart';
 
-class FetchContactsController {
+class FetchContactsController with LoadMoreContactsMixin {
+
   final _fetchContactsInteractor = getIt.get<FetchContactsInteractor>();
-  final streamController = StreamController<Either<Failure, GetContactsSuccess>>();
+  final _loadMoreInternalContacts = getIt.get<LoadMoreInternalContacts>();
+  final streamController = StreamController<Either<Failure, GetContactsSuccess>>();  
 
   void fetchCurrentTomContacts() {
     _fetchContactsInteractor
@@ -17,6 +24,47 @@ class FetchContactsController {
       .listen((event) {
         streamController.add(event);
       });
+  }
+
+  Set<PresentationContact> getContactsFromFetchStream(Either<Failure, GetContactsSuccess> event) {
+    return event.fold<Set<PresentationContact>>(
+      (failure) => <PresentationContact>{},
+      (success) {
+        final currentContacts = success.contacts.expand((contact) => contact.toPresentationContacts());
+        updateLastContactIndex(oldContactsList.length);
+        if (success is GetMoreNetworkContactSuccess) {
+          _handleGetMoreContactsSuccess(currentContacts);
+        } else if (success is NoMoreContactSuccess) {
+          _handleNoMoreContactsSuccess(currentContacts);
+        } else {
+          oldContactsList = currentContacts.toSet();
+        }
+        return oldContactsList;
+      },
+    ).toSet();
+  }
+
+  void _handleGetMoreContactsSuccess(Iterable<PresentationContact> currentContacts) {
+    oldContactsList.addAll(currentContacts);
+    haveMoreCountactsNotifier.value = currentContacts.isNotEmpty;
+    isLoadMore = true;
+  }
+
+  void _handleNoMoreContactsSuccess(Iterable<PresentationContact> currentContacts) {
+    haveMoreCountactsNotifier.value = false;
+    oldContactsList.addAll(currentContacts);
+    isLoadMore = false;
+  }
+
+  void loadMoreContacts({required int offset}) {
+    if (isLoadMore) {
+      isLoadMore = false;
+      _loadMoreInternalContacts
+        .execute(offset: offset)
+        .listen((event) {
+          streamController.add(event);
+        });
+    }
   }
 
   void dispose() {
---
+++ lib/presentation/mixin/load_more_contacts_mixin.dart
@@ -0,0 +1,33 @@
+
+import 'package:fluffychat/pages/new_private_chat/fetch_contacts_controller.dart';
+import 'package:fluffychat/presentation/model/presentation_contact.dart';
+import 'package:flutter/widgets.dart';
+
+mixin LoadMoreContactsMixin {
+  final scrollController = ScrollController();
+
+  final haveMoreCountactsNotifier = ValueNotifier(true);
+
+  final lastContactIndexNotifier = ValueNotifier(0);
+
+  Set<PresentationContact> oldContactsList = {};
+  bool isLoadMore = true;
+
+  void listenForScrollChanged({required FetchContactsController fetchContactsController}) {
+    scrollController.addListener(() {
+      if (isLoadMoreAction) {
+        fetchContactsController.loadMoreContacts(
+          offset: fetchContactsController.lastContactIndexNotifier.value
+        );
+      }
+    });
+  }
+
+  void updateLastContactIndex(int value) {
+    lastContactIndexNotifier.value = value;
+  }
+
+  bool get isLoadMoreAction {
+    return scrollController.offset >= scrollController.position.maxScrollExtent;
+  }
+}
