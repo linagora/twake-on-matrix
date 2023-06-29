36f4940fbd154ceea1c1cddd04c7c39e75bb1eb2
sherlock <quangkhai28999@gmail.com>
2023-06-29 20:10:58 +0700

    TW-192: update controller components and mixin

---
+++ lib/pages/new_group/selected_contacts_map_change_notiifer.dart
@@ -0,0 +1,39 @@
+
+import 'package:fluffychat/presentation/model/presentation_contact.dart';
+import 'package:flutter/widgets.dart';
+
+class SelectedContactsMapChangeNotifier extends ChangeNotifier {
+  final Map<PresentationContact, ValueNotifier<bool>> selectedContactsMap = {};
+  final haveSelectedContactsNotifier = ValueNotifier(false);
+
+  Iterable<PresentationContact> get contactsList 
+    => selectedContactsMap.keys
+      .where((contact) => selectedContactsMap[contact]?.value ?? false);
+
+  void onContactTileTap({required PresentationContact contact}) {
+    final oldVal = selectedContactsMap[contact]?.value ?? false;
+    final newVal = !oldVal;
+    selectedContactsMap.putIfAbsent(contact, () => ValueNotifier(newVal));
+    selectedContactsMap[contact]!.value = newVal;
+    notifyListeners();
+    haveSelectedContactsNotifier.value = contactsList.isNotEmpty;
+  }
+
+  ValueNotifier<bool> getNotifierAtContact(PresentationContact contact) {
+    selectedContactsMap.putIfAbsent(contact, () => ValueNotifier(false));
+    return selectedContactsMap[contact]!;
+  }
+
+  void unselectContact(PresentationContact contact) {
+    if (selectedContactsMap.containsKey(contact)) {
+      selectedContactsMap[contact]!.value = false;
+    }
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    haveSelectedContactsNotifier.dispose();
+  }
+}
