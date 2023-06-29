bcbe7b2dbd08e22672d0228af3c531e13583a922
sherlock <quangkhai28999@gmail.com>
2023-06-29 20:05:46 +0700

    TW-192: add datasource layer

--- lib/data/datasource/tom_contacts_datasource.dart
+++ lib/data/datasource/tom_contacts_datasource.dart
@@ -2,5 +2,5 @@ import 'package:fluffychat/domain/model/contact/contact.dart';
 import 'package:fluffychat/domain/model/contact/contact_query.dart';
 
 abstract class TomContactsDatasource {
-  Future<List<Contact>> searchContacts({required ContactQuery query});
+  Future<List<Contact>> searchContacts({required ContactQuery query, int? limit, int? offset});
 }
