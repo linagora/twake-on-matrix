1d21ba5316cf23f92c3926091eda3adee26e1bad
sherlock <quangkhai28999@gmail.com>
2023-06-29 20:06:31 +0700

    TW-192: add repository layer

--- lib/domain/repository/contact_repository.dart
+++ lib/domain/repository/contact_repository.dart
@@ -2,5 +2,7 @@ import 'package:fluffychat/domain/model/contact/contact.dart';
 import 'package:fluffychat/domain/model/contact/contact_query.dart';
 
 abstract class ContactRepository {
-  Future<List<Contact>> searchContact({required ContactQuery query});
+  Future<List<Contact>> searchContact({required ContactQuery query, int? limit, int? offset});
+
+  Future<List<Contact>> loadMoreContact({required ContactQuery query, int? limit, int? offset});
 }
