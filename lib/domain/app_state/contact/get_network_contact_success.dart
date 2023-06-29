f9e92d6d17ec21dad92d022fce1335784239e6d4
sherlock <quangkhai28999@gmail.com>
2023-06-29 20:08:34 +0700

    TW-192: add load more interactor

--- lib/domain/app_state/contact/get_network_contact_success.dart
+++ lib/domain/app_state/contact/get_network_contact_success.dart
@@ -7,3 +7,17 @@ class GetNetworkContactSuccess extends GetContactsSuccess {
     required Set<Contact> contacts
   }) : super(contacts: contacts);
 }
+
+class GetMoreNetworkContactSuccess extends GetContactsSuccess {
+  const GetMoreNetworkContactSuccess({
+    required Set<Contact> contacts,
+    int? limit,
+    int? offset
+  }) : super(contacts: contacts);
+}
+
+class NoMoreContactSuccess extends GetContactsSuccess {
+  const NoMoreContactSuccess({
+    required Set<Contact> contacts
+  }) : super(contacts: contacts);
+}
