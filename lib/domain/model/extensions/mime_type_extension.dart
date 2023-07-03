5eb82b785ff02971d1b121ad0b904bb7ef3aac06
HuyNguyen <quanghnguyen@linagora.com>
2023-07-03 16:05:04 +0700

    TW-233: Show icon when getting data from the system

--- lib/domain/model/extensions/mime_type_extension.dart
+++ lib/domain/model/extensions/mime_type_extension.dart
@@ -12,21 +12,26 @@ extension MediaTypeExtension on Event {
 
   bool isImageFile() => SupportedPreviewFileTypes.imageMimeTypes.contains(mimeType);
 
-  bool isDocFile() => SupportedPreviewFileTypes.docMimeTypes.contains(mimeType);
+  bool isDocFile() => SupportedPreviewFileTypes.docMimeTypes.contains(mimeType)
+    || SupportedPreviewFileTypes.docFileTypes.contains(fileType!.toLowerCase());
 
-  bool isPowerPointFile() => SupportedPreviewFileTypes.pptMimeTypes.contains(mimeType);
+  bool isPowerPointFile() => SupportedPreviewFileTypes.pptMimeTypes.contains(mimeType)
+    || SupportedPreviewFileTypes.pptFileTypes.contains(fileType!.toLowerCase());
 
-  bool isExcelFile() => SupportedPreviewFileTypes.xlsMimeTypes.contains(mimeType);
+  bool isExcelFile() => SupportedPreviewFileTypes.xlsMimeTypes.contains(mimeType)
+    || SupportedPreviewFileTypes.xlsFileTypes.contains(fileType!.toLowerCase());
 
-  bool isZipFile() => SupportedPreviewFileTypes.zipMimeTypes.contains(mimeType);
+  bool isZipFile() => SupportedPreviewFileTypes.zipMimeTypes.contains(mimeType)
+    || SupportedPreviewFileTypes.zipFileTypes.contains(fileType!.toLowerCase());
 
-  bool isPdfFile() => SupportedPreviewFileTypes.pdfMimeTypes.contains(mimeType);
+  bool isPdfFile() => SupportedPreviewFileTypes.pdfMimeTypes.contains(mimeType)
+    || SupportedPreviewFileTypes.pdfFileTypes.contains(fileType!.toLowerCase());
 
   bool isSupportedPreviewAnotherFile() => SupportedPreviewFileTypes.supportAnotherTypes.contains(mimeType);
 
   String getIcon() {
-    Logs().d('AttachmentExtension::getIcon(): mediaType: $mimeType');
-    if (mimeType?.isEmpty == true) {
+    Logs().d('AttachmentExtension::getIcon(): mediaType: $mimeType || fileType: $fileType');
+    if (mimeType?.isEmpty == true || fileType == null) {
       return ImagePaths.icFileUnKnow;
     }
     if (isDocFile()) {
@@ -44,10 +49,14 @@ extension MediaTypeExtension on Event {
   }
 
   String getFileType(BuildContext context) {
-    if (isSupportedPreviewAnotherFile()) {
-      return L10n.of(context)!.file.toUpperCase();
+    if (fileType != null) {
+      if (isSupportedPreviewAnotherFile()) {
+        return L10n.of(context)!.file.toUpperCase();
+      } else {
+        return fileType!;
+      }
     } else {
-      return fileType!;
+      return L10n.of(context)!.file.toUpperCase();
     }
   }
 }
