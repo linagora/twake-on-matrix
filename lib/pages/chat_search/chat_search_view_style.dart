e447505f321d1d3f4a57ef25bc162db7758b6f39
HuyNguyen <quanghnguyen@linagora.com>
2023-07-05 01:42:20 +0700

    TW-266: Create new chat search screen

---
+++ lib/pages/chat_search/chat_search_view_style.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ChatSearchViewStyle {
+  static double get toolbarHeightChatSearch => 64.0;
+  static double get toolbarHeightOfSliverAppBar => 44.0;
+
+  static EdgeInsetsGeometry get paddingRecentChatsHeaders => const EdgeInsets.symmetric(horizontal: 16);
+  static EdgeInsetsGeometry get paddingLeadingAppBar => const EdgeInsets.symmetric(horizontal: 8);
+  static EdgeInsets get marginIconBack => const EdgeInsets.symmetric(vertical: 12.0);
+  static EdgeInsetsGeometry get contentPaddingAppBar => const EdgeInsets.all(12.0);
+  static EdgeInsetsGeometry get paddingRecentChats => const EdgeInsets.symmetric(horizontal: 16, vertical: 8);
+}
