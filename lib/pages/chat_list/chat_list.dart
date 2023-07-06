1b1e48c7ae3db54830d46360c235ce48d0847514
sherlock <quangkhai28999@gmail.com>
2023-07-06 16:23:45 +0700

    TW-230/only show chat when have message

--- lib/pages/chat_list/chat_list.dart
+++ lib/pages/chat_list/chat_list.dart
@@ -135,6 +135,7 @@ class ChatListController extends State<ChatList>
     .client
     .rooms
     .where(getRoomFilterByActiveFilter(_activeFilterAllChats))
+    .where(isShowInChatList)
     .toList();
 
   bool isSearchMode = false;
@@ -146,6 +147,26 @@ class ChatListController extends State<ChatList>
 
   bool isSearching = false;
   static const String _serverStoreNamespace = 'im.fluffychat.search.server';
+
+  bool isShowInChatList(Room room) {
+    return isDirectChatHaveMessage(room) || isGroupChat(room);
+  }
+
+  bool isGroupChat(Room room) {
+    return !room.isDirectChat;
+  }
+
+  bool isDirectChatHaveMessage(Room room) {
+    return room.isDirectChat && isLastEventInRoomIsMessage(room);
+  }
+
+  bool isLastEventInRoomIsMessage(Room room) {
+    return [
+      EventTypes.Message, 
+      EventTypes.Sticker, 
+      EventTypes.Encrypted,
+    ].contains(room.lastEvent?.type);
+  }
 
   void setServer() async {
     final newServer = await showTextInputDialog(
