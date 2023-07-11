527ab0c95a5530db86c03957ed541e1fa253ce85
Julian KOUNE <jkoune@linagora.com>
2023-07-11 15:22:25 +0200

    feat: add composer focus and ripple to empty view
    This Closes #273

--- lib/pages/chat/direct_chat_empty_view.dart
+++ lib/pages/chat/direct_chat_empty_view.dart
@@ -2,50 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class DirectChatEmptyView extends StatelessWidget {
-  
+  final void Function()? onTap;
+
   const DirectChatEmptyView({
     Key? key,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      constraints: const BoxConstraints(
-        maxWidth: 236,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.04),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            L10n.of(context)!.noMessageHereYet,
-            style: const TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 17,
-              color: Colors.black,
+    return InkWell(
+      borderRadius: BorderRadius.circular(16),
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints(
+          maxWidth: 236,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              L10n.of(context)!.noMessageHereYet,
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 17,
+                color: Colors.black,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            L10n.of(context)!.sendMessageGuide,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 15,
-              color: Color(0xFF818C99),
+            const SizedBox(height: 8),
+            Text(
+              L10n.of(context)!.sendMessageGuide,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: Color(0xFF818C99),
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            '🤗',
-            style: Theme.of(context).textTheme.titleLarge?.merge(const TextStyle(fontSize: 88)),
-          ),
-        ],
+            const SizedBox(height: 8),
+            Text(
+              '🤗',
+              style: Theme.of(context).textTheme.titleLarge?.merge(const TextStyle(fontSize: 88)),
+            ),
+          ],
+        ),
       ),
     );
   }
