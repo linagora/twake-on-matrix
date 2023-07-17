2336c33e3ba1ce39cfa338fb9b951dec334eee83
HuyNguyen <quanghnguyen@linagora.com>
2023-07-17 23:58:41 +0700

    TW-218: Create `ForwardMessageInteractor` to handle forward message actions

---
+++ lib/domain/app_state/forward/forward_message_state.dart
@@ -0,0 +1,38 @@
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:matrix/matrix.dart';
+
+class ForwardMessageLoading extends Success {
+  @override
+  List<Object?> get props => [];
+}
+
+class ForwardMessageSuccess extends Success {
+  final Room room;
+
+  const ForwardMessageSuccess(this.room);
+  @override
+  List<Object?> get props => [room];
+}
+
+class ForwardMessageFailed extends Failure {
+  final dynamic exception;
+
+  const ForwardMessageFailed({required this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
+
+class ForwardMessageIsShareFileState extends Success {
+  final MatrixFile shareFile;
+  final Room room;
+
+  const ForwardMessageIsShareFileState({
+    required this.shareFile,
+    required this.room
+  });
+
+  @override
+  List<Object?> get props => [shareFile, room];
+}
