fe04580e6e43e450b06dceb08aa016f8784e5ea1
Julian KOUNE <jkoune@linagora.com>
2023-07-19 14:04:48 +0200

    fix: split own group chat empty view

--- lib/pages/chat/group_chat_empty_view.dart
+++ lib/pages/chat/group_chat_empty_view.dart
@@ -10,56 +10,60 @@ class GroupChatEmptyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _buildOwnGroupChatEmptyView(context);
+  }
+
+  Container _buildOwnGroupChatEmptyView(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
-      constraints: const BoxConstraints(
-        maxWidth: 256,
-      ),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.08),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const SizedBox(height: 2,),
-          SvgPicture.asset(ImagePaths.icEmptyGroupChat),
-          const SizedBox(height: 26.0,),
-          Text(
-            L10n.of(context)!.youCreatedGroupChat,
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 17,
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
+    padding: const EdgeInsets.only(top: 18, right: 16, left: 16, bottom: 26),
+    constraints: const BoxConstraints(
+      maxWidth: 256,
+    ),
+    decoration: BoxDecoration(
+      color: Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.08),
+      borderRadius: BorderRadius.circular(16),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset(ImagePaths.icEmptyGroupChat),
+        const SizedBox(
+          height: 26.0,
+        ),
+        Text(
+          L10n.of(context)!.youCreatedGroupChat,
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 17,
+            color: Theme.of(context).colorScheme.onSurface,
           ),
-          const SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                L10n.of(context)!.groupChatsCanHave,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 15,
-                  color: LinagoraRefColors.material().neutral[40],
-                ),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              L10n.of(context)!.groupChatsCanHave,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: LinagoraRefColors.material().neutral[40],
               ),
-              const Expanded(child: SizedBox())
-            ],
-          ),
-          const SizedBox(height: 8),
-          _ruleChannel(L10n.of(context)!.upTo100000Members),
-          const SizedBox(height: 8),
-          _ruleChannel(L10n.of(context)!.persistentChatHistory),
-          const SizedBox(height: 8),
-          _ruleChannel(L10n.of(context)!.multiInteractionThreadedDiscussion),
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
+            ),
+            const Spacer()
+          ],
+        ),
+        const SizedBox(height: 8),
+        _ruleChannel(L10n.of(context)!.upTo100000Members),
+        const SizedBox(height: 8),
+        _ruleChannel(L10n.of(context)!.persistentChatHistory),
+        const SizedBox(height: 8),
+        _ruleChannel(L10n.of(context)!.multiInteractionThreadedDiscussion),
+      ],
+    ),
+  );
   }
 
   Widget _ruleChannel(String rule) {
