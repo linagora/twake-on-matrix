5dadd3541560b86c986ca1a0c732e14eab1a674f
sherlock <quangkhai28999@gmail.com>
2023-07-19 10:42:46 +0700

    TW-230: add create direct chat interactor

---
+++ lib/domain/app_state/direct_chat/create_direct_chat_failed.dart
@@ -0,0 +1,11 @@
+import 'package:fluffychat/app_state/failure.dart';
+
+class CreateDirectChatFailed extends Failure {
+
+  final dynamic exception;
+
+  const CreateDirectChatFailed({required this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
---
+++ lib/domain/usecase/create_direct_chat_interactor.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/domain/app_state/direct_chat/create_direct_chat_failed.dart';
+import 'package:fluffychat/domain/app_state/direct_chat/create_direct_chat_loading.dart';
+import 'package:fluffychat/domain/app_state/direct_chat/create_direct_chat_success.dart';
+import 'package:matrix/matrix.dart';
+
+class CreateDirectChatInteractor {
+  Stream<Either<Failure, Success>> execute({
+    required String contactMxId,
+    required Client client,
+    List<StateEvent>? initialState,
+    bool? enableEncryption,
+    bool waitForSync = true,
+    Map<String, dynamic>? powerLevelContentOverride,
+    CreateRoomPreset? preset = CreateRoomPreset.trustedPrivateChat,
+  }) async* {
+    yield const Right(CreateDirectChatLoading());
+    try {
+      final roomId = await client.startDirectChat(
+        contactMxId,
+        initialState: initialState,
+        enableEncryption: enableEncryption,
+        waitForSync: waitForSync,
+        powerLevelContentOverride: powerLevelContentOverride,
+        preset: preset,
+      );
+      yield Right(CreateDirectChatSuccess(roomId: roomId));
+    } catch (e) {
+      yield Left(CreateDirectChatFailed(exception: e));
+    }
+  }
+}
