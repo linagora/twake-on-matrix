404099003e45aa47cd3cfdfaf87c51d86e95cbd0
MinhDV <drminh2807@gmail.com>
2023-07-20 16:57:42 +0700

    chore: Enable flutter analyze check

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -3,5 +3,5 @@ flutter pub get && flutter pub run build_runner build --delete-conflicting-outpu
 flutter pub run import_sorter:main --no-comments
 flutter format lib/ test/
 flutter pub get
-flutter analyze --no-fatal-infos --no-fatal-warnings
+flutter analyze
 flutter pub run dart_code_metrics:metrics lib -r github || true
