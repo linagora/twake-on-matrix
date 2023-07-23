33b7fcf0716bd9850b33ff9463734f11b8d5565e
HuyNguyen <quanghnguyen@linagora.com>
2023-07-23 23:10:41 +0700

    TW-359: Update `FetchContactsController`

--- lib/pages/new_private_chat/fetch_contacts_controller.dart
+++ lib/pages/new_private_chat/fetch_contacts_controller.dart
@@ -1,22 +1,22 @@
-
 import 'dart:async';
-
+import 'package:collection/collection.dart';
 import 'package:dartz/dartz.dart';
 import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
-import 'package:fluffychat/domain/app_state/contact/get_contacts_success.dart';
 import 'package:fluffychat/domain/app_state/contact/get_network_contact_success.dart';
 import 'package:fluffychat/domain/model/extensions/contact/contact_extension.dart';
 import 'package:fluffychat/domain/usecase/fetch_contacts_interactor.dart';
 import 'package:fluffychat/domain/usecase/load_more_internal_contacts.dart';
+import 'package:fluffychat/mixin/comparable_presentation_contact_mixin.dart';
 import 'package:fluffychat/presentation/mixin/load_more_contacts_mixin.dart';
 import 'package:fluffychat/presentation/model/presentation_contact.dart';
 
-class FetchContactsController with LoadMoreContactsMixin {
+class FetchContactsController with LoadMoreContactsMixin, ComparablePresentationContactMixin {
 
   final _fetchContactsInteractor = getIt.get<FetchContactsInteractor>();
   final _loadMoreInternalContacts = getIt.get<LoadMoreInternalContacts>();
-  final streamController = StreamController<Either<Failure, GetContactsSuccess>>();  
+  final streamController = StreamController<Either<Failure, Success>>();
 
   void fetchCurrentTomContacts() {
     _fetchContactsInteractor
@@ -26,35 +26,41 @@ class FetchContactsController with LoadMoreContactsMixin {
       });
   }
 
-  Set<PresentationContact> getContactsFromFetchStream(Either<Failure, GetContactsSuccess> event) {
+  List<PresentationContact> getContactsFromFetchStream(Either<Failure, Success> event) {
     return event.fold<Set<PresentationContact>>(
       (failure) => <PresentationContact>{},
       (success) {
-        final currentContacts = success.contacts.expand((contact) => contact.toPresentationContacts());
-        updateLastContactIndex(oldContactsList.length);
-        if (success is GetMoreNetworkContactSuccess) {
-          _handleGetMoreContactsSuccess(currentContacts);
+        if (success is GetNetworkContactSuccess) {
+          _handleGetNetworkContactSuccess(success);
+        } else if (success is GetMoreNetworkContactSuccess) {
+          _handleGetMoreContactsSuccess(success);
         } else if (success is NoMoreContactSuccess) {
-          _handleNoMoreContactsSuccess(currentContacts);
+          _handleNoMoreContactsSuccess(success);
         } else {
-          oldContactsList = currentContacts.toSet();
           lastContactIndexNotifier.value = oldContactsList.length;
         }
         return oldContactsList;
-      },
-    ).toSet();
+      }
+    ).toSet().sorted((cur, next) => comparePresentationContacts(cur, next));
   }
 
-  void _handleGetMoreContactsSuccess(Iterable<PresentationContact> currentContacts) {
-    oldContactsList.addAll(currentContacts);
-    haveMoreCountactsNotifier.value = currentContacts.isNotEmpty;
+  void _handleGetNetworkContactSuccess(GetNetworkContactSuccess success) {
+    updateLastContactIndex(oldContactsList.length);
+    oldContactsList = success.contacts.expand((contact) => contact.toPresentationContacts()).toSet();
+  }
+
+  void _handleGetMoreContactsSuccess(GetMoreNetworkContactSuccess success) {
+    final presentationContacts = success.contacts.expand((contact) => contact.toPresentationContacts());
+    oldContactsList.addAll(presentationContacts);
+    haveMoreCountactsNotifier.value = success.contacts.isNotEmpty;
     lastContactIndexNotifier.value = oldContactsList.length;
     isLoadMore = true;
   }
 
-  void _handleNoMoreContactsSuccess(Iterable<PresentationContact> currentContacts) {
+  void _handleNoMoreContactsSuccess(NoMoreContactSuccess success) {
+    final presentationContacts = success.contacts.expand((contact) => contact.toPresentationContacts());
     haveMoreCountactsNotifier.value = false;
-    oldContactsList.addAll(currentContacts);
+    oldContactsList.addAll(presentationContacts);
     lastContactIndexNotifier.value = oldContactsList.length;
     isLoadMore = false;
   }
