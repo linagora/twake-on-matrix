6555fc9fa35acd05e4128d56b0044830d7b42f9a
HuyNguyen <quanghnguyen@linagora.com>
2023-07-23 23:08:42 +0700

    TW-359: Create `FeatureFailure`

--- lib/app_state/failure.dart
+++ lib/app_state/failure.dart
@@ -3,3 +3,12 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   const Failure();
 }
+
+abstract class FeatureFailure extends Failure {
+  final dynamic exception;
+
+  const FeatureFailure({this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
