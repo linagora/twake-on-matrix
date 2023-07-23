fcb885d59b65b3ff58067fd984d100a29e37718f
HuyNguyen <quanghnguyen@linagora.com>
2023-07-23 23:09:05 +0700

    TW-359: Create `ViewState` abs class and `UIState` class

--- lib/app_state/success.dart
+++ lib/app_state/success.dart
@@ -3,3 +3,14 @@ import 'package:equatable/equatable.dart';
 abstract class Success extends Equatable {
   const Success();
 }
+
+abstract class ViewState extends Success {}
+
+class UIState extends ViewState {
+  static final idle = UIState();
+
+  UIState() : super();
+
+  @override
+  List<Object?> get props => [];
+}
