14af6b3b8d7304bc7c7d6363b13d4a799b38915e
MinhDV <drminh2807@gmail.com>
2023-07-26 12:33:19 +0700

    TW-318 Upload avatar if needed then create chat room

--- lib/pages/new_group/new_group_info_controller.dart
+++ lib/pages/new_group/new_group_info_controller.dart
@@ -1,9 +1,7 @@
 import 'package:fluffychat/presentation/model/presentation_contact.dart';
 import 'package:fluffychat/pages/new_group/new_group.dart';
-import 'package:fluffychat/utils/dialog/warning_dialog.dart';
 import 'package:fluffychat/utils/string_extension.dart';
 import 'package:fluffychat/widgets/matrix.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:matrix/matrix.dart';
 
 extension NewGroupInfoController on NewGroupController {
@@ -18,30 +16,14 @@ extension NewGroupInfoController on NewGroupController {
 
   void moveToGroupChatScreen() async {
     Logs().d('NewGroupInfoController::moveToGroupChatScreen()');
-    if (numberSelectedImagesNotifier.value != 1) {
-      final client = Matrix.of(context).client;
-      // FIXME: When upload
-      // createNewGroupChatAction(
-      //   matrixClient: client,
-      //   createNewGroupChatRequest: CreateNewGroupChatRequest(
-      //     groupName: groupName,
-      //     invite: getSelectedValidContacts(contactsList)
-      //       .map<String>((contact) => contact.matrixId!)
-      //       .toList(),
-      //     enableEncryption: true,
-      //     urlAvatar: uriAvatar != null ? uriAvatar!.toString() : null,
-      //   ),
-      // );
+    final client = Matrix.of(context).client;
+    if (avatarNotifier.value != null) {
+      uploadAvatarNewGroupChat(
+        matrixClient: client, 
+        entity: avatarNotifier.value!);
     } else {
-      WarningDialog.showWarningDialog(context,onAcceptButton: () => cancelUploadAvatar(context));
+      createNewGroup(uriAvatar: null);
     }
-  }
-
-  void cancelUploadAvatar(BuildContext context) {
-    // FIXME: Dont understand it
-    // uploadAvatarNewGroupChatNotifier.value = const Left(UploadContentFailed(exception: null));
-    removeAllImageSelected();
-    WarningDialog.hideWarningDialog(context);
   }
 
   Set<PresentationContact> getSelectedValidContacts(
