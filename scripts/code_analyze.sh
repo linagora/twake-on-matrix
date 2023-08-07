d63ab239c8f74fbef2d3febeaf52469bd5469e92
MinhDV <drminh2807@gmail.com>
2023-08-07 12:19:40 +0700

    TW-416 Fix CI/CD commands

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ve
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter pub run import_sorter:main --no-comments
-flutter format lib/ test/
+dart format lib/ test/
 flutter pub get
 flutter analyze --no-fatal-infos --no-fatal-warnings
 flutter pub run dart_code_metrics:metrics lib -r github || true
