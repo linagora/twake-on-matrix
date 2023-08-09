13e9295c94b0fd1f1ae425be29594c26603b58f8
sherlock <quangkhai28999@gmail.com>
2023-08-09 10:28:52 +0700

    upgrade the dart language and linagora_design_flutter

--- lib/mixin/comparable_presentation_contact_mixin.dart
+++ lib/mixin/comparable_presentation_contact_mixin.dart
@@ -1,7 +1,7 @@
 
 import 'package:fluffychat/presentation/model/presentation_contact.dart';
 
-class ComparablePresentationContactMixin {
+mixin ComparablePresentationContactMixin {
   int comparePresentationContacts(PresentationContact contact1, PresentationContact contact2) {
     final buffer1 = StringBuffer();
     final buffer2 = StringBuffer();
--- lib/mixin/comparable_presentation_search_mixin.dart
+++ lib/mixin/comparable_presentation_search_mixin.dart
@@ -1,6 +1,6 @@
 import 'package:fluffychat/presentation/model/search/presentation_search.dart';
 
-class ComparablePresentationSearchMixin {
+mixin ComparablePresentationSearchMixin {
   int comparePresentationSearch(PresentationSearch searchResultOne, PresentationSearch searchResultTwo) {
     return searchResultOne.toString().compareTo(searchResultTwo.toString());
   }
