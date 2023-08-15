070f05f964e2f3d5be5ca0c35ab2c4a8c8e585f9
HuyNguyen <quanghnguyen@linagora.com>
2023-08-15 16:02:31 +0700

    TW-429: Create dialog ask accept invite chat

---
+++ lib/pages/chat/dialog_accept_invite_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class DialogAcceptInviteStyle {
+  static const double borderRadiusActionButton = 100;
+  static const double borderRadiusDialog = 28.0;
+  static const EdgeInsetsDirectional paddingActionButton =
+      EdgeInsetsDirectional.symmetric(horizontal: 12.0, vertical: 10.0);
+
+  static const EdgeInsetsDirectional paddingDialog =
+      EdgeInsetsDirectional.symmetric(horizontal: 24);
+
+  static const EdgeInsetsDirectional marginDialog =
+      EdgeInsetsDirectional.symmetric(horizontal: 24);
+
+  static const EdgeInsetsDirectional paddingButton =
+      EdgeInsetsDirectional.symmetric(vertical: 24);
+}
