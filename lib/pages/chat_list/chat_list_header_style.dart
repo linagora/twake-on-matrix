545fa6a7b60a69795b7f98c54ed1c3bdd25b7d37
HuyNguyen <quanghnguyen@linagora.com>
2023-08-24 13:56:35 +0700

    Update size for search widget in chat list

--- lib/pages/chat_list/chat_list_header_style.dart
+++ lib/pages/chat_list/chat_list_header_style.dart
@@ -5,6 +5,5 @@ class ChatListHeaderStyle {
   static ResponsiveUtils responsive = getIt.get<ResponsiveUtils>();
 
   static const double searchRadiusBorder = 24.0;
-  static const double searchBarHeight = 56.0;
   static const double searchBarContainerHeight = 64.0;
 }
