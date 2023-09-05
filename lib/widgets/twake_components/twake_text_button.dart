e13910ca43a98fe2e8ef4a1c2041794df01a74c0
HuyNguyen <quanghnguyen@linagora.com>
2023-09-05 09:43:27 +0700

    TW-503: Create Twake text button

---
+++ lib/widgets/twake_components/twake_text_button.dart
@@ -0,0 +1,82 @@
+import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+
+class TwakeTextButton extends StatelessWidget {
+  final BoxDecoration? buttonDecoration;
+
+  final String message;
+
+  final EdgeInsetsDirectional margin;
+
+  final TextStyle? styleMessage;
+
+  final double? paddingAll;
+
+  final double? size;
+
+  final double? fill;
+
+  final double? weight;
+
+  final OnTapIconButtonCallbackAction? onTap;
+
+  final OnTapDownIconButtonCallbackAction? onTapDown;
+
+  final bool? preferBelow;
+
+  final Color? hoverColor;
+
+  final double? borderHover;
+
+  const TwakeTextButton({
+    Key? key,
+    required this.message,
+    this.styleMessage,
+    this.onTap,
+    this.paddingAll,
+    this.size,
+    this.fill,
+    this.weight,
+    this.preferBelow,
+    this.hoverColor,
+    this.onTapDown,
+    this.margin = const EdgeInsetsDirectional.all(0),
+    this.buttonDecoration,
+    this.borderHover,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        onTap: onTap,
+        onTapDown: (_) => onTapDown?.call(context),
+        radius: paddingAll,
+        hoverColor: hoverColor,
+        borderRadius: BorderRadius.circular(borderHover ?? 0),
+        child: Container(
+          padding: margin,
+          decoration:
+              buttonDecoration ?? const BoxDecoration(shape: BoxShape.circle),
+          child: Tooltip(
+            preferBelow: preferBelow,
+            message: message,
+            child: Padding(
+              padding: EdgeInsets.all(paddingAll ?? 8.0),
+              child: Text(
+                message,
+                style: styleMessage ??
+                    Theme.of(context).textTheme.labelLarge?.copyWith(
+                          color: LinagoraSysColors.material().onPrimary,
+                        ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
