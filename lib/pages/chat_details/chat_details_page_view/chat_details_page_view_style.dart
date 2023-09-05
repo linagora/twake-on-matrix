eccf0378ab6b5a045200dfe2b18b85d6709174e4
HuyNguyen <quanghnguyen@linagora.com>
2023-09-06 02:20:16 +0700

    TW-503: Create page view inside chat details

---
+++ lib/pages/chat_details/chat_details_page_view/chat_details_page_view_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatDetailsPageViewStyle {
+  static const double tabBarPageViewHeight = 72;
+
+  static EdgeInsetsDirectional tabBarPageViewPadding =
+      const EdgeInsetsDirectional.only(
+    start: 16,
+    end: 16,
+    top: 16,
+  );
+
+  static const double tabBarPageViewIndicatorHeight = 3;
+
+  static const double tabBarPageViewIndicatorBonus = 16;
+
+  static const double tabBarPageViewIndicatorBorder = 100;
+}
