7e90bc5d15b0965e5763f7d6cfacbfe578b3c432
HuyNguyen <quanghnguyen@linagora.com>
2023-09-06 02:11:10 +0700

    TW-503: Create chat details actions button

---
+++ lib/pages/chat_details/chat_details_actions_enum.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+enum ChatDetailsActions {
+  addMembers,
+  mute,
+  search,
+  more;
+
+  String getTitle(BuildContext context) {
+    switch (this) {
+      case ChatDetailsActions.addMembers:
+        return L10n.of(context)!.addMembers;
+      case ChatDetailsActions.mute:
+        return L10n.of(context)!.mute;
+      case ChatDetailsActions.search:
+        return L10n.of(context)!.search;
+      case ChatDetailsActions.more:
+        return L10n.of(context)!.more;
+    }
+  }
+
+  IconData getIconData() {
+    switch (this) {
+      case ChatDetailsActions.addMembers:
+        return Icons.person_add;
+      case ChatDetailsActions.mute:
+        return Icons.notifications_none;
+      case ChatDetailsActions.search:
+        return Icons.search;
+      case ChatDetailsActions.more:
+        return Icons.more_horiz;
+    }
+  }
+}
