b442ab0ee1bc2f818ba77834458545ae1a18627b
HuyNguyen <quanghnguyen@linagora.com>
2023-09-05 09:52:32 +0700

    TW-503: Update size and style for searchable_app_bar.dart

--- lib/widgets/app_bars/searchable_app_bar.dart
+++ lib/widgets/app_bars/searchable_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluffychat/pages/chat_list/chat_list_header_style.dart';
 import 'package:fluffychat/pages/dialer/pip/dismiss_keyboard.dart';
 import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
 import 'package:fluffychat/widgets/app_bars/searchable_app_bar_style.dart';
@@ -13,6 +14,7 @@ class SearchableAppBar extends StatelessWidget {
   final String? hintText;
   final TextEditingController textEditingController;
   final Function() toggleSearchMode;
+  final bool isFullScreen;
 
   const SearchableAppBar({
     super.key,
@@ -22,13 +24,26 @@ class SearchableAppBar extends StatelessWidget {
     required this.focusNode,
     required this.textEditingController,
     required this.toggleSearchMode,
+    this.isFullScreen = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      shape: !isFullScreen
+          ? const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(
+                  SearchableAppBarStyle.appBarBorderRadius,
+                ),
+              ),
+            )
+          : null,
       automaticallyImplyLeading: false,
-      toolbarHeight: SearchableAppBarStyle.appBarHeight(context),
+      toolbarHeight: SearchableAppBarStyle.appBarHeight(
+        context,
+        isFullScreen: isFullScreen,
+      ),
       bottom: PreferredSize(
         preferredSize: const Size.fromHeight(1),
         child: Container(
@@ -55,67 +70,72 @@ class SearchableAppBar extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       title: Align(
         alignment: Alignment.centerLeft,
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            TwakeIconButton(
-              icon: Icons.arrow_back,
-              onTap: () => context.pop(),
-              tooltip: L10n.of(context)!.back,
-              paddingAll: 8.0,
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-            ),
-            Expanded(
-              child: ValueListenableBuilder(
-                valueListenable: searchModeNotifier,
-                builder: (context, searchModeNotifier, child) {
-                  if (searchModeNotifier) {
-                    return TextField(
-                      onTapOutside: (event) {
-                        dismissKeyboard();
-                      },
-                      focusNode: focusNode,
-                      autofocus: true,
-                      maxLines: 1,
-                      buildCounter: (
-                        BuildContext context, {
-                        required int currentLength,
-                        required int? maxLength,
-                        required bool isFocused,
-                      }) =>
-                          const SizedBox.shrink(),
-                      maxLength: 200,
-                      cursorHeight: 26,
-                      scrollPadding: const EdgeInsets.all(0),
-                      controller: textEditingController,
-                      decoration: InputDecoration(
-                        contentPadding:
-                            const EdgeInsetsDirectional.only(top: 10),
-                        isCollapsed: true,
-                        hintText: hintText,
-                        hintStyle: Theme.of(context)
-                            .textTheme
-                            .bodyLarge
-                            ?.copyWith(
-                              color: LinagoraRefColors.material().neutral[60],
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (isFullScreen == true) ...[
+                  TwakeIconButton(
+                    icon: Icons.arrow_back,
+                    onTap: () => context.pop(),
+                    tooltip: L10n.of(context)!.back,
+                    paddingAll: 8.0,
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                ] else ...[
+                  const SizedBox(width: 56.0)
+                ],
+                Expanded(
+                  child: ValueListenableBuilder(
+                    valueListenable: searchModeNotifier,
+                    builder: (context, searchModeNotifier, child) {
+                      if (searchModeNotifier) {
+                        return Padding(
+                          padding: const EdgeInsetsDirectional.only(top: 10.0),
+                          child: _textFieldBuilder(context),
+                        );
+                      }
+                      return Text(
+                        title,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              color: Theme.of(context).colorScheme.onSurface,
                             ),
-                      ),
-                    );
-                  }
-                  return Text(
-                    title,
-                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface,
-                        ),
-                  );
-                },
-              ),
-            ),
-            ValueListenableBuilder(
-              valueListenable: searchModeNotifier,
-              builder: (context, searchModeNotifier, child) {
-                if (searchModeNotifier) {
-                  return TwakeIconButton(
-                    onTap: toggleSearchMode,
+                      );
+                    },
+                  ),
+                ),
+                if (isFullScreen) ...[
+                  ValueListenableBuilder(
+                    valueListenable: searchModeNotifier,
+                    builder: (context, searchModeNotifier, child) {
+                      if (searchModeNotifier) {
+                        return TwakeIconButton(
+                          onTap: toggleSearchMode,
+                          tooltip: L10n.of(context)!.close,
+                          icon: Icons.close,
+                          paddingAll: 10.0,
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 6.0,
+                          ),
+                        );
+                      }
+                      return TwakeIconButton(
+                        icon: Icons.search,
+                        onTap: toggleSearchMode,
+                        tooltip: L10n.of(context)!.search,
+                        paddingAll: 10.0,
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      );
+                    },
+                  )
+                ] else ...[
+                  TwakeIconButton(
+                    onTap: () => context.pop(),
                     tooltip: L10n.of(context)!.close,
                     icon: Icons.close,
                     paddingAll: 10.0,
@@ -123,19 +143,62 @@ class SearchableAppBar extends StatelessWidget {
                       vertical: 10.0,
                       horizontal: 6.0,
                     ),
-                  );
-                }
-                return TwakeIconButton(
-                  icon: Icons.search,
-                  onTap: toggleSearchMode,
-                  tooltip: L10n.of(context)!.search,
-                  paddingAll: 10.0,
-                  margin: const EdgeInsets.symmetric(vertical: 10.0),
-                );
-              },
-            )
+                  ),
+                ]
+              ],
+            ),
+            if (!isFullScreen)
+              Padding(
+                padding: SearchableAppBarStyle.textFieldWebPadding,
+                child: _textFieldBuilder(context),
+              )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _textFieldBuilder(BuildContext context) {
+    return TextField(
+      onTapOutside: (event) {
+        dismissKeyboard();
+      },
+      focusNode: focusNode,
+      autofocus: true,
+      maxLines: SearchableAppBarStyle.textFieldMaxLines,
+      buildCounter: (
+        BuildContext context, {
+        required int currentLength,
+        required int? maxLength,
+        required bool isFocused,
+      }) =>
+          const SizedBox.shrink(),
+      maxLength: SearchableAppBarStyle.textFieldMaxLength,
+      cursorHeight: 26,
+      scrollPadding: const EdgeInsets.all(0),
+      controller: textEditingController,
+      decoration: InputDecoration(
+        contentPadding: SearchableAppBarStyle.textFieldContentPadding,
+        isCollapsed: true,
+        filled: !isFullScreen,
+        hintText: hintText,
+        fillColor: Theme.of(context).colorScheme.surface,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(
+            ChatListHeaderStyle.searchRadiusBorder,
+          ),
+        ),
+        prefixIcon: !isFullScreen
+            ? Icon(
+                Icons.search_outlined,
+                color: Theme.of(context).colorScheme.onBackground,
+              )
+            : null,
+        suffixIcon: const SizedBox.shrink(),
+        hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(
+              color: LinagoraRefColors.material().neutral[60],
+            ),
       ),
     );
   }
--- lib/widgets/app_bars/searchable_app_bar_style.dart
+++ lib/widgets/app_bars/searchable_app_bar_style.dart
@@ -5,8 +5,36 @@ import 'package:flutter/cupertino.dart';
 class SearchableAppBarStyle {
   static ResponsiveUtils responsive = getIt.get<ResponsiveUtils>();
 
-  static Size preferredSize(BuildContext context) =>
-      Size.fromHeight(appBarHeight(context));
+  static Size preferredSize(
+    BuildContext context, {
+    bool? isFullScreen = true,
+  }) =>
+      Size.fromHeight(
+        appBarHeight(
+          context,
+          isFullScreen: isFullScreen,
+        ),
+      );
 
-  static double appBarHeight(BuildContext context) => 56;
+  static double appBarHeight(
+    BuildContext context, {
+    bool? isFullScreen = true,
+  }) =>
+      isFullScreen == true ? 56 : 112;
+
+  static EdgeInsetsDirectional textFieldWebPadding =
+      const EdgeInsetsDirectional.only(
+    start: 16,
+    end: 16,
+    bottom: 8,
+  );
+
+  static EdgeInsetsDirectional textFieldContentPadding =
+      const EdgeInsetsDirectional.only(top: 10);
+
+  static const int textFieldMaxLength = 200;
+
+  static const int textFieldMaxLines = 1;
+
+  static const double appBarBorderRadius = 16.0;
 }
