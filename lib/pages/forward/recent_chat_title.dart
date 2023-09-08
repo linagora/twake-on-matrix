9107d2daf90c7a247c40b7be362198f05f50c982
sherlock <quangkhai28999@gmail.com>
2023-09-08 10:56:24 +0700

    TW-403: refactor recentChatList widget

--- lib/pages/forward/recent_chat_title.dart
+++ lib/pages/forward/recent_chat_title.dart
@@ -1,47 +1,22 @@
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
 
 class RecentChatsTitle extends StatelessWidget {
   const RecentChatsTitle({
     super.key,
-    required this.isShowRecentlyChats,
-    required this.toggleRecentChat,
   });
-
-  final bool isShowRecentlyChats;
-
-  final void Function() toggleRecentChat;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Text(
-          L10n.of(context)!.recentlyChats,
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TwakeIconButton(
-                paddingAll: 6.0,
-                buttonDecoration: BoxDecoration(
-                  color:
-                      Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
-                  shape: BoxShape.circle,
-                ),
-                icon:
-                    isShowRecentlyChats ? Icons.expand_less : Icons.expand_more,
-                onTap: toggleRecentChat,
-                tooltip: isShowRecentlyChats
-                    ? L10n.of(context)!.shrink
-                    : L10n.of(context)!.expand,
+          L10n.of(context)!.recentChat,
+          style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                color: LinagoraRefColors.material().neutral,
               ),
-            ],
-          ),
-        )
+        ),
       ],
     );
   }
