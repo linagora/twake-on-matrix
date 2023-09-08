9262092e250f935bb0804ba98025f9ae295eba51
sherlock <quangkhai28999@gmail.com>
2023-09-08 10:56:24 +0700

    TW-403: refactor recentChatList widget

--- lib/pages/forward/recent_chat_list.dart
+++ lib/pages/forward/recent_chat_list.dart
@@ -1,6 +1,10 @@
-import 'package:fluffychat/pages/forward/selectable_chat_list_item.dart';
+import 'package:fluffychat/pages/chat_list/chat_list_item_subtitle.dart';
+import 'package:fluffychat/pages/chat_list/chat_list_item_title.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:fluffychat/widgets/avatar/avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class RecentChatList extends StatelessWidget {
   const RecentChatList({
@@ -30,13 +34,52 @@ class RecentChatList extends StatelessWidget {
           controller: recentChatScrollController,
           itemCount: rooms.length,
           itemBuilder: (BuildContext context, int index) {
-            return SelectableChatListItem(
-              rooms[index],
-              key: Key('selectable_chat_list_item_${rooms[index].id}'),
-              selected: selectedEvents.contains(rooms[index].id),
-              onTap: () {
-                onSelectedChat(rooms[index].id);
-              },
+            final room = rooms[index];
+            final selected = selectedEvents.contains(room.id);
+            return Material(
+              borderRadius: BorderRadius.circular(16.0),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(16.0),
+                onTap: () => onSelectedChat(room.id),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: selected,
+                      onChanged: (value) => onSelectedChat(room.id),
+                      fillColor: MaterialStatePropertyAll(
+                        Theme.of(context).primaryColor,
+                      ),
+                      side: BorderSide(
+                        width: 2.0,
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      ),
+                    ),
+                    Avatar(
+                      mxContent: room.avatar,
+                      name: room.getLocalizedDisplayname(
+                        MatrixLocals(L10n.of(context)!),
+                      ),
+                      onTap: null,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsetsDirectional.only(
+                          top: 8.0,
+                          bottom: 8.0,
+                          start: 8.0,
+                          end: 6.0,
+                        ),
+                        child: Column(
+                          children: [
+                            ChatListItemTitle(room: room),
+                            ChatListItemSubtitle(room: room),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             );
           },
         );
