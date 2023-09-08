2a24fce82932e58cc2a334987c49b6a7849a699f
Julian KOUNE <jkoune@linagora.com>
2023-09-08 16:04:03 +0200

    feat: align input row btns at the bottom in web & fix widget sizes

--- lib/pages/chat/chat_input_row.dart
+++ lib/pages/chat/chat_input_row.dart
@@ -1,16 +1,15 @@
-import 'package:animations/animations.dart';
 import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/pages/chat/chat_input_row_mobile.dart';
 import 'package:fluffychat/pages/chat/chat_input_row_style.dart';
+import 'package:fluffychat/pages/chat/chat_input_row_web.dart';
 import 'package:fluffychat/resource/image_paths.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/widgets/avatar/avatar.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:keyboard_shortcuts/keyboard_shortcuts.dart';
 import 'package:matrix/matrix.dart';
 
 import 'chat.dart';
@@ -30,12 +29,12 @@ class ChatInputRow extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: controller.selectMode
             ? <Widget>[
                 SizedBox(
-                  height: 56,
+                  height: ChatInputRowStyle.chatInputRowHeightWeb,
                   child: TextButton(
                     onPressed: controller.forwardEventsAction,
                     child: Row(
@@ -53,7 +52,7 @@ class ChatInputRow extends StatelessWidget {
                             .status
                             .isSent
                         ? SizedBox(
-                            height: 56,
+                            height: ChatInputRowStyle.chatInputRowHeightWeb,
                             child: TextButton(
                               onPressed: controller.replyAction,
                               child: Row(
@@ -65,7 +64,7 @@ class ChatInputRow extends StatelessWidget {
                             ),
                           )
                         : SizedBox(
-                            height: 56,
+                            height: ChatInputRowStyle.chatInputRowHeightWeb,
                             child: TextButton(
                               onPressed: controller.sendAgainAction,
                               child: Row(
@@ -80,116 +79,30 @@ class ChatInputRow extends StatelessWidget {
                     : Container(),
               ]
             : <Widget>[
-                TwakeIconButton(
-                  tooltip: L10n.of(context)!.more,
-                  margin: const EdgeInsets.only(right: 4.0),
-                  icon: Icons.add_circle_outline,
-                  onTap: () => controller.onSendFileClick(context),
-                ),
+                if (ChatInputRowStyle.responsiveUtils.isMobileOrTablet(context))
+                  Container(
+                    margin: ChatInputRowStyle.chatInputRowMoreBtnMarginMobile,
+                    height: ChatInputRowStyle.chatInputRowHeightMobile,
+                    child: TwakeIconButton(
+                      size: ChatInputRowStyle.chatInputRowMoreBtnSize,
+                      tooltip: L10n.of(context)!.more,
+                      icon: Icons.add_circle_outline,
+                      onTap: () => controller.onSendFileClick(context),
+                    ),
+                  ),
                 if (controller.matrix!.isMultiAccount &&
                     controller.matrix!.hasComplexBundles &&
                     controller.matrix!.currentBundle!.length > 1)
                   Container(
-                    height: 56,
+                    height: ChatInputRowStyle.chatInputRowHeightWeb,
                     alignment: Alignment.center,
                     child: _ChatAccountPicker(controller),
                   ),
                 Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.only(left: 12.0),
-                    margin: const EdgeInsets.only(right: 8.0),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(25)),
-                      color: Theme.of(context).colorScheme.surface,
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: InputBar(
-                            room: controller.room!,
-                            minLines: 1,
-                            maxLines: 8,
-                            autofocus: !PlatformInfos.isMobile,
-                            keyboardType: TextInputType.multiline,
-                            textInputAction: AppConfig.sendOnEnter
-                                ? TextInputAction.send
-                                : null,
-                            onSubmitted: controller.onInputBarSubmitted,
-                            focusNode: controller.inputFocus,
-                            controller: controller.sendController,
-                            decoration: InputDecoration(
-                              hintText: L10n.of(context)!.chatMessage,
-                              hintMaxLines: 1,
-                              hintStyle: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge
-                                  ?.merge(
-                                    Theme.of(context)
-                                        .inputDecorationTheme
-                                        .hintStyle,
-                                  )
-                                  .copyWith(letterSpacing: -0.15),
-                            ),
-                            onChanged: controller.onInputBarChanged,
-                          ),
-                        ),
-                        KeyBoardShortcuts(
-                          keysToPress: {
-                            LogicalKeyboardKey.altLeft,
-                            LogicalKeyboardKey.keyE
-                          },
-                          onKeysPressed: controller.emojiPickerAction,
-                          helpLabel: L10n.of(context)!.emojis,
-                          child: InkWell(
-                            onTap: controller.emojiPickerAction,
-                            hoverColor: Colors.transparent,
-                            child: PageTransitionSwitcher(
-                              transitionBuilder: (
-                                Widget child,
-                                Animation<double> primaryAnimation,
-                                Animation<double> secondaryAnimation,
-                              ) {
-                                return SharedAxisTransition(
-                                  animation: primaryAnimation,
-                                  secondaryAnimation: secondaryAnimation,
-                                  transitionType:
-                                      SharedAxisTransitionType.scaled,
-                                  fillColor: Colors.transparent,
-                                  child: child,
-                                );
-                              },
-                              child: !controller.showEmojiPicker
-                                  ? ValueListenableBuilder(
-                                      valueListenable: controller.inputText,
-                                      builder: (context, value, child) {
-                                        return TwakeIconButton(
-                                          paddingAll: value.isEmpty ? 5.0 : 12,
-                                          tooltip: L10n.of(context)!.emojis,
-                                          onTap: () =>
-                                              controller.emojiPickerAction(),
-                                          icon: Icons.tag_faces,
-                                        );
-                                      },
-                                    )
-                                  : ValueListenableBuilder(
-                                      valueListenable: controller.inputText,
-                                      builder: (context, value, child) {
-                                        return TwakeIconButton(
-                                          paddingAll: value.isEmpty ? 5.0 : 12,
-                                          tooltip: L10n.of(context)!.keyboard,
-                                          onTap: () => controller.inputFocus
-                                              .requestFocus(),
-                                          icon: Icons.keyboard,
-                                        );
-                                      },
-                                    ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: ChatInputRowStyle.responsiveUtils
+                          .isMobileOrTablet(context)
+                      ? _buildMobileInputRow(context)
+                      : _buildWebInputRow(context),
                 ),
                 ValueListenableBuilder(
                   valueListenable: controller.inputText,
@@ -200,10 +113,12 @@ class ChatInputRow extends StatelessWidget {
                     return const SizedBox();
                   },
                   child: Container(
-                    height: 56,
+                    height: ChatInputRowStyle.chatInputRowHeight(context),
                     alignment: Alignment.center,
                     child: TwakeIconButton(
-                      size: ChatInputRowStyle.sendIconButtonSize,
+                      paddingAll:
+                          ChatInputRowStyle.sendButtonPaddingAll(context),
+                      size: ChatInputRowStyle.sendButtonSize(context),
                       onTap: controller.send,
                       tooltip: L10n.of(context)!.send,
                       imagePath: ImagePaths.icSend,
@@ -212,6 +127,53 @@ class ChatInputRow extends StatelessWidget {
                 ),
               ],
       ),
+    );
+  }
+
+  ChatInputRowMobile _buildMobileInputRow(BuildContext context) {
+    return ChatInputRowMobile(
+      inputBar: _buildInputBar(context),
+      showEmojiPicker: controller.showEmojiPicker,
+      inputText: controller.inputText,
+      onEmojiAction: controller.emojiPickerAction,
+      onKeyboardAction: controller.inputFocus.requestFocus,
+    );
+  }
+
+  ChatInputRowWeb _buildWebInputRow(BuildContext context) {
+    return ChatInputRowWeb(
+      inputBar: _buildInputBar(context),
+      showEmojiPicker: controller.showEmojiPicker,
+      inputText: controller.inputText,
+      onTapMoreBtn: () => controller.onSendFileClick(context),
+      onEmojiAction: controller.emojiPickerAction,
+      onKeyboardAction: controller.inputFocus.requestFocus,
+    );
+  }
+
+  InputBar _buildInputBar(BuildContext context) {
+    return InputBar(
+      room: controller.room!,
+      minLines: 1,
+      maxLines: 8,
+      autofocus: !PlatformInfos.isMobile,
+      keyboardType: TextInputType.multiline,
+      textInputAction: AppConfig.sendOnEnter ? TextInputAction.send : null,
+      onSubmitted: controller.onInputBarSubmitted,
+      focusNode: controller.inputFocus,
+      controller: controller.sendController,
+      decoration: InputDecoration(
+        hintText: L10n.of(context)!.chatMessage,
+        hintMaxLines: 1,
+        hintStyle: Theme.of(context)
+            .textTheme
+            .bodyLarge
+            ?.merge(
+              Theme.of(context).inputDecorationTheme.hintStyle,
+            )
+            .copyWith(letterSpacing: -0.15),
+      ),
+      onChanged: controller.onInputBarChanged,
     );
   }
 }
