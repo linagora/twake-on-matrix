e648bcf8acec5e1912b1b03e7de8ce06e9304e88
Julian KOUNE <jkoune@linagora.com>
2023-09-13 11:46:58 +0200

    feat: add new blank screen

---
+++ lib/pages/chat_blank/chat_blank_style.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+
+class ChatBlankStyle {
+  static const double minWidth = 200;
+  static double width(BuildContext context) =>
+      min<double>(MediaQuery.of(context).size.width, ChatBlankStyle.minWidth) /
+      2;
+  static const EdgeInsets elementsPadding =
+      EdgeInsets.only(top: 32.0, bottom: 12.0);
+  static const double textFontSize = 17.0;
+  static const FontWeight textFontWeight = FontWeight.w500;
+  static const double iconSize = 20.0;
+
+  static TextStyle? textStyle(BuildContext context) =>
+      Theme.of(context).textTheme.bodyLarge?.copyWith(
+            fontSize: ChatBlankStyle.textFontSize,
+            fontWeight: ChatBlankStyle.textFontWeight,
+            color: LinagoraRefColors.material().tertiary[20],
+          );
+}
