3345ae1a2a914b8244f195a94282343edab38d9a
HuyNguyen <quanghnguyen@linagora.com>
2023-09-15 13:19:50 +0700

    TW-541: Create `GetPreviewUrlInteractor` and apply for domain layer

---
+++ lib/domain/app_state/preview_url/get_preview_url_loading.dart
@@ -0,0 +1,8 @@
+import 'package:fluffychat/app_state/success.dart';
+
+class GetPreviewURLLoading extends Success {
+  const GetPreviewURLLoading();
+
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/domain/model/media/url_preview.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+
+class UrlPreview with EquatableMixin {
+  final int? matrixImageSize;
+  final String? siteName;
+  final String? imageAlt;
+  final String? description;
+  final String? imageUrl;
+  final int? imageHeight;
+  final int? imageWidth;
+  final String? imageType;
+  final String? imageTitle;
+  final String? title;
+
+  UrlPreview({
+    this.matrixImageSize,
+    this.siteName,
+    this.imageAlt,
+    this.description,
+    this.imageUrl,
+    this.imageHeight,
+    this.imageWidth,
+    this.imageType,
+    this.imageTitle,
+    this.title,
+  });
+
+  Uri? get imageUri => imageUrl != null ? Uri.parse(imageUrl!) : null;
+
+  String? get titlePreview => title ?? siteName ?? imageTitle;
+
+  @override
+  List<Object?> get props => [
+        matrixImageSize,
+        siteName,
+        imageAlt,
+        description,
+        imageUrl,
+        imageHeight,
+        imageType,
+        imageWidth,
+        imageTitle,
+        title,
+      ];
+}
