2eff277c438e2643c2aecc5f46551cd95fe2ccea
HuyNguyen <quanghnguyen@linagora.com>
2023-09-15 13:48:29 +0700

    TW-541: Implement UI for presentation layer

---
+++ lib/widgets/twake_components/twake_preview_link/twake_link_view_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TwakeLinkViewStyle {
+  static const double radiusBorder = 20;
+
+  static const EdgeInsetsDirectional paddingAll =
+      EdgeInsetsDirectional.all(8.0);
+  static const EdgeInsetsDirectional paddingWidgetNoPreview =
+      EdgeInsetsDirectional.only(start: 8.0);
+
+  static const EdgeInsetsDirectional paddingCleanRichText =
+      EdgeInsetsDirectional.only(
+    start: 8.0,
+    end: 8.0,
+    top: 8.0,
+  );
+}
