f082bd69ff74b8f8264a373278070329b2561e58
sherlock <quangkhai28999@gmail.com>
2023-09-22 15:27:53 +0700

    TW-611: refactor event video player

--- lib/pages/chat/events/sending_video_widget.dart
+++ lib/pages/chat/events/sending_video_widget.dart
@@ -1,214 +1,137 @@
-import 'dart:io';
-
-import 'package:chewie/chewie.dart';
 import 'package:fluffychat/pages/chat/events/message_content_style.dart';
+import 'package:fluffychat/presentation/model/file/display_image_info.dart';
+import 'package:fluffychat/widgets/video_viewer_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 import 'package:matrix/matrix.dart';
-import 'package:video_player/video_player.dart';
 
-class SendingVideoWidget extends StatefulWidget {
+class SendingVideoWidget extends StatelessWidget {
   final Event event;
 
   final MatrixVideoFile matrixFile;
 
-  const SendingVideoWidget({
+  final DisplayImageInfo displayImageInfo;
+
+  SendingVideoWidget({
     super.key,
     required this.event,
     required this.matrixFile,
+    required this.displayImageInfo,
   });
 
-  @override
-  State<SendingVideoWidget> createState() => _SendingVideoWidgetState();
-}
-
-class _SendingVideoWidgetState extends State<SendingVideoWidget>
-    with AutomaticKeepAliveClientMixin {
   final sendingFileProgressNotifier = ValueNotifier(SendingVideoStatus.sending);
-  ChewieController? chewieController;
-  VideoPlayerController? videoPlayerController;
-
-  bool isPlay = false;
-
-  @override
-  void dispose() {
-    videoPlayerController?.dispose();
-    chewieController?.dispose();
-    super.dispose();
-  }
-
-  Future<void> initVideoController() async {
-    if (widget.matrixFile.filePath == null) {
-      return;
-    }
-    videoPlayerController = VideoPlayerController.file(
-      File(widget.matrixFile.filePath!),
-    );
-    await videoPlayerController?.initialize();
-    chewieController = ChewieController(
-      videoPlayerController: videoPlayerController!,
-      autoPlay: false,
-      showControlsOnInitialize: false,
-      customControls: const MaterialControls(),
-    );
-    videoPlayerController!.addListener(() {
-      if (!chewieController!.isPlaying && isPlay) {
-        setState(() {
-          isPlay = false;
-        });
-      } else if (chewieController!.isPlaying && isPlay == false) {
-        setState(() {
-          isPlay = true;
-        });
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    /// FIXME: current time, the ticket focus on sending video, so i don't focus on
-    /// video player, which is not have design, user story,
-    /// so the solution for its working quite hard to read :))
-    /// Sincerely sorry to person who read this code in the future.
-    super.build(context);
     _checkSendingFileStatus();
-    final (imageWidth, imageHeight) =
-        _getImageSize(widget.matrixFile.width, widget.matrixFile.height);
-    if (isPlay) {
-      sendingFileProgressNotifier.value = SendingVideoStatus.playing;
-    }
 
     return ValueListenableBuilder<SendingVideoStatus>(
-      key: ValueKey(widget.event.eventId),
+      key: ValueKey(event.eventId),
       valueListenable: sendingFileProgressNotifier,
       builder: ((context, value, child) {
-        return Stack(
-          alignment: Alignment.center,
-          children: [
-            child!,
-            if (value == SendingVideoStatus.sending) ...[
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  const _PlayVideoButton(),
-                  SizedBox(
-                    width: 56,
-                    height: 56,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2,
-                      color: LinagoraRefColors.material().primary[100],
+        return ClipRRect(
+          borderRadius: MessageContentStyle.borderRadiusBubble,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                width: MessageContentStyle.imageBubbleWidth(
+                  displayImageInfo.size.width,
+                ),
+                height: MessageContentStyle.imageBubbleHeight(
+                  displayImageInfo.size.height,
+                ),
+                child:
+                    const BlurHash(hash: MessageContentStyle.defaultBlurHash),
+              ),
+              child!,
+              if (value == SendingVideoStatus.sending) ...[
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    const _PlayVideoButton(),
+                    SizedBox(
+                      width: MessageContentStyle.videoCenterButtonSize,
+                      height: MessageContentStyle.videoCenterButtonSize,
+                      child: CircularProgressIndicator(
+                        strokeWidth: MessageContentStyle.strokeVideoWidth,
+                        color: LinagoraRefColors.material().primary[100],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ] else if (value == SendingVideoStatus.sent) ...[
-              InkWell(
-                onTap: _onPlayVideo,
-                child: const _PlayVideoButton(),
-              )
-            ] else if (value == SendingVideoStatus.playing) ...[
-              const SizedBox(),
-            ] else if (value == SendingVideoStatus.error) ...[
-              const SizedBox(
-                width: 56,
-                height: 56,
-                child: Icon(Icons.error),
-              ),
-            ]
-          ],
+                  ],
+                ),
+              ] else if (value == SendingVideoStatus.sent) ...[
+                InkWell(
+                  onTap: () => _onPlayVideo(context),
+                  child: const _PlayVideoButton(),
+                )
+              ] else if (value == SendingVideoStatus.error) ...[
+                const SizedBox(
+                  width: MessageContentStyle.videoCenterButtonSize,
+                  height: MessageContentStyle.videoCenterButtonSize,
+                  child: Icon(Icons.error),
+                ),
+              ],
+            ],
+          ),
         );
       }),
       child: VideoWidget(
-        chewieController: chewieController,
-        imageHeight: imageHeight,
-        imageWidth: imageWidth,
-        matrixFile: widget.matrixFile,
+        imageHeight: displayImageInfo.size.height,
+        imageWidth: displayImageInfo.size.width,
+        matrixFile: matrixFile,
       ),
     );
   }
 
-  (double, double) _getImageSize(int? imageWidth, int? imageHeight) {
-    if (imageWidth == null || imageHeight == null) {
-      return (
-        MessageContentStyle.imageWidth(context),
-        MessageContentStyle.imageHeight(context)
-      );
+  void _onPlayVideo(BuildContext context) async {
+    if (matrixFile.filePath == null) {
+      return;
     }
-
-    final ratio = MessageContentStyle.imageWidth(context) / imageWidth;
-
-    if (imageWidth <= imageHeight) {
-      return (
-        MessageContentStyle.imageWidth(context),
-        MessageContentStyle.imageHeight(context)
-      );
-    } else {
-      return (MessageContentStyle.imageWidth(context), imageHeight * ratio);
-    }
-  }
-
-  void _onPlayVideo() async {
-    if (chewieController == null || videoPlayerController == null) {
-      await initVideoController();
-    }
-    await chewieController?.play();
-    sendingFileProgressNotifier.value = SendingVideoStatus.playing;
+    await showDialog(
+      context: context,
+      useRootNavigator: false,
+      builder: (context) {
+        return VideoViewerDialog(path: matrixFile.filePath!);
+      },
+    );
   }
 
   void _checkSendingFileStatus() {
-    if ((widget.event.status == EventStatus.sent ||
-            widget.event.status == EventStatus.synced) &&
+    if ((event.status == EventStatus.sent ||
+            event.status == EventStatus.synced) &&
         sendingFileProgressNotifier.value != SendingVideoStatus.sent) {
       sendingFileProgressNotifier.value = SendingVideoStatus.sent;
-    } else if (widget.event.status == EventStatus.error) {
+    } else if (event.status == EventStatus.error) {
       sendingFileProgressNotifier.value = SendingVideoStatus.error;
     }
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
 
 class VideoWidget extends StatelessWidget {
   const VideoWidget({
     super.key,
-    required this.chewieController,
     required this.imageHeight,
     required this.imageWidth,
     required this.matrixFile,
   });
 
-  final ChewieController? chewieController;
   final double imageHeight;
   final double imageWidth;
   final MatrixFile matrixFile;
 
   @override
   Widget build(BuildContext context) {
-    return chewieController != null
-        ? SizedBox(
-            height: imageHeight,
+    return matrixFile.bytes != null
+        ? Image.memory(
+            matrixFile.bytes!,
             width: imageWidth,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12.0),
-              child: Material(
-                color: Colors.black,
-                child: Chewie(controller: chewieController!),
-              ),
-            ),
+            height: imageHeight,
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.medium,
           )
-        : ClipRRect(
-            borderRadius: BorderRadius.circular(12.0),
-            child: matrixFile.bytes != null
-                ? Image.memory(
-                    matrixFile.bytes!,
-                    width: imageWidth,
-                    height: imageHeight,
-                    fit: BoxFit.cover,
-                    filterQuality: FilterQuality.medium,
-                  )
-                : SizedBox(width: imageWidth, height: imageHeight),
-          );
+        : SizedBox(width: imageWidth, height: imageHeight);
   }
 }
 
@@ -218,8 +141,8 @@ class _PlayVideoButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 56,
-      height: 56,
+      width: MessageContentStyle.videoCenterButtonSize,
+      height: MessageContentStyle.videoCenterButtonSize,
       decoration: const BoxDecoration(
         color: Colors.black38,
         shape: BoxShape.circle,
@@ -228,7 +151,7 @@ class _PlayVideoButton extends StatelessWidget {
       child: Icon(
         Icons.play_arrow_rounded,
         color: LinagoraRefColors.material().primary[100],
-        size: 48,
+        size: MessageContentStyle.iconInsideVideoButtonSize,
       ),
     );
   }
@@ -237,6 +160,5 @@ class _PlayVideoButton extends StatelessWidget {
 enum SendingVideoStatus {
   sending,
   sent,
-  playing,
   error,
 }
