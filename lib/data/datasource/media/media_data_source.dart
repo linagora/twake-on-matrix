a5d8ce68ec9771913e1ef39ba00229bd9afa7b88
HuyNguyen <quanghnguyen@linagora.com>
2023-09-22 09:27:52 +0700

    TW-614: Apply logic upload content for web in data layer

--- lib/data/datasource/media/media_data_source.dart
+++ lib/data/datasource/media/media_data_source.dart
@@ -1,6 +1,12 @@
+import 'package:fluffychat/domain/model/media/uploaded_content.dart';
 import 'package:fluffychat/domain/model/media/url_preview.dart';
 
 abstract class MediaDataSource {
+  Future<UploadedContent> uploadContentForWeb(
+    List<int> file, {
+    String? contentType,
+  });
+
   Future<UrlPreview> getUrlPreview({
     required Uri uri,
     int? preferredPreviewTime,
--- lib/data/datasource_impl/media/media_data_source_impl.dart
+++ lib/data/datasource_impl/media/media_data_source_impl.dart
@@ -1,6 +1,8 @@
 import 'package:fluffychat/data/datasource/media/media_data_source.dart';
+import 'package:fluffychat/data/extensions/media/upload_content_response_extension.dart';
 import 'package:fluffychat/data/extensions/media/url_preview_response_extension.dart';
 import 'package:fluffychat/data/network/media/media_api.dart';
+import 'package:fluffychat/domain/model/media/uploaded_content.dart';
 import 'package:fluffychat/domain/model/media/url_preview.dart';
 
 class MediaDataSourceImpl implements MediaDataSource {
@@ -18,5 +20,17 @@ class MediaDataSourceImpl implements MediaDataSource {
       preferredPreviewTime: preferredPreviewTime,
     );
     return response.toUrlPreview();
+  }
+
+  @override
+  Future<UploadedContent> uploadContentForWeb(
+    List<int> file, {
+    String? contentType,
+  }) async {
+    final response = await _mediaAPI.uploadContentForWeb(
+      file,
+      contentType: contentType,
+    );
+    return response.toUploadedContent();
   }
 }
