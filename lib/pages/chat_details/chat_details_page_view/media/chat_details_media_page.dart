1bae9186a7ab01384e36094c4f3886eb37168104
MinhDV <drminh2807@gmail.com>
2023-09-22 16:59:57 +0700

    TW-616 Video player for media page

--- lib/pages/chat_details/chat_details_page_view/media/chat_details_media_page.dart
+++ lib/pages/chat_details/chat_details_page_view/media/chat_details_media_page.dart
@@ -1,22 +1,24 @@
 import 'package:fluffychat/app_state/success.dart';
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/domain/app_state/room/timeline_search_event_state.dart';
-import 'package:fluffychat/pages/chat_details/chat_details_page_view/media/chat_details_media_style.dart';
+import 'package:fluffychat/pages/chat/events/event_video_player.dart';
 import 'package:fluffychat/pages/chat_details/chat_details_page_view/same_type_events_list_controller.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
 import 'package:fluffychat/widgets/mxc_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
-import 'package:linagora_design_flutter/extensions/duration_extension.dart';
 import 'package:matrix/matrix.dart';
 
 class ChatDetailsMediaPage extends StatelessWidget {
   static const _mediaFetchLimit = 20;
   final Future<Timeline> Function() getTimeline;
   final Map<EventId, ImageData>? cacheMap;
+  final DownloadVideoEventCallback handleDownloadVideoEvent;
+
   const ChatDetailsMediaPage({
     Key? key,
     required this.getTimeline,
+    required this.handleDownloadVideoEvent,
     this.cacheMap,
   }) : super(key: key);
 
@@ -37,41 +39,68 @@ class ChatDetailsMediaPage extends StatelessWidget {
             crossAxisCount: 3,
           ),
           itemCount: events.length,
-          itemBuilder: (context, index) => Stack(
-            fit: StackFit.expand,
-            children: [
-              MxcImage(
-                event: events[index],
-                isThumbnail: true,
-                fit: BoxFit.cover,
-                onTapPreview: () {},
-                isPreview: true,
-                placeholder: (context) => BlurHash(
-                  hash:
-                      events[index].blurHash ?? AppConfig.defaultImageBlurHash,
-                ),
-                cacheKey: events[index].eventId,
-                cacheMap: cacheMap,
-              ),
-              if (events[index].messageType == MessageTypes.Video)
-                Positioned(
-                  bottom: ChatDetailsMediaStyle.durationPosition,
-                  right: ChatDetailsMediaStyle.durationPosition,
-                  child: Container(
-                    padding: ChatDetailsMediaStyle.durationPadding,
-                    decoration: ChatDetailsMediaStyle.durationBoxDecoration(
-                      context,
+          itemBuilder: (context, index) =>
+              events[index].messageType == MessageTypes.Image
+                  ? _ImageItem(
+                      event: events[index],
+                      cacheMap: cacheMap,
+                    )
+                  : _VideoItem(
+                      event: events[index],
+                      handleDownloadVideoEvent: handleDownloadVideoEvent,
+                      thumbnailCacheMap: cacheMap,
                     ),
-                    child: Text(
-                      events[index].duration?.mediaTimeLength() ?? "--:--",
-                      style: ChatDetailsMediaStyle.durationTextStyle(context),
-                    ),
-                  ),
-                ),
-            ],
-          ),
         );
       },
+    );
+  }
+}
+
+class _ImageItem extends StatelessWidget {
+  final Event event;
+  final Map<EventId, ImageData>? cacheMap;
+  const _ImageItem({
+    required this.event,
+    this.cacheMap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MxcImage(
+      event: event,
+      isThumbnail: true,
+      fit: BoxFit.cover,
+      onTapPreview: () {},
+      isPreview: true,
+      placeholder: (context) => BlurHash(
+        hash: event.blurHash ?? AppConfig.defaultImageBlurHash,
+      ),
+      cacheKey: event.eventId,
+      cacheMap: cacheMap,
+    );
+  }
+}
+
+class _VideoItem extends StatelessWidget {
+  final Event event;
+  final DownloadVideoEventCallback handleDownloadVideoEvent;
+  final Map<EventId, ImageData>? thumbnailCacheMap;
+  const _VideoItem({
+    required this.event,
+    required this.handleDownloadVideoEvent,
+    this.thumbnailCacheMap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return EventVideoPlayer(
+      event,
+      handleDownloadVideoEvent: handleDownloadVideoEvent,
+      rounded: false,
+      showDuration: true,
+      thumbnailCacheKey: event.eventId,
+      thumbnailCacheMap: thumbnailCacheMap,
+      noResizeThumbnail: true,
     );
   }
 }
