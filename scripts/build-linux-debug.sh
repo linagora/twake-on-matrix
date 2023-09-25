2db28066aee25cc39e056d231d6d186206b0181c
MinhDV <drminh2807@gmail.com>
2023-09-25 14:53:47 +0700

    fix: Build release linux

--- scripts/build-linux-debug.sh
+++ scripts/build-linux-debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
 echo "Setup Linux dependencies"
-sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev libjsoncpp-dev locate libfuse-dev libolm-dev
+sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev libjsoncpp-dev locate libfuse-dev libolm-dev libmpv-dev mpv
 
 # Updating database of locate
 sudo updatedb
