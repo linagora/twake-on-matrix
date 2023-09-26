58abefbb71f674b570ef05a71c43e583dbd18e36
MinhDV <drminh2807@gmail.com>
2023-09-26 20:17:33 +0700

    fix: Highlight file event

--- lib/pages/chat/events/message_download_content.dart
+++ lib/pages/chat/events/message_download_content.dart
@@ -1,5 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/domain/app_state/room/chat_room_search_state.dart';
 import 'package:fluffychat/domain/model/extensions/mime_type_extension.dart';
+import 'package:fluffychat/utils/string_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_matrix_html/color_extension.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
 import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
@@ -12,10 +18,13 @@ class MessageDownloadContent extends StatelessWidget {
   final Event event;
   final void Function(Event event) onFileTapped;
 
+  final ValueNotifier<Either<Failure, Success>>? searchStatus;
+
   const MessageDownloadContent(
     this.event, {
     Key? key,
     required this.onFileTapped,
+    this.searchStatus,
   }) : super(key: key);
 
   @override
@@ -55,15 +64,22 @@ class MessageDownloadContent extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  Text(
-                    filename,
-                    maxLines: 1,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.bold,
+                  if (searchStatus != null) ...[
+                    ValueListenableBuilder(
+                      valueListenable: searchStatus!,
+                      builder: (context, searchStatusValue, child) {
+                        final success = searchStatusValue
+                            .getSuccessOrNull<ChatRoomSearchSuccess>();
+                        final searchKeyword = success?.keyword ?? '';
+                        return _FileNameWithSearchText(
+                          filename: filename,
+                          searchKeyword: searchKeyword,
+                        );
+                      },
                     ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
+                  ] else ...[
+                    _FileNameText(filename: filename),
+                  ],
                   Row(
                     children: [
                       if (sizeString != null)
@@ -82,6 +98,59 @@ class MessageDownloadContent extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class _FileNameWithSearchText extends StatelessWidget {
+  const _FileNameWithSearchText({
+    required this.filename,
+    required this.searchKeyword,
+  });
+
+  final String filename;
+  final String searchKeyword;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      maxLines: 1,
+      text: TextSpan(
+        children: filename.buildHighlightTextSpans(
+          searchKeyword,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onBackground,
+            fontWeight: FontWeight.bold,
+          ),
+          highlightStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onBackground,
+            fontWeight: FontWeight.bold,
+            backgroundColor: CssColor.fromCss('gold'),
+          ),
+        ),
+      ),
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
+
+class _FileNameText extends StatelessWidget {
+  const _FileNameText({
+    required this.filename,
+  });
+
+  final String filename;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      filename,
+      maxLines: 1,
+      style: TextStyle(
+        color: Theme.of(context).colorScheme.onBackground,
+        fontWeight: FontWeight.bold,
+      ),
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
