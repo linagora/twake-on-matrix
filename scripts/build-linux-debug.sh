891f0f19f345d7b79e8775dd06c433756468d7a0
Nguyen Thai <tknguyen@linagora.com>
2023-09-26 15:05:15 +0700

    Use new appimage packaging for Linux builds
    Fixed library name and removed unneeded packages
    
    Use azure mirror, skip tests
    
    Because GH Actions docker is flaky
    
    Use png file for icon
    
    Added additional libraries
    
    Correct the distro codename

--- scripts/build-linux-debug.sh
+++ scripts/build-linux-debug.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ve
 echo "Setup Linux dependencies"
-sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev libjsoncpp-dev locate libfuse-dev libolm-dev
-
-# Updating database of locate
-sudo updatedb
+sudo apt-get install -y clang cmake ninja-build \
+                        pkg-config libgtk-3-dev liblzma-dev \
+                        libjsoncpp-dev libfuse-dev \
+                        libolm-dev libmpv-dev libsecret-1-dev
 
 flutter config --enable-linux-desktop
 flutter clean
