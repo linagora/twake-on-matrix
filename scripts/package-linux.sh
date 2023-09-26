9ce25bcb157a7b589b835860947055a8137c3bcc
Nguyen Thai <tknguyen@linagora.com>
2023-09-26 15:05:15 +0700

    Use new appimage packaging for Linux builds
    Fixed library name and removed unneeded packages
    
    Use azure mirror, skip tests
    
    Because GH Actions docker is flaky
    
    Use png file for icon
    
    Added additional libraries
    
    Correct the distro codename

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Setup AppImageTool for packaging
-echo "Setting up AppImageTool"
-curl -o appimagetool -L "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-chmod +x appimagetool
-sudo mv appimagetool /usr/local/bin/
+# Setup appimage-builder for packaging
+echo "Setting up appimage-builder"
+wget -O appimage-builder-x86_64.AppImage https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage
+chmod +x appimage-builder-x86_64.AppImage
+sudo mv appimage-builder-x86_64.AppImage /usr/local/bin/appimage-builder
 
 echo "Packaging."
-flutter pub global run flutter_distributor:main.dart package --platform linux --targets appimage --skip-clean --flutter-build-args="release"
+export BUILD_TYPE=release
+# This is to set environment variable from a file
+# https://stackoverflow.com/a/45971167/8296391
+set -a; . /etc/os-release; set +a
+appimage-builder --recipe appimage/AppImageBuilder.yml --skip-tests
+mkdir dist && cp ./*.AppImage dist/
