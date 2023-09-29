6a4bd73d1d9bb393fa74d7147b63a0adbf2f3da0
HuyNguyen <quanghnguyen@linagora.com>
2023-09-29 16:28:55 +0700

    TW-692: Update settings profile screen

---
+++ lib/pages/settings_dashboard/settings_profile/settings_profile_view.dart
@@ -0,0 +1,217 @@
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/pages/settings_dashboard/settings_profile/settings_profile.dart';
+import 'package:fluffychat/pages/settings_dashboard/settings_profile/settings_profile_item.dart';
+import 'package:fluffychat/pages/settings_dashboard/settings_profile/settings_profile_view_mobile.dart';
+import 'package:fluffychat/pages/settings_dashboard/settings_profile/settings_profile_view_web.dart';
+import 'package:fluffychat/presentation/model/settings/settings_profile_presentation.dart';
+import 'package:fluffychat/utils/responsive/responsive_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingsProfileView extends StatelessWidget {
+  final SettingsProfileController controller;
+
+  static const ValueKey settingsProfileViewMobileKey =
+      ValueKey('settingsProfileViewMobile');
+
+  static const ValueKey settingsProfileViewWebKey =
+      ValueKey('settingsProfileViewWeb');
+
+  const SettingsProfileView({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final responsive = getIt.get<ResponsiveUtils>();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 24,
+          ),
+          onPressed: () => context.pop(),
+        ),
+        title: Text(
+          L10n.of(context)!.profile,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
+        actions: [
+          ValueListenableBuilder(
+            valueListenable: controller.isEditedProfileNotifier,
+            builder: (context, edited, _) {
+              if (!edited) return const SizedBox();
+              return InkWell(
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+                onTap: () => controller.setDisplayNameAction(),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.symmetric(
+                    vertical: 14,
+                    horizontal: 12,
+                  ),
+                  child: Text(
+                    L10n.of(context)!.done,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+        centerTitle: true,
+      ),
+      backgroundColor: responsive.isWebDesktop(context)
+          ? Theme.of(context).colorScheme.surface
+          : null,
+      body: SingleChildScrollView(
+        padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
+        child: SlotLayout(
+          config: <Breakpoint, SlotLayoutConfig>{
+            const WidthPlatformBreakpoint(
+              end: ResponsiveUtils.minDesktopWidth,
+            ): SlotLayout.from(
+              key: settingsProfileViewMobileKey,
+              builder: (_) {
+                return SettingsProfileViewMobile(
+                  profileNotifier: controller
+                      .settingsDashboardManagerController.profileNotifier,
+                  displayName: controller.displayName,
+                  onAvatarTap: () => controller.setAvatarAction(),
+                  settingsProfileOptions: ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return SettingsProfileItemBuilder(
+                        settingsProfileEnum:
+                            controller.getListProfileMobile[index],
+                        title: controller.getListProfileMobile[index]
+                            .getTitle(context),
+                        settingsProfilePresentation:
+                            SettingsProfilePresentation(
+                          settingsProfileType: controller
+                              .getListProfileMobile[index]
+                              .getSettingsProfileType(),
+                        ),
+                        suffixIcon: controller.getListProfileMobile[index]
+                            .getTrailingIcon(),
+                        leadingIcon: controller.getListProfileMobile[index]
+                            .getLeadingIcon(),
+                        focusNode: controller.getFocusNode(
+                          controller.getListProfileMobile[index],
+                        ),
+                        textEditingController: controller.getController(
+                          controller.getListProfileMobile[index],
+                        ),
+                        onChange: (_, settingsProfileEnum) {
+                          controller
+                              .handleTextEditOnChange(settingsProfileEnum);
+                        },
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemCount: controller.getListProfileMobile.length,
+                  ),
+                );
+              },
+            ),
+            const WidthPlatformBreakpoint(
+              begin: ResponsiveUtils.minDesktopWidth,
+            ): SlotLayout.from(
+              key: settingsProfileViewWebKey,
+              builder: (_) {
+                return SettingsProfileViewWeb(
+                  profileNotifier: controller
+                      .settingsDashboardManagerController.profileNotifier,
+                  displayName: controller.displayName,
+                  basicInfoWidget: ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return SettingsProfileItemBuilder(
+                        settingsProfileEnum:
+                            controller.getListProfileBasicInfo[index],
+                        title: controller.getListProfileBasicInfo[index]
+                            .getTitle(context),
+                        settingsProfilePresentation:
+                            SettingsProfilePresentation(
+                          settingsProfileType: controller
+                              .getListProfileBasicInfo[index]
+                              .getSettingsProfileType(),
+                        ),
+                        suffixIcon: controller.getListProfileBasicInfo[index]
+                            .getTrailingIcon(),
+                        focusNode: controller.getFocusNode(
+                          controller.getListProfileBasicInfo[index],
+                        ),
+                        textEditingController: controller.getController(
+                          controller.getListProfileBasicInfo[index],
+                        ),
+                        onChange: (_, settingsProfileEnum) {
+                          controller
+                              .handleTextEditOnChange(settingsProfileEnum);
+                        },
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemCount: controller.getListProfileBasicInfo.length,
+                  ),
+                  workIdentitiesInfoWidget: ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return SettingsProfileItemBuilder(
+                        settingsProfileEnum:
+                            controller.getListProfileWorkIdentitiesInfo[index],
+                        title: controller
+                            .getListProfileWorkIdentitiesInfo[index]
+                            .getTitle(context),
+                        settingsProfilePresentation:
+                            SettingsProfilePresentation(
+                          settingsProfileType: controller
+                              .getListProfileWorkIdentitiesInfo[index]
+                              .getSettingsProfileType(),
+                        ),
+                        suffixIcon: controller
+                            .getListProfileWorkIdentitiesInfo[index]
+                            .getTrailingIcon(),
+                        focusNode: controller.getFocusNode(
+                          controller.getListProfileWorkIdentitiesInfo[index],
+                        ),
+                        textEditingController: controller.getController(
+                          controller.getListProfileWorkIdentitiesInfo[index],
+                        ),
+                        onChange: (_, settingsProfileEnum) {
+                          controller
+                              .handleTextEditOnChange(settingsProfileEnum);
+                        },
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemCount: controller.getListProfileBasicInfo.length,
+                  ),
+                  onAvatarTap: () => controller.setAvatarAction(),
+                );
+              },
+            ),
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/settings_dashboard/settings_style/settings_style.dart
@@ -0,0 +1,92 @@
+import 'package:collection/collection.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/material.dart';
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/config/setting_keys.dart';
+import 'package:fluffychat/widgets/theme_builder.dart';
+import 'settings_style_view.dart';
+
+class SettingsStyle extends StatefulWidget {
+  const SettingsStyle({Key? key}) : super(key: key);
+
+  @override
+  SettingsStyleController createState() => SettingsStyleController();
+}
+
+class SettingsStyleController extends State<SettingsStyle> {
+  void setWallpaperAction() async {
+    final picked = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      withData: false,
+    );
+    final pickedFile = picked?.files.firstOrNull;
+
+    if (pickedFile == null) return;
+    await Matrix.of(context)
+        .store
+        .setItem(SettingKeys.wallpaper, pickedFile.path);
+    setState(() {});
+  }
+
+  void deleteWallpaperAction() async {
+    Matrix.of(context).wallpaper = null;
+    await Matrix.of(context).store.deleteItem(SettingKeys.wallpaper);
+    setState(() {});
+  }
+
+  void setChatColor(Color? color) async {
+    if (color != null) {
+      AppConfig.colorSchemeSeed = color;
+      ThemeController.of(context).setPrimaryColor(color);
+    }
+  }
+
+  ThemeMode get currentTheme => ThemeController.of(context).themeMode;
+  Color? get currentColor => ThemeController.of(context).primaryColor;
+
+  static final List<Color?> customColors = [
+    AppConfig.chatColor,
+    Colors.blue.shade800,
+    Colors.green.shade800,
+    Colors.orange.shade700,
+    Colors.pink.shade700,
+    Colors.blueGrey.shade600,
+    null,
+  ];
+
+  void switchTheme(ThemeMode? newTheme) {
+    if (newTheme == null) return;
+    switch (newTheme) {
+      case ThemeMode.light:
+        ThemeController.of(context).setThemeMode(ThemeMode.light);
+        break;
+      case ThemeMode.dark:
+        ThemeController.of(context).setThemeMode(ThemeMode.dark);
+        break;
+      case ThemeMode.system:
+        ThemeController.of(context).setThemeMode(ThemeMode.system);
+        break;
+    }
+    setState(() {});
+  }
+
+  void changeFontSizeFactor(double d) {
+    setState(() => AppConfig.fontSizeFactor = d);
+    Matrix.of(context).store.setItem(
+          SettingKeys.fontSizeFactor,
+          AppConfig.fontSizeFactor.toString(),
+        );
+  }
+
+  void changeBubbleSizeFactor(double d) {
+    setState(() => AppConfig.bubbleSizeFactor = d);
+    Matrix.of(context).store.setItem(
+          SettingKeys.bubbleSizeFactor,
+          AppConfig.bubbleSizeFactor.toString(),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) => SettingsStyleView(this);
+}
