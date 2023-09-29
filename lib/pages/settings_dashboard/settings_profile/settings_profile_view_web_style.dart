29e0c958095c5c676d4858dd5c0873aee8f08ed1
HuyNguyen <quanghnguyen@linagora.com>
2023-09-29 16:27:30 +0700

    TW-692: Create `SettingsProfileViewWeb`

---
+++ lib/pages/settings_dashboard/settings_profile/settings_profile_view_web_style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SettingsProfileViewWebStyle {
+  static const double bodyWidth = 640;
+  static const double widthSize = 116;
+  static const double avatarSize = 96;
+  static const double avatarFontSize = 18 * 2.5;
+  static const double positionedBottomSize = 0;
+  static const double positionedRightSize = 0;
+  static const double iconEditBorderWidth = 4;
+  static const double iconEditSize = 24;
+  static const double dividerHeight = 2;
+  static const double radiusCircular = 16;
+
+  static const EdgeInsetsDirectional paddingBody =
+      EdgeInsetsDirectional.all(32);
+
+  static const EdgeInsetsDirectional paddingWidgetBasicInfo =
+      EdgeInsetsDirectional.all(16);
+
+  static const EdgeInsetsDirectional paddingBasicInfoTitle =
+      EdgeInsetsDirectional.only(bottom: 32);
+
+  static const EdgeInsetsDirectional paddingEditIcon =
+      EdgeInsetsDirectional.all(8);
+
+  static const EdgeInsetsDirectional paddingWidgetEditProfileInfo =
+      EdgeInsetsDirectional.symmetric(vertical: 16);
+
+  static const EdgeInsetsDirectional paddingWorkIdentitiesInfoWidget =
+      EdgeInsetsDirectional.only(bottom: 16);
+}
