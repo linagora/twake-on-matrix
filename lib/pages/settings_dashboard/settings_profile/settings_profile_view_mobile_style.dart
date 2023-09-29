5a478a6732859cc853cdf6ce4438695e7d20a76a
HuyNguyen <quanghnguyen@linagora.com>
2023-09-29 16:27:12 +0700

    TW-692: Create `SettingsProfileViewMobile`

---
+++ lib/pages/settings_dashboard/settings_profile/settings_profile_view_mobile_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsProfileViewMobileStyle {
+  static const double widthSize = 116;
+  static const double avatarSize = 96;
+  static const double avatarFontSize = 18 * 2.5;
+  static const double positionedBottomSize = 0;
+  static const double positionedRightSize = 0;
+  static const double iconEditBorderWidth = 4;
+  static const double iconEditSize = 24;
+  static const double dividerHeight = 2;
+
+  static EdgeInsetsDirectional padding =
+      const EdgeInsetsDirectional.symmetric(vertical: 16.0);
+
+  static EdgeInsetsDirectional editIconPadding =
+      const EdgeInsetsDirectional.all(8);
+}
