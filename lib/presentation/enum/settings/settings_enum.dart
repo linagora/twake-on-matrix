14a2691aa52c00e2cc6241b1cf62a129eff5c189
HuyNguyen <quanghnguyen@linagora.com>
2023-09-29 16:24:58 +0700

    TW-692: Create settings, settings profile enum and settings profile presentation

---
+++ lib/presentation/enum/settings/settings_enum.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+enum SettingEnum {
+  chatSettings,
+  privacyAndSecurity,
+  notificationAndSounds,
+  chatFolders,
+  appLanguage,
+  devices,
+  help,
+  logout;
+
+  String titleSettings(BuildContext context) {
+    switch (this) {
+      case SettingEnum.chatSettings:
+        return L10n.of(context)!.chat;
+      case SettingEnum.privacyAndSecurity:
+        return L10n.of(context)!.privacyAndSecurity;
+      case SettingEnum.notificationAndSounds:
+        return L10n.of(context)!.notificationAndSounds;
+      case SettingEnum.chatFolders:
+        return L10n.of(context)!.chatFolders;
+      case SettingEnum.appLanguage:
+        return L10n.of(context)!.appLanguage;
+      case SettingEnum.devices:
+        return L10n.of(context)!.devices;
+      case SettingEnum.help:
+        return L10n.of(context)!.help;
+      case SettingEnum.logout:
+        return L10n.of(context)!.logout;
+    }
+  }
+
+  String subtitleSettings(BuildContext context) {
+    switch (this) {
+      case SettingEnum.chatSettings:
+        return L10n.of(context)!.settingsChatSubtitle;
+      case SettingEnum.privacyAndSecurity:
+        return L10n.of(context)!.settingsPrivacyAndSecuritySubtitle;
+      case SettingEnum.notificationAndSounds:
+        return L10n.of(context)!.settingsNotificationAndSoundsSubtitle;
+      case SettingEnum.chatFolders:
+        return L10n.of(context)!.settingsChatFoldersSubtitle;
+      case SettingEnum.appLanguage:
+        return L10n.of(context)!.settingsAppLanguageSubtitle;
+      case SettingEnum.devices:
+        return L10n.of(context)!.settingsDevicesSubtitle;
+      case SettingEnum.help:
+        return L10n.of(context)!.settingsHelpSubtitle;
+      default:
+        return '';
+    }
+  }
+
+  IconData iconLeading() {
+    switch (this) {
+      case SettingEnum.chatSettings:
+        return Icons.chat_bubble_outline_outlined;
+      case SettingEnum.privacyAndSecurity:
+        return Icons.lock;
+      case SettingEnum.notificationAndSounds:
+        return Icons.notifications_none;
+      case SettingEnum.chatFolders:
+        return Icons.folder_outlined;
+      case SettingEnum.appLanguage:
+        return Icons.language;
+      case SettingEnum.devices:
+        return Icons.devices;
+      case SettingEnum.help:
+        return Icons.question_mark;
+      case SettingEnum.logout:
+        return Icons.logout_outlined;
+    }
+  }
+
+  bool get isHideTrailingIcon => this == SettingEnum.logout;
+}
