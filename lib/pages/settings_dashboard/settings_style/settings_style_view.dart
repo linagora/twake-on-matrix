fdb4e6f8587837e862310293631159cdb0926006
HuyNguyen <quanghnguyen@linagora.com>
2023-09-29 16:28:34 +0700

    TW-692: Update settings screen

--- lib/pages/settings_dashboard/settings_style/settings_style_view.dart
+++ lib/pages/settings_dashboard/settings_style/settings_style_view.dart
@@ -1,10 +1,10 @@
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 import 'package:fluffychat/widgets/layouts/max_width_body.dart';
-import '../../config/app_config.dart';
-import '../../widgets/matrix.dart';
 import 'settings_style.dart';
 
 class SettingsStyleView extends StatelessWidget {
