34e496dfb651e62e57b4afd2118f76225597eb49
Julian KOUNE <jkoune@linagora.com>
2023-10-02 15:12:44 +0200

    feat: new encrypted message design

---
+++ lib/pages/chat/events/encrypted_content_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EncryptedContentStyle {
+  static const EdgeInsets parentPadding = EdgeInsets.symmetric(
+    horizontal: 8,
+  );
+  static const EdgeInsets leadingIconPadding = EdgeInsets.all(5);
+  static const double leadingIconSize = 20;
+  static const double leadingAndTextGap = 8;
+  static const double textMaxWidth = 165;
+}
---
+++ lib/pages/chat/events/encrypted_mixin.dart
@@ -0,0 +1,75 @@
+import 'package:fluffychat/pages/bootstrap/bootstrap_dialog.dart';
+import 'package:fluffychat/pages/chat/events/message_content_style.dart';
+import 'package:fluffychat/utils/adaptive_bottom_sheet.dart';
+import 'package:fluffychat/utils/date_time_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:fluffychat/widgets/avatar/avatar.dart';
+import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+mixin EncryptedMixin {
+  void verifyOrRequestKey(BuildContext context, Event event) async {
+    final l10n = L10n.of(context)!;
+    if (event.content['can_request_session'] != true) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            event.type == EventTypes.Encrypted
+                ? l10n.needPantalaimonWarning
+                : event.calcLocalizedBodyFallback(
+                    MatrixLocals(l10n),
+                  ),
+          ),
+        ),
+      );
+      return;
+    }
+    final client = Matrix.of(context).client;
+    if (client.isUnknownSession && client.encryption!.crossSigning.enabled) {
+      final success = await BootstrapDialog(
+        client: Matrix.of(context).client,
+      ).show(context);
+      if (success != true) return;
+    }
+    event.requestKey();
+    final sender = event.senderFromMemoryOrFallback;
+    await showAdaptiveBottomSheet(
+      context: context,
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          leading: CloseButton(onPressed: Navigator.of(context).pop),
+          title: Text(
+            l10n.whyIsThisMessageEncrypted,
+            style:
+                const TextStyle(fontSize: MessageContentStyle.appBarFontSize),
+          ),
+        ),
+        body: SafeArea(
+          child: ListView(
+            padding: const EdgeInsets.all(16),
+            children: [
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: Avatar(
+                  mxContent: sender.avatarUrl,
+                  name: sender.calcDisplayname(),
+                ),
+                title: Text(sender.calcDisplayname()),
+                subtitle: Text(event.originServerTs.localizedTime(context)),
+                trailing: const Icon(Icons.lock_outlined),
+              ),
+              const Divider(),
+              Text(
+                event.calcLocalizedBodyFallback(
+                  MatrixLocals(l10n),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
