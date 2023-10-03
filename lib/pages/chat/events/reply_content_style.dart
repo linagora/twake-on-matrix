b8e92c22b04390b76c304b994f1a2b63026c8cd4
Julian KOUNE <jkoune@linagora.com>
2023-10-03 17:08:15 +0200

    TW-741: redesign reply content style

---
+++ lib/pages/chat/events/reply_content_style.dart
@@ -0,0 +1,53 @@
+import 'package:fluffychat/config/app_config.dart';
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+
+class ReplyContentStyle {
+  static const double fontSizeDisplayName = AppConfig.messageFontSize * 0.76;
+  static const double fontSizeDisplayContent = AppConfig.messageFontSize * 0.88;
+
+  static const EdgeInsets replyParentContainerPadding = EdgeInsets.only(
+    left: 4,
+    right: 8.0,
+    top: 8.0,
+    bottom: 8.0,
+  );
+
+  static BoxDecoration replyParentContainerDecoration(
+    BuildContext context,
+    bool ownMessage,
+  ) {
+    return BoxDecoration(
+      color: ownMessage
+          ? LinagoraRefColors.material().primary[95]
+          : Theme.of(context).colorScheme.surfaceTint.withOpacity(0.08),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+
+  static const double prefixBarWidth = 3.0;
+  static BoxDecoration prefixBarDecoration(BuildContext context) {
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(2),
+      color: Theme.of(context).colorScheme.primary,
+    );
+  }
+
+  static const double prefixAndDisplayNameSpacing = 6.0;
+  static TextStyle? displayNameTextStyle(BuildContext context) {
+    return Theme.of(context).textTheme.titleSmall?.copyWith(
+          color: Theme.of(context).colorScheme.onSurfaceVariant,
+          fontWeight: FontWeight.bold,
+          fontSize: fontSizeDisplayName,
+        );
+  }
+
+  static TextStyle? replyBodyTextStyle(BuildContext context) {
+    return Theme.of(context).textTheme.bodySmall?.copyWith(
+          color: LinagoraRefColors.material().neutral[50],
+          fontWeight: FontWeight.w500,
+          overflow: TextOverflow.ellipsis,
+          fontSize: fontSizeDisplayContent,
+        );
+  }
+}
