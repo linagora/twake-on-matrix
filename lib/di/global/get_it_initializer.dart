696501277c706d937d87b9aad8d96e818dc8e8ba
HuyNguyen <quanghnguyen@linagora.com>
2023-10-04 00:54:03 +0700

    TW-692: Create `UploadProfileInteractor`

--- lib/di/global/get_it_initializer.dart
+++ lib/di/global/get_it_initializer.dart
@@ -41,6 +41,7 @@ import 'package:fluffychat/domain/usecase/send_file_interactor.dart';
 import 'package:fluffychat/domain/usecase/send_file_on_web_interactor.dart';
 import 'package:fluffychat/domain/usecase/send_image_interactor.dart';
 import 'package:fluffychat/domain/usecase/send_images_interactor.dart';
+import 'package:fluffychat/domain/usecase/settings/upload_profile_interactor.dart';
 import 'package:fluffychat/event/twake_event_dispatcher.dart';
 import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:get_it/get_it.dart';
@@ -176,6 +177,9 @@ class GetItInitializer {
     );
     getIt.registerSingleton<TimelineSearchEventInteractor>(
       TimelineSearchEventInteractor(),
+    );
+    getIt.registerSingleton<UploadProfileInteractor>(
+      UploadProfileInteractor(),
     );
   }
 }
---
+++ lib/domain/app_state/settings/upload_profile_loading.dart
@@ -0,0 +1,8 @@
+import 'package:fluffychat/app_state/success.dart';
+
+class UploadProfileLoading extends Success {
+  const UploadProfileLoading();
+
+  @override
+  List<Object?> get props => [];
+}
