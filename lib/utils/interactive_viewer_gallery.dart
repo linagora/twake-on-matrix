f97629ef88f467d01d08c73f3c491ffe2b21826f
sherlock <quangkhai28999@gmail.com>
2023-10-03 17:16:45 +0700

    TW-730: add custom components to build animation when open to view image and video

---
+++ lib/utils/interactive_viewer_gallery.dart
@@ -0,0 +1,88 @@
+import 'package:fluffychat/utils/custom_disissible.dart';
+import 'package:flutter/material.dart';
+
+/// A callback for the [InteractiveViewerBoundary] that is called when the scale
+/// changed.
+typedef ScaleChanged = void Function(double scale);
+
+/// Builds an [InteractiveViewer] and provides callbacks that are called when a
+/// horizontal boundary has been hit.
+///
+/// The callbacks are called when an interaction ends by listening to the
+/// [InteractiveViewer.onInteractionEnd] callback.
+class InteractiveviewerGallery extends StatefulWidget {
+  const InteractiveviewerGallery({
+    super.key,
+    required this.itemBuilder,
+    this.maxScale = 2.5,
+    this.minScale = 1.0,
+    this.handleDragStart,
+    this.handleDragEnd,
+  });
+
+  /// The item content
+  final Widget itemBuilder;
+
+  final double maxScale;
+
+  final double minScale;
+
+  final Function(DragStartDetails dragStartDetails)? handleDragStart;
+
+  final Function(DragEndDetails dragEndDetails)? handleDragEnd;
+
+  @override
+  State<InteractiveviewerGallery> createState() =>
+      _InteractiveviewerGalleryState();
+}
+
+class _InteractiveviewerGalleryState extends State<InteractiveviewerGallery>
+    with SingleTickerProviderStateMixin {
+  TransformationController? _transformationController;
+
+  /// The controller to animate the transformation value of the
+  /// [InteractiveViewer] when it should reset.
+  late AnimationController _animationController;
+  Animation<Matrix4>? _animation;
+
+  /// `true` when an source is zoomed in to disable the [CustomDismissible].
+  bool _enableDismiss = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _transformationController = TransformationController();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    )
+      ..addListener(() {
+        _transformationController!.value =
+            _animation?.value ?? Matrix4.identity();
+      })
+      ..addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed && !_enableDismiss) {
+          setState(() {
+            _enableDismiss = true;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomDismissible(
+      onDismissed: () => Navigator.of(context).pop(),
+      enabled: _enableDismiss,
+      child: widget.itemBuilder,
+    );
+  }
+}
