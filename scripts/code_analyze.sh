6d126bf8a51c4a04d94f05dfe68042a9df81fa64
MinhDV <drminh2807@gmail.com>
2023-10-09 20:24:38 +0700

    chore: code analyzer unused checks

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ve
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 dart format --set-exit-if-changed lib/ test/
-flutter analyze
+dart run dependency_validator && flutter analyze
