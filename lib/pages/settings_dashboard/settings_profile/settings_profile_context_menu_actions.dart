840b6317bbe9691aa54ea8346b676e4253cf9432
HuyNguyen <quanghnguyen@linagora.com>
2023-10-10 14:14:15 +0700

    TW-780: Add context menu for settings profile in web

---
+++ lib/pages/settings_dashboard/settings_profile/settings_profile_context_menu_actions.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+enum SettingsProfileContextMenuActions {
+  edit,
+  delete;
+
+  String getTitle(BuildContext context) {
+    switch (this) {
+      case SettingsProfileContextMenuActions.edit:
+        return L10n.of(context)!.changeProfilePhoto;
+      case SettingsProfileContextMenuActions.delete:
+        return L10n.of(context)!.removeYourAvatar;
+    }
+  }
+
+  IconData getIcon() {
+    switch (this) {
+      case SettingsProfileContextMenuActions.edit:
+        return Icons.camera_alt_outlined;
+      case SettingsProfileContextMenuActions.delete:
+        return Icons.delete;
+    }
+  }
+}
