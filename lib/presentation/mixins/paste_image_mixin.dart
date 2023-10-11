65727b68cacd560f6f9e1615bd7fb2e92b79ede9
sherlock <quangkhai28999@gmail.com>
2023-10-11 15:28:42 +0700

    TW-731: remove copy/paste images in mobile for now because of load image in memory

--- lib/presentation/mixins/paste_image_mixin.dart
+++ lib/presentation/mixins/paste_image_mixin.dart
@@ -11,7 +11,6 @@ import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 mixin PasteImageMixin {
   Future<void> pasteImage(BuildContext context, Room room) async {
-    await Clipboard.instance.initReader();
     if (!(await Clipboard.instance.isReadableImageFormat())) {
       TwakeSnackBar.show(context, L10n.of(context)!.fileFormatNotSupported);
       Logs().e('PasteImageMixin::pasteImage(): not readable image format');
