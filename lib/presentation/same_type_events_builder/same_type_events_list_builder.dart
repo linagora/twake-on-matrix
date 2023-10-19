8e4725254b15ec17c519d89b01d369227a424c2c
HuyNguyen <quanghnguyen@linagora.com>
2023-10-19 12:21:35 +0700

    TW-826: Update `SameTypeEventsBuilder` and `SameTypeEventsController`

---
+++ lib/presentation/same_type_events_builder/same_type_events_list_builder.dart
@@ -0,0 +1,46 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/presentation/same_type_events_builder/same_type_events_controller.dart';
+import 'package:fluffychat/widgets/twake_components/twake_loading/center_loading_indicator.dart';
+import 'package:flutter/material.dart';
+
+class SameTypeEventsBuilder extends StatelessWidget {
+  final SameTypeEventsBuilderController controller;
+
+  /// The builder must return a sliver.
+  final Widget Function(BuildContext, Either<Failure, Success>, Widget?)
+      builder;
+
+  const SameTypeEventsBuilder({
+    Key? key,
+    required this.controller,
+    required this.builder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        ValueListenableBuilder(
+          valueListenable: controller.refreshing,
+          builder: (context, refreshing, child) => SliverToBoxAdapter(
+            child:
+                refreshing ? const CenterLoadingIndicator() : const SizedBox(),
+          ),
+        ),
+        ValueListenableBuilder(
+          valueListenable: controller.eventsNotifier,
+          builder: builder,
+        ),
+        ValueListenableBuilder(
+          valueListenable: controller.loadingMore,
+          builder: (context, loadingMore, child) => SliverToBoxAdapter(
+            child:
+                loadingMore ? const CenterLoadingIndicator() : const SizedBox(),
+          ),
+        )
+      ],
+    );
+  }
+}
