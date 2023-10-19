2e94ac41388c3a600e14201d8b06ab8a7755b319
HuyNguyen <quanghnguyen@linagora.com>
2023-10-19 12:23:35 +0700

    TW-826: Update UI for media, links page

--- lib/pages/chat_details/chat_details_page_view/media/chat_details_media_style.dart
+++ lib/pages/chat_details/chat_details_page_view/media/chat_details_media_style.dart
@@ -1,10 +1,22 @@
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:flutter/material.dart';
 
 class ChatDetailsMediaStyle {
+  static ResponsiveUtils responsive = getIt.get<ResponsiveUtils>();
+
   static const durationPadding = EdgeInsets.symmetric(
     horizontal: 4,
     vertical: 2,
   );
+
+  static int crossAxisCount(BuildContext context) {
+    if (responsive.isWebDesktop(context)) {
+      return 5;
+    } else {
+      return 3;
+    }
+  }
 
   static Decoration durationBoxDecoration(BuildContext context) =>
       ShapeDecoration(
