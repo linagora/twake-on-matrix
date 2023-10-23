b11c30457e5f3800340f3186fc78fc4180eedb35
MinhDV <drminh2807@gmail.com>
2023-10-23 10:19:30 +0700

    feat: new search part 1 - responsive

--- lib/pages/chat_list/chat_list_header_style.dart
+++ lib/pages/chat_list/chat_list_header_style.dart
@@ -1,6 +1,9 @@
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/utils/responsive/responsive_utils.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
 
 class ChatListHeaderStyle {
   static ResponsiveUtils responsive = getIt.get<ResponsiveUtils>();
@@ -16,4 +19,29 @@ class ChatListHeaderStyle {
   );
 
   static const EdgeInsetsDirectional paddingZero = EdgeInsetsDirectional.zero;
+
+  static InputDecoration searchInputDecoration(BuildContext context) {
+    return InputDecoration(
+      filled: true,
+      contentPadding: ChatListHeaderStyle.paddingZero,
+      fillColor: Theme.of(context).colorScheme.surface,
+      border: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(
+          ChatListHeaderStyle.searchRadiusBorder,
+        ),
+      ),
+      hintText: L10n.of(context)!.search,
+      hintStyle: Theme.of(context).textTheme.titleMedium?.copyWith(
+            color: LinagoraRefColors.material().neutral[60],
+          ),
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      prefixIcon: Icon(
+        Icons.search_outlined,
+        size: ChatListHeaderStyle.searchIconSize,
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+      suffixIcon: const SizedBox.shrink(),
+    );
+  }
 }
