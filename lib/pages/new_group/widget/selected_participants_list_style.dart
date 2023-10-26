a14f2673dc83777b889490f3f3d5490af473bdb4
sherlock <quangkhai28999@gmail.com>
2023-10-26 11:51:55 +0700

    TW-816: update selected participants to have chips

---
+++ lib/pages/new_group/widget/selected_participants_list_style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SelectedParticipantsListStyle {
+  static const EdgeInsetsDirectional paddingAll =
+      EdgeInsetsDirectional.only(top: 12.0, start: 16.0, end: 16.0);
+
+  static const double borderRadiusChip = 16.0;
+
+  static const EdgeInsetsDirectional labelChipPadding =
+      EdgeInsetsDirectional.only(
+    start: 8.0,
+    top: 2.0,
+    bottom: 2.0,
+  );
+
+  static const double avatarChipSize = 24.0;
+
+  static const EdgeInsetsDirectional contactPadding =
+      EdgeInsetsDirectional.only(
+    start: 16.0,
+    end: 16.0,
+    bottom: 8.0,
+  );
+}
