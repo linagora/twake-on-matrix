7d0d34f553dc0aba1c5d0aa1817d6b87460811fc
MinhDV <drminh2807@gmail.com>
2023-10-27 16:07:13 +0700

    TW-800 Search add empty view

--- lib/pages/chat_search/chat_search_style.dart
+++ lib/pages/chat_search/chat_search_style.dart
@@ -4,8 +4,20 @@ class ChatSearchStyle {
   static const EdgeInsetsGeometry inputPadding =
       EdgeInsetsDirectional.only(start: 8, top: 16, bottom: 16, end: 16);
 
-  static const EdgeInsetsGeometry itemPadding = EdgeInsetsDirectional.all(8);
+  static const EdgeInsetsGeometry itemMargin =
+      EdgeInsetsDirectional.symmetric(horizontal: 16);
+
+  static const EdgeInsetsGeometry itemPadding =
+      EdgeInsetsDirectional.only(end: 8);
 
   static const EdgeInsetsGeometry avatarPadding =
-      EdgeInsetsDirectional.only(end: 8);
+      EdgeInsetsDirectional.symmetric(horizontal: 8, vertical: 16);
+
+  static const EdgeInsetsGeometry emptyPadding = EdgeInsetsDirectional.all(16);
+
+  static const double emptyGap = 128.0;
+
+  static const double itemHeight = 90.0;
+
+  static const double itemBorderRadius = 12.0;
 }
