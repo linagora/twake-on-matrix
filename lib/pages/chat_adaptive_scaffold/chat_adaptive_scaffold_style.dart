1cd4a45f1e3618531f357c8737d8e030324d1000
MinhDV <drminh2807@gmail.com>
2023-10-30 14:31:25 +0700

    feat(newSearch): move style to file

---
+++ lib/pages/chat_adaptive_scaffold/chat_adaptive_scaffold_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ChatAdaptiveScaffoldStyle {
+  static const webPaddingRight = EdgeInsetsDirectional.only(end: 16);
+  static final borderRadius = BorderRadius.circular(16);
+}
