f72985958b1c6441af76dca3c1f111491c1e41fd
sherlock <quangkhai28999@gmail.com>
2023-10-31 22:32:52 +0700

    TW-746: refactor download file extension

--- lib/utils/matrix_sdk_extensions/download_file_extension.dart
+++ lib/utils/matrix_sdk_extensions/download_file_extension.dart
@@ -7,94 +7,139 @@ import 'package:matrix/matrix.dart';
 import 'package:path_provider/path_provider.dart';
 
 extension DownloadFileExtension on Event {
+  bool canContainAttachment() {
+    return [EventTypes.Message, EventTypes.Sticker].contains(type);
+  }
+
+  bool isSending() {
+    return status.isSending;
+  }
+
+  Uri? getAttachmentOrThumbnailMxcUrl(bool getThumbnail) {
+    return attachmentOrThumbnailMxcUrl(getThumbnail: getThumbnail);
+  }
+
+  bool isEncryptionDisabled(bool isEncrypted) {
+    return isEncrypted && !room.client.encryptionEnabled;
+  }
+
+  Future<FileInfo?> downloadOrRetrieveAttachment(
+    Uri mxcUrl,
+    String savePath,
+  ) async {
+    final database = room.client.database;
+    final attachment = await database?.getFileEntity(mxcUrl);
+
+    final mediaApi = getIt.get<MediaAPI>();
+    final downloadLink = mxcUrl.getDownloadLink(room.client);
+
+    if (attachment != null) {
+      return FileInfo(
+        filename,
+        attachment.path,
+        content.tryGet<int>('size') ?? attachment.lengthSync(),
+      );
+    }
+
+    final downloadResponse = await mediaApi.downloadFileInfo(
+      uriPath: downloadLink,
+      savePath: savePath,
+    );
+    if (downloadResponse.statusCode == 200) {
+      return FileInfo(
+        filename,
+        savePath,
+        content.tryGet<int>('size') ?? File(savePath).lengthSync(),
+        progressCallback: downloadResponse.onReceiveProgress,
+      );
+    }
+    throw ('getFileInfo: Download file $filename failed');
+  }
+
+  // Decrypt the file if it's encrypted.
+  Future<FileInfo?> decryptFile(
+    FileInfo? fileInfo,
+    Uri mxcUrl,
+    String decryptedPath, {
+    getThumbnail = false,
+  }) async {
+    final encryptedService = EncryptedService();
+    final fileMap = getThumbnail ? infoMap['thumbnail_file'] : content['file'];
+    if (!fileMap['key']['key_ops'].contains('decrypt')) {
+      throw ("getFileInfo: Missing 'decrypt' in 'key_ops'.");
+    }
+
+    final encryptedFile = EncryptedFileInfo.fromJson(fileMap);
+
+    if (!File(decryptedPath).existsSync()) {
+      final isSuccess = await encryptedService.decryptFile(
+        fileInfo: fileInfo!,
+        encryptedFileInfo: encryptedFile,
+        outputFile: File(decryptedPath),
+      );
+
+      if (!isSuccess) {
+        throw ('getFileInfo: Unable to decrypt file');
+      }
+    }
+
+    return FileInfo(
+      body,
+      decryptedPath,
+      content.tryGet<int>('size') ?? File(decryptedPath).lengthSync(),
+      progressCallback: fileInfo?.progressCallback,
+    );
+  }
+
   Future<FileInfo?> getFileInfo({
     getThumbnail = false,
   }) async {
-    if (![EventTypes.Message, EventTypes.Sticker].contains(type)) {
+    if (!canContainAttachment()) {
       throw ("getFileInfo: This event has the type '$type' and so it can't contain an attachment.");
     }
 
-    if (status.isSending) {
+    if (isSending()) {
       final localFile = room.sendingFilePlaceholders[eventId];
       if (localFile != null) return FileInfo.fromMatrixFile(localFile);
     }
 
-    final mxcUrl = attachmentOrThumbnailMxcUrl(getThumbnail: getThumbnail);
+    final mxcUrl = getAttachmentOrThumbnailMxcUrl(getThumbnail);
     if (mxcUrl == null) {
       throw "getFileInfo: This event hasn't any attachment or thumbnail.";
     }
+
     final isEncrypted =
         getThumbnail ? isThumbnailEncrypted : isAttachmentEncrypted;
-    if (isEncrypted && !room.client.encryptionEnabled) {
+    if (isEncryptionDisabled(isEncrypted)) {
       throw ('getFileInfo: Encryption is not enabled in your Client.');
     }
+
     final tempDirectory = await _getFileStoreDirectory();
+
     final decryptedPath =
         '$tempDirectory/${Uri.encodeComponent(mxcUrl.toString())}-decrypted';
-    if (await File(decryptedPath).exists()) {
+    if (File(decryptedPath).existsSync()) {
       return FileInfo(
         body,
         decryptedPath,
-        content.tryGet<int>('size') ?? await File(decryptedPath).length(),
+        content.tryGet<int>('size') ?? File(decryptedPath).lengthSync(),
       );
     }
 
-    final database = room.client.database;
-    final attachment = await database?.getFileEntity(mxcUrl);
-    FileInfo? fileInfo;
+    final fileInfo = await downloadOrRetrieveAttachment(
+      mxcUrl,
+      '$tempDirectory/${Uri.encodeComponent(mxcUrl.toString())}',
+    );
 
-    final savePath = '$tempDirectory/${Uri.encodeComponent(mxcUrl.toString())}';
-    final mediaApi = getIt.get<MediaAPI>();
-
-    final downloadLink = mxcUrl.getDownloadLink(room.client);
-    if (attachment == null) {
-      final downloadResponse = await mediaApi.downloadFileInfo(
-        uriPath: downloadLink,
-        savePath: savePath,
-      );
-      if (downloadResponse.statusCode == 200) {
-        fileInfo = FileInfo(
-          filename,
-          savePath,
-          content.tryGet<int>('size') ?? await File(savePath).length(),
-          progressCallback: downloadResponse.onReceiveProgress,
-        );
-      }
-    } else {
-      fileInfo = FileInfo(
-        filename,
-        attachment.path,
-        content.tryGet<int>('size') ?? await attachment.length(),
-      );
-    }
-
-    final encryptedService = EncryptedService();
-    // Decrypt the file
-    if (isEncrypted) {
-      final fileMap =
-          getThumbnail ? infoMap['thumbnail_file'] : content['file'];
-      if (!fileMap['key']['key_ops'].contains('decrypt')) {
-        throw ("getFileInfo: Missing 'decrypt' in 'key_ops'.");
-      }
-      final encryptedFile = EncryptedFileInfo.fromJson(fileMap);
-      if (!await File(decryptedPath).exists()) {
-        final isSuccess = await encryptedService.decryptFile(
-          fileInfo: fileInfo!,
-          encryptedFileInfo: encryptedFile,
-          outputFile: File(decryptedPath),
-        );
-
-        if (!isSuccess) {
-          throw ('getFileInfo: Unable to decrypt file');
-        }
-      }
-      fileInfo = FileInfo(
-        body,
+    if (isEncrypted && fileInfo != null) {
+      return await decryptFile(
+        fileInfo,
+        mxcUrl,
         decryptedPath,
-        content.tryGet<int>('size') ?? await File(decryptedPath).length(),
-        progressCallback: fileInfo?.progressCallback,
+        getThumbnail: getThumbnail,
       );
     }
+
     return fileInfo;
   }
 
