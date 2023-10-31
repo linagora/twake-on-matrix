a5ec13d26e5ccde84c35d9646c1c05fa54ecc0c4
Julian KOUNE <jkoune@linagora.com>
2023-10-31 15:17:56 +0100

    TW-877: Encrypted Icon before display name

--- lib/pages/chat_list/chat_list_item_title.dart
+++ lib/pages/chat_list/chat_list_item_title.dart
@@ -2,11 +2,13 @@ import 'package:fluffychat/pages/chat_list/chat_list_item_title_style.dart';
 import 'package:fluffychat/presentation/decorators/chat_list/title_text_style_decorator/title_text_style_view.dart';
 import 'package:fluffychat/presentation/mixins/chat_list_item_mixin.dart';
 import 'package:fluffychat/pages/chat_list/chat_list_item_style.dart';
+import 'package:fluffychat/resource/image_paths.dart';
 import 'package:fluffychat/utils/date_time_extension.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/utils/room_status_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 import 'package:matrix/matrix.dart';
 
@@ -29,6 +31,16 @@ class ChatListItemTitle extends StatelessWidget with ChatListItemMixin {
             children: [
               Row(
                 children: [
+                  if (room.encrypted)
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 4, top: 2, bottom: 2),
+                      child: SvgPicture.asset(
+                        ImagePaths.icEncrypted,
+                        width: 20,
+                        height: 20,
+                      ),
+                    ),
                   Flexible(
                     child: Text(
                       displayName,
