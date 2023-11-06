caf7d524cb459d1b4bc7b8522f83f246a9a939b9
HuyNguyen <quanghnguyen@linagora.com>
2023-11-07 01:44:24 +0700

    TW-894: Create `ChatGetPinnedEventsInteractor` to get pinned message in chat

---
+++ lib/domain/app_state/room/chat_get_pinned_events_state.dart
@@ -0,0 +1,39 @@
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/initial.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:matrix/matrix.dart';
+
+class ChatGetPinnedEventsInitial extends Initial {
+  @override
+  List<Object?> get props => [];
+}
+
+class ChatGetPinnedEventsLoading extends Success {
+  @override
+  List<Object?> get props => [];
+}
+
+class ChatGetPinnedEventsSuccess extends Success {
+  final List<Event?> pinnedEvents;
+
+  const ChatGetPinnedEventsSuccess({
+    required this.pinnedEvents,
+  });
+
+  @override
+  List<Object?> get props => [pinnedEvents];
+}
+
+class ChatGetPinnedEventsNoResult extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class ChatGetPinnedEventsFailure extends Failure {
+  final dynamic exception;
+
+  const ChatGetPinnedEventsFailure({required this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
