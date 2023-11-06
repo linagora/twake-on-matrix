e392127a7ac86d5e542bdd941ee9ab93818329c1
HuyNguyen <quanghnguyen@linagora.com>
2023-11-07 01:44:24 +0700

    TW-894: Create `ChatGetPinnedEventsInteractor` to get pinned message in chat

---
+++ lib/domain/usecase/room/chat_get_pinned_events_interactor.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/domain/app_state/room/chat_get_pinned_events_state.dart';
+import 'package:matrix/matrix.dart';
+
+class ChatGetPinnedEventsInteractor {
+  Stream<Either<Failure, Success>> execute({
+    required Room room,
+  }) async* {
+    Logs().d(
+      "ChatGetPinnedEventsInteractor()::execute()::roomId: ${room.id}",
+    );
+    yield Right(ChatGetPinnedEventsLoading());
+    try {
+      final pinnedEvents = room.pinnedEventIds;
+      final completers = pinnedEvents.map<Completer<Event?>>((id) {
+        final completer = Completer<Event?>();
+        room.getEventById(id).then((value) => completer.complete(value));
+        return completer;
+      });
+      final result = await Future.wait(
+        completers.map((event) => event.future).toList(),
+      );
+      if (result.isEmpty) {
+        yield Left(ChatGetPinnedEventsNoResult());
+        return;
+      } else {
+        Logs().d(
+          "ChatGetPinnedEventsInteractor()::execute()::result: ${result.length}",
+        );
+        result.sort((currentEvent, nextEvent) {
+          final currentPinnedTime =
+              currentEvent?.originServerTs ?? DateTime.now();
+          final nextPinnedTime = nextEvent?.originServerTs ?? DateTime.now();
+          return currentPinnedTime.compareTo(nextPinnedTime);
+        });
+        yield Right(ChatGetPinnedEventsSuccess(pinnedEvents: result));
+        return;
+      }
+    } on MatrixException catch (exception) {
+      Logs().e(
+        "ChatGetPinnedEventsInteractor()::execute()::MatrixException: ${exception.error}",
+      );
+      yield Left(ChatGetPinnedEventsFailure(exception: exception));
+    }
+  }
+}
