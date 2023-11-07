78df106e17ddab16e7566feb8b39d9bbad55f278
HuyNguyen <quanghnguyen@linagora.com>
2023-11-07 16:27:40 +0700

    TW-887: Refactor `Contact` model

--- lib/domain/model/extensions/contact/tom_contact_extension.dart
+++ lib/domain/model/extensions/contact/tom_contact_extension.dart
@@ -5,7 +5,7 @@ import 'package:fluffychat/domain/model/contact/tom_contact.dart';
 extension TomContactExtension on TomContact {
   Contact toContact(ContactStatus status) {
     return Contact(
-      emails: mail != null ? {mail!} : {},
+      email: mail,
       displayName: displayName ?? uid,
       phoneNumber: phoneNumber,
       matrixId: address,
