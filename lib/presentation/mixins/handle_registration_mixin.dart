84f091658c7127187b2bf36cb503b95c3dbcb23c
sherlock <quangkhai28999@gmail.com>
2023-11-08 16:06:30 +0700

    fixup! TW-903: handle jump to other apps in registration success page

--- lib/presentation/mixins/handle_registration_mixin.dart
+++ lib/presentation/mixins/handle_registration_mixin.dart
@@ -2,10 +2,10 @@ import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/presentation/model/registration_done_model.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/widgets/twake_app.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 import 'package:matrix/matrix.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 mixin HandleRegistrationMixin {
   void handleRegistrationDone(String uri) async {
@@ -16,11 +16,7 @@ mixin HandleRegistrationMixin {
       TwakeApp.router.go('/home', extra: uri);
     } else {
       try {
-        if (await canLaunchUrlString(registrationDoneModel.openApp!)) {
-          launchUrl(Uri.parse(registrationDoneModel.openApp!));
-        } else {
-          _openAppInStore(registrationDoneModel);
-        }
+        await launchUrl(Uri.parse(registrationDoneModel.openApp!));
       } catch (e) {
         _openAppInStore(registrationDoneModel);
       }
@@ -35,7 +31,13 @@ mixin HandleRegistrationMixin {
       }
     } else {
       if (registrationDoneModel.playStoreUrl != null) {
-        await launchUrl(Uri.parse(registrationDoneModel.playStoreUrl!));
+        final browser = ChromeSafariBrowser();
+        browser.open(
+          url: WebUri(registrationDoneModel.playStoreUrl!),
+          settings: ChromeSafariBrowserSettings(
+            shareState: CustomTabsShareState.SHARE_STATE_ON,
+          ),
+        );
       }
     }
   }
