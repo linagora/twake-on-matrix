bcb0d0814b796f8f36415fc10f7426b4d9b4a432
HuyNguyen <quanghnguyen@linagora.com>
2023-11-09 22:47:00 +0700

    Hotfix: Create popup items specific to different routers

--- lib/widgets/mixins/popup_menu_widget_mixin.dart
+++ lib/widgets/mixins/popup_menu_widget_mixin.dart
@@ -5,7 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
 
 mixin PopupMenuWidgetMixin {
-  Widget popupItem(
+  Widget popupItemByTwakeAppRouter(
     BuildContext context,
     String nameAction, {
     IconData? iconAction,
@@ -26,36 +26,90 @@ mixin PopupMenuWidgetMixin {
         }
         onCallbackAction!.call();
       },
-      child: Padding(
-        padding: padding ?? const EdgeInsetsDirectional.all(12),
-        child: SizedBox(
-          child: Row(
-            children: [
-              if (iconAction != null)
-                Icon(
-                  iconAction,
-                  size: iconSize ?? 20,
-                  color: colorIcon ?? Colors.black,
-                ),
-              if (imagePath != null)
-                SvgPicture.asset(
-                  imagePath,
-                  width: iconSize ?? 20,
-                  height: iconSize ?? 20,
-                  fit: BoxFit.fill,
-                ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Text(
-                  nameAction,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .copyWith(color: LinagoraSysColors.material().onSurface),
-                ),
+      child: _popupItemBuilder(
+        context,
+        nameAction,
+        iconAction: iconAction,
+        imagePath: imagePath,
+        colorIcon: colorIcon,
+        iconSize: iconSize,
+        styleName: styleName,
+        padding: padding,
+      ),
+    );
+  }
+
+  Widget popupItemByNavigator(
+    BuildContext context,
+    String nameAction, {
+    IconData? iconAction,
+    String? imagePath,
+    Color? colorIcon,
+    double? iconSize,
+    TextStyle? styleName,
+    EdgeInsets? padding,
+    OnTapIconButtonCallbackAction? onCallbackAction,
+    bool isClearCurrentPage = true,
+  }) {
+    return InkWell(
+      onTap: () {
+        if (isClearCurrentPage) {
+          Navigator.pop(context);
+        }
+        onCallbackAction!.call();
+      },
+      child: _popupItemBuilder(
+        context,
+        nameAction,
+        iconAction: iconAction,
+        imagePath: imagePath,
+        colorIcon: colorIcon,
+        iconSize: iconSize,
+        styleName: styleName,
+        padding: padding,
+      ),
+    );
+  }
+
+  Widget _popupItemBuilder(
+    BuildContext context,
+    String nameAction, {
+    IconData? iconAction,
+    String? imagePath,
+    Color? colorIcon,
+    double? iconSize,
+    TextStyle? styleName,
+    EdgeInsets? padding,
+  }) {
+    return Padding(
+      padding: padding ?? const EdgeInsetsDirectional.all(12),
+      child: SizedBox(
+        child: Row(
+          children: [
+            if (iconAction != null)
+              Icon(
+                iconAction,
+                size: iconSize ?? 20,
+                color: colorIcon ?? Colors.black,
               ),
-            ],
-          ),
+            if (imagePath != null)
+              SvgPicture.asset(
+                imagePath,
+                width: iconSize ?? 20,
+                height: iconSize ?? 20,
+                fit: BoxFit.fill,
+              ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Text(
+                nameAction,
+                style: styleName ??
+                    Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: LinagoraSysColors.material().onSurface,
+                        ),
+              ),
+            ),
+          ],
         ),
       ),
     );
