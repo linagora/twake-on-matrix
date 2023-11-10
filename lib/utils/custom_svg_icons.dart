ef0bb29c1d74fbcbb2f5cffdbae52f7e87697a53
sherlock <quangkhai28999@gmail.com>
2023-11-10 17:21:14 +0700

    TW-931: update assets folder

--- lib/utils/custom_svg_icons.dart
+++ lib/utils/custom_svg_icons.dart
@@ -1,6 +1,3 @@
 class CustomSVGIcons {
   static String baseAssetUrl = 'assets/';
-
-  static String titleChatListLight = '${baseAssetUrl}twake_light.svg';
-  static String titleChatListDark = '${baseAssetUrl}twake_dark.svg';
 }
