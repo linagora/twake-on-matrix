8a097ae068543fce6a3bfde16c41ca34b215bd41
HuyNguyen <quanghnguyen@linagora.com>
2023-11-11 01:14:07 +0700

    TW-886: Handle request contacts permission

--- lib/domain/contact_manager/contacts_manager.dart
+++ lib/domain/contact_manager/contacts_manager.dart
@@ -4,8 +4,12 @@ import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/app_state/contact/get_contacts_state.dart';
 import 'package:fluffychat/domain/model/contact/contact.dart';
+import 'package:fluffychat/domain/model/contact/contacts_manager_agruments.dart';
 import 'package:fluffychat/domain/usecase/get_all_contacts_interactor.dart';
+import 'package:fluffychat/utils/permission_service.dart';
+import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:matrix/matrix.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 enum SyncContactsState {
   initial,
@@ -13,10 +17,23 @@ enum SyncContactsState {
   synchronized,
 }
 
+enum DisplayWaringContactsBannerState {
+  show,
+  hide,
+}
+
 class ContactsManager {
   SyncContactsState _syncContactsState = SyncContactsState.initial;
 
+  DisplayWaringContactsBannerState _displayWaringContactsBannerState =
+      DisplayWaringContactsBannerState.show;
+
+  bool _isNotDisplayWarningAgainCacheMemory = false;
+
   final _getAllContactsInteractor = getIt.get<GetAllContactsInteractor>();
+
+  final PermissionHandlerService permissionHandlerService =
+      PermissionHandlerService();
 
   Set<Contact> _tomContacts = {};
 
@@ -27,22 +44,43 @@ class ContactsManager {
   Set<Contact> get phonebookContacts => _phonebookContacts;
 
   bool get firstSynchronizing =>
-      _syncContactsState == SyncContactsState.synchronizing;
+      _syncContactsState == SyncContactsState.synchronizing ||
+      _syncContactsState == SyncContactsState.initial;
 
   bool get firstSynchronized =>
       _syncContactsState == SyncContactsState.synchronized;
 
-  StreamSubscription? contactsStreamSubscription;
+  StreamController<ContactsManagerArguments> contactsStream =
+      StreamController<ContactsManagerArguments>.broadcast();
 
-  void initialSynchronizeContacts() {
+  void initialSynchronizeContacts() async {
+    if (PlatformInfos.isMobile && !_isNotDisplayWarningAgainCacheMemory) {
+      await _handleRequestContactsPermission();
+    }
     if (!firstSynchronized) {
       _syncContactsState = SyncContactsState.synchronizing;
       _getAllContacts();
+    } else {
+      _handleContactsHasDataChange();
+    }
+  }
+
+  Future<void> _handleRequestContactsPermission() async {
+    final currentContactsPermissionStatus =
+        await permissionHandlerService.requestContactsPermissionActions();
+    if (currentContactsPermissionStatus == PermissionStatus.granted) {
+      _isNotDisplayWarningAgainCacheMemory = true;
+      _displayWaringContactsBannerState = DisplayWaringContactsBannerState.hide;
+    } else {
+      if (!_isNotDisplayWarningAgainCacheMemory) {
+        _displayWaringContactsBannerState =
+            DisplayWaringContactsBannerState.show;
+      }
     }
   }
 
   void _getAllContacts() {
-    contactsStreamSubscription = _getAllContactsInteractor
+    _getAllContactsInteractor
         .execute(limit: AppConfig.maxFetchContacts)
         .listen((event) {
       event.fold(
@@ -51,7 +89,7 @@ class ContactsManager {
           if (success is GetContactsSuccess) {
             _tomContacts = success.tomContacts.toSet();
             _phonebookContacts = {};
-            _syncContactsState = SyncContactsState.synchronized;
+            _handleContactsHasDataChange();
             Logs().d(
               "ContactManagerMixin()::getAllContacts(): TomContacts: ${_tomContacts.length}",
             );
@@ -62,5 +100,21 @@ class ContactsManager {
         },
       );
     });
+  }
+
+  void closeContactsWarningBanner() {
+    _displayWaringContactsBannerState = DisplayWaringContactsBannerState.hide;
+    _isNotDisplayWarningAgainCacheMemory = true;
+    _handleContactsHasDataChange();
+  }
+
+  void _handleContactsHasDataChange() {
+    _syncContactsState = SyncContactsState.synchronized;
+    contactsStream.add(
+      ContactsManagerArguments(
+        tomContacts: tomContacts,
+        displayWaringContactsBannerState: _displayWaringContactsBannerState,
+      ),
+    );
   }
 }
