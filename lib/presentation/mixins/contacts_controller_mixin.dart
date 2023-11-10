61de80289ae60c7ba1136f1dfeeb761a89e61d9c
HuyNguyen <quanghnguyen@linagora.com>
2023-11-11 01:16:00 +0700

    TW-886: Handle logic and UI for request contacts permission

--- lib/presentation/mixins/contacts_controller_mixin.dart
+++ lib/presentation/mixins/contacts_controller_mixin.dart
@@ -2,6 +2,7 @@ import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/contact_manager/contacts_manager.dart';
 import 'package:fluffychat/domain/model/contact/contact_type.dart';
+import 'package:fluffychat/domain/model/contact/contacts_manager_agruments.dart';
 import 'package:fluffychat/domain/model/extensions/contact/contacts_extension.dart';
 import 'package:fluffychat/presentation/extensions/contact/presentation_contact_extension.dart';
 import 'package:fluffychat/presentation/model/presentation_contact.dart';
@@ -19,6 +20,9 @@ mixin class ContactsControllerMixin {
   final ValueNotifier<List<PresentationContact>> presentationContactNotifier =
       ValueNotifier([]);
 
+  final ValueNotifier<bool> isShowContactsWarningBannerNotifier =
+      ValueNotifier(false);
+
   final FocusNode searchFocusNode = FocusNode();
 
   final Debouncer _debouncer = Debouncer(
@@ -26,13 +30,14 @@ mixin class ContactsControllerMixin {
     initialValue: '',
   );
 
-  final contactManager = getIt.get<ContactsManager>();
+  final contactsManager = getIt.get<ContactsManager>();
 
   bool get isExternalContact =>
       textEditingController.text.isValidMatrixId &&
       textEditingController.text.startsWith("@");
 
   void initialFetchContacts() async {
+    _listenContactsDataChange();
     presentationContactNotifier.addListener(() {
       Logs().d(
         'ContactManagerMixin()::initSearchContacts: ${presentationContactNotifier.value.length}',
@@ -45,19 +50,12 @@ mixin class ContactsControllerMixin {
     _debouncer.values.listen((keyword) {
       _handleSearchContacts(keyword);
     });
-    if (!contactManager.firstSynchronized) {
-      contactManager.initialSynchronizeContacts();
-      contactManager.contactsStreamSubscription?.onDone(() {
-        _handlePresentationContactChange();
-      });
-    } else {
-      _handlePresentationContactChange();
-    }
+    contactsManager.initialSynchronizeContacts();
   }
 
   void _handleSearchContacts(String keyword) {
     final contactsMatched =
-        contactManager.tomContacts.toList().searchContacts(keyword);
+        contactsManager.tomContacts.toList().searchContacts(keyword);
     if (contactsMatched.isEmpty && isExternalContact) {
       _handleSearchExternalContacts(keyword);
     } else {
@@ -77,9 +75,32 @@ mixin class ContactsControllerMixin {
   }
 
   void _handlePresentationContactChange() {
-    presentationContactNotifier.value = contactManager.tomContacts
+    presentationContactNotifier.value = contactsManager.tomContacts
         .expand((contact) => contact.toPresentationContacts())
         .toList();
+  }
+
+  void _handleContactsPermissionChange(ContactsManagerArguments arg) {
+    isShowContactsWarningBannerNotifier.value =
+        arg.displayWaringContactsBannerState ==
+            DisplayWaringContactsBannerState.show;
+  }
+
+  void closeContactsWarningBanner() {
+    contactsManager.closeContactsWarningBanner();
+  }
+
+  void _listenContactsDataChange() {
+    contactsManager.contactsStream.stream.listen((arg) {
+      Logs().d(
+        "ContactManagerMixin()::_listenContactsDataChange(): TomContacts - ${arg.tomContacts}",
+      );
+      Logs().d(
+        "ContactManagerMixin()::_listenContactsDataChange(): displayWaringContactsBannerState - ${arg.displayWaringContactsBannerState}",
+      );
+      _handlePresentationContactChange();
+      _handleContactsPermissionChange(arg);
+    });
   }
 
   void openSearchBar() {
