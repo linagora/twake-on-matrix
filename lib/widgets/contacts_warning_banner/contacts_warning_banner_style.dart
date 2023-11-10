60b4f5af533a0ef0eb623dcc09c1b527fc15c662
HuyNguyen <quanghnguyen@linagora.com>
2023-11-11 01:15:23 +0700

    TW-886: Create `ContactsWarningBannerView`

---
+++ lib/widgets/contacts_warning_banner/contacts_warning_banner_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ContactsWarningBannerStyle {
+  static const double borderWarningBanner = 16.0;
+  static const double borderHoverButtonWaningBanner = 100.0;
+
+  static EdgeInsets marginWarningBanner = const EdgeInsets.symmetric(
+    horizontal: 16,
+    vertical: 8,
+  );
+
+  static EdgeInsets paddingWarningBanner = const EdgeInsets.all(16);
+
+  static EdgeInsets paddingForContentBanner = const EdgeInsets.only(bottom: 16);
+
+  static EdgeInsetsDirectional marginButtonWarningBanner =
+      const EdgeInsetsDirectional.symmetric(
+    vertical: 10.0,
+    horizontal: 24.0,
+  );
+
+  static EdgeInsets paddingRightButton = const EdgeInsets.only(
+    right: 10,
+  );
+}
