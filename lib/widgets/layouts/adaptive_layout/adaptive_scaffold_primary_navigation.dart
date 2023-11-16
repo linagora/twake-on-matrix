effa6e9b86be86ee738aca0b53a5f20d5a263f23
HuyNguyen <quanghnguyen@linagora.com>
2023-11-16 10:43:23 +0700

    TW-909: Remove popup menu when click avt

--- lib/widgets/layouts/adaptive_layout/adaptive_scaffold_primary_navigation.dart
+++ lib/widgets/layouts/adaptive_layout/adaptive_scaffold_primary_navigation.dart
@@ -1,25 +1,21 @@
 import 'dart:async';
 
 import 'package:fluffychat/event/twake_inapp_event_types.dart';
-import 'package:fluffychat/pages/chat_list/client_chooser_button.dart';
 import 'package:fluffychat/widgets/layouts/adaptive_layout/adaptive_scaffold_primary_navigation_view.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class AdaptiveScaffoldPrimaryNavigation extends StatefulWidget {
   final List<NavigationRailDestination> getNavigationRailDestinations;
   final int? selectedIndex;
   final Function(int)? onDestinationSelected;
-  final Function(Object)? onSelected;
 
   const AdaptiveScaffoldPrimaryNavigation({
     super.key,
     required this.getNavigationRailDestinations,
     this.selectedIndex,
     this.onDestinationSelected,
-    this.onSelected,
   });
 
   @override
@@ -36,21 +32,6 @@ class _AdaptiveScaffoldPrimaryNavigationState
   StreamSubscription? onAccountDataSubscription;
 
   Client get client => Matrix.of(context).client;
-
-  List<PopupMenuEntry<Object>> _bundleMenuItems(BuildContext context) {
-    return <PopupMenuEntry<Object>>[
-      PopupMenuItem(
-        value: SettingsAction.settings,
-        child: Row(
-          children: [
-            const Icon(Icons.settings_outlined),
-            const SizedBox(width: 18),
-            Text(L10n.of(context)!.settings),
-          ],
-        ),
-      ),
-    ];
-  }
 
   void _getCurrentProfile(Client client) async {
     final profile = await client.getProfileFromUserId(
@@ -93,9 +74,7 @@ class _AdaptiveScaffoldPrimaryNavigationState
       getNavigationRailDestinations: widget.getNavigationRailDestinations,
       selectedIndex: widget.selectedIndex,
       onDestinationSelected: widget.onDestinationSelected,
-      onSelected: widget.onSelected,
       profileNotifier: profileNotifier,
-      itemBuilder: _bundleMenuItems,
     );
   }
 }
