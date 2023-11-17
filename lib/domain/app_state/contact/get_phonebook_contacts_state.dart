ff8d238f79552a155b816ab5e7624d7650b20b00
MinhDV <drminh2807@gmail.com>
2023-11-17 09:55:16 +0700

    TW-888 Create `PhonebookContact` datasource

---
+++ lib/domain/app_state/contact/get_phonebook_contacts_state.dart
@@ -0,0 +1,38 @@
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/initial.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/domain/model/contact/contact.dart';
+
+class GetPhonebookContactsInitial extends Initial {
+  const GetPhonebookContactsInitial() : super();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class GetPhonebookContactsLoading extends Success {
+  final int progress;
+
+  const GetPhonebookContactsLoading({required this.progress});
+
+  @override
+  List<Object?> get props => [progress];
+}
+
+class GetPhonebookContactsSuccess extends Success {
+  final List<Contact> contacts;
+
+  const GetPhonebookContactsSuccess({required this.contacts});
+
+  @override
+  List<Object?> get props => [contacts];
+}
+
+class GetPhonebookContactsFailure extends Failure {
+  final dynamic exception;
+
+  const GetPhonebookContactsFailure({required this.exception});
+
+  @override
+  List<Object?> get props => [exception];
+}
