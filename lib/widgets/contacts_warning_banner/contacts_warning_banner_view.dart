251af5b643c85e66bba6b43a90c1071af04e55fb
HuyNguyen <quanghnguyen@linagora.com>
2023-11-17 10:22:37 +0700

    TW-888: Refactor `ContactsManager`

--- lib/widgets/contacts_warning_banner/contacts_warning_banner_view.dart
+++ lib/widgets/contacts_warning_banner/contacts_warning_banner_view.dart
@@ -1,5 +1,3 @@
-import 'package:fluffychat/di/global/get_it_initializer.dart';
-import 'package:fluffychat/domain/contact_manager/contacts_manager.dart';
 import 'package:fluffychat/presentation/enum/contacts/warning_contacts_banner_enum.dart';
 import 'package:fluffychat/widgets/contacts_warning_banner/contacts_warning_banner_style.dart';
 import 'package:fluffychat/widgets/twake_components/twake_text_button.dart';
@@ -9,16 +7,19 @@ import 'package:flutter/material.dart';
 class ContactsWarningBannerView extends StatelessWidget {
   final ValueNotifier<WarningContactsBannerState> warningBannerNotifier;
   final bool isShowMargin;
+  final Function()? closeContactsWarningBanner;
+  final Function()? goToSettingsForPermissionActions;
 
   const ContactsWarningBannerView({
     super.key,
     required this.warningBannerNotifier,
     this.isShowMargin = true,
+    this.closeContactsWarningBanner,
+    this.goToSettingsForPermissionActions,
   });
 
   @override
   Widget build(BuildContext context) {
-    final contactsManager = getIt.get<ContactsManager>();
     return ValueListenableBuilder(
       valueListenable: warningBannerNotifier,
       builder: (context, state, child) {
@@ -53,7 +54,7 @@ class ContactsWarningBannerView extends StatelessWidget {
                     Padding(
                       padding: ContactsWarningBannerStyle.paddingRightButton,
                       child: TwakeTextButton(
-                        onTap: contactsManager.closeContactsWarningBanner,
+                        onTap: closeContactsWarningBanner,
                         message: L10n.of(context)!.notNow,
                         borderHover: ContactsWarningBannerStyle
                             .borderHoverButtonWaningBanner,
@@ -69,7 +70,7 @@ class ContactsWarningBannerView extends StatelessWidget {
                       message: L10n.of(context)!.allow,
                       borderHover: ContactsWarningBannerStyle
                           .borderHoverButtonWaningBanner,
-                      onTap: contactsManager.goToSettingsForPermissionActions,
+                      onTap: goToSettingsForPermissionActions,
                       styleMessage:
                           Theme.of(context).textTheme.labelLarge?.copyWith(
                                 color: Theme.of(context).colorScheme.onPrimary,
