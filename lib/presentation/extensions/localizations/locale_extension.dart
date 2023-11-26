cb600b24dda301d7b05bd9d317669038cc23d5ec
HuyNguyen <quanghnguyen@linagora.com>
2023-11-26 13:39:18 +0700

    TW-1032: Trigger rebuild MaterialApp when changing locale

--- lib/presentation/extensions/localizations/locale_extension.dart
+++ lib/presentation/extensions/localizations/locale_extension.dart
@@ -1,18 +1,22 @@
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 
 extension LocaleExtension on Locale {
   String getLanguageNameByCurrentLocale(BuildContext context) {
     switch (languageCode) {
       case 'fr':
-        return L10n.of(context)!.languageFrench;
+        return LocaleNames.of(context)!.nameOf('fr') ??
+            L10n.of(context)!.languageFrench;
       case 'en':
-        return L10n.of(context)!.languageEnglish;
+        return LocaleNames.of(context)!.nameOf('en') ??
+            L10n.of(context)!.languageEnglish;
       case 'vi':
-        return L10n.of(context)!.languageVietnamese;
+        return LocaleNames.of(context)!.nameOf('vi') ??
+            L10n.of(context)!.languageVietnamese;
       case 'ru':
-        return L10n.of(context)!.languageRussian;
-      case 'ar':
+        return LocaleNames.of(context)!.nameOf('ru') ??
+            L10n.of(context)!.languageRussian;
       default:
         return '';
     }
