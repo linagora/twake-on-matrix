f196456c88eb5e39879053497bdd26c2f7c0790d
HuyNguyen <quanghnguyen@linagora.com>
2023-11-26 13:35:03 +0700

    TW-1032: Create `LanguageCodeConstants`

---
+++ lib/config/localizations/language_code_constants.dart
@@ -0,0 +1,6 @@
+class LanguageCodeConstants {
+  static const String english = 'en';
+  static const String french = 'fr';
+  static const String vietnamese = 'vi';
+  static const String russian = 'ru';
+}
