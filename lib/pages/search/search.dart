961244658ed26a1aefa1ea66eaa046f2ac1673ca
sherlock <quangkhai28999@gmail.com>
2023-11-29 15:32:49 +0700

    TW-989: add server search to global search

--- lib/pages/search/search.dart
+++ lib/pages/search/search.dart
@@ -6,14 +6,18 @@ import 'package:fluffychat/domain/app_state/search/search_state.dart';
 import 'package:fluffychat/domain/usecase/search/pre_search_recent_contacts_interactor.dart';
 import 'package:fluffychat/pages/search/search_contacts_and_chats_controller.dart';
 import 'package:fluffychat/pages/search/search_view.dart';
+import 'package:fluffychat/pages/search/server_search_controller.dart';
 import 'package:fluffychat/presentation/mixins/comparable_presentation_search_mixin.dart';
 import 'package:fluffychat/presentation/model/presentation_contact_constant.dart';
 import 'package:fluffychat/presentation/model/search/presentation_search.dart';
 import 'package:fluffychat/utils/dialog/twake_dialog.dart';
 import 'package:fluffychat/utils/extension/build_context_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:fluffychat/utils/string_extension.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart' hide SearchController;
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
@@ -32,15 +36,46 @@ class SearchController extends State<Search>
   static const int limitPrefetchedRecentChats = 3;
   static const int limitSearchingPrefetchedRecentContacts = 30;
   static const int limitPrefetchedRecentContacts = 5;
+  static const _prefixLengthHighlight = 20;
 
   SearchContactsAndChatsController? searchContactAndRecentChatController;
+  final serverSearchController = ServerSearchController();
 
   final _preSearchRecentContactsInteractor =
       getIt.get<PreSearchRecentContactsInteractor>();
+
   final preSearchRecentContactsNotifier =
       ValueNotifier<Either<Failure, Success>>(Right(SearchInitial()));
 
   final TextEditingController textEditingController = TextEditingController();
+
+  bool get isShowMore =>
+      textEditingController.value.text.isNotEmpty &&
+      searchContactAndRecentChatController != null &&
+      searchContactAndRecentChatController!
+          .isShowChatsAndContactsNotifier.value;
+
+  String get searchWord => textEditingController.text;
+
+  String getBodyText(Event event, String searchWord) {
+    final senderName = event.senderFromMemoryOrFallback.calcDisplayname(
+      i18n: MatrixLocals(L10n.of(context)!),
+    );
+
+    final bodyContent = event
+        .calcLocalizedBodyFallback(
+          MatrixLocals(L10n.of(context)!),
+          hideEdit: true,
+          hideReply: true,
+          plaintextBody: true,
+          removeMarkdown: true,
+        )
+        .substringToHighlight(
+          searchWord,
+          prefixLength: _prefixLengthHighlight,
+        );
+    return '$senderName: $bodyContent';
+  }
 
   void fetchPreSearchRecentContacts() {
     _preSearchRecentContactsInteractor
@@ -129,6 +164,7 @@ class SearchController extends State<Search>
     SchedulerBinding.instance.addPostFrameCallback((_) async {
       if (mounted) {
         searchContactAndRecentChatController?.init();
+        serverSearchController.init();
         fetchPreSearchRecentContacts();
         textEditingController.addListener(() {
           onSearchBarChanged(textEditingController.text);
@@ -139,6 +175,7 @@ class SearchController extends State<Search>
 
   void onSearchBarChanged(String keyword) {
     searchContactAndRecentChatController?.onSearchBarChanged(keyword);
+    serverSearchController.onSearchBarChanged(keyword);
   }
 
   void onCloseSearchTapped() {
@@ -152,6 +189,7 @@ class SearchController extends State<Search>
   @override
   void dispose() {
     searchContactAndRecentChatController?.dispose();
+    serverSearchController.dispose();
     super.dispose();
   }
 
