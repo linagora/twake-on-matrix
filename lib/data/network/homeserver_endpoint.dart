c0627e6fa277c87ab522c36dff8d6c687ec4edaf
sherlock <quangkhai28999@gmail.com>
2023-11-29 15:26:46 +0700

    TW-989: update homeserver endpoint to have server search

--- lib/data/network/homeserver_endpoint.dart
+++ lib/data/network/homeserver_endpoint.dart
@@ -3,6 +3,7 @@ import 'package:fluffychat/data/network/service_path.dart';
 class HomeserverEndpoint {
   static const String homeserverMediaPath = '/_matrix/media';
   static const String homeserverAPIVersion = 'v3';
+  static const String homeserverClientPath = '/_matrix/client';
 
   static final ServicePath uploadMediaServicePath = ServicePath(
     '/upload',
@@ -11,11 +12,22 @@ class HomeserverEndpoint {
   static final ServicePath getPreviewUrlServicePath = ServicePath(
     '/preview_url',
   );
+
+  static final ServicePath searchPath = ServicePath(
+    '/search',
+  );
 }
 
 extension ServicePathHomeserver on ServicePath {
   String generateHomeserverMediaEndpoint({
     String rootPath = HomeserverEndpoint.homeserverMediaPath,
+    String apiVersion = HomeserverEndpoint.homeserverAPIVersion,
+  }) {
+    return '$rootPath/$apiVersion$path';
+  }
+
+  String generateHomeserverServerSearchPath({
+    String rootPath = HomeserverEndpoint.homeserverClientPath,
     String apiVersion = HomeserverEndpoint.homeserverAPIVersion,
   }) {
     return '$rootPath/$apiVersion$path';
