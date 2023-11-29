a5f88cbb5dc9e44fdc43d0e1c5f9ece824703a0d
sherlock <quangkhai28999@gmail.com>
2023-11-29 15:32:13 +0700

    TW-989: refactor search debouncer for reuse

---
+++ lib/pages/search/search_debouncer_mixin.dart
@@ -0,0 +1,30 @@
+import 'package:debounce_throttle/debounce_throttle.dart';
+import 'package:matrix/matrix.dart';
+
+mixin SearchDebouncerMixin {
+  static const _debouncerIntervalInMilliseconds = 300;
+
+  Debouncer<String>? _debouncer;
+
+  void initializeDebouncer(void Function(String)? onDebouncerChanged) {
+    _debouncer = Debouncer(
+      const Duration(milliseconds: _debouncerIntervalInMilliseconds),
+      initialValue: '',
+    );
+
+    _debouncer?.values.listen((keyword) async {
+      Logs().d(
+        "SearchContactAndRecentChatController::_initializeDebouncer: searchKeyword: $keyword",
+      );
+      onDebouncerChanged?.call(keyword);
+    });
+  }
+
+  void setDebouncerValue(String keyword) {
+    _debouncer?.value = keyword;
+  }
+
+  void disposeDebouncer() {
+    _debouncer?.cancel();
+  }
+}
