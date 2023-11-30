e8634942884cfab80d82bb57163f3c64d4876faa
MinhDV <drminh2807@gmail.com>
2023-11-30 09:48:55 +0700

    TW-1049 Support multi account

--- lib/presentation/extensions/client_extension.dart
+++ lib/presentation/extensions/client_extension.dart
@@ -1,5 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:fluffychat/presentation/enum/chat_list/chat_list_enum.dart';
+import 'package:fluffychat/utils/string_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:matrix/matrix.dart';
@@ -24,4 +25,6 @@ extension ClientExtension on Client {
   }
 
   String mxid(BuildContext context) => userID ?? L10n.of(context)!.user;
+
+  String? get pusherNotificationClientIdentifier => userID?.sha256Hash;
 }
