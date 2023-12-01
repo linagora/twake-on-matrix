c46a4122ff14f8fd12ef31af27c704723bdbf510
sherlock <quangkhai28999@gmail.com>
2023-12-01 11:52:19 +0700

    TW-1012: refactor send file dialog with new design

--- lib/pages/chat/send_file_dialog.dart
+++ lib/pages/chat/send_file_dialog.dart
@@ -1,37 +1,59 @@
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/usecase/send_media_on_web_with_caption_interactor.dart';
+import 'package:fluffychat/pages/chat/input_bar/focus_suggestion_controller.dart';
+import 'package:fluffychat/pages/chat/send_file_dialog_view.dart';
 import 'package:fluffychat/presentation/extensions/send_file_web_extension.dart';
 import 'package:fluffychat/utils/dialog/twake_dialog.dart';
 import 'package:fluffychat/utils/localized_exception_extension.dart';
 import 'package:fluffychat/utils/twake_snackbar.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-
 import 'package:matrix/matrix.dart';
 
-import 'package:fluffychat/utils/size_string.dart';
 import '../../utils/resize_image.dart';
 
 class SendFileDialog extends StatefulWidget {
-  final Room room;
+  final Room? room;
   final List<MatrixFile> files;
 
   const SendFileDialog({
-    required this.room,
+    this.room,
     required this.files,
     Key? key,
   }) : super(key: key);
 
   @override
-  SendFileDialogState createState() => SendFileDialogState();
+  SendFileDialogController createState() => SendFileDialogController();
 }
 
-class SendFileDialogState extends State<SendFileDialog> {
+class SendFileDialogController extends State<SendFileDialog> {
   bool origImage = false;
 
   /// Images smaller than 20kb don't need compression.
   static const int minSizeToCompress = 20 * 1024;
 
-  Future<void> _send() async {
+  final sendMediaOnWebWithCaptionInteractor =
+      getIt.get<SendMediaOnWebWithCaptionInteractor>();
+
+  final focusSuggestionController = FocusSuggestionController();
+
+  final TextEditingController textEditingController = TextEditingController();
+
+  void sendMediaWithCaption() {
+    if (widget.room == null) {
+      Logs().e("sendMediaWithCaption:: room is null");
+      Navigator.of(context).pop();
+      return;
+    }
+    sendMediaOnWebWithCaptionInteractor.execute(
+      room: widget.room!,
+      media: widget.files.first,
+      caption: textEditingController.text,
+    );
+    Navigator.of(context).pop();
+  }
+
+  Future<void> sendWithoutCaption() async {
     for (var file in widget.files) {
       // ignore: unused_local_variable
       MatrixImageFile? thumbnail;
@@ -44,13 +66,15 @@ class SendFileDialogState extends State<SendFileDialog> {
           },
         );
       }
-      // ignore: unused_local_variable
-      final scaffoldMessenger = ScaffoldMessenger.of(context);
-      widget.room
+      if (widget.room == null) {
+        Logs().e("sendMediaWithCaption:: room is null");
+        Navigator.of(context).pop();
+        return;
+      }
+      widget.room!
           .sendFileOnWebEvent(
         file,
         thumbnail: thumbnail,
-        shrinkImageMaxDimension: origImage ? null : 1600,
       )
           .catchError((e) {
         TwakeSnackBar.show(context, (e as Object).toLocalizedString(context));
@@ -62,57 +86,16 @@ class SendFileDialogState extends State<SendFileDialog> {
     return;
   }
 
+  void send() {
+    if (textEditingController.text.isEmpty) {
+      sendWithoutCaption();
+    } else {
+      sendMediaWithCaption();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var sendStr = L10n.of(context)!.sendFile;
-    final bool allFilesAreImages =
-        widget.files.every((file) => file is MatrixImageFile);
-    final sizeString = widget.files
-        .fold<double>(0, (p, file) => p + (file.bytes?.length ?? 0))
-        .sizeString;
-    final fileName = widget.files.length == 1
-        ? widget.files.single.name
-        : L10n.of(context)!.countFiles(widget.files.length.toString());
-
-    if (allFilesAreImages) {
-      sendStr = L10n.of(context)!.sendImage;
-    } else if (widget.files.every((file) => file is MatrixAudioFile)) {
-      sendStr = L10n.of(context)!.sendAudio;
-    } else if (widget.files.every((file) => file is MatrixVideoFile)) {
-      sendStr = L10n.of(context)!.sendVideo;
-    }
-    Widget contentWidget;
-    if (allFilesAreImages) {
-      contentWidget = Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Flexible(
-            child: Image.memory(
-              widget.files.first.bytes!,
-              fit: BoxFit.contain,
-            ),
-          ),
-        ],
-      );
-    } else {
-      contentWidget = Text('$fileName ($sizeString)');
-    }
-    return AlertDialog(
-      title: Text(sendStr),
-      content: contentWidget,
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            // just close the dialog
-            Navigator.of(context, rootNavigator: false).pop();
-          },
-          child: Text(L10n.of(context)!.cancel),
-        ),
-        TextButton(
-          onPressed: _send,
-          child: Text(L10n.of(context)!.send),
-        ),
-      ],
-    );
+    return SendFileDialogView(this);
   }
 }
