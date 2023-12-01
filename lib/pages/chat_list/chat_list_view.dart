183456d9a88fc7ad864198ed1ffecb64b4065044
Julian KOUNE <jkoune@linagora.com>
2023-12-01 16:42:43 +0100

    TW-991: creating DM with no encryption by default, hiding create chat btn on web

--- lib/pages/chat_list/chat_list_view.dart
+++ lib/pages/chat_list/chat_list_view.dart
@@ -89,7 +89,7 @@ class ChatListView extends StatelessWidget {
                       ),
                       MenuItemButton(
                         leadingIcon: const Icon(Icons.group),
-                        onPressed: () => controller.goToNewGroupChat(),
+                        onPressed: () => controller.goToNewGroupChat(context),
                         child: Text(L10n.of(context)!.newChat),
                       ),
                     ],
