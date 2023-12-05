33ceaac38b1d7c75b591aa466d1a7f883bebeef4
HuyNguyen <quanghnguyen@linagora.com>
2023-12-06 01:40:37 +0700

    TW-858: Implement  `LookupMatchContact` in data layer

--- lib/data/repository/contact/tom_contact_repository_impl.dart
+++ lib/data/repository/contact/tom_contact_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:fluffychat/data/datasource/tom_contacts_datasource.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/model/contact/contact.dart';
 import 'package:fluffychat/domain/model/contact/contact_query.dart';
+import 'package:fluffychat/domain/model/contact/lookup_mxid_request.dart';
 import 'package:fluffychat/domain/repository/contact_repository.dart';
 
 class TomContactRepositoryImpl implements ContactRepository {
@@ -22,5 +23,16 @@ class TomContactRepositoryImpl implements ContactRepository {
     );
 
     yield response;
+  }
+
+  @override
+  Future<List<Contact>> lookupMatchContact({
+    required ContactQuery query,
+    LookupMxidRequest? lookupMxidRequest,
+  }) async {
+    return datasource.fetchContacts(
+      query: query,
+      lookupMxidRequest: lookupMxidRequest,
+    );
   }
 }
