14d352f576e1892a99316f50519d78f0134ce58f
HuyNguyen <quanghnguyen@linagora.com>
2023-12-06 01:44:09 +0700

    TW-858: Implement and update UI for chat profile info

--- lib/pages/chat_profile_info/chat_profile_info_style.dart
+++ lib/pages/chat_profile_info/chat_profile_info_style.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ProfileInfoStyle {
+class ChatProfileInfoStyle {
+  static const double copyIconSize = 20;
   static const double maxWidth = 416;
+  static const double textSpacing = 8;
+
+  static const double avatarFontSize = 36;
+
+  static BorderRadius emailBorderRadius = BorderRadius.circular(16);
 
   static const EdgeInsetsGeometry mainPadding =
       EdgeInsets.symmetric(horizontal: 20, vertical: 16);
@@ -14,12 +20,9 @@ class ProfileInfoStyle {
   static const EdgeInsetsGeometry emailMargin =
       EdgeInsets.symmetric(vertical: 12);
 
-  static BorderRadius emailBorderRadius = BorderRadius.circular(16);
-
   static const EdgeInsetsGeometry textPadding =
       EdgeInsets.symmetric(horizontal: 16);
 
-  static const double textSpacing = 8;
-
-  static const double avatarFontSize = 36;
+  static const EdgeInsetsGeometry titleSharedMediaAndFilesPadding =
+      EdgeInsets.only(top: 30);
 }
