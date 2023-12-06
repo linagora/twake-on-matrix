44636a673e26b0771f513f24672cc2031f7629d4
sherlock <quangkhai28999@gmail.com>
2023-12-06 10:38:37 +0700

    TW-898: move child of message to another class so that it's easier to read

---
+++ lib/pages/chat/events/message/message_content_builder.dart
@@ -0,0 +1,225 @@
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/pages/chat/events/message/display_name_widget.dart';
+import 'package:fluffychat/pages/chat/events/message_time.dart';
+import 'package:fluffychat/utils/date_time_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:fluffychat/pages/chat/events/message/reply_content_widget.dart';
+import 'package:fluffychat/pages/chat/events/message_content.dart';
+import 'package:matrix/matrix.dart';
+import 'package:fluffychat/utils/string_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'dart:math';
+
+class MessageContentBuilder extends StatelessWidget {
+  final Event event;
+  final Timeline timeline;
+  final BoxConstraints availableBubbleContraints;
+  final void Function(String)? scrollToEventId;
+  final void Function(Event)? onSelect;
+  final Event? nextEvent;
+  final bool selectMode;
+
+  const MessageContentBuilder({
+    super.key,
+    required this.event,
+    required this.timeline,
+    required this.availableBubbleContraints,
+    this.onSelect,
+    this.nextEvent,
+    this.scrollToEventId,
+    this.selectMode = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = Theme.of(context).colorScheme.onBackground;
+    final displayEvent = event.getDisplayEvent(timeline);
+    final noPadding = {
+      MessageTypes.File,
+      MessageTypes.Audio,
+    }.contains(event.messageType);
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: noPadding || event.timelineOverlayMessage ? 0 : 8,
+      ),
+      child: IntrinsicWidth(
+        stepWidth: _getSizeMessageBubbleWidth(
+          context,
+          maxWidth: availableBubbleContraints.maxWidth,
+          ownMessage: event.isOwnMessage,
+          hideDisplayName: nextEvent != null &&
+              event.hideDisplayName(
+                nextEvent!,
+              ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            if (event.relationshipType == RelationshipTypes.reply)
+              ReplyContentWidget(
+                event: event,
+                timeline: timeline,
+                scrollToEventId: scrollToEventId,
+                ownMessage: event.isOwnMessage,
+              ),
+            Stack(
+              children: [
+                MessageContent(
+                  displayEvent,
+                  textColor: textColor,
+                  endOfBubbleWidget: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8.0,
+                      right: 4.0,
+                    ),
+                    child: SelectionContainer.disabled(
+                      child: MessageTime(
+                        timelineOverlayMessage: event.timelineOverlayMessage,
+                        event: event,
+                        ownMessage: event.isOwnMessage,
+                        timeline: timeline,
+                        room: event.room,
+                      ),
+                    ),
+                  ),
+                  backgroundColor: event.isOwnMessage
+                      ? Theme.of(
+                          context,
+                        ).colorScheme.primaryContainer
+                      : Theme.of(
+                          context,
+                        ).colorScheme.surface,
+                  onTapSelectMode: () => selectMode
+                      ? onSelect!(
+                          event,
+                        )
+                      : null,
+                  onTapPreview: !selectMode ? () {} : null,
+                  ownMessage: event.isOwnMessage,
+                ),
+                if (event.timelineOverlayMessage)
+                  Positioned(
+                    right: 8,
+                    bottom: 4.0,
+                    child: SelectionContainer.disabled(
+                      child: MessageTime(
+                        timelineOverlayMessage: event.timelineOverlayMessage,
+                        event: event,
+                        ownMessage: event.isOwnMessage,
+                        timeline: timeline,
+                        room: event.room,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            if (event.hasAggregatedEvents(
+              timeline,
+              RelationshipTypes.edit,
+            ))
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 4.0 * AppConfig.bubbleSizeFactor,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.edit_outlined,
+                      color: textColor.withAlpha(
+                        164,
+                      ),
+                      size: 14,
+                    ),
+                    Text(
+                      ' - ${displayEvent.originServerTs.localizedTimeShort(context)}',
+                      style: TextStyle(
+                        color: textColor.withAlpha(
+                          164,
+                        ),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  double? _getSizeMessageBubbleWidth(
+    BuildContext context, {
+    required double maxWidth,
+    bool ownMessage = false,
+    bool hideDisplayName = false,
+  }) {
+    final isNotSupportCalcSize = {
+      MessageTypes.File,
+      MessageTypes.Image,
+      MessageTypes.Video,
+    }.contains(event.messageType);
+
+    if (ownMessage || hideDisplayName || isNotSupportCalcSize) {
+      return null;
+    }
+
+    final sizeWidthDisplayName = _getSizeDisplayName(
+      context,
+      maxWidth,
+    ).width;
+
+    final sizeWidthMessageText = _getSizeMessageText(
+      context,
+      maxWidth,
+    ).width;
+
+    return max<double>(sizeWidthDisplayName, sizeWidthMessageText);
+  }
+
+  TextPainter _getSizeDisplayName(
+    BuildContext context,
+    double maxWidth,
+  ) {
+    return TextPainter(
+      text: TextSpan(
+        text: event.senderFromMemoryOrFallback
+            .calcDisplayname()
+            .shortenDisplayName(
+              maxCharacters: DisplayNameWidget.maxCharactersDisplayNameBubble,
+            ),
+        style: Theme.of(context).textTheme.labelMedium?.copyWith(
+              color: Theme.of(
+                context,
+              ).colorScheme.primary,
+            ),
+      ),
+      maxLines: 2,
+      textDirection: TextDirection.ltr,
+    )..layout(minWidth: 0, maxWidth: maxWidth);
+  }
+
+  TextPainter _getSizeMessageText(
+    BuildContext context,
+    double maxWidth,
+  ) {
+    return TextPainter(
+      text: TextSpan(
+        text: event.calcLocalizedBodyFallback(
+          MatrixLocals(L10n.of(context)!),
+          hideReply: true,
+        ),
+        style: Theme.of(context).textTheme.labelMedium?.copyWith(
+              color: Theme.of(
+                context,
+              ).colorScheme.primary,
+            ),
+      ),
+      textDirection: TextDirection.ltr,
+    )..layout(minWidth: 0, maxWidth: maxWidth);
+  }
+}
---
+++ lib/pages/chat/events/message/message_content_with_timestamp_builder.dart
@@ -0,0 +1,247 @@
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/pages/chat/context_item_chat_action.dart';
+import 'package:fluffychat/pages/chat/events/message/display_name_widget.dart';
+import 'package:fluffychat/pages/chat/events/message/message.dart';
+import 'package:fluffychat/pages/chat/events/message/message_content_builder.dart';
+import 'package:fluffychat/pages/chat/events/message/message_style.dart';
+import 'package:fluffychat/pages/chat/events/message/multi_platform_message_container.dart';
+import 'package:fluffychat/pages/chat/events/message_reactions.dart';
+import 'package:fluffychat/pages/chat/events/message_time.dart';
+import 'package:fluffychat/utils/date_time_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
+import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/utils/responsive/responsive_utils.dart';
+import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
+import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
+
+class MessageContentWithTimestampBuilder extends StatelessWidget {
+  final Event event;
+  final Event? nextEvent;
+  final void Function(Event)? onSelect;
+  final void Function(String)? scrollToEventId;
+  final ValueNotifier<String?> isHoverNotifier;
+  final bool longPressSelect;
+  final bool selected;
+  final Timeline timeline;
+  final List<ContextMenuItemChatAction> listHorizontalActionMenu;
+  final OnMenuAction? onMenuAction;
+  final bool selectMode;
+
+  static final responsiveUtils = getIt.get<ResponsiveUtils>();
+
+  const MessageContentWithTimestampBuilder({
+    super.key,
+    required this.event,
+    this.nextEvent,
+    this.longPressSelect = false,
+    this.onSelect,
+    this.scrollToEventId,
+    this.selected = false,
+    this.selectMode = true,
+    required this.timeline,
+    required this.isHoverNotifier,
+    required this.listHorizontalActionMenu,
+    this.onMenuAction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final displayTime = event.type == EventTypes.RoomCreate ||
+        nextEvent == null ||
+        !event.originServerTs.sameEnvironment(nextEvent!.originServerTs);
+    final noBubble = {
+          MessageTypes.Sticker,
+        }.contains(event.messageType) &&
+        !event.redacted;
+
+    final timelineText = {
+      MessageTypes.Text,
+      MessageTypes.BadEncrypted,
+    }.contains(event.messageType);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment:
+          event.isOwnMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        if (event.isOwnMessage)
+          _menuActionsRowBuilder(context, event.isOwnMessage),
+        Container(
+          alignment:
+              event.isOwnMessage ? Alignment.topRight : Alignment.topLeft,
+          padding: EdgeInsetsDirectional.only(
+            top: MessageStyle.messageSpacing(
+              displayTime,
+              nextEvent,
+              event,
+            ),
+            start: 8,
+            end: selected || responsiveUtils.isDesktop(context) ? 8 : 0,
+          ),
+          child: MultiPlatformSelectionMode(
+            event: event,
+            longPressSelect: longPressSelect,
+            useInkWell: !PlatformInfos.isWeb,
+            onSelect: onSelect,
+            child: Stack(
+              alignment: event.isOwnMessage
+                  ? AlignmentDirectional.bottomStart
+                  : AlignmentDirectional.bottomEnd,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: MessageStyle.bubbleBorderRadius,
+                        color: event.isOwnMessage
+                            ? Theme.of(context).colorScheme.primaryContainer
+                            : Theme.of(context).colorScheme.surface,
+                      ),
+                      padding: noBubble
+                          ? const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            )
+                          : EdgeInsets.only(
+                              left: 8 * AppConfig.bubbleSizeFactor,
+                              right: 8 * AppConfig.bubbleSizeFactor,
+                              top: 8 * AppConfig.bubbleSizeFactor,
+                              bottom: event.timelineOverlayMessage
+                                  ? 8 * AppConfig.bubbleSizeFactor
+                                  : 0 * AppConfig.bubbleSizeFactor,
+                            ),
+                      constraints: BoxConstraints(
+                        maxWidth: MessageStyle.messageBubbleWidth(
+                          context,
+                        ),
+                      ),
+                      child: LayoutBuilder(
+                        builder: (
+                          context,
+                          availableBubbleContraints,
+                        ) =>
+                            Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            nextEvent != null &&
+                                        event.hideDisplayName(nextEvent!) ||
+                                    event.hideDisplayNameInBubbleChat
+                                ? const SizedBox(height: 0)
+                                : DisplayNameWidget(
+                                    event: event,
+                                  ),
+                            IntrinsicHeight(
+                              child: Stack(
+                                alignment: Alignment.bottomRight,
+                                children: [
+                                  MessageContentBuilder(
+                                    event: event,
+                                    timeline: timeline,
+                                    availableBubbleContraints:
+                                        availableBubbleContraints,
+                                    onSelect: onSelect,
+                                    nextEvent: nextEvent,
+                                    scrollToEventId: scrollToEventId,
+                                    selectMode: selectMode,
+                                  ),
+                                  if (timelineText)
+                                    Positioned(
+                                      child: SelectionContainer.disabled(
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                            left: 6,
+                                            right: 8.0,
+                                            bottom: 4.0,
+                                          ),
+                                          child: MultiPlatformSelectionMode(
+                                            useInkWell: PlatformInfos.isWeb,
+                                            longPressSelect: longPressSelect,
+                                            onSelect: onSelect,
+                                            event: event,
+                                            child: MessageTime(
+                                              timelineOverlayMessage:
+                                                  event.timelineOverlayMessage,
+                                              room: event.room,
+                                              event: event,
+                                              ownMessage: event.isOwnMessage,
+                                              timeline: timeline,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    if (event.hasAggregatedEvents(
+                      timeline,
+                      RelationshipTypes.reaction,
+                    ))
+                      const SizedBox(height: 24),
+                  ],
+                ),
+                if (event.hasAggregatedEvents(
+                  timeline,
+                  RelationshipTypes.reaction,
+                )) ...[
+                  Positioned(
+                    left: 8,
+                    right: 0,
+                    bottom: 0,
+                    child: MessageReactions(event, timeline),
+                  ),
+                  const SizedBox(width: 4),
+                ],
+              ],
+            ),
+          ),
+        ),
+        if (!event.isOwnMessage)
+          _menuActionsRowBuilder(context, event.isOwnMessage),
+      ],
+    );
+  }
+
+  Widget _menuActionsRowBuilder(BuildContext context, bool ownMessage) {
+    return ValueListenableBuilder(
+      valueListenable: isHoverNotifier,
+      builder: (context, isHover, child) {
+        if (isHover != null && isHover.contains(event.eventId) && !selected) {
+          return child!;
+        }
+        return const SizedBox();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: listHorizontalActionMenu.map((item) {
+            return Padding(
+              padding: const EdgeInsetsDirectional.symmetric(horizontal: 4),
+              child: TwakeIconButton(
+                icon: item.action.getIcon(),
+                imagePath: item.action.getImagePath(),
+                tooltip: item.action.getTitle(context),
+                preferBelow: false,
+                onTapDown: (context) => onMenuAction!(
+                  context,
+                  item.action,
+                  event,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
