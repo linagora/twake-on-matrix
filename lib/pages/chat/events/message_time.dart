d468ca969258b23d0d57226b1167565296f0f39e
sherlock <quangkhai28999@gmail.com>
2023-12-06 18:07:12 +0700

    TW-898: add pin icon in chat message

--- lib/pages/chat/events/message_time.dart
+++ lib/pages/chat/events/message_time.dart
@@ -1,11 +1,14 @@
 import 'package:fluffychat/pages/chat/events/message_time_style.dart';
 import 'package:fluffychat/pages/chat/seen_by_row.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
+import 'package:fluffychat/widgets/twake_components/twake_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
 import 'package:matrix/matrix.dart';
 import 'package:linagora_design_flutter/colors/linagora_state_layer.dart';
 import 'package:fluffychat/utils/room_status_extension.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class MessageTime extends StatelessWidget {
   const MessageTime({
@@ -43,6 +46,19 @@ class MessageTime extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
+          if (event.isPinned) ...[
+            TwakeIconButton(
+              tooltip: L10n.of(context)!.pin,
+              icon: Icons.push_pin_outlined,
+              size: 14.0,
+              paddingAll: 0,
+              margin: EdgeInsets.zero,
+              iconColor: timelineOverlayMessage
+                  ? Colors.white
+                  : LinagoraRefColors.material().neutral[50],
+            ),
+            const SizedBox(width: 4.0),
+          ],
           Text(
             DateFormat("HH:mm").format(event.originServerTs),
             style: Theme.of(context).textTheme.bodySmall?.merge(
