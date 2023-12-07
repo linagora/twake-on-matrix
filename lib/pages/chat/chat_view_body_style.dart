49ff4bc4bc3c7424e45241cc6f5a52ac0b86be71
HuyNguyen <quanghnguyen@linagora.com>
2023-12-07 11:22:43 +0700

    Update size for input bar in chat

--- lib/pages/chat/chat_view_body_style.dart
+++ lib/pages/chat/chat_view_body_style.dart
@@ -4,4 +4,6 @@ import 'package:flutter/material.dart';
 class ChatViewBodyStyle {
   static double bottomSheetPadding(BuildContext context) =>
       TwakeThemes.isColumnMode(context) ? 16.0 : 8.0;
+
+  static double inputMessageWidgetMaxWidth = 800.0;
 }
