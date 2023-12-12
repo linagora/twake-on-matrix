7b4afe80420f224c7707be120b8f7d9e47ca5dd8
Nguyen Thai <tknguyen@linagora.com>
2023-12-12 15:14:08 +0700

    Make sure xcbeautify is installed

--- scripts/prepare-ios.sh
+++ scripts/prepare-ios.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+# Use alternate beautifier
+brew install xcbeautify
 cd ios
 pod install && pod update
