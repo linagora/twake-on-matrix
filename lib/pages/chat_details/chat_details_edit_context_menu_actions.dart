13201f20e4223f1f05a620093e0c012c94ed19b5
HuyNguyen <quanghnguyen@linagora.com>
2023-12-13 01:38:58 +0700

    TW-902: Move Chat details in go_router to inner router

--- lib/pages/chat_details/chat_details_edit_context_menu_actions.dart
+++ lib/pages/chat_details/chat_details_edit_context_menu_actions.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
-enum ChatDetailsEditContextMenuActions {
+enum EditChatAvatarContextMenuActions {
   edit,
   delete;
 
   String getTitle(BuildContext context) {
     switch (this) {
-      case ChatDetailsEditContextMenuActions.edit:
+      case EditChatAvatarContextMenuActions.edit:
         return L10n.of(context)!.changeChatAvatar;
-      case ChatDetailsEditContextMenuActions.delete:
+      case EditChatAvatarContextMenuActions.delete:
         return L10n.of(context)!.removeYourAvatar;
     }
   }
 
   IconData getIcon() {
     switch (this) {
-      case ChatDetailsEditContextMenuActions.edit:
+      case EditChatAvatarContextMenuActions.edit:
         return Icons.camera_alt_outlined;
-      case ChatDetailsEditContextMenuActions.delete:
+      case EditChatAvatarContextMenuActions.delete:
         return Icons.delete;
     }
   }
