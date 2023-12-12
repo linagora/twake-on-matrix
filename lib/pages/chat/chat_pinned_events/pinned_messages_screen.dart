a1f501c9138e5318730876ad9a32d07a476067cc
sherlock <quangkhai28999@gmail.com>
2023-12-12 14:26:34 +0700

    TW-898: add pinned messages screen in web

--- lib/pages/chat/chat_pinned_events/pinned_messages_screen.dart
+++ lib/pages/chat/chat_pinned_events/pinned_messages_screen.dart
@@ -20,8 +20,13 @@ class PinnedMessagesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          L10n.of(context)!.pinnedMessages(controller.events.length),
+        title: ValueListenableBuilder(
+          valueListenable: controller.eventsNotifier,
+          builder: (context, events, child) {
+            return Text(
+              L10n.of(context)!.pinnedMessages(events.length),
+            );
+          },
         ),
         leading: TwakeIconButton(
           tooltip: L10n.of(context)!.back,
@@ -47,48 +52,90 @@ class PinnedMessagesScreen extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: PinnedMessagesStyle.paddingListMessages(context),
-              child: ListView.custom(
-                controller: controller.scrollController,
-                shrinkWrap: true,
-                reverse: true,
-                childrenDelegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    // The message at this index:
-                    final currentEventIndex =
-                        controller.events.length - index - 1;
-                    final event = controller.events[currentEventIndex];
-                    final nextEvent = currentEventIndex > 0
-                        ? controller.events[currentEventIndex - 1]
-                        : null;
-                    final previousEvent =
-                        currentEventIndex < controller.events.length - 1
-                            ? controller.events[currentEventIndex + 1]
+              child: ValueListenableBuilder(
+                valueListenable: controller.eventsNotifier,
+                builder: (context, events, child) {
+                  return ListView.custom(
+                    controller: controller.scrollController,
+                    shrinkWrap: true,
+                    reverse: true,
+                    childrenDelegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        // The message at this index:
+                        final currentEventIndex = events.length - index - 1;
+                        final event = events[currentEventIndex];
+                        final nextEvent = currentEventIndex > 0
+                            ? events[currentEventIndex - 1]
                             : null;
-                    return Builder(
-                      builder: (context) {
-                        return GestureDetector(
-                          onSecondaryTapDown: (details) {
-                            controller.handleContextMenuActionInEachMessage(
-                              context,
-                              event,
-                            );
-                          },
-                          child: Message(
-                            event!,
-                            previousEvent: previousEvent,
-                            nextEvent: nextEvent,
-                            timeline: controller.widget.timeline!,
-                            isHoverNotifier: controller.isHoverNotifier,
-                            listHorizontalActionMenu: const [],
-                            longPressSelect: true,
-                            selectMode: false,
-                          ),
+                        final previousEvent =
+                            currentEventIndex < events.length - 1
+                                ? events[currentEventIndex + 1]
+                                : null;
+                        return Message(
+                          event!,
+                          previousEvent: previousEvent,
+                          nextEvent: nextEvent,
+                          timeline: controller.widget.timeline!,
+                          isHoverNotifier: controller.isHoverNotifier,
+                          listHorizontalActionMenu: const [],
+                          longPressSelect: true,
+                          selectMode: false,
+                          menuChildren: (context) => controller
+                              .pinnedMessagesActionsList
+                              .map(
+                                (action) => InkWell(
+                                  onTap: () {
+                                    action.onTap?.call(extra: event);
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Container(
+                                    height: PinnedMessagesStyle
+                                        .heightContextMenuItem,
+                                    padding: const EdgeInsets.all(
+                                      PinnedMessagesStyle
+                                          .paddingAllContextMenuItem,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        if (action.imagePath != null)
+                                          SvgPicture.asset(
+                                            action.imagePath!,
+                                            colorFilter: ColorFilter.mode(
+                                              action.color ??
+                                                  Theme.of(context)
+                                                      .colorScheme
+                                                      .primary,
+                                              BlendMode.srcIn,
+                                            ),
+                                          ),
+                                        if (action.iconData != null)
+                                          Icon(
+                                            action.iconData,
+                                            color: action.color,
+                                          ),
+                                        if (action.imagePath != null ||
+                                            action.iconData != null)
+                                          PinnedMessagesStyle
+                                              .paddingIconAndUnpin,
+                                        Text(
+                                          action.text,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelLarge
+                                              ?.copyWith(color: action.color),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
                         );
                       },
-                    );
-                  },
-                  childCount: controller.events.length,
-                ),
+                      childCount: controller.eventsNotifier.value.length,
+                    ),
+                  );
+                },
               ),
             ),
           ),
