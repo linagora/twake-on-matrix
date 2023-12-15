191b9d0e3c2b52cbe947f82d0bb0f52211b3cac9
MinhDV <drminh2807@gmail.com>
2023-12-15 15:02:11 +0700

    TW-1098 [Search] Load more in Server side Search

---
+++ lib/domain/model/search/server_side_search_categories.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:matrix/matrix.dart';
+
+class ServerSideSearchCategories extends Equatable {
+  final String searchTerm;
+  final SearchFilter? searchFilter;
+
+  const ServerSideSearchCategories({
+    required this.searchTerm,
+    this.searchFilter,
+  });
+
+  @override
+  List<Object?> get props => [searchTerm, searchFilter];
+
+  Categories get searchCategories {
+    return Categories(
+      roomEvents: RoomEventsCriteria(
+        searchTerm: searchTerm,
+        groupings: Groupings(
+          groupBy: [Group(key: GroupKey.roomId)],
+        ),
+        keys: [KeyKind.contentBody],
+        orderBy: SearchOrder.recent,
+        filter: searchFilter,
+      ),
+    );
+  }
+}
