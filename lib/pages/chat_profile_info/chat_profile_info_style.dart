642b62371a46f20d39b3f00e070b7252eb719f2c
Julian KOUNE <jkoune@linagora.com>
2023-12-18 03:25:19 +0100

    TW-1125/TW-1128: Improve Matrix ID design

--- lib/pages/chat_profile_info/chat_profile_info_style.dart
+++ lib/pages/chat_profile_info/chat_profile_info_style.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ChatProfileInfoStyle {
+  static const double iconPadding = 8;
+  static const double iconSize = 24;
   static const double copyIconSize = 20;
 
   static const double maxWidth = 416;
-  static const double textSpacing = 8;
+  static const double textSpacing = 4;
 
   static const double avatarFontSize = 36;
 
-  static BorderRadius emailBorderRadius = BorderRadius.circular(16);
+  static BorderRadius copiableContainerBorderRadius = BorderRadius.circular(16);
 
   static const EdgeInsetsGeometry mainPadding =
       EdgeInsets.symmetric(horizontal: 20, vertical: 16);
@@ -16,13 +18,14 @@ class ChatProfileInfoStyle {
   static const EdgeInsetsGeometry backIconPadding =
       EdgeInsets.symmetric(vertical: 8, horizontal: 4);
 
-  static const EdgeInsetsGeometry emailPadding = EdgeInsets.all(16);
+  static const EdgeInsetsGeometry copiableContainerPadding =
+      EdgeInsets.all(12.0);
 
-  static const EdgeInsetsGeometry emailMargin =
+  static const EdgeInsetsGeometry copiableContainerMargin =
       EdgeInsets.symmetric(vertical: 12);
 
   static const EdgeInsetsGeometry textPadding =
-      EdgeInsets.symmetric(horizontal: 16);
+      EdgeInsets.symmetric(horizontal: 8);
 
   static const EdgeInsetsGeometry titleSharedMediaAndFilesPadding =
       EdgeInsets.only(top: 30);
