4ec8a92299c6aaca95af139f71b1cd25da09ee63
sherlock <quangkhai28999@gmail.com>
2023-12-24 16:05:40 +0700

    TW-1127: refactor file tile in message content to reuse

--- lib/pages/chat/events/message_download_content.dart
+++ lib/pages/chat/events/message_download_content.dart
@@ -1,11 +1,6 @@
-import 'package:fluffychat/domain/model/extensions/mime_type_extension.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
-import 'package:fluffychat/utils/string_extension.dart';
+import 'package:fluffychat/widgets/file_tile_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_matrix_html/color_extension.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
-import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
 import 'package:matrix/matrix.dart';
 
 class MessageDownloadContent extends StatelessWidget {
@@ -36,101 +31,13 @@ class MessageDownloadContent extends StatelessWidget {
               onFileTapped?.call(event);
             }
           : null,
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        decoration: ShapeDecoration(
-          color: LinagoraSysColors.material().surfaceTint.withOpacity(0.08),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: SvgPicture.asset(
-                event.getIcon(),
-                width: 36,
-                height: 36,
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  _FileNameText(
-                    filename: filename,
-                    highlightText: highlightText,
-                  ),
-                  Row(
-                    children: [
-                      if (sizeString != null)
-                        _TextInformationOfFile(value: sizeString),
-                      const _TextInformationOfFile(value: " · "),
-                      Flexible(
-                        child: _TextInformationOfFile(
-                          value: event.getFileType(context),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      child: FileTileWidget(
+        mimeType: event.mimeType,
+        fileType: filetype,
+        filename: filename,
+        highlightText: highlightText,
+        sizeString: sizeString,
       ),
-    );
-  }
-}
-
-class _FileNameText extends StatelessWidget {
-  const _FileNameText({
-    required this.filename,
-    this.highlightText,
-  });
-
-  final String filename;
-  final String? highlightText;
-
-  @override
-  Widget build(BuildContext context) {
-    return RichText(
-      maxLines: 1,
-      text: TextSpan(
-        children: filename.buildHighlightTextSpans(
-          highlightText ?? '',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onBackground,
-            fontWeight: FontWeight.bold,
-          ),
-          highlightStyle: TextStyle(
-            color: Theme.of(context).colorScheme.onBackground,
-            fontWeight: FontWeight.bold,
-            backgroundColor: CssColor.fromCss('gold'),
-          ),
-        ),
-      ),
-      overflow: TextOverflow.ellipsis,
-    );
-  }
-}
-
-class _TextInformationOfFile extends StatelessWidget {
-  final String value;
-  const _TextInformationOfFile({required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      value,
-      style: Theme.of(context).textTheme.bodySmall!.copyWith(
-            color: LinagoraRefColors.material().neutral,
-          ),
-      maxLines: 1,
-      overflow: TextOverflow.ellipsis,
     );
   }
 }
