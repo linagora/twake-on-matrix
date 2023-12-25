17f3355aae084fa8fb24e2afef3ff6fd0ebd2bb0
HuyNguyen <quanghnguyen@linagora.com>
2023-12-25 15:20:32 +0700

    Create auto homeserver picker for web

---
+++ lib/pages/auto_homeserver_picker/auto_homeserver_picker_view.dart
@@ -0,0 +1,112 @@
+import 'package:fluffychat/pages/auto_homeserver_picker/auto_homeserver_picker.dart';
+import 'package:fluffychat/pages/twake_welcome/twake_welcome_view_style.dart';
+import 'package:fluffychat/resource/image_paths.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:linagora_design_flutter/twake_screen/twake_welcome_screen_style.dart';
+
+class AutoHomeserverPickerView extends StatelessWidget {
+  final AutoHomeserverPickerController controller;
+  const AutoHomeserverPickerView({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinagoraSysColors.material().linearGradientStartingPage,
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  L10n.of(context)!.welcomeTo,
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: LinagoraSysColors.material().onSurfaceVariant,
+                      ),
+                ),
+                SvgPicture.asset(
+                  ImagePaths.logoTwakeWelcome,
+                  width: TwakeWelcomeViewStyle.logoWidth,
+                  height: TwakeWelcomeViewStyle.logoHeight,
+                ),
+                Padding(
+                  padding: TwakeWelcomeScreenStyle.descriptionPadding,
+                  child: Text(
+                    L10n.of(context)!.descriptionWelcomeTo,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: LinagoraSysColors.material().onSurfaceVariant,
+                        ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ValueListenableBuilder(
+                  valueListenable: controller.showButtonRetryNotifier,
+                  builder: (context, isShowRetry, child) {
+                    if (isShowRetry) return child!;
+                    return const CircularProgressIndicator.adaptive();
+                  },
+                  child: const SizedBox(),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              child: ValueListenableBuilder(
+                valueListenable: controller.showButtonRetryNotifier,
+                builder: (context, isShowRetry, child) {
+                  if (isShowRetry) return child!;
+                  return const SizedBox();
+                },
+                child: Padding(
+                  padding: TwakeWelcomeScreenStyle.buttonPadding,
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    focusColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    onTap: controller.retryCheckHomeserver,
+                    child: Container(
+                      height: TwakeWelcomeScreenStyle.buttonHeight,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                      ),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: ShapeDecoration(
+                        color: LinagoraSysColors.material().primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            TwakeWelcomeScreenStyle.buttonRadius,
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          L10n.of(context)!.startMessaging,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge
+                              ?.copyWith(
+                                color: LinagoraSysColors.material().onPrimary,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
