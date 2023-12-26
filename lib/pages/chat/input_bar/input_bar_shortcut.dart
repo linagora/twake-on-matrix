b1c61fe3276eedbb02bb56d54dd96f42acdc7a10
sherlock <quangkhai28999@gmail.com>
2023-12-26 11:50:11 +0700

    TW-1161: remove flutter shortcut

--- lib/pages/chat/input_bar/input_bar_shortcut.dart
+++ lib/pages/chat/input_bar/input_bar_shortcut.dart
@@ -1,7 +1,6 @@
 import 'package:fluffychat/pages/chat/input_bar/focus_suggestion_controller.dart';
 import 'package:fluffychat/presentation/extensions/text_editting_controller_extension.dart';
 import 'package:fluffychat/utils/one_time_debouncer.dart';
-import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as flutter;
 import 'package:matrix/matrix.dart';
@@ -13,8 +12,6 @@ class InputBarShortcuts extends StatelessWidget {
 
   final Room? room;
 
-  final VoidCallback? handlePaste;
-
   final ValueChanged<String>? onEnter;
 
   final FocusSuggestionController? focusSuggestionController;
@@ -22,7 +19,6 @@ class InputBarShortcuts extends StatelessWidget {
   InputBarShortcuts({
     super.key,
     required this.child,
-    this.handlePaste,
     this.room,
     this.controller,
     this.onEnter,
@@ -35,21 +31,6 @@ class InputBarShortcuts extends StatelessWidget {
   Widget build(BuildContext context) {
     return CallbackShortcuts(
       bindings: {
-        if (handlePaste != null)
-          SingleActivator(
-            flutter.LogicalKeyboardKey.keyV,
-            meta: PlatformInfos.isMacKeyboardPlatform,
-            control: !PlatformInfos.isMacKeyboardPlatform,
-          ): () async {
-            handlePaste!();
-          },
-        SingleActivator(
-          flutter.LogicalKeyboardKey.keyC,
-          meta: PlatformInfos.isMacKeyboardPlatform,
-          control: !PlatformInfos.isMacKeyboardPlatform,
-        ): () {
-          controller?.copyText();
-        },
         const SingleActivator(
           flutter.LogicalKeyboardKey.enter,
           shift: true,
@@ -72,13 +53,6 @@ class InputBarShortcuts extends StatelessWidget {
           flutter.LogicalKeyboardKey.arrowDown,
         ): () {
           focusSuggestionController?.down();
-        },
-        SingleActivator(
-          flutter.LogicalKeyboardKey.keyX,
-          meta: PlatformInfos.isMacKeyboardPlatform,
-          control: !PlatformInfos.isMacKeyboardPlatform,
-        ): () {
-          controller?.cutText();
         },
       },
       child: child,
