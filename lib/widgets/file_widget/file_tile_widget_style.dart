68e5b32be581cfa84b069b3c4ef39744993df7a7
sherlock <quangkhai28999@gmail.com>
2023-12-27 11:32:30 +0700

    TW-1171: update style solutions for message bubble and sendfiledialog

---
+++ lib/widgets/file_widget/file_tile_widget_style.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_matrix_html/color_extension.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
+import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
+
+class FileTileWidgetStyle {
+  const FileTileWidgetStyle();
+
+  EdgeInsets get paddingFileTileAll =>
+      const EdgeInsets.only(left: 8.0, right: 16.0);
+
+  Color get backgroundColor =>
+      LinagoraSysColors.material().surfaceTint.withOpacity(0.08);
+
+  BorderRadiusGeometry get borderRadius => BorderRadius.circular(12);
+
+  EdgeInsets get paddingIcon => const EdgeInsets.only(right: 8);
+
+  CrossAxisAlignment get crossAxisAlignment => CrossAxisAlignment.start;
+
+  double get iconSize => 48;
+
+  double get imageSize => 40;
+
+  Color? get fileInfoColor => LinagoraRefColors.material().tertiary[20];
+
+  TextStyle highlightTextStyle(BuildContext context) {
+    return TextStyle(
+      color: Theme.of(context).colorScheme.onBackground,
+      fontWeight: FontWeight.bold,
+      backgroundColor: CssColor.fromCss('gold'),
+    );
+  }
+
+  TextStyle? textStyle(BuildContext context) {
+    return Theme.of(context).textTheme.bodyLarge?.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        );
+  }
+
+  TextStyle textInformationStyle(BuildContext context) {
+    return Theme.of(context).textTheme.bodySmall!.copyWith(
+          color: fileInfoColor,
+        );
+  }
+
+  EdgeInsets get imagePadding => const EdgeInsets.all(4.0);
+
+  Widget get paddingBottomText => const SizedBox(height: 0.0);
+
+  Widget get paddingRightIcon => const SizedBox(width: 8.0);
+}
