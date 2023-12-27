751ba713b82df714e1c177c23771e96974afaa4f
sherlock <quangkhai28999@gmail.com>
2023-12-27 15:45:13 +0700

    TW-1162: copy multiple images in web from platform

--- lib/presentation/mixins/paste_image_mixin.dart
+++ lib/presentation/mixins/paste_image_mixin.dart
@@ -1,7 +1,4 @@
-import 'dart:typed_data';
-
 import 'package:fluffychat/pages/chat/send_file_dialog.dart';
-import 'package:fluffychat/presentation/model/clipboard/clipboard_image_info.dart';
 import 'package:fluffychat/utils/clipboard.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/twake_snackbar.dart';
@@ -22,41 +19,28 @@ mixin PasteImageMixin {
       Logs().e('PasteImageMixin::pasteImage(): not readable image format');
       return;
     }
-    Uint8List? imageData;
-    ClipboardImageInfo? imageClipboard;
+    List<MatrixFile?>? matrixFiles;
     if (PlatformInfos.isWeb) {
-      imageData = await Clipboard.instance
-          .pasteImageUsingBytes(reader: clipboardReader);
-    } else {
-      imageClipboard = await Clipboard.instance.pasteImageUsingStream();
-      if (imageClipboard == null) {
-        TwakeSnackBar.show(context, L10n.of(context)!.pasteImageFailed);
-        return;
-      }
-      // FIXME: need to update the SendFileDialog to have FileInfo inside
-      // after update we can use stream to read files instead of convert into raw image data
-      final data = await imageClipboard.stream.toList();
-      imageData = Uint8List.fromList(
-        data.expand((Uint8List uint8List) => uint8List).toList(),
-      );
+      matrixFiles = await Clipboard.instance
+          .pasteImagesUsingBytes(reader: clipboardReader);
     }
-    if (imageData == null || imageData.isEmpty) {
+    if (matrixFiles == null || matrixFiles.isEmpty) {
       TwakeSnackBar.show(context, L10n.of(context)!.pasteImageFailed);
       return;
     }
-
+    final nonNullableFiles = matrixFiles
+        .where(
+          (matrixFile) => matrixFile != null,
+        )
+        .cast<MatrixFile>()
+        .toList();
     await showDialog(
       context: context,
       useRootNavigator: PlatformInfos.isWeb,
       builder: (context) {
         return SendFileDialog(
           room: room,
-          files: [
-            MatrixImageFile(
-              name: imageClipboard?.fileName ?? 'copied',
-              bytes: imageData,
-            ),
-          ],
+          files: nonNullableFiles,
         );
       },
     );
