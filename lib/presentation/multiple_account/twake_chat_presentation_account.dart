1ff0270c3922c3e3d17b6ae3b7c228c18c169f16
HuyNguyen <quanghnguyen@linagora.com>
2023-12-28 16:08:58 +0700

    TW-1193: Set account as active
    (cherry picked from commit 285128ba6c2227117dec888ce4eb8d34a729eb1c)

--- lib/presentation/multiple_account/twake_chat_presentation_account.dart
+++ lib/presentation/multiple_account/twake_chat_presentation_account.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:linagora_design_flutter/multiple_account/models/twake_presentation_account.dart';
+import 'package:matrix/matrix.dart';
 
 class TwakeChatPresentationAccount extends TwakePresentationAccount {
+  final Client clientAccount;
+
   const TwakeChatPresentationAccount({
+    required this.clientAccount,
     required String accountName,
     required String accountId,
     required Widget avatar,
@@ -13,4 +17,13 @@ class TwakeChatPresentationAccount extends TwakePresentationAccount {
           avatar: avatar,
           accountActiveStatus: accountActiveStatus,
         );
+
+  @override
+  List<Object?> get props => [
+        clientAccount,
+        accountName,
+        accountId,
+        avatar,
+        accountActiveStatus,
+      ];
 }
