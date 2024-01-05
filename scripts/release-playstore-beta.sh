51d50016b30e34eda4896f775a1f8c1c28e0e6b0
Nguyen Thai <tknguyen@linagora.com>
2024-01-05 14:28:14 +0700

    Fix working directory for prepare scripts

--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter build appbundle --release
+cd android
 bundle exec fastlane deploy_internal_test
