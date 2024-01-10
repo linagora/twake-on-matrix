96ad8735c54c6d24d7251ad37504c5d279ce16e1
HuyNguyen <quanghnguyen@linagora.com>
2024-01-10 15:31:58 +0700

    TW-1302: Create empty search widget
    (cherry picked from commit 388f162d576e5a88251eb60e4c4f1a55b937e7ae)

--- lib/resource/image_paths.dart
+++ lib/resource/image_paths.dart
@@ -45,6 +45,7 @@ class ImagePaths {
   static String get icUnpin => _getImagePath('ic_unpin.svg');
   static String get icJumpTo => _getImagePath('ic_jump_to.svg');
   static String get logoTwakeWelcome => _getImagePath('logo_twake_welcome.svg');
+  static String get icEmptySearch => _getImagePath('ic_empty_search.svg');
 
   static String _getImagePath(String imageName) {
     return AssetsPaths.images + imageName;
