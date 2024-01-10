388f162d576e5a88251eb60e4c4f1a55b937e7ae
HuyNguyen <quanghnguyen@linagora.com>
2024-01-10 15:31:58 +0700

    TW-1302: Create empty search widget

---
+++ lib/widgets/search/empty_search_widget_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EmptySearchWidgetStyle {
+  static const double iconWidth = 136;
+
+  static const double iconHeight = 120;
+
+  static const EdgeInsetsGeometry textPadding = EdgeInsets.only(top: 8);
+
+  static const EdgeInsetsGeometry bodyPadding = EdgeInsets.only(top: 48);
+}
