680975de34bba7d37eec530bc29db369021d46f1
Julian KOUNE <jkoune@linagora.com>
2024-01-12 12:04:13 +0100

    TW-1255: Support selectable pinned events
    (cherry picked from commit f318fb3d1b549d4d9d5332ae064f8ed4b281259a)

--- lib/domain/enums/pinned_messages_action_enum.dart
+++ lib/domain/enums/pinned_messages_action_enum.dart
@@ -1,4 +1,5 @@
 enum PinnedMessagesActionEnum {
   pin,
   unpin,
+  unpinAll,
 }
--- lib/pages/chat/chat_pinned_events/pinned_messages_style.dart
+++ lib/pages/chat/chat_pinned_events/pinned_messages_style.dart
@@ -1,6 +1,8 @@
 import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/resource/image_paths.dart';
 import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PinnedMessagesStyle {
   static final responsiveUtils = getIt.get<ResponsiveUtils>();
@@ -12,7 +14,15 @@ class PinnedMessagesStyle {
 
   static const SizedBox paddingIconAndUnpin = SizedBox(width: 4.0);
 
-  static const double unpinButtonHeight = 72;
+  static const double unpinButtonHeight = 56;
+  static const double unpinButtonWidth = 556;
+  static const EdgeInsets actionBarParentPadding = EdgeInsets.all(16.0);
+  static const EdgeInsets actionBarPadding = EdgeInsets.symmetric(
+    horizontal: 8.0,
+  );
+  static const double actionBarBorderRadius = 16.0;
+
+  static Widget unpinIcon() => SvgPicture.asset(ImagePaths.icUnpin, height: 18);
 
   static const double paddingAllContextMenuItem = 12;
 
