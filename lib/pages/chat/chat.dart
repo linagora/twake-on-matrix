ed667eceb36fbc629c201201cbb43e4cfef4b376
Julian KOUNE <jkoune@linagora.com>
2024-01-18 18:23:50 +0100

    TW-1358: remove unnecessary select actions
    (cherry picked from commit 10e03f27c72cc8cb25ae32788b8cb5de057caa28)

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -729,10 +729,10 @@ class ChatController extends State<Chat>
         .any((cl) => selectedEvents.first.senderId == cl!.userID);
   }
 
-  void forwardEventsAction() async {
-    if (selectedEvents.length == 1) {
+  void forwardEventsAction({Event? event}) async {
+    if (selectedEvents.isEmpty && event != null) {
       Matrix.of(context).shareContent =
-          selectedEvents.first.getDisplayEvent(timeline!).content;
+          event.getDisplayEvent(timeline!).content;
       Logs().d(
         "forwardEventsAction():: shareContent: ${Matrix.of(context).shareContent}",
       );
@@ -1316,8 +1316,7 @@ class ChatController extends State<Chat>
         replyAction(replyTo: event);
         break;
       case ChatHorizontalActionMenu.forward:
-        onSelectMessage(event);
-        forwardEventsAction();
+        forwardEventsAction(event: event);
         break;
       case ChatHorizontalActionMenu.more:
         handleContextMenuAction(
@@ -1371,15 +1370,13 @@ class ChatController extends State<Chat>
         onSelectMessage(event);
         break;
       case ChatContextMenuActions.copyMessage:
-        onSelectMessage(event);
-        copySingleEventAction();
+        event.copy(context, timeline!);
         break;
       case ChatContextMenuActions.pinChat:
         pinEventAction(event);
         break;
       case ChatContextMenuActions.forward:
-        onSelectMessage(event);
-        forwardEventsAction();
+        forwardEventsAction(event: event);
         break;
       case ChatContextMenuActions.downloadFile:
         downloadFileAction(context, event);
