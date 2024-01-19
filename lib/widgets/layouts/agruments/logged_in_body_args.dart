c23b60339d4f3b98947907d810d61d1bdd2afb35
sherlock <quangkhai28999@gmail.com>
2024-01-19 10:26:57 +0700

    TW-1262: go to room when click to notification in other accounts

--- lib/widgets/layouts/agruments/logged_in_body_args.dart
+++ lib/widgets/layouts/agruments/logged_in_body_args.dart
@@ -1,12 +1,16 @@
 import 'package:fluffychat/widgets/layouts/agruments/app_adaptive_scaffold_body_args.dart';
 
 class LoggedInBodyArgs extends AbsAppAdaptiveScaffoldBodyArgs {
+  final String? roomIdFromNoti;
+
   const LoggedInBodyArgs({
     required super.newActiveClient,
+    this.roomIdFromNoti,
   });
 
   @override
   List<Object?> get props => [
         newActiveClient,
+        roomIdFromNoti,
       ];
 }
