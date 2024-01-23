f53f743b6813fa2a7a994368dfba227f675e965c
sherlock <quangkhai28999@gmail.com>
2024-01-23 17:35:59 +0700

    TW-1381: add delete button for each item in send file dialog
    (cherry picked from commit 38c474a0cbd579d77e39168e335859c005e7309b)

--- lib/pages/chat/send_file_dialog/send_file_dialog_view.dart
+++ lib/pages/chat/send_file_dialog/send_file_dialog_view.dart
@@ -1,18 +1,13 @@
 import 'package:fluffychat/pages/chat/input_bar/input_bar.dart';
+import 'files_listview_widget.dart';
+import 'media_page_view_widget.dart';
 import 'send_file_dialog.dart';
-import 'package:fluffychat/pages/image_viewer/image_viewer.dart';
 import 'package:fluffychat/presentation/enum/chat/send_media_with_caption_status_enum.dart';
-import 'package:fluffychat/presentation/extensions/send_file_web_extension.dart';
-import 'package:fluffychat/utils/interactive_viewer_gallery.dart';
-import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_file_extension.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
-import 'package:fluffychat/widgets/file_widget/file_tile_widget.dart';
-import 'package:fluffychat/widgets/hero_page_route.dart';
 import 'package:flutter/material.dart';
 import 'send_file_dialog_style.dart';
 
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:matrix/matrix.dart';
 
 class SendFileDialogView extends StatelessWidget {
   final SendFileDialogController controller;
@@ -40,22 +35,28 @@ class SendFileDialogView extends StatelessWidget {
                 children: [
                   Padding(
                     padding: SendFileDialogStyle.headerPadding,
-                    child: Text(
-                      controller.isSendMediaWithCaption
-                          ? L10n.of(context)!.sendImages(1)
-                          : L10n.of(context)!
-                              .sendFiles(controller.files.length),
-                      style: Theme.of(context).textTheme.titleLarge,
+                    child: ValueListenableBuilder(
+                      valueListenable: controller.filesNotifier,
+                      builder: (context, files, child) {
+                        return Text(
+                          controller.isSendMediaWithCaption
+                              ? L10n.of(context)!.sendImages(1)
+                              : L10n.of(context)!.sendFiles(files.length),
+                          style: Theme.of(context).textTheme.titleLarge,
+                        );
+                      },
                     ),
                   ),
                 ],
               ),
               const SizedBox(height: 16.0),
               controller.isSendMediaWithCaption
-                  ? _MediaPageViewWidget(files: controller.files)
-                  : _FilesListViewWidget(
-                      files: controller.files,
+                  ? MediaPageViewWidget(files: controller.filesNotifier)
+                  : FilesListViewWidget(
+                      filesNotifier: controller.filesNotifier,
                       room: controller.widget.room,
+                      onRemoveFile: controller.onRemoveFile,
+                      thumbnails: controller.thumbnails,
                     ),
               const SizedBox(height: 16.0),
               InkWell(
@@ -141,124 +142,6 @@ class SendFileDialogView extends StatelessWidget {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _MediaPageViewWidget extends StatelessWidget {
-  const _MediaPageViewWidget({
-    required this.files,
-  });
-
-  final List<MatrixFile> files;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context, rootNavigator: PlatformInfos.isWeb).push(
-          HeroPageRoute(
-            builder: (context) {
-              return InteractiveViewerGallery(
-                itemBuilder: ImageViewer(
-                  imageData: files.first.bytes,
-                ),
-              );
-            },
-          ),
-        );
-      },
-      child: SizedBox(
-        width: SendFileDialogStyle.imageSize,
-        height: SendFileDialogStyle.imageSize,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(
-            SendFileDialogStyle.imageBorderRadius,
-          ),
-          child: files.first.bytes != null
-              ? Image.memory(
-                  files.first.bytes!,
-                  fit: BoxFit.cover,
-                )
-              : const SizedBox.shrink(),
-        ),
-      ),
-    );
-  }
-}
-
-class _FilesListViewWidget extends StatelessWidget {
-  final List<MatrixFile> files;
-
-  final Room? room;
-
-  const _FilesListViewWidget({
-    required this.files,
-    this.room,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(
-        maxHeight: SendFileDialogStyle.maxHeightFilesListView,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(
-          SendFileDialogStyle.listViewBorderRadius,
-        ),
-        color: SendFileDialogStyle.listViewBackgroundColor(context),
-      ),
-      child: SingleChildScrollView(
-        child: ListView.builder(
-          itemCount: files.length,
-          shrinkWrap: true,
-          padding: SendFileDialogStyle.paddingFilesListView,
-          itemBuilder: (BuildContext context, int index) {
-            final file = files[index];
-            final child = Padding(
-              padding: SendFileDialogStyle.paddingFileTile,
-              child: FileTileWidget(
-                mimeType: file.mimeType,
-                filename: file.name,
-                fileType: file.fileExtension,
-                sizeString: file.sizeString,
-                backgroundColor:
-                    SendFileDialogStyle.listViewBackgroundColor(context),
-              ),
-            );
-            Future<MatrixImageFile?>? getThumbnailFuture;
-            if (file is MatrixImageFile) {
-              getThumbnailFuture = room?.generateThumbnail(file);
-            } else if (file is MatrixVideoFile) {
-              getThumbnailFuture = room?.generateVideoThumbnail(file);
-            } else {
-              return child;
-            }
-
-            return FutureBuilder(
-              future: getThumbnailFuture,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return child;
-                }
-                return Padding(
-                  padding: SendFileDialogStyle.paddingFileTile,
-                  child: FileTileWidget(
-                    mimeType: file.mimeType,
-                    filename: file.name,
-                    fileType: file.fileExtension,
-                    sizeString: file.sizeString,
-                    backgroundColor:
-                        SendFileDialogStyle.listViewBackgroundColor(context),
-                    imageBytes: snapshot.data?.bytes,
-                  ),
-                );
-              },
-            );
-          },
         ),
       ),
     );
