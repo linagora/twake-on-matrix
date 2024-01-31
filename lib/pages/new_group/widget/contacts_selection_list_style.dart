c506b8c83b775eabd9eab66b84c61a0fdc2f15fd
HuyNguyen <quanghnguyen@linagora.com>
2024-01-31 10:40:18 +0700

    TW-1411: Update padding for no search result in selection members
    (cherry picked from commit 37fb89ef97680821957056b970a2fbf1e5322315)

--- lib/pages/new_group/widget/contacts_selection_list_style.dart
+++ lib/pages/new_group/widget/contacts_selection_list_style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 class ContactsSelectionListStyle {
-  static const notFoundPadding = EdgeInsetsDirectional.only(start: 8.0);
+  static const notFoundPadding = EdgeInsetsDirectional.only(top: 12.0);
   static const listPaddingTop = 8.0;
 }
