32b5eeae8500394fe053215b3c5f5c826f2bfb94
HuyNguyen <quanghnguyen@linagora.com>
2024-02-01 10:00:50 +0700

    TW-1437: Update background color for Contacts tab

--- lib/pages/contacts_tab/contacts_tab_view.dart
+++ lib/pages/contacts_tab/contacts_tab_view.dart
@@ -3,6 +3,7 @@ import 'package:fluffychat/pages/contacts_tab/contacts_tab.dart';
 import 'package:fluffychat/pages/contacts_tab/contacts_tab_body_view.dart';
 import 'package:fluffychat/pages/contacts_tab/contacts_tab_view_style.dart';
 import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 
 class ContactsTabView extends StatelessWidget {
   final ContactsTabController contactsController;
@@ -27,6 +28,7 @@ class ContactsTabView extends StatelessWidget {
           textEditingController: contactsController.textEditingController,
         ),
       ),
+      backgroundColor: LinagoraSysColors.material().onPrimary,
       bottomNavigationBar: bottomNavigationBar,
       body: ContactsTabBodyView(contactsController),
     );
