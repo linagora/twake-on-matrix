c86ca2d007785bf77ac8e31e034e48ebd770c0e2
sherlock <quangkhai28999@gmail.com>
2024-02-06 15:11:33 +0700

    TW-217: update new UX for share and forward screen
    (cherry picked from commit 88fa22ba4e1c1dbbf587d2536f93be99da0f6e2f)

---
+++ lib/pages/forward/recent_chat_list_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecentChatListStyle {
+  static BorderRadius borderRadiusItem = BorderRadius.circular(16);
+
+  static const paddingVerticalBetweenItem = EdgeInsetsDirectional.only(
+    top: 8.0,
+    bottom: 8.0,
+  );
+
+  static const paddingHorizontalBetweenItem = EdgeInsetsDirectional.only(
+    start: 8.0,
+    end: 6.0,
+  );
+}
