01ba02871318ca9117166b290ba32d00cac7952b
sherlock <quangkhai28999@gmail.com>
2024-02-21 11:12:24 +0700

    TW-713: update image viewer view

---
+++ lib/pages/image_viewer/context_menu_item_image_viewer.dart
@@ -0,0 +1,81 @@
+import 'package:fluffychat/pages/image_viewer/context_menu_item_image_viewer_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ContextMenuItemImageViewer extends StatelessWidget {
+  final String title;
+
+  final IconData? icon;
+
+  final String? imagePath;
+
+  final Color? color;
+
+  final bool haveDivider;
+
+  final VoidCallback? onTap;
+
+  const ContextMenuItemImageViewer({
+    super.key,
+    this.icon,
+    this.imagePath,
+    this.color,
+    this.haveDivider = true,
+    this.onTap,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: SizedBox(
+          width: ContextMenuItemImageViewerStyle.width,
+          height: ContextMenuItemImageViewerStyle.height,
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(width: 12),
+                    if (icon != null)
+                      Icon(
+                        icon,
+                        color: color ?? Theme.of(context).colorScheme.onSurface,
+                      ),
+                    if (imagePath != null)
+                      SvgPicture.asset(
+                        imagePath!,
+                        colorFilter: ColorFilter.mode(
+                          color ?? Theme.of(context).colorScheme.onSurface,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    const SizedBox(width: 12),
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: color ??
+                                Theme.of(context).colorScheme.onSurface,
+                            letterSpacing: ContextMenuItemImageViewerStyle
+                                .titleLetterSpace,
+                          ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              if (haveDivider)
+                Divider(
+                  height: ContextMenuItemImageViewerStyle.dividerHeight,
+                  color: ContextMenuItemImageViewerStyle.dividerColor(context),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
