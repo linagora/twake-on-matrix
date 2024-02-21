8ff0ed05b4ee128272fcdafcd19862208e96b328
sherlock <quangkhai28999@gmail.com>
2024-02-21 11:12:24 +0700

    TW-713: update image viewer view
    (cherry picked from commit dae6b67868dc8ed28bfcbcb98c49c0fcb86e57fc)

---
+++ lib/pages/image_viewer/context_menu_item_image_viewer_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContextMenuItemImageViewerStyle {
+  static const double width = 200;
+
+  static const double height = 48;
+
+  static const double dividerHeight = 1;
+
+  static Color dividerColor(BuildContext context) =>
+      Theme.of(context).colorScheme.surfaceTint.withOpacity(0.16);
+
+  static double titleLetterSpace = -0.15;
+}
