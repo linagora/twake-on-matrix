df882fa1a3f6b15515be0bc6d313801620ccef6a
hieubt <trunghieu.bui201@gmail.com>
2024-02-23 11:39:28 +0700

    TW-1153: Add filter duplicated phone numbers
    (cherry picked from commit 712d7c8a7c2a616355670d0789bca5bb04309e0a)

--- lib/data/datasource_impl/contact/phonebook_contact_datasource_impl.dart
+++ lib/data/datasource_impl/contact/phonebook_contact_datasource_impl.dart
@@ -7,7 +7,8 @@ class PhonebookContactDatasourceImpl implements PhonebookContactDatasource {
   Future<List<Contact>> fetchContacts() async {
     final phonebookContacts =
         await FlutterContacts.getContacts(withProperties: true);
-    return phonebookContacts
+
+    final listAllContacts = phonebookContacts
         .expand(
           (contact) => [
             ...contact.emails.map(
@@ -25,5 +26,45 @@ class PhonebookContactDatasourceImpl implements PhonebookContactDatasource {
           ],
         )
         .toList();
+
+    return removeDuplicatedPhoneNumbers(listAllContacts);
+  }
+
+  List<Contact> removeDuplicatedPhoneNumbers(List<Contact> listContacts) {
+    final listVisitedPhoneNumbers = <String>[];
+    final listFilteredContacts = <Contact>[];
+
+    for (final contact in listContacts) {
+      final phoneNumber = contact.phoneNumber;
+      if (phoneNumber != null) {
+        final normalizedPhoneNumber = normalizePhoneNumber(phoneNumber);
+        if (listVisitedPhoneNumbers.contains(normalizedPhoneNumber)) {
+          final contactsWithSamePhoneNumber =
+              listFilteredContacts.where((filteredContact) {
+            final filteredPhoneNumber = filteredContact.phoneNumber;
+            if (filteredPhoneNumber != null) {
+              return filteredContact.displayName == contact.displayName &&
+                  normalizePhoneNumber(filteredPhoneNumber) ==
+                      normalizedPhoneNumber;
+            }
+            return false;
+          });
+          if (contactsWithSamePhoneNumber.isEmpty) {
+            listFilteredContacts.add(contact);
+          } else {
+            continue;
+          }
+        } else {
+          listVisitedPhoneNumbers.add(normalizedPhoneNumber);
+          listFilteredContacts.add(contact);
+        }
+      }
+    }
+
+    return listFilteredContacts;
+  }
+
+  String normalizePhoneNumber(String phoneNumber) {
+    return phoneNumber.replaceAll(RegExp(r'\D'), '');
   }
 }
