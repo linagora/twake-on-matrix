f5f83d772ff179a746768b5a094af9af076989ed
HuyNguyen <quanghnguyen@linagora.com>
2024-02-26 09:52:49 +0700

    TW-1489: Fix select mode cannot rebuild in chat list

--- lib/pages/contacts_tab/contacts_appbar_style.dart
+++ lib/pages/contacts_tab/contacts_appbar_style.dart
@@ -3,13 +3,22 @@ import 'package:flutter/cupertino.dart';
 class ContactsAppbarStyle {
   static const Size preferredSizeAppBar = Size.fromHeight(120);
 
+  static const double iconSize = 24.0;
+
+  static const double textFieldHeight = 64.0;
+
+  static const double toolbarHeight = 56.0;
+
+  static const double leadingWidth = 76.0;
+
+  static const double textFieldBorderRadius = 24.0;
+
+  static AlignmentGeometry alignment = AlignmentDirectional.centerStart;
+
   static EdgeInsetsDirectional appbarPadding =
       const EdgeInsetsDirectional.symmetric(
     horizontal: 16.0,
   );
 
-  static const EdgeInsetsDirectional titlePadding = EdgeInsetsDirectional.only(
-    bottom: 24,
-    top: 4,
-  );
+  static EdgeInsetsDirectional contentPadding = EdgeInsetsDirectional.zero;
 }
