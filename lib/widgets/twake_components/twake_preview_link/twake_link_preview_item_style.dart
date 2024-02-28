ae2669c719b2240b35cca4036ef3e7d2c311904d
HuyNguyen <quanghnguyen@linagora.com>
2024-02-29 00:38:39 +0700

    TW-1498: Improve preview link to prevent resizing

--- lib/widgets/twake_components/twake_preview_link/twake_link_preview_item_style.dart
+++ lib/widgets/twake_components/twake_preview_link/twake_link_preview_item_style.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 class TwakeLinkPreviewItemStyle {
   static const double radiusBorder = 20;
 
+  static const double linkIconSize = 40;
+
+  static const SizedBox skeletonizerTextPadding = SizedBox(height: 8.0);
+
   static const EdgeInsets paddingPreviewImage = EdgeInsets.only(left: 8.0);
 
   static const EdgeInsetsDirectional paddingTitle = EdgeInsetsDirectional.only(
     start: 8,
     end: 8,
     top: 8,
-    bottom: 2,
+    bottom: 8,
   );
 
   static const EdgeInsetsDirectional paddingSubtitle =
@@ -29,6 +33,6 @@ class TwakeLinkPreviewItemStyle {
     top: 8.0,
   );
 
-  static const double heightMxcImagePreview = 96;
-  static const double maxHeightPreviewLarge = 480;
+  static const double heightMxcImagePreview = 120;
+  static const double maxHeightPreviewItem = 120;
 }
