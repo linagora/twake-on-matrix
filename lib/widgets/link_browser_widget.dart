a14e855bf9fa234f4b683a2550150a10cfd8d23c
HuyNguyen <quanghnguyen@linagora.com>
2024-03-11 12:08:58 +0700

    TW-984: Create `LinkBrowserWidget`
    (cherry picked from commit fe7deee904b442105335999441c57b52768174f5)

---
+++ lib/widgets/link_browser_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+
+class LinkBrowserWidget extends StatelessWidget {
+  final Uri uri;
+  final Widget child;
+
+  const LinkBrowserWidget({Key? key, required this.uri, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Link(uri: uri, builder: (context, function) => child);
+  }
+}
