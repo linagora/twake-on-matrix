0bdaf732aedeb40b52255b3f49865d650298d3fb
HuyNguyen <quanghnguyen@linagora.com>
2024-03-11 12:11:25 +0700

    TW-984: Implement func and UI for app grid dashboard

---
+++ lib/pages/app_grid/app_grid_dashboard_controller.dart
@@ -0,0 +1,91 @@
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/di/global/get_it_initializer.dart';
+import 'package:fluffychat/domain/app_state/app_grid/get_app_grid_configuration_state.dart';
+import 'package:fluffychat/domain/model/app_grid/linagora_applications.dart';
+import 'package:fluffychat/domain/usecase/app_grid/get_app_grid_configuration_interactor.dart';
+import 'package:fluffychat/pages/app_grid/app_grid_dashboard_view.dart';
+import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
+
+class AppGridDashboard extends StatefulWidget {
+  const AppGridDashboard({super.key});
+
+  @override
+  State<AppGridDashboard> createState() => AppGridDashboardController();
+}
+
+class AppGridDashboardController extends State<AppGridDashboard> {
+  final _getAppGridConfigurationInteractor =
+      getIt.get<GetAppGridConfigurationInteractor>();
+
+  final isOpenAppGridDashboardNotifier = ValueNotifier<bool>(false);
+
+  final linagoraApplications = ValueNotifier<LinagoraApplications?>(null);
+
+  void _getAppGridConfiguration() {
+    _getAppGridConfigurationInteractor
+        .execute(
+      appGridConfigurationPath: AppConfig.appGridConfigurationPath,
+    )
+        .listen((event) {
+      event.fold(
+        (failure) {
+          if (failure is GetAppGridConfigurationFailure) {
+            Logs().e(
+              'AppGridDashboardController::_getAppGridConfiguration(): $failure',
+            );
+          }
+        },
+        (success) {
+          if (success is GetAppGridConfigurationSuccess) {
+            handleGetAppGridDashboardSuccess(success.linagoraApplications);
+          }
+        },
+      );
+    });
+  }
+
+  void handleGetAppGridDashboardSuccess(LinagoraApplications linagoraApps) {
+    try {
+      Logs().d(
+        'AppGridDashboardController::handleGetAppGridDashboardSuccess(): $linagoraApps',
+      );
+      linagoraApplications.value = linagoraApps;
+    } on FlutterError catch (e) {
+      Logs().e(
+        'AppGridDashboardController::handleShowAppDashboard(): $e',
+      );
+    }
+  }
+
+  void hideAppGridDashboard() {
+    try {
+      if (isOpenAppGridDashboardNotifier.value == false) return;
+      isOpenAppGridDashboardNotifier.value = false;
+    } on FlutterError catch (e) {
+      Logs().e(
+        'AppGridDashboardController::hideAppGridDashboard(): $e',
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    _getAppGridConfiguration();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    isOpenAppGridDashboardNotifier.dispose();
+    linagoraApplications.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppGridDashboardView(
+      controller: this,
+    );
+  }
+}
---
+++ lib/pages/app_grid/app_grid_dashboard_item.dart
@@ -0,0 +1,67 @@
+import 'package:fluffychat/domain/model/app_grid/linagora_app.dart';
+import 'package:fluffychat/pages/app_grid/app_grid_dashboard_item_style.dart';
+import 'package:fluffychat/resource/image_paths.dart';
+import 'package:fluffychat/widgets/link_browser_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AppGridDashboardItem extends StatelessWidget {
+  static const _webOnlyWindowName = '_blank';
+
+  final LinagoraApp app;
+
+  const AppGridDashboardItem(this.app, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LinkBrowserWidget(
+      uri: app.appUri,
+      child: TapRegion(
+        behavior: HitTestBehavior.opaque,
+        onTapInside: (_) async {
+          await launchUrl(
+            Uri.parse(app.appUri.toString()),
+            webOnlyWindowName: _webOnlyWindowName,
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: Container(
+            width: AppGridDashboardItemStyle.itemWidth,
+            padding: AppGridDashboardItemStyle.padding,
+            child: Column(
+              children: [
+                app.iconName.endsWith("svg")
+                    ? SvgPicture.asset(
+                        ImagePaths.getConfigurationImagePath(app.iconName),
+                        width: AppGridDashboardItemStyle.iconAppSize,
+                        height: AppGridDashboardItemStyle.iconAppSize,
+                        fit: BoxFit.fill,
+                      )
+                    : Image.asset(
+                        ImagePaths.getConfigurationImagePath(app.iconName),
+                        width: AppGridDashboardItemStyle.iconAppSize,
+                        height: AppGridDashboardItemStyle.iconAppSize,
+                        fit: BoxFit.fill,
+                      ),
+                Padding(
+                  padding: AppGridDashboardItemStyle.textPadding,
+                  child: Text(
+                    app.appName,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
