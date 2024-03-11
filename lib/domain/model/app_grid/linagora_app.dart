8522d926f4681673c2d0484e33d7befe0832e0c2
HuyNguyen <quanghnguyen@linagora.com>
2024-03-11 12:08:28 +0700

    TW-984: Implement app grid data in domain layer

---
+++ lib/domain/model/app_grid/linagora_app.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'linagora_app.g.dart';
+
+@JsonSerializable(explicitToJson: true, includeIfNull: false)
+class LinagoraApp with EquatableMixin {
+  @JsonKey(name: 'appName')
+  final String appName;
+
+  @JsonKey(name: 'icon')
+  final String iconName;
+
+  @JsonKey(name: 'appLink')
+  final Uri appUri;
+
+  final String? androidPackageId;
+  final String? iosUrlScheme;
+  final String? iosAppStoreLink;
+
+  LinagoraApp(
+    this.appName,
+    this.iconName,
+    this.appUri, {
+    this.androidPackageId,
+    this.iosUrlScheme,
+    this.iosAppStoreLink,
+  });
+
+  factory LinagoraApp.fromJson(Map<String, dynamic> json) =>
+      _$LinagoraAppFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LinagoraAppToJson(this);
+
+  @override
+  List<Object?> get props => [
+        appName,
+        iconName,
+        appUri,
+        androidPackageId,
+        iosUrlScheme,
+        iosAppStoreLink,
+      ];
+}
