b882899ce591e557c7cc8092ed11a318a158f234
hieubt <trunghieu.bui201@gmail.com>
2024-03-13 21:14:27 +0700

    TW-1421: ellipsis navigation label

--- lib/widgets/layouts/adaptive_layout/adaptive_scaffold_primary_navigation_style.dart
+++ lib/widgets/layouts/adaptive_layout/adaptive_scaffold_primary_navigation_style.dart
@@ -6,6 +6,13 @@ class AdaptiveScaffoldPrimaryNavigationStyle {
   static const EdgeInsetsDirectional dividerPadding =
       EdgeInsetsDirectional.only(bottom: 16);
 
+  static TextStyle? labelTextStyle(BuildContext context) {
+    return Theme.of(context).textTheme.labelMedium?.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+          overflow: TextOverflow.ellipsis,
+        );
+  }
+
   static const double primaryNavigationWidth = 80;
   static const double avatarSize = 56;
   static const double dividerSize = 2;
