245e6e41608af8314ba4197da21735fbdad1eb32
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-03-16 12:10:04 +0000

    TW-1456: profile info views created

---
+++ lib/pages/profile_info/profile_info_view_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProfileInfoViewStyle {
+
+  static const EdgeInsetsGeometry backIconPadding =
+      EdgeInsets.symmetric(vertical: 8, horizontal: 4);
+  static const EdgeInsets navigationAppBarPadding =
+      EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0);
+}
