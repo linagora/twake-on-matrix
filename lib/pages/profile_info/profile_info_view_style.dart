5dfca218c508eedaf7c6b98c06e4e19f0fa5a523
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-03-18 09:58:01 +0000

    TW-1456: participant list item style created
    (cherry picked from commit 3bb724f2b4f599a9faf669a6c62fc93d65ce4b40)

--- lib/pages/profile_info/profile_info_view_style.dart
+++ lib/pages/profile_info/profile_info_view_style.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ProfileInfoViewStyle {
-
   static const EdgeInsetsGeometry backIconPadding =
       EdgeInsets.symmetric(vertical: 8, horizontal: 4);
   static const EdgeInsets navigationAppBarPadding =
