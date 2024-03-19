350755489ef0363609b287bebc6c6b44e3c81a44
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-03-19 20:54:34 +0100

    TW-1456: optionnal duration param added to snackbar
    (cherry picked from commit fecf45049b445340dae915cc26ddf6e42bf14f61)

--- lib/pages/profile_info/profile_info_body/profile_info_body.dart
+++ lib/pages/profile_info/profile_info_body/profile_info_body.dart
@@ -7,6 +7,10 @@ import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/app_state/contact/lookup_match_contact_state.dart';
 import 'package:fluffychat/domain/usecase/contacts/lookup_match_contact_interactor.dart';
 import 'package:fluffychat/pages/profile_info/profile_info_body/profile_info_body_view.dart';
+import 'package:fluffychat/presentation/model/presentation_contact_constant.dart';
+import 'package:fluffychat/presentation/model/search/presentation_search.dart';
+import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,14 +19,11 @@ import 'package:matrix/matrix.dart';
 class ProfileInfoBody extends StatefulWidget {
   const ProfileInfoBody({
     required this.user,
-    required this.parentContext,
     this.onNewChatOpen,
     Key? key,
   }) : super(key: key);
 
   final User? user;
-
-  final BuildContext parentContext;
 
   final void Function()? onNewChatOpen;
 
@@ -43,8 +44,6 @@ class ProfileInfoBodyController extends State<ProfileInfoBody> {
 
   User? get user => widget.user;
 
-  BuildContext get parentContext => widget.parentContext;
-
   bool get isOwnProfile => user?.id == user?.room.client.userID;
 
   void lookupMatchContactAction() {
@@ -58,9 +57,68 @@ class ProfileInfoBodyController extends State<ProfileInfoBody> {
         );
   }
 
-  void openNewChat(String roomId) {
-    parentContext.go('/rooms/$roomId');
-    if (widget.onNewChatOpen != null) widget.onNewChatOpen!();
+  void openNewChat() {
+    if (user == null) return;
+    final roomId = Matrix.of(context).client.getDirectChatFromUserId(user!.id);
+
+    if (roomId == null) {
+      if (!PlatformInfos.isMobile && widget.onNewChatOpen != null) {
+        widget.onNewChatOpen!();
+      }
+
+      _goToDraftChat(
+        context: context,
+        path: "rooms",
+        contactPresentationSearch: user!.toContactPresentationSearch(),
+      );
+    } else {
+      if (PlatformInfos.isMobile) {
+        Navigator.of(context)
+            .popUntil((route) => route.settings.name == "/rooms/room");
+      } else {
+        if (widget.onNewChatOpen != null) widget.onNewChatOpen!();
+      }
+
+      context.go('/rooms/$roomId');
+    }
+  }
+
+  void _goToDraftChat({
+    required BuildContext context,
+    required String path,
+    required ContactPresentationSearch contactPresentationSearch,
+  }) {
+    if (contactPresentationSearch.matrixId !=
+        Matrix.of(context).client.userID) {
+      Router.neglect(
+        context,
+        () => PlatformInfos.isMobile
+            ? context.push(
+                '/$path/draftChat',
+                extra: {
+                  PresentationContactConstant.receiverId:
+                      contactPresentationSearch.matrixId ?? '',
+                  PresentationContactConstant.email:
+                      contactPresentationSearch.email ?? '',
+                  PresentationContactConstant.displayName:
+                      contactPresentationSearch.displayName ?? '',
+                  PresentationContactConstant.status: '',
+                },
+              )
+            : context.go(
+                '/$path/draftChat',
+                extra: {
+                  PresentationContactConstant.receiverId:
+                      contactPresentationSearch.matrixId ?? '',
+                  PresentationContactConstant.email:
+                      contactPresentationSearch.email ?? '',
+                  PresentationContactConstant.displayName:
+                      contactPresentationSearch.displayName ?? '',
+                  PresentationContactConstant.status: '',
+                },
+              ),
+      );
+    }
   }
 
   @override
