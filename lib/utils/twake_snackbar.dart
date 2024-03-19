4d8c5ee6055430aa7be68e4607d9ba8005db49f9
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-03-19 20:54:34 +0100

    TW-1456: optionnal duration param added to snackbar
    (cherry picked from commit fecf45049b445340dae915cc26ddf6e42bf14f61)

--- lib/utils/twake_snackbar.dart
+++ lib/utils/twake_snackbar.dart
@@ -2,6 +2,8 @@ import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:flutter/material.dart';
 
+const Duration _snackBarDefaultDisplayDuration = Duration(milliseconds: 4000);
+
 class TwakeSnackBarStyle {
   static ResponsiveUtils responsiveUtils = getIt.get<ResponsiveUtils>();
 
@@ -21,11 +23,18 @@ class TwakeSnackBarStyle {
 }
 
 class TwakeSnackBar {
-  static void show(BuildContext context, String message) {
+  static void show(
+    BuildContext context,
+    String message, {
+    Duration duration = _snackBarDefaultDisplayDuration,
+  }) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         width: TwakeSnackBarStyle.widthSnackBar(context),
         padding: TwakeSnackBarStyle.snackBarPadding,
+        duration: duration,
         content: Text(
           message,
           style: Theme.of(context).textTheme.bodyMedium?.copyWith(
