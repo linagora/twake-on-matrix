e2fa0efcc645d4715eaa00115cb8e403b4fb30df
HuyNguyen <quanghnguyen@linagora.com>
2024-03-26 17:15:36 +0700

    TW-1605: Fix display thumbnail when user sent a video

--- lib/domain/model/extensions/platform_file/platform_file_extension.dart
+++ lib/domain/model/extensions/platform_file/platform_file_extension.dart
@@ -2,13 +2,26 @@ import 'package:file_picker/file_picker.dart';
 import 'package:matrix/matrix.dart';
 
 extension PlatformFileListExtension on PlatformFile {
-  MatrixFile toMatrixFile() {
+  MatrixFile toMatrixFile({
+    required String temporaryDirectoryPath,
+  }) {
     return MatrixFile.fromMimeType(
       bytes: bytes,
       name: name,
-      filePath: '',
+      filePath: path ?? '$temporaryDirectoryPath/$name',
       readStream: readStream,
       sizeInBytes: size,
+    );
+  }
+
+  FileInfo toFileInfo({
+    required String temporaryDirectoryPath,
+  }) {
+    return FileInfo(
+      name,
+      path ?? '$temporaryDirectoryPath/$name',
+      size,
+      readStream: readStream,
     );
   }
 }
