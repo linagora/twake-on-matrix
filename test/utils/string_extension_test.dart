39d1c0fe02b3f4a3edc284719c3d3efa28e61a70
hieubt <trunghieu.bui201@gmail.com>
2024-03-27 17:42:26 +0700

    TW-1589: Write test for `isContainsHttpProtocol` and `removeHttpProtocol`

--- test/utils/string_extension_test.dart
+++ test/utils/string_extension_test.dart
@@ -49,4 +49,133 @@ void main() {
       expect(result, equals(expectedPhoneNumber));
     });
   });
+
+  group(
+      '[isContainsHttpProtocol] TEST\n'
+      'GIVEN a string\n'
+      'USING isContainsUrl function\n'
+      'IF the text contains a URL\n'
+      'THEN should return true\n'
+      'ELSE should return false\n', () {
+    final testMap = <String, bool>{
+      'https://www.example.com': true,
+      'https://www.example.com/': true,
+      'https://www.example.com/watch?v=ohg5sjyrha0': true,
+      'https://example.com/test/test-a-link/check/1608?test=1': true,
+      'https://www.example.com/watch?v=ohg5sjyrha0&feature=related': true,
+      'https://example.com/test/test-a-link/check/1608': true,
+      'http://www.example.com': true,
+      'http://www.example.com/': true,
+      'http://www.example.com/watch?v=ohg5sjyrha0': true,
+      'http://example.com/test/test-a-link/check/1608/': true,
+      'http://example.com/test/test-a-link/check/1608/?test=1': true,
+      'https://example': true,
+      'www.example.com': false,
+      'www.example.com/': false,
+      'www.example.com/watch?v=ohg5sjyrha0': false,
+      'example.com': false,
+      'example.com/test/test-a-link/check/1608': false,
+      'example.com/test/test-a-link/check/1608?test=1': false,
+      'example.com/test/test-a-link/check/1608/': false,
+      'example.com/test/test-a-link/check/1608/?test=1': false,
+      'https:/www.example.com': false,
+      'http//www.example.com': false,
+      'example.com/test test-a-link/check/1608': false,
+      'example.com/test/test-a-link/check/1608?test': false,
+      'example.com/test/test-a-link/check/1608/?test==1': false,
+      'example.com/test/test-a-link/check/1608/?test=1&': false,
+      'hello world': false,
+      'hello world.com': false,
+      'hello world.com/test': false,
+      'hello world.com/test/test-a-link': false,
+      'hello world.com/test/test-a-link/check': false,
+      'hello world.com/test/test-a-link/check/1608': false,
+      'hello world.com/test/test-a-link/check/1608?test': false,
+      'hello world.com/test/test-a-link/check/1608/?test==1': false,
+      'hello world.com/test/test-a-link/check/1608/?test=1&': false,
+      'this is a test string': false,
+    };
+
+    for (final entry in testMap.entries) {
+      test('Testing: ${entry.key} => Expected: ${entry.value}', () {
+        final result = entry.key.isContainsHttpProtocol();
+        expect(result, entry.value);
+      });
+    }
+  });
+
+  group(
+      '[removeHttpProtocol] TEST\n'
+      'GIVEN a string\n'
+      'USING removeHttpProtocol function\n'
+      'IF the URL starts with http:// or https://\n'
+      'THEN should return the URL without the protocol\n'
+      'ELSE should return the string unchanged\n', () {
+    final testMap = <String, String>{
+      'https://www.example.com': 'www.example.com',
+      'https://www.example.com/': 'www.example.com/',
+      'https://www.example.com/watch?v=ohg5sjyrha0':
+          'www.example.com/watch?v=ohg5sjyrha0',
+      'https://example.com/test/test-a-link/check/1608?test=1':
+          'example.com/test/test-a-link/check/1608?test=1',
+      'https://www.example.com/watch?v=ohg5sjyrha0&feature=related':
+          'www.example.com/watch?v=ohg5sjyrha0&feature=related',
+      'https://example.com/test/test-a-link/check/1608':
+          'example.com/test/test-a-link/check/1608',
+      'http://www.example.com': 'www.example.com',
+      'http://www.example.com/': 'www.example.com/',
+      'http://www.example.com/watch?v=ohg5sjyrha0':
+          'www.example.com/watch?v=ohg5sjyrha0',
+      'http://example.com/test/test-a-link/check/1608/':
+          'example.com/test/test-a-link/check/1608/',
+      'http://example.com/test/test-a-link/check/1608/?test=1':
+          'example.com/test/test-a-link/check/1608/?test=1',
+      'https://example': 'example',
+      'www.example.com': 'www.example.com',
+      'www.example.com/': 'www.example.com/',
+      'www.example.com/watch?v=ohg5sjyrha0':
+          'www.example.com/watch?v=ohg5sjyrha0',
+      'example.com': 'example.com',
+      'example.com/test/test-a-link/check/1608':
+          'example.com/test/test-a-link/check/1608',
+      'example.com/test/test-a-link/check/1608?test=1':
+          'example.com/test/test-a-link/check/1608?test=1',
+      'example.com/test/test-a-link/check/1608/':
+          'example.com/test/test-a-link/check/1608/',
+      'example.com/test/test-a-link/check/1608/?test=1':
+          'example.com/test/test-a-link/check/1608/?test=1',
+      'https:/www.example.com': 'https:/www.example.com',
+      'http//www.example.com': 'http//www.example.com',
+      'example.com/test test-a-link/check/1608':
+          'example.com/test test-a-link/check/1608',
+      'example.com/test/test-a-link/check/1608?test':
+          'example.com/test/test-a-link/check/1608?test',
+      'example.com/test/test-a-link/check/1608/?test==1':
+          'example.com/test/test-a-link/check/1608/?test==1',
+      'example.com/test/test-a-link/check/1608/?test=1&':
+          'example.com/test/test-a-link/check/1608/?test=1&',
+      'hello world': 'hello world',
+      'hello world.com': 'hello world.com',
+      'hello world.com/test': 'hello world.com/test',
+      'hello world.com/test/test-a-link': 'hello world.com/test/test-a-link',
+      'hello world.com/test/test-a-link/check':
+          'hello world.com/test/test-a-link/check',
+      'hello world.com/test/test-a-link/check/1608':
+          'hello world.com/test/test-a-link/check/1608',
+      'hello world.com/test/test-a-link/check/1608?test':
+          'hello world.com/test/test-a-link/check/1608?test',
+      'hello world.com/test/test-a-link/check/1608/?test==1':
+          'hello world.com/test/test-a-link/check/1608/?test==1',
+      'hello world.com/test/test-a-link/check/1608/?test=1&':
+          'hello world.com/test/test-a-link/check/1608/?test=1&',
+      'this is a test string': 'this is a test string',
+    };
+
+    for (final entry in testMap.entries) {
+      test('Testing: ${entry.key} => Expected: ${entry.value}', () {
+        final result = entry.key.removeHttpProtocol();
+        expect(result, entry.value);
+      });
+    }
+  });
 }
