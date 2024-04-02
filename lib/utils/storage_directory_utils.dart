3f5f45e057cfc8a9e57a8c5f790b6d34adbc01e0
--global <quangkhai28999@gmail.com>
2024-04-02 17:03:14 +0700

    TW-1573: Implement save file to downloads folder

--- lib/utils/storage_directory_utils.dart
+++ lib/utils/storage_directory_utils.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/utils/exception/save_to_downloads_exception.dart';
+import 'package:matrix/matrix.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:external_path/external_path.dart';
 
 class StorageDirectoryUtils {
   StorageDirectoryUtils._();
@@ -33,5 +39,39 @@ class StorageDirectoryUtils {
     final downloadInAppFolder =
         await StorageDirectoryUtils.instance.getDownloadFolderInApp();
     return '$downloadInAppFolder/$eventId/$fileName';
+  }
+
+  Future<String?> getTwakeDownloadsFolderInDevice() async {
+    try {
+      final downloadPath = await ExternalPath.getExternalStoragePublicDirectory(
+        ExternalPath.DIRECTORY_DOWNLOADS,
+      );
+      if (downloadPath.isNotEmpty == true) {
+        return '$downloadPath/${AppConfig.applicationName}';
+      }
+      throw SaveToDownloadsException(error: 'Download path is empty');
+    } catch (e) {
+      Logs().e('StorageDirectoryUtils:: getDownloadFolderDevice: $e');
+    }
+    return null;
+  }
+
+  Future<String> getAvailableFilePath(String filePath) async {
+    String availableFilePath = filePath;
+    final positionOfDot = filePath.lastIndexOf('.');
+    String extension = '';
+    String fileName = '';
+    int counter = 1;
+    if (positionOfDot != -1) {
+      extension = filePath.substring(positionOfDot);
+      fileName = filePath.substring(0, positionOfDot);
+    } else {
+      fileName = filePath;
+    }
+    while (await File(availableFilePath).exists()) {
+      availableFilePath = '$fileName ($counter)$extension';
+      counter++;
+    }
+    return availableFilePath;
   }
 }
---
+++ test/files/get_available_file_path_test.dart
@@ -0,0 +1,174 @@
+import 'dart:io';
+import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/utils/storage_directory_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Future<File> createMockFile(String filePath) async {
+  final file = File('$folderTestName/$filePath');
+  await file.create(recursive: true);
+  return file;
+}
+
+const folderTestName = 'generated';
+
+void main() async {
+  if (PlatformInfos.isWeb) {
+    return;
+  }
+  test("""WHEN exist only one file in the folder,
+          THEN the function should create another file with format fileName (1)
+      """, () async {
+    final file = await createMockFile('file1.pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+    await file.delete();
+    expect(fileAvailable, '$folderTestName/file1 (1).pdf');
+  });
+
+  test("""WHEN exist only two files in the folder,
+          THEN the function should create another file with format fileName (2)
+      """, () async {
+    final file = await createMockFile('file1.pdf');
+    final file1 = await createMockFile('file1 (1).pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+      file1.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/file1 (2).pdf');
+  });
+
+  test("""WHEN exist only two files in the folder,
+          THEN the function should create another file with format fileName (7)
+      """, () async {
+    final file = await createMockFile('file1.pdf');
+    final file1 = await createMockFile('file1 (1).pdf');
+    final file2 = await createMockFile('file1 (2).pdf');
+    final file3 = await createMockFile('file1 (3).pdf');
+    final file4 = await createMockFile('file1 (4).pdf');
+    final file5 = await createMockFile('file1 (5).pdf');
+    final file6 = await createMockFile('file1 (6).pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+      file1.delete(),
+      file2.delete(),
+      file3.delete(),
+      file4.delete(),
+      file5.delete(),
+      file6.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/file1 (7).pdf');
+  });
+
+  test("""WHEN exist a file with name already have counter in the folder,
+          THEN the function should create another file with format fileName (6) (1)
+      """, () async {
+    final file = await createMockFile('file1 (6).pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/file1 (6) (1).pdf');
+  });
+
+  test("""WHEN exist a file that contains dot,
+          THEN the function should create another file with format fileName (1)
+      """, () async {
+    final file = await createMockFile('my.document.v1.pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/my.document.v1 (1).pdf');
+  });
+
+  test("""WHEN exist a file that contains no dot,
+          THEN the function should create another file with format fileName (1)
+      """, () async {
+    final file = await createMockFile('text');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/text (1)');
+  });
+
+  test("""WHEN exist a file that start with a dot,
+          THEN the function should create another file with format fileName (1)
+      """, () async {
+    final file = await createMockFile('.DS_Store');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/ (1).DS_Store');
+  });
+
+  test("""WHEN exist a file that have extension contains multiple dots,
+          THEN the function should create another file with format fileName (1)
+      """, () async {
+    final file = await createMockFile('filename.tar.gz');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/filename.tar (1).gz');
+  });
+
+  test(
+      """WHEN exist multiple files which have the same name and different counter,
+          AND file name exists in the folder are file, file (1) and file (3),
+          THEN the function should create another file with format fileName (2)
+      """, () async {
+    final file = await createMockFile('file.pdf');
+    final file1 = await createMockFile('file (1).pdf');
+    final file3 = await createMockFile('file (3).pdf');
+    final fileAvailable =
+        await StorageDirectoryUtils.instance.getAvailableFilePath(
+      file.path,
+    );
+
+    await Future.wait([
+      file.delete(),
+      file1.delete(),
+      file3.delete(),
+    ]);
+    expect(fileAvailable, '$folderTestName/file (2).pdf');
+  });
+
+  final testDirectory = Directory(folderTestName);
+  if (await testDirectory.exists()) {
+    await testDirectory.delete();
+  }
+}
