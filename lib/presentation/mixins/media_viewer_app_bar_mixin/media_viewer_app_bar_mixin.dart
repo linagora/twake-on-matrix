b5ccad3400382c18ad69aa122ef2920d3c6293b9
Terence Zafindratafa <tzafindratafa@linagora.com>
2024-04-05 10:02:00 +0200

    TW-1621: improve forward screen to dialog
    (cherry picked from commit c4fd88ae5ae4eb42b780aaf74e039cd4eb0f314e)

--- lib/presentation/mixins/media_viewer_app_bar_mixin/media_viewer_app_bar_mixin.dart
+++ lib/presentation/mixins/media_viewer_app_bar_mixin/media_viewer_app_bar_mixin.dart
@@ -1,12 +1,14 @@
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/pages/forward/forward.dart';
 import 'package:fluffychat/presentation/enum/chat/media_viewer_popup_result_enum.dart';
+import 'package:fluffychat/presentation/mixins/media_viewer_app_bar_mixin/media_viewer_app_bar_mixin_style.dart';
 import 'package:fluffychat/presentation/model/pop_result_from_forward.dart';
 import 'package:fluffychat/utils/extension/build_context_extension.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
 import 'package:matrix/matrix.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
 
@@ -29,16 +31,43 @@ mixin MediaViewerAppBarMixin {
     Event? event,
   ) async {
     Matrix.of(context).shareContent = event?.content;
-    final result = await showDialog(
-      context: context,
-      useSafeArea: false,
-      useRootNavigator: false,
-      builder: (c) => const Forward(),
-    );
+    final responsive = getIt.get<ResponsiveUtils>();
+
+    final result = responsive.isMobile(context)
+        ? await _showForwardMobileDialog(context)
+        : await _showForwardWebDialog(context);
+
     if (result is PopResultFromForward) {
       Navigator.of(context).pop<PopResultFromForward>();
     }
   }
+
+  Future<PopResultFromForward?> _showForwardMobileDialog(
+    BuildContext context,
+  ) async =>
+      await showDialog(
+        context: context,
+        useSafeArea: false,
+        useRootNavigator: false,
+        builder: (c) => const Forward(),
+      );
+
+  Future<PopResultFromForward?> _showForwardWebDialog(
+    BuildContext context,
+  ) async =>
+      await showDialog(
+        context: context,
+        useRootNavigator: false,
+        builder: (c) => AlertDialog(
+          backgroundColor: LinagoraRefColors.material().primary[100],
+          surfaceTintColor: LinagoraRefColors.material().primary[100],
+          content: const SizedBox(
+            width: MediaViewerAppBarMixinStyle.fixedForwardActionDialogWidth,
+            height: MediaViewerAppBarMixinStyle.fixedForwardActionDialogHeight,
+            child: Forward(),
+          ),
+        ),
+      );
 
   void showInChat(
     BuildContext context,
