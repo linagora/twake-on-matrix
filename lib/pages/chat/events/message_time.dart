e72baf175f59c223848004438f19bd2e4c71b52d
hieubt <trunghieu.bui201@gmail.com>
2024-04-07 19:01:36 +0700

    TW-1666: Fix timeline scale too much

--- lib/pages/chat/events/message_time.dart
+++ lib/pages/chat/events/message_time.dart
@@ -29,69 +29,62 @@ class MessageTime extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
+    return Container(
+      padding: timelineOverlayMessage
+          ? const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 8,
+            )
+          : null,
+      decoration: timelineOverlayMessage
+          ? BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: LinagoraStateLayer(Colors.black).opacityLayer3,
+            )
+          : null,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          WidgetSpan(
-            child: Container(
-              padding: timelineOverlayMessage
-                  ? const EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 8,
-                    )
-                  : null,
-              decoration: timelineOverlayMessage
-                  ? BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: LinagoraStateLayer(Colors.black).opacityLayer3,
-                    )
-                  : null,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  if (event.isPinned) ...[
-                    TwakeIconButton(
-                      tooltip: L10n.of(context)!.pin,
-                      icon: Icons.push_pin_outlined,
-                      size: MessageStyle.pushpinIconSize,
-                      paddingAll: MessageStyle.paddingAllPushpin,
-                      margin: EdgeInsets.zero,
-                      iconColor: timelineOverlayMessage
-                          ? Colors.white
-                          : LinagoraRefColors.material().neutral[50],
-                    ),
-                    const SizedBox(width: 4.0),
-                  ],
-                  Text(
-                    DateFormat("HH:mm").format(event.originServerTs),
-                    style: Theme.of(context).textTheme.bodySmall?.merge(
-                          TextStyle(
-                            color: timelineOverlayMessage
-                                ? Colors.white
-                                : LinagoraRefColors.material().tertiary[30],
-                            letterSpacing: 0.4,
-                          ),
-                        ),
-                  ),
-                  if (ownMessage) ...[
-                    SizedBox(width: MessageTimeStyle.paddingTimeAndIcon),
-                    SeenByRow(
-                      timelineOverlayMessage: timelineOverlayMessage,
-                      participants: timeline.room.getParticipants(),
-                      getSeenByUsers: room.getSeenByUsers(
-                        timeline,
-                        eventId: event.eventId,
-                      ),
-                      eventStatus: event.status,
-                      event: event,
-                    ),
-                  ],
-                ],
-              ),
+          if (event.isPinned) ...[
+            TwakeIconButton(
+              tooltip: L10n.of(context)!.pin,
+              icon: Icons.push_pin_outlined,
+              size: MessageStyle.pushpinIconSize,
+              paddingAll: MessageStyle.paddingAllPushpin,
+              margin: EdgeInsets.zero,
+              iconColor: timelineOverlayMessage
+                  ? Colors.white
+                  : LinagoraRefColors.material().neutral[50],
             ),
+            const SizedBox(width: 4.0),
+          ],
+          Text(
+            DateFormat("HH:mm").format(event.originServerTs),
+            textScaler: const TextScaler.linear(1.0),
+            style: Theme.of(context).textTheme.bodySmall?.merge(
+                  TextStyle(
+                    color: timelineOverlayMessage
+                        ? Colors.white
+                        : LinagoraRefColors.material().tertiary[30],
+                    letterSpacing: 0.4,
+                  ),
+                ),
           ),
+          if (ownMessage) ...[
+            SizedBox(width: MessageTimeStyle.paddingTimeAndIcon),
+            SeenByRow(
+              timelineOverlayMessage: timelineOverlayMessage,
+              participants: timeline.room.getParticipants(),
+              getSeenByUsers: room.getSeenByUsers(
+                timeline,
+                eventId: event.eventId,
+              ),
+              eventStatus: event.status,
+              event: event,
+            ),
+          ],
         ],
       ),
     );
