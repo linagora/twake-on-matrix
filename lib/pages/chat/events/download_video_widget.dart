42900686926a46572311f8954e9373b64952360b
--global <quangkhai28999@gmail.com>
2024-04-15 09:36:21 +0700

    TW-1573: disable media app bar when downloading video
    (cherry picked from commit 6c0f402ef53a38cca42665283deaf6e0b3a515f5)

--- lib/pages/chat/events/download_video_widget.dart
+++ lib/pages/chat/events/download_video_widget.dart
@@ -6,7 +6,6 @@ import 'package:fluffychat/pages/image_viewer/media_viewer_app_bar.dart';
 import 'package:fluffychat/pages/image_viewer/media_viewer_app_bar_web.dart';
 import 'package:fluffychat/presentation/mixins/handle_video_download_mixin.dart';
 import 'package:fluffychat/presentation/mixins/play_video_action_mixin.dart';
-import 'package:fluffychat/utils/extension/value_notifier_extension.dart';
 import 'package:fluffychat/utils/localized_exception_extension.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/twake_snackbar.dart';
@@ -34,7 +33,7 @@ class _DownloadVideoWidgetState extends State<DownloadVideoWidget>
   final downloadProgressNotifier = ValueNotifier(0.0);
   final cancelToken = CancelToken();
 
-  final ValueNotifier<bool> showAppbarPreview = ValueNotifier(true);
+  final ValueNotifier<bool> showAppbarPreview = ValueNotifier(false);
 
   @override
   void initState() {
@@ -89,108 +88,104 @@ class _DownloadVideoWidgetState extends State<DownloadVideoWidget>
   Widget build(BuildContext context) {
     return Material(
       color: Colors.black,
-      child: GestureDetector(
-        onTap: () => showAppbarPreview.toggle(),
-        child: Stack(
-          alignment: Alignment.topLeft,
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                MxcImage(
-                  event: widget.event,
-                  fit: BoxFit.cover,
-                ),
-                ValueListenableBuilder<DownloadVideoState>(
-                  valueListenable: _downloadStateNotifier,
-                  builder: (context, downloadState, child) {
-                    switch (downloadState) {
-                      case DownloadVideoState.loading:
-                        return InkWell(
-                          onTap: downloadState == DownloadVideoState.loading
-                              ? null
-                              : _downloadAction,
-                          child: Center(
-                            child: Stack(
-                              children: [
-                                const CenterVideoButton(
-                                  icon: Icons.play_arrow,
-                                ),
-                                SizedBox(
-                                  width:
-                                      MessageContentStyle.videoCenterButtonSize,
-                                  height:
-                                      MessageContentStyle.videoCenterButtonSize,
-                                  child: ValueListenableBuilder(
-                                    valueListenable: downloadProgressNotifier,
-                                    builder: (context, progress, child) {
-                                      return CircularProgressIndicator(
-                                        strokeWidth: 2,
-                                        color: LinagoraRefColors.material()
-                                            .primary[100],
-                                        value: PlatformInfos.isWeb
-                                            ? null
-                                            : progress,
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      case DownloadVideoState.initial:
-                        return InkWell(
-                          onTap: _downloadAction,
-                          child: const Center(
-                            child: CenterVideoButton(
-                              icon: Icons.play_arrow,
-                            ),
-                          ),
-                        );
-                      case DownloadVideoState.done:
-                        return InkWell(
-                          onTap: () {
-                            if (path != null) {
-                              playVideoAction(
-                                context,
-                                path!,
-                                event: widget.event,
-                              );
-                            }
-                          },
-                          child: const Center(
-                            child: CenterVideoButton(
-                              icon: Icons.play_arrow,
-                            ),
-                          ),
-                        );
-                      case DownloadVideoState.failed:
-                        return InkWell(
-                          onTap: _downloadAction,
-                          child: const Center(
-                            child: CenterVideoButton(
-                              icon: Icons.error,
-                            ),
-                          ),
-                        );
-                    }
-                  },
-                ),
-              ],
-            ),
-            if (PlatformInfos.isMobile) ...[
-              MediaViewerAppBar(
-                showAppbarPreviewNotifier: showAppbarPreview,
+      child: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              MxcImage(
                 event: widget.event,
+                fit: BoxFit.cover,
               ),
-            ] else ...[
-              MediaViewerAppBarWeb(
-                event: widget.event,
+              ValueListenableBuilder<DownloadVideoState>(
+                valueListenable: _downloadStateNotifier,
+                builder: (context, downloadState, child) {
+                  switch (downloadState) {
+                    case DownloadVideoState.loading:
+                      return InkWell(
+                        onTap: downloadState == DownloadVideoState.loading
+                            ? null
+                            : _downloadAction,
+                        child: Center(
+                          child: Stack(
+                            children: [
+                              const CenterVideoButton(
+                                icon: Icons.play_arrow,
+                              ),
+                              SizedBox(
+                                width:
+                                    MessageContentStyle.videoCenterButtonSize,
+                                height:
+                                    MessageContentStyle.videoCenterButtonSize,
+                                child: ValueListenableBuilder(
+                                  valueListenable: downloadProgressNotifier,
+                                  builder: (context, progress, child) {
+                                    return CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                      color: LinagoraRefColors.material()
+                                          .primary[100],
+                                      value:
+                                          PlatformInfos.isWeb ? null : progress,
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    case DownloadVideoState.initial:
+                      return InkWell(
+                        onTap: _downloadAction,
+                        child: const Center(
+                          child: CenterVideoButton(
+                            icon: Icons.play_arrow,
+                          ),
+                        ),
+                      );
+                    case DownloadVideoState.done:
+                      return InkWell(
+                        onTap: () {
+                          if (path != null) {
+                            playVideoAction(
+                              context,
+                              path!,
+                              event: widget.event,
+                            );
+                          }
+                        },
+                        child: const Center(
+                          child: CenterVideoButton(
+                            icon: Icons.play_arrow,
+                          ),
+                        ),
+                      );
+                    case DownloadVideoState.failed:
+                      return InkWell(
+                        onTap: _downloadAction,
+                        child: const Center(
+                          child: CenterVideoButton(
+                            icon: Icons.error,
+                          ),
+                        ),
+                      );
+                  }
+                },
               ),
             ],
+          ),
+          if (PlatformInfos.isMobile) ...[
+            MediaViewerAppBar(
+              showAppbarPreviewNotifier: showAppbarPreview,
+              event: widget.event,
+            ),
+          ] else ...[
+            MediaViewerAppBarWeb(
+              event: widget.event,
+            ),
           ],
-        ),
+        ],
       ),
     );
   }
