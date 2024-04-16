9cc46ff73d2a01c7d68dd63ba92bb9cab77ddead
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-04-16 13:15:41 +0200

    TW-1584: download on mobile ok

---
+++ lib/pages/chat_details/chat_details_page_view/files/chat_details_files_item_web/chat_details_files_item_view_web.dart
@@ -0,0 +1,77 @@
+import 'package:fluffychat/pages/chat_details/chat_details_page_view/files/chat_details_files_item_web/chat_details_files_item_web.dart';
+import 'package:fluffychat/pages/chat_details/chat_details_page_view/files/chat_details_files_row/chat_details_file_row.dart';
+import 'package:fluffychat/pages/chat_details/chat_details_page_view/files/chat_details_files_item/chat_details_files_item_style.dart';
+import 'package:fluffychat/presentation/model/chat/downloading_state_presentation_model.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
+import 'package:fluffychat/widgets/file_widget/download_file_tile_widget.dart';
+import 'package:fluffychat/widgets/file_widget/message_file_tile_style.dart';
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+
+class ChatDetailsFilesViewWeb extends StatelessWidget {
+  const ChatDetailsFilesViewWeb({
+    super.key,
+    required this.controller,
+  });
+
+  final ChatDetailsFileItemWebState controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final filename = controller.event.filename;
+    final filetype = controller.event.fileType;
+    final sizeString = controller.event.sizeString;
+
+    return ValueListenableBuilder(
+      valueListenable: controller.downloadFileStateNotifier,
+      builder: (context, DownloadPresentationState state, child) {
+        if (state is DownloadingPresentationState) {
+          return DownloadFileTileWidget(
+            mimeType: controller.event.mimeType,
+            fileType: filetype,
+            filename: filename,
+            sizeString: sizeString,
+            style: const MessageFileTileStyle(),
+            downloadFileStateNotifier: controller.downloadFileStateNotifier,
+            onCancelDownload: () {
+              controller.downloadFileStateNotifier.value =
+                  const NotDownloadPresentationState();
+              controller.downloadManager
+                  .cancelDownload(controller.event.eventId);
+            },
+          );
+        }
+
+        return InkWell(
+          hoverColor: LinagoraSysColors.material().surfaceVariant,
+          onTap: () {
+            if (state is FileWebDownloadedPresentationState) {
+              controller.handlePreviewWeb(
+                event: controller.event,
+                context: context,
+              );
+            } else {
+              controller.downloadFileStateNotifier.value =
+                  const DownloadingPresentationState();
+              controller.downloadManager.download(
+                event: controller.event,
+              );
+              controller.trySetupDownloadingStreamSubcription();
+            }
+          },
+          child: Padding(
+            padding: ChatDetailsFileTileStyle().paddingFileTileAll,
+            child: ChatDetailsFileTileRow(
+              mimeType: controller.event.mimeType,
+              fileType: filetype,
+              filename: filename,
+              sizeString: sizeString,
+              style: ChatDetailsFileTileStyle(),
+              sentDate: controller.event.originServerTs,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/pages/chat_details/chat_details_page_view/files/chat_details_files_row/chat_details_file_row_style.dart
+++ lib/pages/chat_details/chat_details_page_view/files/chat_details_files_row/chat_details_file_row_style.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
 
-class ChatDetailsFileRow {
+class ChatDetailsFileRowStyle {
   static TextStyle textInformationStyle(BuildContext context) {
     return Theme.of(context).textTheme.bodySmall!.copyWith(
           color: LinagoraSysColors.material().tertiary,
