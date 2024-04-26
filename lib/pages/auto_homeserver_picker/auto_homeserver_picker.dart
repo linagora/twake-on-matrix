e03b88d7a60316e43b5e480a09161e2ed5487430
HuyNguyen <quanghnguyen@linagora.com>
2024-04-26 15:32:19 +0700

    TW-1711: Error handle with wrong login

--- lib/pages/auto_homeserver_picker/auto_homeserver_picker.dart
+++ lib/pages/auto_homeserver_picker/auto_homeserver_picker.dart
@@ -10,7 +10,7 @@ import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
-import 'package:universal_html/html.dart' as html;
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class AutoHomeserverPicker extends StatefulWidget {
   final bool? loggedOut;
@@ -162,7 +162,7 @@ class AutoHomeserverPickerController extends State<AutoHomeserverPicker>
         }
       }
       Logs().d(
-        "AutoHomeserverPickerController: _initializeAutoHomeserverPicker: PlatForm ${AppConfig.platform}",
+        "AutoHomeserverPickerController::_setupAutoHomeserverPicker: PlatForm ${AppConfig.platform}",
       );
       final loginToken = getQueryParameter('loginToken');
       if (loginToken != null || loginToken?.isNotEmpty == true) {
@@ -173,14 +173,23 @@ class AutoHomeserverPickerController extends State<AutoHomeserverPicker>
                     AppConfig.homeserver,
                   ),
                 );
-        await TwakeDialog.showFutureLoadingDialogFullScreen(
+        final result = await TwakeDialog.showFutureLoadingDialogFullScreen(
           future: () => Matrix.of(context).getLoginClient().login(
                 LoginType.mLoginToken,
                 token: loginToken,
                 initialDeviceDisplayName: PlatformInfos.clientName,
               ),
         );
-        _resetLocationPath();
+        if (result.error != null) {
+          autoHomeserverPickerUIState.value = AutoHomeServerPickerFailureState(
+            error: L10n.of(context)!.invalidLoginToken,
+          );
+          resetLocationPathWithLoginToken(
+            route: 'home',
+          );
+        } else {
+          resetLocationPathWithLoginToken();
+        }
         return;
       }
       if (_isSaasPlatform) {
@@ -193,10 +202,6 @@ class AutoHomeserverPickerController extends State<AutoHomeserverPicker>
         autoHomeserverPickerUIState.value = AutoHomeServerPickerInitialState();
       }
     }
-  }
-
-  void _resetLocationPath() {
-    html.window.history.replaceState({}, '', '/#/rooms');
   }
 
   @override
