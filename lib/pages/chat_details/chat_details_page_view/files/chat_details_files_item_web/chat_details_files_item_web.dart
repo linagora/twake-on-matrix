145ff9cd6c27ad56d0fd0db29b7cd8957134e1d7
--global <quangkhai28999@gmail.com>
2024-04-29 09:46:51 +0700

    TW-1685: refactor UI and mixin to work in video media type

--- lib/pages/chat_details/chat_details_page_view/files/chat_details_files_item_web/chat_details_files_item_web.dart
+++ lib/pages/chat_details/chat_details_page_view/files/chat_details_files_item_web/chat_details_files_item_web.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:fluffychat/pages/chat_details/chat_details_page_view/files/chat_details_files_item_web/chat_details_files_item_view_web.dart';
+import 'package:fluffychat/utils/manager/download_manager/download_file_state.dart';
 import 'package:fluffychat/widgets/mixins/download_file_on_web_mixin.dart';
 import 'package:fluffychat/widgets/mixins/handle_download_and_preview_file_mixin.dart';
 import 'package:fluffychat/widgets/twake_app.dart';
@@ -24,8 +23,12 @@ class ChatDetailsFileItemWebState extends State<ChatDetailsFileItemWeb>
   Event get event => widget.event;
 
   @override
-  Future<void> get handlePreview => handlePreviewWeb(
+  void handleDownloadMatrixFileSuccessDone({
+    required DownloadMatrixFileSuccessState success,
+  }) =>
+      handlePreviewWeb(
         event: widget.event,
+        matrixFile: success.matrixFile,
         context: TwakeApp.routerKey.currentContext!,
       );
 
