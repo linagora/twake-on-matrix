76df2c40133d412b9516bca74a6e577f55895cf0
HuyNguyen <quanghnguyen@linagora.com>
2024-05-15 13:16:00 +0700

    TW-1758: Refactor and add cancel token and progress upload on mobile

--- lib/data/network/media/media_api.dart
+++ lib/data/network/media/media_api.dart
@@ -18,7 +18,11 @@ class MediaAPI {
 
   MediaAPI();
 
-  Future<UploadFileResponse> uploadFile({required FileInfo fileInfo}) async {
+  Future<UploadFileResponse> uploadFileMobile({
+    required FileInfo fileInfo,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) async {
     final dioHeaders = _client.getHeaders();
     dioHeaders[HttpHeaders.contentLengthHeader] =
         await File(fileInfo.filePath).length();
@@ -31,6 +35,8 @@ class MediaAPI {
           queryParameters: {
             'fileName': fileInfo.fileName,
           },
+          cancelToken: cancelToken,
+          onReceiveProgress: onReceiveProgress,
           options: Options(headers: dioHeaders),
         )
         .onError((error, stackTrace) => throw Exception(error));
@@ -44,7 +50,7 @@ class MediaAPI {
     ProgressCallback? onReceiveProgress,
   }) async {
     final dioHeaders = _client.getHeaders();
-    dioHeaders[HttpHeaders.contentLengthHeader] = file.bytes!.length;
+    dioHeaders[HttpHeaders.contentLengthHeader] = file.bytes?.length;
     dioHeaders[HttpHeaders.contentTypeHeader] = file.mimeType;
     final response = await _client
         .postToGetBody(
