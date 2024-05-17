5aed3ebc4217ab1a15d0eeb927e712922faf2e4e
HuyNguyen <quanghnguyen@linagora.com>
2024-05-17 18:05:20 +0700

    TW-1768: Define saas environment for mobile [hardcode]
    (cherry picked from commit 2789506aa1fa82db0ceab04e2df1c4eeea284aab)

---
+++ lib/config/config_saas/config_saas.dart
@@ -0,0 +1,10 @@
+class ConfigurationSaas {
+  static const String registrationUrl = 'https://sign-up.stg.lin-saas.com/';
+
+  static const String twakeWorkplaceHomeserver =
+      'https://matrix.stg.lin-saas.com/';
+
+  static const String homeserver = 'https://matrix.stg.lin-saas.com/';
+
+  static const String platform = 'saas';
+}
