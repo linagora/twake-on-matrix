c8a0cc66c5d91eba03ed3362a011769ce32f869b
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-05-20 17:55:05 +0200

    TW-1735: search external contact widget added on search view

---
+++ lib/pages/search/search_external_contact_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchExternalContactStyle {
+  static EdgeInsetsGeometry contentPadding = const EdgeInsets.symmetric(
+    horizontal: 16,
+    vertical: 12,
+  );
+
+  static BorderRadius borderRadius = BorderRadius.circular(8.0);
+}
