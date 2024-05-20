3360e6fc3182af8ccd926a9efeffd34732a7557e
HuyNguyen <quanghnguyen@linagora.com>
2024-05-20 16:17:43 +0700

    TW-1772: Showing no contact in new chat screen

--- lib/pages/new_private_chat/new_private_chat.dart
+++ lib/pages/new_private_chat/new_private_chat.dart
@@ -8,6 +8,7 @@ import 'package:fluffychat/presentation/model/contact/presentation_contact.dart'
 import 'package:fluffychat/presentation/model/search/presentation_search.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -32,10 +33,14 @@ class NewPrivateChatController extends State<NewPrivateChat>
   @override
   void initState() {
     super.initState();
-    initialFetchContacts(
-      client: Matrix.of(context).client,
-      matrixLocalizations: MatrixLocals(L10n.of(context)!),
-    );
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      if (mounted) {
+        initialFetchContacts(
+          client: Matrix.of(context).client,
+          matrixLocalizations: MatrixLocals(L10n.of(context)!),
+        );
+      }
+    });
     // FIXME: Find out solution for disable load more in search
     // searchContactsController.onSearchKeywordChanged = (searchKey) {
     //   disableLoadMoreInSearch();
