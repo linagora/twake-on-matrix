2f43473730bf40bdb0878603277f17d8677ae16c
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-05-21 14:23:10 +0200

    TW-1735: update matrix dart sdk

--- lib/pages/search/search_external_contact.dart
+++ lib/pages/search/search_external_contact.dart
@@ -2,7 +2,7 @@ import 'package:fluffychat/domain/model/contact/contact_type.dart';
 import 'package:fluffychat/pages/new_private_chat/widget/expansion_contact_list_tile.dart';
 import 'package:fluffychat/pages/search/search.dart';
 import 'package:fluffychat/pages/search/search_external_contact_style.dart';
-import 'package:fluffychat/presentation/model/presentation_contact.dart';
+import 'package:fluffychat/presentation/model/contact/presentation_contact.dart';
 import 'package:fluffychat/presentation/model/search/presentation_search.dart';
 import 'package:flutter/material.dart' hide SearchController;
 
