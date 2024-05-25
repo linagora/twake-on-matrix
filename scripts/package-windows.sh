b89ecd41e505343c4864330cd23e8770f0971ff5
--global <quangkhai28999@gmail.com>
2024-05-25 11:00:09 +0700

    TW-1781 Fix ci failed

--- scripts/package-windows.sh
+++ scripts/package-windows.sh
@@ -5,4 +5,5 @@ curl -OL "https://files.jrsoftware.org/is/6/innosetup-6.2.2.exe"
 ./innosetup-6.2.2.exe //verysilent
 
 echo "Packaging."
+flutter pub get
 flutter pub global run flutter_distributor:main.dart package --platform windows --targets exe --skip-clean --flutter-build-args="release"
