b5316988b396172d78422b2fb5f61f3d6a32ded3
--global <quangkhai28999@gmail.com>
2024-05-25 11:00:09 +0700

    TƯ-1781 Fix ci failed

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -3,5 +3,6 @@ flutter config --enable-web
 flutter clean
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get
 flutter build web --release --verbose --source-maps --base-href="/web/"
 cp config.sample.json ./build/web/config.json
