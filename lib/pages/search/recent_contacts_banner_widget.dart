3b9a5615b81484de81ada9366ced2e1c7b8d950d
--global <quangkhai28999@gmail.com>
2024-05-27 10:42:14 +0700

    TW-1793: recent chats are not recent

--- lib/pages/search/recent_contacts_banner_widget.dart
+++ lib/pages/search/recent_contacts_banner_widget.dart
@@ -1,22 +1,25 @@
 import 'package:fluffychat/pages/search/recent_contacts_banner_widget_style.dart';
 import 'package:fluffychat/pages/search/search.dart';
 import 'package:fluffychat/utils/display_name_widget.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/widgets/avatar/avatar.dart';
 import 'package:flutter/material.dart' hide SearchController;
+import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class PreSearchRecentContactsContainer extends StatelessWidget {
   final SearchController searchController;
-  final List<User> contactsList;
+  final List<Room> recentRooms;
   const PreSearchRecentContactsContainer({
     super.key,
     required this.searchController,
-    required this.contactsList,
+    required this.recentRooms,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (contactsList.isEmpty) {
+    if (recentRooms.isEmpty) {
       return const SizedBox.shrink();
     } else {
       return Align(
@@ -27,11 +30,10 @@ class PreSearchRecentContactsContainer extends StatelessWidget {
           shrinkWrap: true,
           physics: const ClampingScrollPhysics(),
           scrollDirection: Axis.horizontal,
-          itemCount: contactsList.length,
+          itemCount: recentRooms.length,
           itemBuilder: (context, index) {
             return PreSearchRecentContactWidget(
-              user: contactsList[index],
-              searchController: searchController,
+              room: recentRooms[index],
             );
           },
         ),
@@ -41,20 +43,19 @@ class PreSearchRecentContactsContainer extends StatelessWidget {
 }
 
 class PreSearchRecentContactWidget extends StatelessWidget {
-  final SearchController searchController;
-  final User user;
+  final Room room;
   const PreSearchRecentContactWidget({
     super.key,
-    required this.user,
-    required this.searchController,
+    required this.room,
   });
 
   @override
   Widget build(BuildContext context) {
+    final displayName = room.getLocalizedDisplayname(
+      MatrixLocals(L10n.of(context)!),
+    );
     return InkWell(
-      onTap: () {
-        searchController.goToChatScreenFormRecentChat(user);
-      },
+      onTap: () => context.go('/rooms/${room.id}'),
       child: SizedBox(
         width: RecentContactsBannerWidgetStyle.chatRecentContactItemWidth,
         child: Column(
@@ -64,15 +65,15 @@ class PreSearchRecentContactWidget extends StatelessWidget {
               width: RecentContactsBannerWidgetStyle.avatarWidthSize,
               height: RecentContactsBannerWidgetStyle.avatarWidthSize,
               child: Avatar(
-                mxContent: user.avatarUrl,
-                name: user.displayName ?? "",
+                mxContent: room.avatar,
+                name: displayName,
               ),
             ),
             Padding(
               padding:
                   RecentContactsBannerWidgetStyle.chatRecentContactItemPadding,
               child: BuildDisplayName(
-                profileDisplayName: user.displayName ?? "",
+                profileDisplayName: displayName,
               ),
             ),
           ],
