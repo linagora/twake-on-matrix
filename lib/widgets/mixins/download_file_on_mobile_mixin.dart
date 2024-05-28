d7265ef549437ba70fe2e5f3a8ebffb25413a68c
Terence ZAFINDRATAFA <t.zafindratafa@gmail.com>
2024-05-28 17:13:57 +0200

    TW-1702: regression fix + tests added for download on mobile

--- lib/widgets/mixins/download_file_on_mobile_mixin.dart
+++ lib/widgets/mixins/download_file_on_mobile_mixin.dart
@@ -6,6 +6,7 @@ import 'package:fluffychat/app_state/failure.dart';
 import 'package:fluffychat/app_state/success.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/presentation/model/chat/downloading_state_presentation_model.dart';
+import 'package:fluffychat/utils/exception/downloading_exception.dart';
 import 'package:fluffychat/utils/manager/download_manager/download_file_state.dart';
 import 'package:fluffychat/utils/manager/storage_directory_manager.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/download_file_extension.dart';
@@ -16,6 +17,8 @@ import 'package:matrix/matrix.dart';
 
 mixin DownloadFileOnMobileMixin<T extends StatefulWidget> on State<T> {
   final downloadManager = getIt.get<DownloadManager>();
+
+  final storageDirectoryManager = StorageDirectoryManager();
 
   final downloadFileStateNotifier = ValueNotifier<DownloadPresentationState>(
     const NotDownloadPresentationState(),
@@ -66,15 +69,17 @@ mixin DownloadFileOnMobileMixin<T extends StatefulWidget> on State<T> {
   }
 
   Future<void> checkFileInDownloadsInApp() async {
-    final filePath =
-        await StorageDirectoryManager.instance.getFilePathInAppDownloads(
+    final filePath = await storageDirectoryManager.getFilePathInAppDownloads(
       eventId: event.eventId,
       fileName: event.filename,
     );
-    final file = File(filePath);
+    updateStateIfFileExists(File(filePath));
+  }
+
+  Future<void> updateStateIfFileExists(File file) async {
     if (await file.exists() && await file.length() == event.getFileSize()) {
       downloadFileStateNotifier.value = DownloadedPresentationState(
-        filePath: filePath,
+        filePath: file.path,
       );
       return;
     }
@@ -89,9 +94,15 @@ mixin DownloadFileOnMobileMixin<T extends StatefulWidget> on State<T> {
   void setupDownloadingProcess(Either<Failure, Success> resultEvent) {
     resultEvent.fold(
       (failure) {
-        Logs()
-            .e('$T::setupDownloadingProcess::onDownloadingProcess(): $failure');
-        downloadFileStateNotifier.value = const NotDownloadPresentationState();
+        Logs().e('MessageDownloadContent::onDownloadingProcess(): $failure');
+        if (failure is DownloadFileFailureState &&
+            failure.exception is CancelDownloadingException) {
+          downloadFileStateNotifier.value =
+              const NotDownloadPresentationState();
+        } else {
+          downloadFileStateNotifier.value =
+              DownloadErrorPresentationState(error: failure);
+        }
         streamSubscription?.cancel();
       },
       (success) {
