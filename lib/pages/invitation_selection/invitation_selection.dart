81288cdec486f424d437cbca1d223875cab79640
HuyNguyen <quanghnguyen@linagora.com>
2024-06-04 10:02:45 +0700

    TW-1841: Fix back screen when use go_router and navigator

--- lib/pages/invitation_selection/invitation_selection.dart
+++ lib/pages/invitation_selection/invitation_selection.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
-import 'package:go_router/go_router.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:matrix/matrix.dart';
@@ -94,7 +93,7 @@ class InvitationSelectionController
   }
 
   void inviteSuccessAction() {
-    context.pop();
+    Navigator.of(context).pop();
   }
 
   @override
