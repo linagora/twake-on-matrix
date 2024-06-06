17760806f291389836d2ac9240b4adef5e22a3d2
sherlock <43041967+sherlockvn@users.noreply.github.com>
2024-06-06 21:30:56 +0700

    hot-fix: fix the error l10n not found after build runner build (#1855)

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -ve
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 dart format --set-exit-if-changed lib/ test/
-flutter pub get # In flutter 3.22, flutter gen-l10n is not working properly, so we need to run flutter pub get again
 flutter analyze
