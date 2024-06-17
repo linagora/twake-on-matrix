e2e76685e28f5dda1d6fc2c49dd56eb7daaa6dcf
Quang Huy Nguyen <quanghnguyen@linagora.com>
2024-06-17 22:57:59 +0700

    TW-1791: Improve display contacts on multiple homeserver (#1857)
    * TW-1791: Refresh contacts when switch another account or login new account
    
    * TW-1791: Write unit test for this case
    
    * TW-1791: Refresh contacts when login or logout
    
    * TW-1791: Fix missing `setUpToMServices` when switch account

--- lib/app_state/success.dart
+++ lib/app_state/success.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fluffychat/app_state/failure.dart';
 
 abstract class Success extends Equatable {
   const Success();
@@ -9,5 +10,10 @@ extension SuccessExtension on Either {
   T? getSuccessOrNull<T extends Success>({T? fallbackValue}) => fold(
         (failure) => fallbackValue,
         (success) => success is T ? success : fallbackValue,
+      );
+
+  T? getFailureOrNull<T extends Failure>({T? fallbackValue}) => fold(
+        (failure) => failure is T ? failure : fallbackValue,
+        (success) => fallbackValue,
       );
 }
