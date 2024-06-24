d48e220bfb6a3a37bed2919712cb9a11b52bbfe6
hieubt <trunghieu.bui201@gmail.com>
2024-06-24 11:01:14 +0700

    TW-1769: Update app grid items

--- lib/pages/app_grid/app_grid_dashboard_item_style.dart
+++ lib/pages/app_grid/app_grid_dashboard_item_style.dart
@@ -1,10 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppGridDashboardItemStyle {
-  static const double iconAppSize = 56.0;
-  static const double itemWidth = 98.0;
-  static const double borderRadius = 16.0;
+  static const double iconAppSize = 42.0;
+  static const double itemWidth = 80.0;
 
-  static const EdgeInsets padding = EdgeInsets.symmetric(vertical: 8);
+  static const EdgeInsets padding = EdgeInsets.only(
+    bottom: 4,
+    top: 8,
+  );
   static const EdgeInsets textPadding = EdgeInsets.only(top: 8);
+
+  static const BorderRadius borderRadius =
+      BorderRadius.all(Radius.circular(10));
+
+  static TextStyle? textStyle(BuildContext context) =>
+      Theme.of(context).textTheme.bodySmall;
 }
