629cd0b6e867ffb83bb11680abca0df5940737cf
HuyNguyen <quanghnguyen@linagora.com>
2024-06-25 15:41:20 +0700

    TW-1760: Create models for upload file info

---
+++ lib/utils/manager/upload_manager/models/upload_file_info.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/utils/manager/upload_manager/models/upload_info.dart';
+
+class UploadFileInfo extends UploadInfo {
+  final StreamController<Either<Failure, Success>> uploadStateStreamController;
+  final Stream<Either<Failure, Success>> uploadStream;
+  final CancelToken cancelToken;
+
+  UploadFileInfo({
+    required super.txid,
+    required this.uploadStateStreamController,
+    required this.uploadStream,
+    required this.cancelToken,
+  });
+
+  @override
+  List<Object?> get props => [
+        txid,
+        uploadStateStreamController,
+        uploadStream,
+        cancelToken,
+      ];
+}
