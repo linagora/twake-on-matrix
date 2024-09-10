c8288d37e7d3573f1e91f0b52a18906412196139
khaled.njim <knjim@linagora.com>
2024-09-10 02:15:03 +0100

    TW-1996 added test for contact_status_widget

---
+++ test/pages/new_private_chat/widget/contact_status_widget_test.dart
@@ -0,0 +1,47 @@
+import 'package:fluffychat/pages/new_private_chat/widget/contact_status_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluffychat/domain/model/contact/contact_status.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
+
+void main() {
+  group('ContactStatusWidget', () {
+    testWidgets('renders correctly for inactive status',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: L10n.localizationsDelegates,
+          supportedLocales: L10n.supportedLocales,
+          home: Scaffold(
+            body: ContactStatusWidget(status: ContactStatus.inactive),
+          ),
+        ),
+      );
+
+      expect(find.byType(SvgPicture), findsOneWidget);
+
+      expect(find.byType(Text), findsOneWidget);
+
+      final svgPicture = tester.widget<SvgPicture>(find.byType(SvgPicture));
+      expect(
+        svgPicture.colorFilter,
+        ColorFilter.mode(
+          LinagoraRefColors.material().neutral[60]!,
+          BlendMode.srcIn,
+        ),
+      );
+
+      final text = tester.widget<Text>(find.byType(Text));
+      expect(text.style?.color, LinagoraRefColors.material().neutral[60]);
+      expect(
+        text.style?.fontSize,
+        Theme.of(tester.element(find.byType(ContactStatusWidget)))
+            .textTheme
+            .bodySmall
+            ?.fontSize,
+      );
+    });
+  });
+}
