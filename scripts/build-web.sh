9d0913eae74211139215222f50796f1aef979885
Nguyen Thai <tknguyen@linagora.com>
2024-09-26 12:02:15 +0700

    Added support for base href customization

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ve
+TWAKECHAT_BASE_HREF=${TWAKECHAT_BASE_HREF:-/web/}
 flutter config --enable-web
 flutter clean
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
-flutter build web --release --verbose --source-maps --base-href="/web/"
+flutter build web --release --verbose --source-maps --base-href="$TWAKECHAT_BASE_HREF"
 cp config.sample.json ./build/web/config.json
