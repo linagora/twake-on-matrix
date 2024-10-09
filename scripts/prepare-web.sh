1207a97235a933438c0d42fe3951f532d33ea267
Pierre 'McFly' Marty <pmarty@linagora.com>
2024-10-09 22:13:28 +0200

    fix: olm.zip url is dead
    	- Updates to download from the permalink of the
    	  latest release of the maintainer's Github repo.

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -ve
-rm -r assets/js/package
-cd assets/js/ && curl -L 'https://gitlab.com/famedly/company/frontend/libraries/olm/-/jobs/artifacts/master/download?job=build:js' > olm.zip && cd ../../
-cd assets/js/ && unzip olm.zip && cd ../../
-cd assets/js/ && rm olm.zip && cd ../../
-cd assets/js/ && mv javascript package && cd ../../
+
+mkdir -p assets/js && cd assets/js
+test -d package && rm -r package || echo "nothing to clear!"
+
+curl -L 'https://github.com/famedly/olm/releases/latest/download/olm.zip' > olm.zip
+unzip olm.zip
+rm olm.zip
+mv javascript package
