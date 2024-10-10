8e7a27e2a8fbc2201ac6b2d5f16075d4bc21b451
Pierre 'McFly' Marty <pmarty@linagora.com>
2024-10-10 15:58:34 +0200

    feat(scripts): prepare-web uses olm version from pubspec

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -ve
+OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm | tr -d '"^~' )
+DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v${OLM_VERSION}/olm.zip"
 
 mkdir -p assets/js && cd assets/js
 test -d package && rm -r package || echo "nothing to clear!"
 
-curl -L 'https://github.com/famedly/olm/releases/latest/download/olm.zip' > olm.zip
+curl -OLSs $DOWNLOAD_PATH
 unzip olm.zip
 rm olm.zip
 mv javascript package
