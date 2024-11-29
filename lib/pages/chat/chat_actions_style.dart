b6edfec0db64c63d7adcad58fcc8922bb8bcfe1c
khaled.njim <knjim@linagora.com>
2024-11-29 11:16:43 +0100

    TW-1846 Update icons and text style for image picker

--- lib/pages/chat/chat_actions_style.dart
+++ lib/pages/chat/chat_actions_style.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/colors/linagora_sys_colors.dart';
 
 class ChatActionsStyle {
-  static const Color colorBackgroundGalleryBottom = Color(0xFFFFAB00);
+  static const Color colorBackgroundGalleryBottom = Colors.transparent;
   static const Color colorBackgroundDocumentBottom = Color(0xFFEDE7F6);
   static const Color colorBackgroundLocationBottom = Color(0xFFE0F2F1);
   static const Color colorBackgroundContactBottom = Color(0xFFFCE4EC);
 
-  static const Color colorGalleryIcon = Color(0xFFFFFFFF);
-  static const Color colorDocumentIcon = Color(0xFF7C4DFF);
+  static Color colorGalleryIcon = LinagoraSysColors.material().primary;
+  static Color colorDocumentIcon = LinagoraSysColors.material().tertiary;
   static const Color colorLocationIcon = Color(0xFF00BFA5);
   static const Color colorContactIcon = Color(0xFF880E4F);
 }
