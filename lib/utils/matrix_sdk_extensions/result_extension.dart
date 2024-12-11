519c3d2e083abc5cb93cb2387ab721da11e7cb44
HuyNguyen <quanghnguyen@linagora.com>
2024-12-11 16:41:11 +0700

    TW-2111: Fix replies do not appear in search results.

--- lib/utils/matrix_sdk_extensions/result_extension.dart
+++ lib/utils/matrix_sdk_extensions/result_extension.dart
@@ -1,6 +1,8 @@
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 extension ResultExtension on Result {
   Event? getEvent(BuildContext context) {
@@ -14,13 +16,25 @@ extension ResultExtension on Result {
     return Event.fromMatrixEvent(result!, room);
   }
 
-  bool isDisplayableResult({BuildContext? context}) {
+  bool isDisplayableResult({
+    BuildContext? context,
+    required String searchWord,
+  }) {
     if (context == null) {
       return false;
     }
     final event = getEvent(context);
-    return event != null &&
-        (event.relationshipType == null ||
-            event.relationshipType != RelationshipTypes.reply);
+    if (event == null) {
+      return false;
+    }
+    final bodyContent = event.calcLocalizedBodyFallback(
+      MatrixLocals(L10n.of(context)!),
+      hideEdit: true,
+      hideReply: true,
+      plaintextBody: true,
+      removeMarkdown: true,
+    );
+
+    return bodyContent.toLowerCase().contains(searchWord.toLowerCase());
   }
 }
