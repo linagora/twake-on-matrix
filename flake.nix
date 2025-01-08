2755d53fb691f97c57837208f98957bd669ff351
Pierre 'McFly' Marty <pmarty@linagora.com>
2025-01-08 15:26:06 +0100

    chore(flake): update androidsdk config

--- flake.nix
+++ flake.nix
@@ -18,18 +18,13 @@
         };
         androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
         androidComposition = androidEnv.composeAndroidPackages {
-          cmdLineToolsVersion = "8.0"; # emulator related: newer versions are not only compatible with avdmanager
-          platformToolsVersion = "34.0.4";
-          buildToolsVersions = [ "30.0.3" "33.0.2" "34.0.0" ];
-          platformVersions = [ "28" "31" "32" "33" "34" ];
-          abiVersions = [ "x86_64" ]; # emulator related: on an ARM machine, replace "x86_64" with
-          # either "armeabi-v7a" or "arm64-v8a", depending on the architecture of your workstation.
+          ## Details about default values: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
+          #
+          buildToolsVersions = [ "30.0.3" ];
+          platformVersions = [ "31" "32" "33" "34" ];
+          cmakeVersions = [ "3.18.1" ];
           includeNDK = true;
-          ndkVersions = ["23.1.7779620"];
-          cmakeVersions = [ "3.18.1" ];          
-          includeSystemImages = true; # emulator related: system images are needed for the emulator.
-          systemImageTypes = [ "google_apis" "google_apis_playstore" ];
-          includeEmulator = true; # emulator related: if it should be enabled or not
+          ndkVersion = "23.1.7779620";
           useGoogleAPIs = true;
           extraLicenses = [
             "android-googletv-license"
@@ -39,7 +34,8 @@
             "google-gdk-license"
             "intel-android-extra-license"
             "intel-android-sysimage-license"
-            "mips-android-sysimage-license"            ];
+            "mips-android-sysimage-license"
+          ];
         };
         androidSdk = androidComposition.androidsdk;
       in
@@ -51,39 +47,42 @@
           JAVA_HOME = jdk17.home;
           FLUTTER_ROOT = flutter324;
           DART_ROOT = "${flutter324}/bin/cache/dart-sdk";
-          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/33.0.2/aapt2";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/30.0.3/aapt2";
           QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X
           # NB: due to the emulator's bundled qt version, it currently does not start with QT_QPA_PLATFORM="wayland".
           # Maybe one day this will be supported.
           buildInputs = [
             androidSdk
-            cmake
             flutter324
-            gradle_7
+            gradle
             jdk17
-            google-chrome
-            chromedriver
+            # android-tools
+            # android-udev-rules
 
-            # Linux Build
-            fribidi.dev
-            jsoncpp.dev
-            libass.dev
-            libdrm.dev
-            libepoxy.dev
-            libgbm
-            libsecret.dev
-            libsysprof-capture
-            mpv-unwrapped.dev
-            olm
-            pcre2.dev
-            pkg-config
-            rhash
-            webkitgtk_4_0.dev
-            xorg.libXdmcp.dev
+            # # web chrome target
+            # google-chrome
+            # chromedriver
+            #
+            # # Linux target
+            # fribidi.dev
+            # jsoncpp.dev
+            # libass.dev
+            # libdrm.dev
+            # libepoxy.dev
+            # libgbm
+            # libsecret.dev
+            # libsysprof-capture
+            # mpv-unwrapped.dev
+            # olm
+            # pcre2.dev
+            # pkg-config
+            # rhash
+            # webkitgtk_4_0.dev
+            # xorg.libXdmcp.dev
           ];
           # emulator related: vulkan-loader and libGL shared libs are necessary for hardware decoding
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [vulkan-loader libGL]}";
-          CMAKE_PREFIX_PATH = "${pkgs.lib.makeLibraryPath [libsecret.dev gtk3.dev]}";
+          # LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [vulkan-loader libGL]}";
+          # CMAKE_PREFIX_PATH = "${pkgs.lib.makeLibraryPath [libsecret.dev gtk3.dev]}";
           # Globally installed packages, which are installed through `dart pub global activate package_name`,
           # are located in the `$PUB_CACHE/bin` directory.
           shellHook = ''
@@ -92,6 +91,8 @@
             else
               export PATH="$PATH:$PUB_CACHE/bin"
             fi
+
+            flutter doctor -v
           '';
         };
       }
