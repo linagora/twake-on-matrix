b1111bdeb76687d2448a207adf1749b779a64fe6
Pierre 'McFly' Marty <pmarty@linagora.com>
2025-01-08 19:00:41 +0100

    chore(flake): lightweight web config

--- flake.nix
+++ flake.nix
@@ -16,54 +16,56 @@
             "olm-3.2.16"
           ];
         };
-        androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
-        androidComposition = androidEnv.composeAndroidPackages {
-          ## Details about default values: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
-          #
-          buildToolsVersions = [ "30.0.3" ];
-          platformVersions = [ "31" "32" "33" "34" ];
-          cmakeVersions = [ "3.18.1" ];
-          includeNDK = true;
-          ndkVersion = "23.1.7779620";
-          useGoogleAPIs = true;
-          extraLicenses = [
-            "android-googletv-license"
-            "android-sdk-arm-dbt-license"
-            "android-sdk-license"
-            "android-sdk-preview-license"
-            "google-gdk-license"
-            "intel-android-extra-license"
-            "intel-android-sysimage-license"
-            "mips-android-sysimage-license"
-          ];
-        };
-        androidSdk = androidComposition.androidsdk;
+        # androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
+        # androidComposition = androidEnv.composeAndroidPackages {
+        #   ## Details about default values: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
+        #   #
+        #   buildToolsVersions = [ "30.0.3" ];
+        #   platformVersions = [ "31" "32" "33" "34" ];
+        #   cmakeVersions = [ "3.18.1" ];
+        #   includeNDK = true;
+        #   ndkVersion = "23.1.7779620";
+        #   useGoogleAPIs = true;
+        #   extraLicenses = [
+        #     "android-googletv-license"
+        #     "android-sdk-arm-dbt-license"
+        #     "android-sdk-license"
+        #     "android-sdk-preview-license"
+        #     "google-gdk-license"
+        #     "intel-android-extra-license"
+        #     "intel-android-sysimage-license"
+        #     "mips-android-sysimage-license"
+        #   ];
+        # };
+        # androidSdk = androidComposition.androidsdk;
       in
       {
         devShell = with pkgs; mkShell rec {
-          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          # ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          # ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
           CHROME_EXECUTABLE = "google-chrome-stable";
-          JAVA_HOME = jdk17.home;
+          # JAVA_HOME = jdk17.home;
           FLUTTER_ROOT = flutter324;
           DART_ROOT = "${flutter324}/bin/cache/dart-sdk";
-          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/30.0.3/aapt2";
+          # GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/30.0.3/aapt2";
           QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X
           # NB: due to the emulator's bundled qt version, it currently does not start with QT_QPA_PLATFORM="wayland".
           # Maybe one day this will be supported.
           buildInputs = [
-            androidSdk
+            ## General needs
             flutter324
-            gradle
-            jdk17
-            # android-tools
-            # android-udev-rules
-
             # # web chrome target
             # google-chrome
             # chromedriver
-            #
-            # # Linux target
+
+            ## Android target
+            # androidSdk
+            # gradle
+            # jdk17
+            # android-tools
+            # android-udev-rules
+
+            ## Linux target
             # fribidi.dev
             # jsoncpp.dev
             # libass.dev
