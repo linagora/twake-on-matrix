2bc61b809130c80b98a7407dfe4d237bbb3655e2
HuyNguyen <quanghnguyen@linagora.com>
2025-02-04 15:48:28 +0700

    Integration federation request token module

--- lib/modules/federation_identity_request_token/domain/models/federation_token_request.dart
+++ lib/modules/federation_identity_request_token/domain/models/federation_token_request.dart
@@ -3,15 +3,18 @@ import 'package:equatable/equatable.dart';
 class FederationTokenRequest with EquatableMixin {
   final String federationUrl;
   final String mxid;
+  final String token;
 
   FederationTokenRequest({
     required this.federationUrl,
     required this.mxid,
+    required this.token,
   });
 
   @override
   List<Object?> get props => [
         federationUrl,
         mxid,
+        token,
       ];
 }
