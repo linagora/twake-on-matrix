e8afe0aa62e387b6b41ca77914e15fa5d1d60e54
Dat PHAM HOANG <dphamhoang@linagora.com>
2025-02-27 17:49:37 +0700

    [SYNC-CONTACT-V2] Unit test for TwakeLookupPhonebookContact

---
+++ lib/domain/exception/contacts/twake_lookup_exceptions.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class TwakeLookupChunkException with EquatableMixin implements Exception {
+  final String? message;
+
+  TwakeLookupChunkException(this.message);
+  
+  @override
+  List<Object?> get props => [message];
+}
