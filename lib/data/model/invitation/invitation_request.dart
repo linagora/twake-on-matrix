3f2e58e93ec61417993565d8226e7cb5215c958a
HuyNguyen <quanghnguyen@linagora.com>
2025-03-27 15:02:45 +0700

    TW-2270: Integration API in data layer

---
+++ lib/data/model/invitation/invitation_request.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'invitation_request.g.dart';
+
+@JsonSerializable()
+class InvitationRequest with EquatableMixin {
+  @JsonKey(name: "contact")
+  final String? contact;
+  @JsonKey(name: "medium")
+  final String? medium;
+
+  InvitationRequest({
+    this.contact,
+    this.medium,
+  });
+
+  factory InvitationRequest.fromJson(Map<String, dynamic> json) =>
+      _$InvitationRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InvitationRequestToJson(this);
+
+  @override
+  List<Object?> get props => [
+        contact,
+        medium,
+      ];
+}
