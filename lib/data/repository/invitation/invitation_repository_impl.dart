76a9ee33a544daf0eaca08260ec68f658ee900d9
HuyNguyen <quanghnguyen@linagora.com>
2025-04-03 15:55:29 +0700

    TW-2272: Integration invitation status API

--- lib/data/repository/invitation/invitation_repository_impl.dart
+++ lib/data/repository/invitation/invitation_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:fluffychat/data/datasource/invitation/invitation_datasource.dart';
 import 'package:fluffychat/data/model/invitation/generate_invitation_link_response.dart';
 import 'package:fluffychat/data/model/invitation/invitation_request.dart';
+import 'package:fluffychat/data/model/invitation/invitation_status_response.dart';
 import 'package:fluffychat/data/model/invitation/send_invitation_response.dart';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/repository/invitation/invitation_repository.dart';
@@ -21,5 +22,14 @@ class InvitationRepositoryImpl implements InvitationRepository {
     required InvitationRequest request,
   }) {
     return _invitationDatasource.sendInvitation(request: request);
+  }
+
+  @override
+  Future<InvitationStatusResponse> getInvitationStatus({
+    required String invitationId,
+  }) {
+    return _invitationDatasource.getInvitationStatus(
+      invitationId: invitationId,
+    );
   }
 }
--- lib/domain/repository/invitation/invitation_repository.dart
+++ lib/domain/repository/invitation/invitation_repository.dart
@@ -1,5 +1,6 @@
 import 'package:fluffychat/data/model/invitation/generate_invitation_link_response.dart';
 import 'package:fluffychat/data/model/invitation/invitation_request.dart';
+import 'package:fluffychat/data/model/invitation/invitation_status_response.dart';
 import 'package:fluffychat/data/model/invitation/send_invitation_response.dart';
 
 abstract class InvitationRepository {
@@ -9,5 +10,9 @@ abstract class InvitationRepository {
 
   Future<GenerateInvitationLinkResponse> generateInvitationLink({
     required InvitationRequest request,
+  });
+
+  Future<InvitationStatusResponse> getInvitationStatus({
+    required String invitationId,
   });
 }
