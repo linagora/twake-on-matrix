63a13e679992f766c93fadbd5bb35f90a2f7ecad
Dat PHAM HOANG <dphamhoang@linagora.com>
2025-05-29 14:59:21 +0700

    TW-2361 Disable/Enable for send message permission

---
+++ lib/pages/chat/disabled_chat_input_row.dart
@@ -0,0 +1,48 @@
+import 'package:fluffychat/pages/chat/disabled_chat_input_row_style.dart';
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+class DisabledChatInputRow extends StatelessWidget {
+  const DisabledChatInputRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(
+        DisabledChatInputRowStyle.chatDisabledBottomBarPadding,
+      ),
+      color: Theme.of(context).colorScheme.surfaceContainerHighest,
+      height: DisabledChatInputRowStyle.chatDisabledBottomBarHeight,
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.info_outline,
+              size: DisabledChatInputRowStyle.chatDisabledBottomBarIconSize,
+              color: LinagoraRefColors.material().tertiary[30],
+            ),
+            const SizedBox(
+              width: DisabledChatInputRowStyle.chatDisabledBottomBarIconSpacing,
+            ),
+            Flexible(
+              child: Text(
+                L10n.of(context)!.noChatPermissionMessage,
+                maxLines: 2,
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      color: LinagoraRefColors.material().tertiary[30],
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
