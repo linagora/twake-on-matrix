a64fc268a1156c0e31a3df7512b2aa51a086f966
Dat PHAM HOANG <dphamhoang@linagora.com>
2025-06-06 10:19:45 +0700

    TW-2349 Delete event interactor

---
+++ lib/domain/usecase/room/delete_event_interactor.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/domain/app_state/room/delete_event_state.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
+import 'package:matrix/matrix.dart';
+
+class DeleteEventInteractor {
+  Stream<Either<Failure, Success>> execute(
+    Event event,
+  ) async* {
+    if (!event.canDelete) {
+      yield Left(NoPermissionToDeleteEvent());
+    }
+
+    try {
+      await event.redactEvent();
+    } catch (e) {
+      yield Left(DeleteEventFailure());
+      return;
+    }
+
+    try {
+      await event.remove();
+      yield Right(DeleteEventSuccess());
+    } catch (e) {
+      yield Left(RemoveLocalEventFailure());
+    }
+  }
+}
