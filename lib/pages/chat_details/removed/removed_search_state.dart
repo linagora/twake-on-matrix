846c82910d46bcfe150c5e6019440124c2e0ba56
HuyNguyen <quanghnguyen@linagora.com>
2025-06-12 11:52:50 +0700

    TW-2383: Impl removed users

---
+++ lib/pages/chat_details/removed/removed_search_state.dart
@@ -0,0 +1,36 @@
+import 'package:fluffychat/app_state/failure.dart';
+import 'package:fluffychat/app_state/initial.dart';
+import 'package:fluffychat/app_state/success.dart';
+import 'package:matrix/matrix.dart';
+
+class RemovedSearchInitialState extends Initial {
+  @override
+  List<Object?> get props => [];
+}
+
+class RemovedSearchSuccessState extends Success {
+  final List<User> removedMember;
+  final String keyword;
+
+  const RemovedSearchSuccessState({
+    required this.removedMember,
+    required this.keyword,
+  });
+
+  @override
+  List<Object?> get props => [
+        removedMember,
+        keyword,
+      ];
+}
+
+class RemovedSearchEmptyState extends Failure {
+  final String keyword;
+
+  const RemovedSearchEmptyState({
+    required this.keyword,
+  });
+
+  @override
+  List<Object?> get props => [keyword];
+}
