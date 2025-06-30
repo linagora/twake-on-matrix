5b35f7adecb9c7d708c4160a56f34999c8e93f9c
dab246 <tdvu@linagora.com>
2025-06-30 11:00:38 +0700

    TW-2060 Allow drag to select multiple text on chat event list

--- lib/pages/chat/events/formatted_text_widget.dart
+++ lib/pages/chat/events/formatted_text_widget.dart
@@ -1,6 +1,4 @@
-import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/pages/chat/events/html_message.dart';
-import 'package:fluffychat/utils/responsive/responsive_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart' hide Visibility;
 
@@ -18,7 +16,6 @@ class FormattedTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final responsiveUtils = getIt.get<ResponsiveUtils>();
     var html = event.formattedText;
 
     if (event.messageType == MessageTypes.Emote) {
@@ -27,24 +24,12 @@ class FormattedTextWidget extends StatelessWidget {
     final bigEmotes =
         event.onlyEmotes && event.numberEmotes > 0 && event.numberEmotes <= 10;
 
-    if (responsiveUtils.isMobile(context)) {
-      return HtmlMessage(
-        html: html,
-        defaultTextStyle: Theme.of(context).textTheme.bodyLarge,
-        linkStyle: linkStyle,
-        room: event.room,
-        emoteSize: bigEmotes ? fontSize * 3 : fontSize * 1.5,
-      );
-    }
-
-    return SelectionArea(
-      child: HtmlMessage(
-        html: html,
-        defaultTextStyle: Theme.of(context).textTheme.bodyLarge,
-        linkStyle: linkStyle,
-        room: event.room,
-        emoteSize: bigEmotes ? fontSize * 3 : fontSize * 1.5,
-      ),
+    return HtmlMessage(
+      html: html,
+      defaultTextStyle: Theme.of(context).textTheme.bodyLarge,
+      linkStyle: linkStyle,
+      room: event.room,
+      emoteSize: bigEmotes ? fontSize * 3 : fontSize * 1.5,
     );
   }
 }
