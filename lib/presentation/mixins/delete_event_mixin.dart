d08a0441e00b7c443fd519e8c14ae36fbfe15cd1
DatDang <tddang@linagora.com>
2025-07-07 14:32:39 +0700

    Add confirmation dialog before delete message

--- lib/presentation/mixins/delete_event_mixin.dart
+++ lib/presentation/mixins/delete_event_mixin.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:fluffychat/di/global/get_it_initializer.dart';
 import 'package:fluffychat/domain/app_state/room/delete_event_state.dart';
 import 'package:fluffychat/domain/usecase/room/delete_event_interactor.dart';
+import 'package:fluffychat/utils/dialog/twake_dialog.dart';
+import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/twake_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
@@ -13,6 +15,14 @@ mixin DeleteEventMixin {
   StreamSubscription? _streamSubscription;
 
   Future<void> deleteEventAction(BuildContext context, Event event) async {
+    final confirmResult = await showConfirmAlertDialog(
+      context: context,
+      title: L10n.of(context)!.deleteMessageConfirmationTitle,
+      okLabel: L10n.of(context)!.deleteMessage,
+      cancelLabel: L10n.of(context)!.cancel,
+      showCloseButton: PlatformInfos.isWeb,
+    );
+    if (confirmResult == ConfirmResult.cancel) return;
     _streamSubscription = deleteEventInteractor.execute(event).listen((state) {
       state.fold(
         (failure) {
