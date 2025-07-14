4cb547766437e5eeb6df06274e87e6fc3c60bbf2
Huy Quang Nguyen <quanghnguyen@linagora.com>
2025-07-14 14:13:59 +0700

    Implement quick role picker for user role assignment (#2442)

--- lib/domain/model/room/room_extension.dart
+++ lib/domain/model/room/room_extension.dart
@@ -251,7 +251,7 @@ extension RoomExtension on Room {
     if (members.isEmpty) return [];
     return members.where((final User member) {
       final powerLevel = member.powerLevel;
-      return powerLevel >= DefaultPowerLevelMember.admin.powerLevel &&
+      return powerLevel >= DefaultPowerLevelMember.moderator.powerLevel &&
           member.membership == Membership.join;
     }).toList();
   }
@@ -286,5 +286,9 @@ extension RoomExtension on Room {
       ..sort(
         (small, great) => great.powerLevel.compareTo(small.powerLevel),
       );
+  }
+
+  bool canUpdateRoleInRoom(User user) {
+    return ownPowerLevel > user.powerLevel;
   }
 }
