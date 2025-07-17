dba71698cb9613f99fe997fe61327103980cfe40
hoangdat <hoangdat@users.noreply.github.com>
2025-07-17 02:48:37 +0000

    deploy: eca50174ae573a9dcbc13a8014f6f3cfa6207ac6

--- 2449/flutter_service_worker.js
+++ 2449/flutter_service_worker.js
@@ -711,7 +711,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ed03b32b35556debb06c6d28afd6e868
 "auth.html": "753913b429e0217a2c870bdafd63d063",
 "version.json": "decc3a8206a8b91100f677d94bb2be7d",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
-"flutter_bootstrap.js": "75dd0b751b3019ea229446bea0054ab9",
+"flutter_bootstrap.js": "35310430c41d685b5f5981eb173d3c09",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
