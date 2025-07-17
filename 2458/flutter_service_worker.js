3853cfda1603b6b3364f48d9324197cc7c8eab5c
nqhhdev <nqhhdev@users.noreply.github.com>
2025-07-17 04:01:11 +0000

    deploy: 754547f04c447c0e3d2aebf3d010337ab2925f81

--- 2458/flutter_service_worker.js
+++ 2458/flutter_service_worker.js
@@ -711,7 +711,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ed03b32b35556debb06c6d28afd6e868
 "auth.html": "753913b429e0217a2c870bdafd63d063",
 "version.json": "decc3a8206a8b91100f677d94bb2be7d",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
-"flutter_bootstrap.js": "7a118dd39c067cbdc0baa26959d3e640",
+"flutter_bootstrap.js": "40efa1b5b107bef49a1e3a20952f1fb1",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
