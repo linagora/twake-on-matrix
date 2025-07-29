a67145685df9a31e762575b6c3c217c841211821
nqhhdev <nqhhdev@users.noreply.github.com>
2025-07-29 10:47:25 +0000

    deploy: e1274ad372be4039ea1f12b97415063b0b84a274

--- 2483/flutter_bootstrap.js
+++ 2483/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "999327261"
+    serviceWorkerVersion: "3082906105"
   }
 });
--- 2483/flutter_service_worker.js
+++ 2483/flutter_service_worker.js
@@ -711,7 +711,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ed03b32b35556debb06c6d28afd6e868
 "auth.html": "753913b429e0217a2c870bdafd63d063",
 "version.json": "0c0dd49fa71775836a5d668f008614ae",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
-"flutter_bootstrap.js": "3e81de0b9c283dfc53bccfaf3e242e79",
+"flutter_bootstrap.js": "b3f602ad69879d6dd0730040f8a168d6",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
