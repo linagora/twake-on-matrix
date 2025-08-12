cb866f15c9a9921a8fb32c35e2530b6a0b05dd9c
Dat PHAM HOANG <dphamhoang@linagora.com>
2025-08-12 15:11:50 +0700

    TF #2439 Remove get address book datasource

--- lib/domain/repository/contact/address_book_repository.dart
+++ lib/domain/repository/contact/address_book_repository.dart
@@ -2,8 +2,6 @@ import 'package:fluffychat/data/model/addressbook/address_book_request.dart';
 import 'package:fluffychat/data/model/addressbook/address_book_response.dart';
 
 abstract class AddressBookRepository {
-  Future<AddressbookResponse> getAddressBook();
-
   Future<AddressbookResponse> updateAddressBook({
     required AddressBookRequest request,
   });
