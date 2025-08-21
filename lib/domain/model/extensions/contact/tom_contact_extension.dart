ada215a31d721056e50c00f5ccca8006ccf67b79
HuyNguyen <quanghnguyen@linagora.com>
2025-08-21 18:42:29 +0700

    Fix include address in contact conversion and filtering

--- lib/domain/model/extensions/contact/tom_contact_extension.dart
+++ lib/domain/model/extensions/contact/tom_contact_extension.dart
@@ -6,7 +6,7 @@ import 'package:fluffychat/domain/model/contact/tom_contact.dart';
 extension TomContactExtension on TomContact {
   Contact toContact(ContactStatus status) {
     return Contact(
-      emails: mail != null
+      emails: mail != null || address != null
           ? {
               Email(
                 address: mail ?? '',
@@ -37,7 +37,10 @@ extension TomContactExtension on TomContact {
 extension TomContactsExtension on Set<TomContact> {
   Set<TomContact> removeUnknownTomContact() {
     return where(
-      (tomContact) => tomContact.mail != null || tomContact.phoneNumber != null,
+      (tomContact) =>
+          tomContact.mail != null ||
+          tomContact.phoneNumber != null ||
+          tomContact.address != null,
     ).map((contact) => contact).toSet();
   }
 }
