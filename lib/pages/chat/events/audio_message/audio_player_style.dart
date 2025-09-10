aac8d9445df123f08ac7edba07c034addbc1d34d
HuyNguyen <quanghnguyen@linagora.com>
2025-09-10 21:13:49 +0700

    TW-2524: Add audio mixin for wave count and waveform calculations in audio player

--- lib/pages/chat/events/audio_message/audio_player_style.dart
+++ lib/pages/chat/events/audio_message/audio_player_style.dart
@@ -4,7 +4,16 @@ import 'package:flutter/material.dart';
 class AudioPlayerStyle {
   static const minWaveCount = 32;
 
+  static const maxWaveWidth = 4.0;
+
+  static const maxBodyContentWidth = 88.0;
+
   static int maxWaveCount(BuildContext context) {
-    return (MessageStyle.messageBubbleWidth(context) - 88) ~/ 4;
+    return (MessageStyle.messageBubbleWidth(context) - maxBodyContentWidth) ~/
+        maxWaveWidth;
   }
+
+  static const minWaveHeight = 4.0;
+
+  static const maxWaveHeight = 26.0;
 }
