8d89bab95562832a3a0b83204503ffa3dcc598e6
HuyNguyen <quanghnguyen@linagora.com>
2025-09-15 00:49:47 +0700

    TW #2524: Enhance audio player widget with file name and size display

--- lib/pages/chat/events/audio_message/audio_player_style.dart
+++ lib/pages/chat/events/audio_message/audio_player_style.dart
@@ -1,5 +1,6 @@
 import 'package:fluffychat/pages/chat/events/message/message_style.dart';
 import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 
 class AudioPlayerStyle {
   static const minWaveCount = 32;
@@ -16,4 +17,9 @@ class AudioPlayerStyle {
   static const minWaveHeight = 4.0;
 
   static const maxWaveHeight = 26.0;
+
+  static TextStyle? textInformationStyle(BuildContext context) =>
+      Theme.of(context).textTheme.bodySmall!.copyWith(
+            color: LinagoraRefColors.material().tertiary[20],
+          );
 }
