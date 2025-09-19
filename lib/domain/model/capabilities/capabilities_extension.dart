753d9216bd6fdb038163c9049e515c9b69b4269d
Dang Dat <tddang@linagora.com>
2025-09-19 16:28:03 +0700

    fixup! TW-2541 Apply capabilities settings to profile page

--- lib/domain/model/capabilities/capabilities_extension.dart
+++ lib/domain/model/capabilities/capabilities_extension.dart
@@ -2,10 +2,14 @@ import 'package:matrix/matrix.dart';
 
 extension CapabilitiesExtension on Capabilities {
   bool get canEditAvatar {
-    return additionalProperties['m.set_avatar_url']?['enabled'] == true;
+    const key = 'm.set_avatar_url';
+    return additionalProperties[key]?['enabled'] == true ||
+        additionalProperties[key]?['enabled'] == null;
   }
 
   bool get canEditDisplayName {
-    return additionalProperties['m.set_displayname']?['enabled'] == true;
+    const key = 'm.set_displayname';
+    return additionalProperties[key]?['enabled'] == true ||
+        additionalProperties[key]?['enabled'] == null;
   }
 }
