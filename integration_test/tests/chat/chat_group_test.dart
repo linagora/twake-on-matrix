d236c9f7638cc2d3bcdcd2fb504d2962ed699b72
thuyenh1081 <thuyenh1081@gmail.com>
2025-10-02 17:51:59 +0700

    add script View profile of a members in a group

--- integration_test/tests/chat/chat_group_test.dart
+++ integration_test/tests/chat/chat_group_test.dart
@@ -3,6 +3,7 @@ import 'package:fluffychat/pages/chat/event_info_dialog.dart';
 import 'package:fluffychat/widgets/avatar/avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../../base/base_scenario.dart';
 import '../../base/test_base.dart';
 import '../../help/soft_assertion_helper.dart';
 import '../../robots/chat_group_detail_robot.dart';
@@ -194,6 +195,27 @@ void main() {
       // verify info dialog is shown
       final numberOfResult = (await ChatSearchViewRobot($).getListOfChatSeach()).length;
       expect(numberOfResult == 2, isTrue, reason: "expect is 2 but got: $numberOfResult");
+    },
+  );
+
+  TestBase().runPatrolTest(
+    description: 'View profile of a members in a group',
+    test: ($) async {
+      final s = SoftAssertHelper();
+      //goto chat
+      await HomeRobot($).gotoChatListScreen();
+
+      //open a group
+      const groupTest = String.fromEnvironment('GroupTest');
+      await ChatScenario($).openChatGroupByTitle(groupTest);
+
+      //open group Info
+      await ChatScenario($).openGroupChatInfo();
+
+      //Verify profile Info of all member
+      await ChatDetailScenario($).verifyProfileInfoOfAllMember(s);
+
+      s.verifyAll();
     },
   );
 }
