40fb25bf48eb51066f49eef598f519d0cb2b8f29
Dat H. Pham <dphamhoang@linagora.com>
2025-10-14 14:49:06 +0700

    Fix format for generated files

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -ve
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+# Ensure generated localization files are formatted correctly
+dart format --set-exit-if-changed lib/generated/l10n/
 dart format --set-exit-if-changed lib/ test/
 flutter analyze
