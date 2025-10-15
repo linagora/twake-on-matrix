8df7062675871671db8e0439111459f68dd50bc7
Dang Dat <tddang@linagora.com>
2025-10-15 13:38:02 +0700

    fixup! HOTFIX Build vodozemac web

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -8,6 +8,7 @@ version=$(expr "$version" : '\^*\(.*\)')
 git clone https://github.com/famedly/dart-vodozemac.git -b ${version} .vodozemac
 cd .vodozemac
 rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 cargo install flutter_rust_bridge_codegen
 flutter_rust_bridge_codegen build-web --dart-root dart --rust-root $(readlink -f rust) --release --verbose
 cd ..
