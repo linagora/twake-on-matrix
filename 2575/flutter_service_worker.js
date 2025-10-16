885b5914aa3f889ce36faf9bf01b9b9a2a8d22e1
nqhhdev <nqhhdev@users.noreply.github.com>
2025-10-16 02:23:23 +0000

    deploy: 3cd13498b39abe2751d8cf78196510f8969a2a5e

--- 2575/flutter_service_worker.js
+++ 2575/flutter_service_worker.js
@@ -738,7 +738,7 @@ const RESOURCES = {"flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "pkg/vodozemac_bindings_dart_bg.wasm": "3b98e98654d27f98fb57df6dfaab4b54",
 "pkg/vodozemac_bindings_dart.js": "b5bde3f81cd214e89aaa74876cbf76b6",
 "pkg/package.json": "5e17fd65e76b4fd073085bcc4a65de15",
-"flutter_bootstrap.js": "da1767297f6f67bca2dcfc175238885e",
+"flutter_bootstrap.js": "5989486340dd4f1eee79c341ed31321a",
 "version.json": "bbf2c76b5751d1ad2447a14856af32b7",
 "main.dart.js": "8ca5217097eead32ff5e5fa38c319a9b"};
 // The application shell files that are downloaded before a service worker can
