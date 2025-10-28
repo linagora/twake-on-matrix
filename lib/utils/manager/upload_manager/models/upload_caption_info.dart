10116c21bbe8401b881b247a741e46e17c72fe0d
Huy Quang Nguyen <quanghnguyen@linagora.com>
2025-10-28 11:57:00 +0700

    TW-2626: Fix Can’t send caption with video and file (#2627)

--- lib/utils/manager/upload_manager/models/upload_caption_info.dart
+++ lib/utils/manager/upload_manager/models/upload_caption_info.dart
@@ -1,12 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 class UploadCaptionInfo with EquatableMixin {
+  final String txid;
   final String caption;
 
   UploadCaptionInfo({
     required this.caption,
+    required this.txid,
   });
 
   @override
-  List<Object?> get props => [caption];
+  List<Object?> get props => [caption, txid];
 }
