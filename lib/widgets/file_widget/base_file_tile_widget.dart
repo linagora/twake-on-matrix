acafd31c72d20c2a58eedbc758ce3110af85e277
HuyNguyen <quanghnguyen@linagora.com>
2025-11-14 23:29:50 +0700

    fixup! TW-2679: Improve display caption handling for media, file types and download file name

--- lib/widgets/file_widget/base_file_tile_widget.dart
+++ lib/widgets/file_widget/base_file_tile_widget.dart
@@ -1,10 +1,15 @@
 import 'dart:typed_data';
 
+import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/pages/chat/events/message_content_style.dart';
 import 'package:fluffychat/utils/extension/mime_type_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
 import 'package:fluffychat/utils/string_extension.dart';
 import 'package:fluffychat/widgets/file_widget/file_tile_widget_style.dart';
+import 'package:fluffychat/widgets/twake_components/twake_preview_link/twake_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:matrix/matrix.dart';
 
 class BaseFileTileWidget extends StatelessWidget {
   const BaseFileTileWidget({
@@ -20,6 +25,7 @@ class BaseFileTileWidget extends StatelessWidget {
     this.style = const FileTileWidgetStyle(),
     this.ownMessage = false,
     required this.subTitle,
+    this.event,
   });
 
   final TwakeMimeType mimeType;
@@ -33,59 +39,81 @@ class BaseFileTileWidget extends StatelessWidget {
   final FileTileWidgetStyle style;
   final Widget Function(BuildContext) subTitle;
   final bool ownMessage;
+  final Event? event;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: style.paddingFileTileAll,
-      decoration: ShapeDecoration(
-        color: backgroundColor ??
-            style.backgroundColor(context, ownMessage: ownMessage),
-        shape: RoundedRectangleBorder(
-          borderRadius: style.borderRadius,
-        ),
-      ),
-      child: Row(
-        crossAxisAlignment: style.crossAxisAlignment,
-        children: [
-          if (imageBytes != null)
-            Padding(
-              padding: style.imagePadding,
-              child: ClipRRect(
-                borderRadius: style.borderRadius,
-                child: Image.memory(
-                  imageBytes!,
-                  width: style.imageSize,
-                  height: style.imageSize,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          if (imageBytes == null)
-            SvgPicture.asset(
-              fileTileIcon ?? mimeType.getIcon(fileType: fileType),
-              width: style.iconSize,
-              height: style.iconSize,
-            ),
-          style.paddingRightIcon,
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(height: 4.0),
-                FileNameText(
-                  filename: filename,
-                  highlightText: highlightText,
-                  style: style,
-                ),
-                subTitle(context),
-                style.paddingBottomText,
-              ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: style.paddingFileTileAll,
+          decoration: ShapeDecoration(
+            color: backgroundColor ??
+                style.backgroundColor(context, ownMessage: ownMessage),
+            shape: RoundedRectangleBorder(
+              borderRadius: style.borderRadius,
             ),
           ),
+          child: Row(
+            crossAxisAlignment: style.crossAxisAlignment,
+            children: [
+              if (imageBytes != null)
+                Padding(
+                  padding: style.imagePadding,
+                  child: ClipRRect(
+                    borderRadius: style.borderRadius,
+                    child: Image.memory(
+                      imageBytes!,
+                      width: style.imageSize,
+                      height: style.imageSize,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              if (imageBytes == null)
+                SvgPicture.asset(
+                  fileTileIcon ?? mimeType.getIcon(fileType: fileType),
+                  width: style.iconSize,
+                  height: style.iconSize,
+                ),
+              style.paddingRightIcon,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const SizedBox(height: 4.0),
+                    FileNameText(
+                      filename: filename,
+                      highlightText: highlightText,
+                      style: style,
+                    ),
+                    subTitle(context),
+                    style.paddingBottomText,
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (event != null && event!.isMediaAndFilesWithCaption()) ...[
+          const SizedBox(height: 8.0),
+          TwakeLinkPreview(
+            key: ValueKey('TwakeLinkPreview%${event!.eventId}%'),
+            event: event!,
+            localizedBody: event!.body,
+            ownMessage: ownMessage,
+            fontSize: AppConfig.messageFontSize * AppConfig.fontSizeFactor,
+            linkStyle: MessageContentStyle.linkStyleMessageContent(
+              context,
+            ),
+            richTextStyle: event!.getMessageTextStyle(context),
+            isCaption: event!.isMediaAndFilesWithCaption(),
+          ),
         ],
-      ),
+      ],
     );
   }
 }
