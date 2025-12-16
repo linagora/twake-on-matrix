0bd637d20cc5cd5c8e944963c42884c85c4e1c2a
Dang Dat <tddang@linagora.com>
2025-12-16 13:42:10 +0700

    fixup! HOTFIX Share QR image

--- lib/pages/personal_qr/personal_qr.dart
+++ lib/pages/personal_qr/personal_qr.dart
@@ -14,6 +14,7 @@ import 'package:matrix/matrix.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:path/path.dart' as path;
 
 class PersonalQr extends StatefulWidget {
   const PersonalQr({super.key});
@@ -100,7 +101,7 @@ class PersonalQrController extends State<PersonalQr> {
   Future<File> _createTempFile(Uint8List imageBytes) async {
     final tempDir = await getTemporaryDirectory();
     final timestamp = DateTime.now().millisecondsSinceEpoch;
-    final filePath = '${tempDir.path}/qr_code_$timestamp.png';
+    final filePath = path.join(tempDir.path, 'qr_code_$timestamp.png');
     final file = File(filePath);
     await file.writeAsBytes(imageBytes);
     return file;
