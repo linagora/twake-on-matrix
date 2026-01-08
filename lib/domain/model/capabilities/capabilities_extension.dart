30190382ec71537dce862e98c15247c0e95a7779
Dang Dat <tddang@linagora.com>
2026-01-08 11:06:26 +0700

    [Migrate Matrix] Fix no common settings edit button

--- lib/domain/model/capabilities/capabilities_extension.dart
+++ lib/domain/model/capabilities/capabilities_extension.dart
@@ -5,15 +5,47 @@ extension CapabilitiesExtension on Capabilities {
     return mProfileFields?.enabled != false;
   }
 
+  bool get _canEditAvatarProfileField {
+    if (!_canEditProfileFields) return false;
+
+    final allowed = mProfileFields?.allowed;
+    final disallowed = mProfileFields?.disallowed;
+    if (allowed != null) {
+      return allowed.contains('avatar_url');
+    } else if (disallowed != null) {
+      return !disallowed.contains('avatar_url');
+    } else {
+      return true;
+    }
+  }
+
+  bool get _canEditDisplayNameProfileField {
+    if (!_canEditProfileFields) return false;
+
+    final allowed = mProfileFields?.allowed;
+    final disallowed = mProfileFields?.disallowed;
+    if (allowed != null) {
+      return allowed.contains('displayname');
+    } else if (disallowed != null) {
+      return !disallowed.contains('displayname');
+    } else {
+      return true;
+    }
+  }
+
   bool get canEditAvatar {
     const key = 'm.set_avatar_url';
     final properties = additionalProperties[key] as Map<String, dynamic>?;
-    return _canEditProfileFields && properties?['enabled'] != false;
+    return _canEditAvatarProfileField &&
+        mSetAvatarUrl?.enabled != false &&
+        properties?['enabled'] != false;
   }
 
   bool get canEditDisplayName {
     const key = 'm.set_displayname';
     final properties = additionalProperties[key] as Map<String, dynamic>?;
-    return _canEditProfileFields && properties?['enabled'] != false;
+    return _canEditDisplayNameProfileField &&
+        mSetDisplayname?.enabled != false &&
+        properties?['enabled'] != false;
   }
 }
