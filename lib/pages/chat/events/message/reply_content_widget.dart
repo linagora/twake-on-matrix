0d48c023b8c14bbeef48ab38c91386d9cd3fef4b
Dang Dat <tddang@linagora.com>
2026-01-13 13:52:42 +0700

    [Migrate Matrix] Update reply event null handling

--- lib/pages/chat/events/message/reply_content_widget.dart
+++ lib/pages/chat/events/message/reply_content_widget.dart
@@ -23,21 +23,28 @@ class ReplyContentWidget extends StatelessWidget {
     return FutureBuilder<Event?>(
       future: event.getReplyEvent(timeline),
       builder: (context, snapshot) {
-        final replyEvent = snapshot.data ??
-            Event(
-              eventId: event.relationshipEventId!,
-              content: {
-                'msgtype': 'm.text',
-                'body': '...',
-              },
-              senderId: event.senderId,
-              type: 'm.room.message',
-              room: event.room,
-              status: EventStatus.sent,
-              originServerTs: DateTime.now(),
-            );
+        Event? replyEvent;
+        if (snapshot.data != null) {
+          replyEvent = snapshot.data;
+        } else if (event.relationshipEventId != null) {
+          replyEvent = Event(
+            eventId: event.relationshipEventId!,
+            content: {
+              'msgtype': 'm.text',
+              'body': '...',
+            },
+            senderId: event.senderId,
+            type: 'm.room.message',
+            room: event.room,
+            status: EventStatus.sent,
+            originServerTs: DateTime.now(),
+          );
+        }
+
+        if (replyEvent == null) return const SizedBox();
+
         return OptionalInkWell(
-          onTap: () => scrollToEventId?.call(replyEvent.eventId),
+          onTap: () => scrollToEventId?.call(replyEvent!.eventId),
           isEnabled: scrollToEventId != null,
           child: Padding(
             padding: ReplyContentStyle.marginReplyContent,
