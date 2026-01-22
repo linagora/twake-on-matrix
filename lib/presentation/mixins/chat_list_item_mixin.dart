e15f846e49f0c9199263988e6e3fcd2494ff4a31
HuyNguyen <quanghnguyen@linagora.com>
2026-01-22 23:17:36 +0700

    fixup! TW-2848: State even in chat list must be in italic

--- lib/presentation/mixins/chat_list_item_mixin.dart
+++ lib/presentation/mixins/chat_list_item_mixin.dart
@@ -46,6 +46,7 @@ mixin ChatListItemMixin {
           overflow: TextOverflow.ellipsis,
           style: ListItemStyle.subtitleTextStyle(
             fontFamily: 'Inter',
+          ).copyWith(
             fontStyle: event.text.isEmpty ? FontStyle.italic : null,
           ),
         );
