d1bf6de6006d47aeeee841b8cf92ceebd727deb4
Huy Quang Nguyen <quanghnguyen@linagora.com>
2026-02-25 16:56:29 +0700

    TW-2879: Improve when I add a new user, they are added as Member (#2880)

--- lib/domain/usecase/room/ban_user_interactor.dart
+++ lib/domain/usecase/room/ban_user_interactor.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:fluffychat/app_state/failure.dart';
 import 'package:fluffychat/app_state/success.dart';
+import 'package:fluffychat/config/default_power_level_member.dart';
 import 'package:fluffychat/domain/app_state/room/ban_user_state.dart';
 import 'package:matrix/matrix.dart';
 
@@ -14,6 +15,8 @@ class BanUserInteractor {
         return;
       }
 
+      await _reducePowerLevelIfNeeded(user);
+
       await user.ban();
 
       yield const Right(BanUserSuccess());
@@ -25,6 +28,47 @@ class BanUserInteractor {
       }
     } catch (error) {
       yield Left(BanUserFailure(exception: error));
+    }
+  }
+
+  /// Reduces the power level of a user to member level before banning.
+  ///
+  /// This is a best-effort operation. If it fails, the error is logged
+  /// but the ban operation continues. This prevents issues when banning
+  /// users with elevated privileges.
+  Future<void> _reducePowerLevelIfNeeded(User user) async {
+    if (user.powerLevel <= DefaultPowerLevelMember.member.powerLevel) {
+      return;
+    }
+
+    final powerLevelEvent = user.room.getState(EventTypes.RoomPowerLevels);
+
+    // Skip power level reduction if state is null to avoid overwriting
+    // server data
+    if (powerLevelEvent == null) {
+      return;
+    }
+
+    try {
+      final powerMap = Map<String, dynamic>.from(powerLevelEvent.content);
+      final usersMap = Map<String, dynamic>.from(
+        powerMap['users'] as Map<String, dynamic>? ?? {},
+      );
+      powerMap['users'] = usersMap;
+
+      usersMap[user.id] = DefaultPowerLevelMember.member.powerLevel;
+
+      await user.room.client.setRoomStateWithKey(
+        user.room.id,
+        EventTypes.RoomPowerLevels,
+        '',
+        powerMap,
+      );
+    } catch (e) {
+      Logs().w(
+        'Failed to reduce power level for ${user.id} in room ${user.room.id}, proceeding with ban anyway',
+        e,
+      );
     }
   }
 }
