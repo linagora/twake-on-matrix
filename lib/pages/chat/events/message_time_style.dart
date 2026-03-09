8536a5ff30f89d32f3fb688e2fec86ef0ab4e1b3
ClementG63 <clement.guyon1@gmail.com>
2026-03-09 16:37:36 +0100

    fix: correct read receipt indicator in chat list

--- lib/pages/chat/events/message_time_style.dart
+++ lib/pages/chat/events/message_time_style.dart
@@ -1,33 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 
-class MessageTimeStyle {
+final class MessageTimeStyle {
   static Color? timelineColor(bool timelineOverlayMessage) =>
       timelineOverlayMessage
       ? Colors.white
       : LinagoraRefColors.material().neutral[50];
 
   static double get timelineLetterSpacing => 0.4;
-
   static double get paddingTimeAndIcon => 8;
   static double get seenByRowIconSize => 16;
-  static Color seenByRowIconPrimaryColor(
+
+  static Color readReceiptColor(bool seenByOthers) => seenByOthers
+      ? LinagoraSysColors.material().secondary
+      : LinagoraRefColors.material().neutral[50]!;
+
+  static Color seenByRowIconPrimaryColor(bool timelineOverlayMessage) =>
+      timelineOverlayMessage ? Colors.white : readReceiptColor(true);
+
+  static Color seenByRowIconSecondaryColor(
     bool timelineOverlayMessage,
-    BuildContext context,
-  ) => timelineOverlayMessage
-      ? Colors.white
-      : LinagoraSysColors.material().secondary;
-  static Color? seenByRowIconSecondaryColor(
-    bool timelineOverlayMessage,
-    BuildContext context,
-  ) => timelineOverlayMessage
-      ? Theme.of(context).colorScheme.onPrimary
-      : LinagoraRefColors.material().neutral[50];
+    ColorScheme colorScheme,
+  ) => timelineOverlayMessage ? colorScheme.onPrimary : readReceiptColor(false);
 
   static TextStyle? textStyle(
-    BuildContext context,
     bool timelineOverlayMessage,
-  ) => Theme.of(context).textTheme.bodySmall?.merge(
+    TextTheme textTheme,
+  ) => textTheme.bodySmall?.merge(
     TextStyle(color: timelineColor(timelineOverlayMessage), letterSpacing: 0.4),
   );
 }
