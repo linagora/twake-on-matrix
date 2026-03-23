fce036430284f8f7bd5616d3df65e62291e868f7
Dang Dat <tddang@linagora.com>
2026-03-23 14:14:09 +0700

    TW-2839 Enable ConsoleLogger

--- lib/utils/logging/loggers/console_logger.dart
+++ lib/utils/logging/loggers/console_logger.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'dart:developer' as developer;
+import 'dart:js_interop';
+
+import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:web/web.dart' show console;
 import '../log_types.dart';
 import '../logger.dart';
 
@@ -12,16 +16,24 @@ class ConsoleLogger implements Logger {
 
     final timestamp = entry.timestamp.toIso8601String();
     final level = entry.level.toString().split('.').last.toUpperCase();
-    final message = '$timestamp [$level] ${entry.message}';
+    String message = '$timestamp [$level] ${entry.message}';
 
     if (entry.error != null) {
-      debugPrint('$message\nError: ${entry.error}\nStack: ${entry.stackTrace}');
-    } else {
-      debugPrint(message);
+      message += '\nError: ${entry.error}';
+    }
+
+    if (entry.stackTrace != null) {
+      message += '\nStack: ${entry.stackTrace}';
     }
 
     if (entry.context != null && entry.context!.isNotEmpty) {
-      debugPrint('Context: ${entry.context}');
+      message += '\nContext: ${entry.context}';
+    }
+
+    if (PlatformInfos.isWeb) {
+      console.log(message.toJS);
+    } else {
+      developer.log(message);
     }
   }
 }
