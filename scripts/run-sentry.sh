138f5fca23f95f0cf16ada6c815e00eb1426b49e
Dang Dat <tddang@linagora.com>
2026-03-24 14:30:07 +0700

    TW-2839 Fix sentry missing sha git history

--- scripts/run-sentry.sh
+++ scripts/run-sentry.sh
@@ -4,7 +4,7 @@
 
 if [ -n "${SENTRY_ORG:-}" ] && [ -n "${SENTRY_PROJECT:-}" ] && [ -n "${SENTRY_AUTH_TOKEN:-}" ]; then
   echo "Sentry configuration found (Org, Project, Auth Token). Running sentry_dart_plugin..."
-  dart run sentry_dart_plugin --log-level=debug
+  dart run sentry_dart_plugin --log-level=debug --ignore-missing
 else
   echo "Skipping sentry_dart_plugin: Missing SENTRY_ORG, SENTRY_PROJECT, or SENTRY_AUTH_TOKEN."
 fi
