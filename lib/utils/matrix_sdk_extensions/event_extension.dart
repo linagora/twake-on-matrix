1686e5a26b5057f248f445b12f675fde855134a2
HuyNguyen <quanghnguyen@linagora.com>
2026-03-27 15:13:10 +0700

    TW-2965: Enable autoplay of GIFs in chat by downloading full images

--- lib/utils/matrix_sdk_extensions/event_extension.dart
+++ lib/utils/matrix_sdk_extensions/event_extension.dart
@@ -605,6 +605,12 @@ extension LocalizedBody on Event {
     return thisInfoMap['size'] is int &&
         (thisInfoMap['size'] as int) <= database.maxFileSize;
   }
+
+  /// Returns true if this event contains a GIF image.
+  /// GIFs should be displayed as full images (not thumbnails) to preserve animation.
+  bool get isGifImage {
+    return messageType == MessageTypes.Image && mimeType == 'image/gif';
+  }
 }
 
 extension FutureEventExtension on Event {
