df3c8964c0a915f3dd9d41baa352a1f571fab851
Huy Quang Nguyen <quanghnguyen@linagora.com>
2026-05-13 17:00:04 +0700

    TW-3018: Highlight only message bubble, not unread divider (#3035)
    * TW-3018: Highlight only message bubble, not unread divider
    
    * fixup! TW-3018: Highlight only message bubble, not unread divider
    
    * fixup! fixup! TW-3018: Highlight only message bubble, not unread divider

--- lib/pages/chat/chat_event_list_item.dart
+++ lib/pages/chat/chat_event_list_item.dart
@@ -5,7 +5,6 @@ import 'package:fluffychat/utils/matrix_sdk_extensions/filtered_timeline_extensi
 import 'package:fluffychat/widgets/context_menu/context_menu_action.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
-import 'package:scroll_to_index/scroll_to_index.dart';
 
 class ChatEventListItem extends StatelessWidget {
   const ChatEventListItem({
@@ -29,81 +28,77 @@ class ChatEventListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     if (!event.isVisibleInGui) return const SizedBox();
 
-    return AutoScrollTag(
-      key: ValueKey(event.eventId),
-      index: index,
-      controller: controller.scrollController,
-      highlightColor: Theme.of(context).highlightColor,
-      child: Message(
-        key: GlobalObjectKey(event.eventId),
-        event,
-        matrixState: controller.matrix!,
-        onSwipe: (direction) => controller.replyAction(replyTo: event),
-        onAvatarTap: (Event event) => controller.onContactTap(
-          contactPresentationSearch: event.senderFromMemoryOrFallback
-              .toContactPresentationSearch(),
-          context: context,
-          path: 'rooms',
-        ),
-        onSelect: controller.onSelectMessage,
-        selectMode: controller.selectMode,
-        maxWidth: constraints.maxWidth,
-        scrollToEventId: (String eventId) =>
-            controller.scrollToEventId(eventId, highlight: true),
-        selected: controller.selectedEvents.any(
-          (e) => e.eventId == event.eventId,
-        ),
-        timeline: controller.timeline!,
-        previousEvent: previousEvent,
-        nextEvent: nextEvent,
-        onHover: (isHover, event) => controller.onHover(isHover, event),
-        isHoverNotifier: controller.focusHover,
-        listHorizontalActionMenu: controller.listHorizontalActionMenuBuilder(
-          event,
-        ),
-        onMenuAction: controller.handleHorizontalActionMenu,
-        hideKeyboardChatScreen: controller.onHideKeyboardAndEmoji,
-        markedUnreadLocation: controller.unreadReceivedMessageLocation,
-        timestampCallback: (event) {
-          controller.handleDisplayStickyTimestamp(event.originServerTs);
-        },
-        onEventVisible: controller.onEventVisible,
-        onDisplayEmojiReaction: controller.onDisplayEmojiReaction,
-        onHideEmojiReaction: controller.onHideEmojiReaction,
-        listAction: controller.listHorizontalActionMenuBuilder(event).map((
-          action,
-        ) {
-          return ContextMenuAction(name: action.action.name);
-        }).toList(),
-        onPickEmojiReaction: () {},
-        onSelectEmojiReaction: (emoji, event) {
-          controller.sendEmojiAction(emoji: emoji, event: event);
-        },
-        onForward: (event) {
-          controller.forwardEventsAction(event: event);
-        },
-        onReply: (event) {
-          controller.replyAction(replyTo: event);
-        },
-        onCopy: controller.copyEventsAction,
-        onPin: (event) {
-          controller.pinEventAction(event);
-        },
-        onSaveToDownload: (event) =>
-            controller.saveSelectedEventToDownloadAndroid(context, event),
-        onSaveToGallery: (event) =>
-            controller.saveSelectedEventToGallery(context, event),
-        onTapMoreButton: controller.handleOnTapMoreButtonOnWeb,
-        onEdit: (event) {
-          controller.editAction(editEvent: event);
-        },
-        onDelete: (context, event) {
-          controller.deleteEventAction(context, event);
-        },
-        recentEmojiFuture: controller.getRecentReactionsInteractor.execute(),
-        onReport: controller.reportEventAction,
-        onRetryTextMessage: controller.retryTextMessage,
+    return Message(
+      key: GlobalObjectKey(event.eventId),
+      event,
+      autoScrollController: controller.scrollController,
+      scrollIndex: index,
+      matrixState: controller.matrix!,
+      onSwipe: (direction) => controller.replyAction(replyTo: event),
+      onAvatarTap: (Event event) => controller.onContactTap(
+        contactPresentationSearch: event.senderFromMemoryOrFallback
+            .toContactPresentationSearch(),
+        context: context,
+        path: 'rooms',
       ),
+      onSelect: controller.onSelectMessage,
+      selectMode: controller.selectMode,
+      maxWidth: constraints.maxWidth,
+      scrollToEventId: (String eventId) =>
+          controller.scrollToEventId(eventId, highlight: true),
+      selected: controller.selectedEvents.any(
+        (e) => e.eventId == event.eventId,
+      ),
+      timeline: controller.timeline!,
+      previousEvent: previousEvent,
+      nextEvent: nextEvent,
+      onHover: (isHover, event) => controller.onHover(isHover, event),
+      isHoverNotifier: controller.focusHover,
+      listHorizontalActionMenu: controller.listHorizontalActionMenuBuilder(
+        event,
+      ),
+      onMenuAction: controller.handleHorizontalActionMenu,
+      hideKeyboardChatScreen: controller.onHideKeyboardAndEmoji,
+      markedUnreadLocation: controller.unreadReceivedMessageLocation,
+      timestampCallback: (event) {
+        controller.handleDisplayStickyTimestamp(event.originServerTs);
+      },
+      onEventVisible: controller.onEventVisible,
+      onDisplayEmojiReaction: controller.onDisplayEmojiReaction,
+      onHideEmojiReaction: controller.onHideEmojiReaction,
+      listAction: controller.listHorizontalActionMenuBuilder(event).map((
+        action,
+      ) {
+        return ContextMenuAction(name: action.action.name);
+      }).toList(),
+      onPickEmojiReaction: () {},
+      onSelectEmojiReaction: (emoji, event) {
+        controller.sendEmojiAction(emoji: emoji, event: event);
+      },
+      onForward: (event) {
+        controller.forwardEventsAction(event: event);
+      },
+      onReply: (event) {
+        controller.replyAction(replyTo: event);
+      },
+      onCopy: controller.copyEventsAction,
+      onPin: (event) {
+        controller.pinEventAction(event);
+      },
+      onSaveToDownload: (event) =>
+          controller.saveSelectedEventToDownloadAndroid(context, event),
+      onSaveToGallery: (event) =>
+          controller.saveSelectedEventToGallery(context, event),
+      onTapMoreButton: controller.handleOnTapMoreButtonOnWeb,
+      onEdit: (event) {
+        controller.editAction(editEvent: event);
+      },
+      onDelete: (context, event) {
+        controller.deleteEventAction(context, event);
+      },
+      recentEmojiFuture: controller.getRecentReactionsInteractor.execute(),
+      onReport: controller.reportEventAction,
+      onRetryTextMessage: controller.retryTextMessage,
     );
   }
 }
---
+++ lib/pages/chat/events/message/message_selected_widget.dart
@@ -0,0 +1,59 @@
+import 'package:fluffychat/config/themes.dart';
+import 'package:fluffychat/pages/chat/events/message/message.dart';
+import 'package:fluffychat/utils/extension/event_status_custom_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
+import 'package:matrix/matrix.dart';
+
+class MessageSelectedWidget extends StatelessWidget {
+  final Event event;
+  final bool selected;
+  final bool selectMode;
+  final Widget child;
+
+  const MessageSelectedWidget({
+    super.key,
+    required this.event,
+    required this.selected,
+    required this.selectMode,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: Message.responsiveUtils.isMobile(context) ? 8.0 : 0,
+      ),
+      color: Theme.of(context).primaryColor.withAlpha(0),
+      constraints: const BoxConstraints(
+        maxWidth: TwakeThemes.columnWidth * 2.5,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          if (selectMode && event.redacted)
+            const SizedBox(width: 20)
+          else if (selectMode && event.status.isAvailable)
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Icon(
+                selected ? Icons.check_circle_rounded : Icons.circle_outlined,
+                color: selected
+                    ? LinagoraSysColors.material().primary
+                    : Colors.black,
+                size: 20,
+              ),
+            ),
+          Expanded(
+            flex: 9,
+            child: Align(
+              alignment: AlignmentDirectional.centerEnd,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
