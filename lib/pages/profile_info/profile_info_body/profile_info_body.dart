2ade4f1c230f0a9a49c6c07c5907d867264b8d92
HuyNguyen <quanghnguyen@linagora.com>
2026-05-27 14:42:35 +0700

    fixup! TW-3038: Exclude edit events to prevent duplicate search results for edited messages

--- lib/pages/profile_info/profile_info_body/profile_info_body.dart
+++ lib/pages/profile_info/profile_info_body/profile_info_body.dart
@@ -119,7 +119,7 @@ class ProfileInfoBodyController extends State<ProfileInfoBody>
     } else {
       if (PlatformInfos.isMobile) {
         Navigator.of(context).popUntil(
-          (Route route) => route.settings.name?.startsWith('/rooms/') ?? false,
+          (route) => route.settings.name?.startsWith('/rooms/room_') ?? false,
         );
       } else {
         if (widget.onNewChatOpen != null) widget.onNewChatOpen!();
