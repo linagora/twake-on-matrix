3bb533c0962cbd3b6c0d9fa163408a8131f1aac3
aparesy <paresya888@gmail.com>
2026-06-16 14:27:22 +0700

    fixup! fixup! TW-3131: Add Diacritic / NFD Normalization in Search

---
+++ lib/domain/services/search/search_engine_impl.dart
@@ -0,0 +1,39 @@
+part of 'search_engine.dart';
+
+List<NormalizationStep> _buildPipeline(SearchOptions options) {
+  return [
+    if (!options.diacriticSensitive) const DiacriticStripStep(),
+    if (!options.caseSensitive) const LowerCaseStep(),
+  ];
+}
+
+List<T> _matchAnyField<T>(
+  String needle,
+  List<T> haystack, {
+  required List<String? Function(T)> fieldExtractors,
+  SearchOptions options = const SearchOptions(),
+}) {
+  if (needle.isEmpty) return <T>[];
+  final pipeline = _buildPipeline(options);
+  String apply(String input) =>
+      pipeline.fold<String>(input, (v, step) => step.normalize(v));
+  final normNeedle = apply(needle);
+  return haystack.where((item) {
+    return fieldExtractors.any((extract) {
+      return apply(extract(item) ?? '').contains(normNeedle);
+    });
+  }).toList();
+}
+
+bool _matchesText(
+  String needle,
+  String haystack, {
+  SearchOptions options = const SearchOptions(),
+}) {
+  return _matchAnyField(
+    needle,
+    [haystack],
+    fieldExtractors: [(String s) => s],
+    options: options,
+  ).isNotEmpty;
+}
