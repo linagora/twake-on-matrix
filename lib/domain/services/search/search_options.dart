d4391da520fbce5cbaa08eef65fd3fb866006a37
aparesy <paresya888@gmail.com>
2026-06-17 14:23:37 +0700

    fixup! fixup! fixup! fixup! TW-3131: Add Diacritic / NFD Normalization in Search

--- lib/domain/services/search/search_options.dart
+++ lib/domain/services/search/search_options.dart
@@ -1,9 +1,14 @@
+// Can be extended in the future, e.g. with fuzzy matching.
+enum SearchMode { exact, substring }
+
 class SearchOptions {
   final bool caseSensitive;
   final bool diacriticSensitive;
+  final SearchMode mode;
 
   const SearchOptions({
     this.caseSensitive = false,
     this.diacriticSensitive = true,
+    this.mode = SearchMode.substring,
   });
 }
