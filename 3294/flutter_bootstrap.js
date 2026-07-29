a175c17f7f402f401081bb8108de493d2cc183b1
9clg6 <9clg6@users.noreply.github.com>
2026-07-29 15:10:43 +0000

    deploy: efcc86b1b8706e60c9e991574e8faa8e5afa606f

--- 3294/flutter_bootstrap.js
+++ 3294/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2538129072"
+    serviceWorkerVersion: "3377834075"
   }
 });
