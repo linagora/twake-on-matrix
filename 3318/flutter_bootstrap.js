76f3d9936de132f03f7ce675eb7a6312662541d2
Crash-- <Crash--@users.noreply.github.com>
2026-08-30 05:45:56 +0000

    deploy: 65155730e66e503d93fa302ab524845f2169d69d

--- 3318/flutter_bootstrap.js
+++ 3318/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3827701354"
+    serviceWorkerVersion: "3648898095"
   }
 });
