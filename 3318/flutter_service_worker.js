3d6d900f6c8bf5490e5d9f2c725286b302124486
Crash-- <Crash--@users.noreply.github.com>
2026-08-30 09:45:16 +0000

    deploy: 6d6931e189d29e49078c9889c70c4c82c2bfd0e3

--- 3318/flutter_service_worker.js
+++ 3318/flutter_service_worker.js
@@ -790,7 +790,7 @@ const RESOURCES = {"version.json": "27568c2f13cdafedaa3deb3e083f2f00",
 "splash/img/icon_twp.png": "5f3bf8fc4a58850bf5dae7b6f33043fc",
 "splash/style.css": "aef88eeb741da0bd13a5cc5b49d90598",
 "style.css": "1273e6902fff3be80d3a34cdb7a05326",
-"flutter_bootstrap.js": "faa6e2e4934f5e49bd836e29a6ea22fb",
+"flutter_bootstrap.js": "2149f2214636e9e58283fe7adb1c7e3e",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a"};
 // The application shell files that are downloaded before a service worker can
 // start.
