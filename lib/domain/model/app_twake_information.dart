3ddbd05a6dfbfe8f858d4e3bc170c5d1ac6acd4a
Johann Bournazel <johann.bournazel@protonmail.com>
2026-09-14 09:28:13 +0100

    refactor: migrate AppTwakeInformation to Freezed

--- lib/domain/model/app_twake_information.dart
+++ lib/domain/model/app_twake_information.dart
@@ -1,31 +1,23 @@
-import 'package:equatable/equatable.dart';
-import 'package:twake_chat/domain/model/common_settings_information.dart';
-import 'package:json_annotation/json_annotation.dart';
+// ignore_for_file: invalid_annotation_target
 
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:twake_chat/domain/model/common_settings_information.dart';
+
+part 'app_twake_information.freezed.dart';
 part 'app_twake_information.g.dart';
 
-@JsonSerializable()
-class AppTwakeInformation with EquatableMixin {
+@freezed
+abstract class AppTwakeInformation with _$AppTwakeInformation {
   static const String appTwakeInformationKey = 'app.twake.chat';
 
   static const String supportContactKey = 'support_contact';
 
-  @JsonKey(name: 'common_settings')
-  CommonSettingsInformation? commonSettingsInformation;
-
-  @JsonKey(name: 'enable_invitations')
-  final bool? isInvitationEnabled;
-
-  AppTwakeInformation({
-    this.commonSettingsInformation,
-    this.isInvitationEnabled,
-  });
+  const factory AppTwakeInformation({
+    @JsonKey(name: 'common_settings')
+    CommonSettingsInformation? commonSettingsInformation,
+    @JsonKey(name: 'enable_invitations') bool? isInvitationEnabled,
+  }) = _AppTwakeInformation;
 
   factory AppTwakeInformation.fromJson(Map<String, dynamic> json) =>
       _$AppTwakeInformationFromJson(json);
-
-  Map<String, dynamic> toJson() => _$AppTwakeInformationToJson(this);
-
-  @override
-  List<Object?> get props => [commonSettingsInformation, isInvitationEnabled];
 }
